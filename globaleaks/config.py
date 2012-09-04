ee821599fc563701f49fb0b76d6a9cb3778aff3a
Nara Stabocchi <nara@globaleaks.org>
2012-09-04 23:54:29 +0000

    Fix path in config.py

--- globaleaks/config.py
+++ globaleaks/config.py
@@ -3,8 +3,4 @@ from cyclone.util import ObjectDict as OD
 glbackend = OD()
 
 # XXX move all these variables to a config file
-<<<<<<< HEAD
-glbackend.glclient_path = '/home/x/code/web/GLClient/www/'
-=======
 glbackend.glclient_path = '/tmp/'
->>>>>>> f3d35f91807eb3e55f785d7d36005ce35c32d558
