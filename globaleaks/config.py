1e03d4794c677cc113ee45596544dd8ef9991af8
Arturo Filastò <arturo@filasto.net>
2012-09-26 17:32:37 +0000

    Minor change to config logic

--- globaleaks/config.py
+++ globaleaks/config.py
@@ -1,6 +1,12 @@
+import os
 from cyclone.util import ObjectDict as OD
 
 glbackend = OD()
 
 # XXX move all these variables to a config file
-glbackend.glclient_path = '/tmp/'
+try:
+    path = os.path.abspath('../../GLClient/www/')
+except:
+    path = '/tmp'
+print "Path %s" % path
+glbackend.glclient_path = path
