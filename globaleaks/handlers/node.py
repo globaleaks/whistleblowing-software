438992ecc2a817ce7d8231b97f999f981f312eba
Arturo Filastò <arturo@filasto.net>
2012-10-14 21:21:34 +0000

    Add good init script for scheduler.

--- globaleaks/handlers/node.py
+++ globaleaks/handlers/node.py
@@ -13,26 +13,24 @@ from globaleaks import models
 
 from cyclone.web import RequestHandler, asynchronous
 
-def create_node(contextDict):
+def create_node(context_dict):
     context = models.admin.Context()
-    context.name = contextDict['name']
-    context.description = contextDict['description']
-    context.fields = contextDict['fields']
-    context.selectable_receiver = contextDict['selectable_receiver']
-    context.escalation_threshold = contextDict['escalation_threshold']
-    context.languages_supported = contextDict['languages_supported']
+    context.name = context_dict['name']
+    context.description = context_dict['description']
+    context.fields = context_dict['fields']
+    context.selectable_receiver = context_dict['selectable_receiver']
+    context.escalation_threshold = context_dict['escalation_threshold']
+    context.languages_supported = context_dict['languages_supported']
 
 class Node(RequestHandler):
 
     @asynchronous
     @inlineCallbacks
     def get(self):
-        print "Got!"
         from globaleaks.messages import dummy
 
         context = models.admin.Context()
         context_description_dicts = yield context.list_description_dicts()
-        print context_description_dicts
         response = {"contexts": context_description_dicts,
                     "public_statistics": dummy.base.publicStatisticsDict,
                     "node_properties": dummy.base.nodePropertiesDict,
@@ -40,6 +38,5 @@ class Node(RequestHandler):
                     "hidden_service": "cnwoecowiecnirnio23rn23io.onion",
                     "url_schema": "/",
                     "name": "don't track us: AUTOVELOX"}
-        print "Sending %s" % response
         self.write(response)
         self.finish()
