7688707f55c153759997fdb79dae19a1c2e3ebaa
Arturo Filastò <arturo@filasto.net>
2012-10-15 15:12:27 +0000

    Write up better docstrings for scheduler * Move the retries selection to the job * Implementy useful pretty date function * Add better debug logging

--- globaleaks/jobs/base.py
+++ globaleaks/jobs/base.py
@@ -10,15 +10,55 @@ import datetime
 from twisted.internet import reactor
 
 class Job(object):
-    def __init__(self, scheduledTime=time.time(), delay=None):
-        self.scheduledTime = scheduledTime
+    """
+    This is the base class from which jobs should heredit.
+
+    retries: through this parameter you can specify a list of intervals after
+    which a retry should happen. This is time in seconds.
+    XXX in the future we may want to support a cronish like syntax for such
+        timeouts (1m, 1h, 2d, 1m etc.)
+    """
+    retries = [2,3,5]
+    def __init__(self, scheduled_time=time.time(), delay=None):
+        """
+        scheduled_time: the number of seconds since epoch in local time for when
+        the job should be run.
+
+        delay: the delay in seconds to wait for running the job after
+        scheduled_time. (if you don't pass scheduled_time delay will run the
+        job delay seconds after now)
+
+        """
+        self.scheduledTime = scheduled_time
 
         self.args = ()
         self.manager = None
         self.running = False
         self.failures = []
+
         if delay:
             self.scheduledTime += delay
+
+    def failed(self):
+        """
+        Place here all the logic to handle the failure of one run of the
+        object.
+        We will by default attempt to re-run the Job the number of retry times
+        that are specified in self.retries
+        """
+        raise NotImplemented
+
+    def failedRetries(self):
+        """
+        We have failed also all the retries handle this appropriately.
+
+        When we reach this point this job is considered to have not worked and
+        we should not make any more attempts at running it again.
+
+        Ideally we just want to log this, and move on with out life.
+        """
+        raise NotImplemented
+
 
     def __str__(self):
         return str("%s - %s - %s" % (self.__class__, self.scheduledTime, self.running))
