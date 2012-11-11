ba2ac171d5ef5cbbdcdadec7ccff813a528f039c
vecna <vecna@delirandom.net>
2012-11-11 02:24:01 +0100

    expanded submission testing & generation

--- shooter/submission.sh
+++ shooter/submission.sh
@@ -11,6 +11,10 @@ else
     echo "context_gus $context_gus"
 fi
 
+
+beforecount=`$SHOOTER A5 GET oid itip print-elements`
+if [ $? != 0 ]; then echo "\tError in A5 GET" && exit; fi
+
 submission_gus=`$SHOOTER U2 GET cid $context_gus print-submission_gus`
 if [ $? != 0 ]; then echo "\tError in U2 GET" && exit; fi
 echo "submission_gus $submission_gus"
@@ -23,3 +27,13 @@ if [ $? != 0 ]; then echo "\tError in U3 POST" && exit; fi
 
 $SHOOTER U4 POST sid $submission_gus verbose
 if [ $? != 0 ]; then echo "\tError in U4 POST" && exit; fi
+
+$SHOOTER A6 GET oid tip 
+if [ $? != 0 ]; then echo "\tError in A6 GET (force tip job)" && exit; fi
+
+aftercount=`$SHOOTER A5 GET oid itip print-elements`
+if [ $? != 0 ]; then echo "\tError in A5 GET" && exit; fi
+
+if [ $((beforecount + 1)) != $aftercount ]; then
+    echo "serious error in tip generation :("
+fi
