80908f4485e3eed7a155e3dff6fbf3c6a18dccb3
vecna <vecna@delirandom.net>
2012-11-16 04:25:42 +0100

    partially implemented vote and Tip operations

--- globaleaks/handlers/tip.py
+++ globaleaks/handlers/tip.py
@@ -8,39 +8,113 @@
 #   Contains all the logic for handling tip related operations.
 
 from twisted.internet.defer import inlineCallbacks
-from cyclone.web import asynchronous, HTTPError
+from cyclone.web import asynchronous
 
 from globaleaks.handlers.base import BaseHandler
-from globaleaks.models.externaltip import  ReceiverTip
+from globaleaks.models.externaltip import ReceiverTip, WhistleblowerTip,\
+    TipGusNotFoundError, TipReceiptNotFoundError, TipPertinenceExpressed
 from globaleaks.utils import log
+import globaleaks.messages.base
 import json
+
+def is_receiver_token(tip_token):
+    """
+    @param tip_token: the received string from /tip/$whatever
+    @return: True if is a tip_gus format, false if is not
+    """
+
+    try:
+        retcheck = globaleaks.messages.base.tipGUS().validate(tip_token)
+    except:
+        retcheck = True
+
+    return not retcheck
 
 class TipRoot(BaseHandler):
 
-    log.debug("[D] %s %s " % (__file__, __name__), "Class TipRoot", "BaseHandler", BaseHandler)
+    @asynchronous
+    @inlineCallbacks
+    def get(self, tip_token):
+        log.debug("[D] %s %s " % (__file__, __name__), "Class TipRoot", "get", "tip_token", tip_token)
+
+        # tip_token can be: a tip_gus for a receiver, or a WhistleBlower receipt, understand
+        # the format, help in addrressing which kind of Tip need to be handled.
+
+        if is_receiver_token(tip_token):
+
+            requested_t = ReceiverTip()
+
+            try:
+                tip_description = yield requested_t.receiver_get_single(tip_token)
+                self.set_status(200)
+                self.write(tip_description)
+
+            except TipGusNotFoundError, e:
+                self.set_status(e.http_status)
+                self.write({'error_message' : e.error_message, 'error_code' : e.error_code})
+
+        else:
+
+            requested_t = WhistleblowerTip()
+
+            try:
+                tip_description = yield requested_t.whistleblower_get_single(tip_token)
+                self.set_status(200)
+                self.write(tip_description)
+
+            except TipReceiptNotFoundError, e:
+                self.set_status(e.http_status)
+                self.write({'error_message' : e.error_message, 'error_code' : e.error_code})
+
+        self.finish()
 
     @asynchronous
     @inlineCallbacks
-    def get(self, receipt):
-        log.debug("[D] %s %s " % (__file__, __name__), "Class TipRoot", "get", "receipt", receipt)
-        print "Processing %s" % receipt
-        tip = Tip()
+    def post(self, tip_token, *arg, **kw):
+        """
+        root of /tip/ POST manage delete, forwarding and pertinence options.
+        Only the receiver may have this properties.
+        """
+        log.debug("[D] %s %s " % (__file__, __name__), "Class TipRoot", "post", "tip_token", tip_token)
 
-        tip_dict = yield tip.lookup(receipt)
+        request = json.loads(self.request.body)
 
-        self.write(tip_dict)
+        if not request:
+            # this need a dedicated entry in error dict
+            self.set_status(408)
+            self.write({'error_message' : 'expected messages in POST', 'error_code' : 123})
+
+        elif is_receiver_token(tip_token):
+
+            requested_t = ReceiverTip()
+
+            try:
+                if request['total_delete']:
+                    yield requested_t.total_delete(tip_token)
+                elif request['personal_delete']:
+                    yield requested_t.personal_delete(tip_token)
+                elif request['is_pertinent']:
+                    yield requested_t.pertinence_vote(tip_token, request['is_pertinent'])
+
+                self.set_status(200)
+
+            except TipGusNotFoundError, e:
+                self.set_status(e.http_status)
+                self.write({'error_message' : e.error_message, 'error_code' : e.error_code})
+
+            except TipPertinenceExpressed, e:
+                self.set_status(e.http_status)
+                self.write({'error_message' : e.error_message, 'error_code' : e.error_code})
+
+            # TODO, error handling for lacking of total_delete privileges
+
+        else:
+            # this need a dedicated entry in error dict
+            self.set_status(410)
+            self.write({'error_message' : 'receiver Tip not used', 'error_code' : 123})
+
         self.finish()
 
-
-    """
-    root of /tip/ POST handle *deleting* and *forwarding* options,
-    they are checked in the tip-properties
-    (assigned by the tip propetary), only the receiver may have
-    this properties
-    """
-    def post(self, tip_gus, *arg, **kw):
-        log.debug("[D] %s %s " % (__file__, __name__), "Class TipRoot", "post", "tip_gus", tip_gus)
-        pass
 
 class TipComment(BaseHandler):
 
