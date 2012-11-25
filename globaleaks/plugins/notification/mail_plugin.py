8d9ce9e9744b5aa11505b35c3d16ea33affd8f22
vecna <vecna@delirandom.net>
2012-11-25 18:59:07 +0100

    implemented first scratch of email plugin

--- globaleaks/plugins/notification/mail_plugin.py
+++ globaleaks/plugins/notification/mail_plugin.py
@@ -16,90 +16,72 @@ class MailNotification(GLPlugin):
         self.admin_fields = {'server' : 'text', 'port': 'int', 'password' : 'text', 'username':'text', 'ssl' : 'bool' }
         self.receiver_fields = {'mail_address' : 'text'}
 
-    def validate_admin_opt(self, admin_fields):
-        log.debug("[%s] admin_fields %s" % ( self.__class__.__name__, admin_fields))
-        return True
+    def validate_admin_opt(self, pushed_af):
+
+        if self._get_SMTP(pushed_af['server'], pushed_af['port'], pushed_af['ssl'],
+                pushed_af['username'], pushed_af['password']):
+            return True
+        else
+            return False
 
     def validate_receiver_opt(self, admin_fields, receiver_fields):
         log.debug("[%s] receiver_fields %s (with admin %s)" % ( self.__class__.__name__, receiver_fields, admin_fields))
         return True
 
+    def _append_email():
+        # TODO use http://docs.python.org/2/library/email
+        # before was used:
+
+        body = string.join(("From: GLBackend postino <%s>" % username,
+                            "To: Estimeed Receiver <%s>" % receiver_addr,
+                            "Subject: %s" % subject, text), "\r\n")
+        return body
+
+    def _get_SMTP(server, port, tls, username, password):
+
+        try:
+            socket = smtplib.SMTP(server + ':' + port)
+            if tls:
+                socket.starttls()
+            socket.login(username, password)
+
+        except smtplib.SMTPConnectError:
+            # XXX log.plugin need to be defined and used from GLPlugin inherit
+            log.debug("Error, Connection error to %s:%d" % (server, port) )
+            return None
+        except smtplib.SMTPAuthenticationError:
+            log.debug("Error, Invalid Login/Password provided for server %s (%s)" % (server
+                    username) )
+            return None
+
+        return socket
+
+    # NYI, would use _append_email and continuosly checking the time delta
+    #      admin fields need the digest time delta specified inside.
     def digest_check(self, settings, stored_data, new_data):
-        # not yet called!
         pass
 
     def do_notify(self, settings, stored_data):
-        pass
 
+        af = settings['admin_field']
+        rf = settings['receiver_field']
 
-I_WANT_TO_BE_SPAMMED = False
+        body = self._create_email(stored_data)
 
-# would be remove at the end
-def do_not_email(subject, body, receiver_addr):
-    from globaleaks.utils import gltime
+        smtpsock = self._get_SMTP(af['server'], af['port'], af['ssl'],
+                af['username'], af['password'])
 
-    with file("/tmp/do_not_email_please", 'a+') as f:
-        f.write('\r\nTime: %s' % gltime.utcPrettyDateNow())
-        f.write('\r\nDest: %s' % receiver_addr)
-        f.write('\r\nSubject: %s\r\n' % subject)
-        f.write(body)
-        f.write("\r\n---\r\n")
+        try:
+            smtpsock.sendmail(af['username'], [ rf['mail_addr'] ], body)
+            smtpsock.quit()
 
-def GLBMailService(eventdate, infotext, receiver_addr):
-    """
-    This is a temporary email supports not yet pluginized
-    """
-    username='globaleaksnode1@gmail.com'
-    password='Antani1234'
-    serverport='smtp.gmail.com:587'
+            log.debug("Success in email %s " % tip_gus)
+            retval = True
 
-    subject = "Email notification for %s" % (receiver_addr.split('@')[0])
+        except smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused:
 
-    text = "In %s an info for you: [%s]" % (eventdate, infotext)
+            # remind, other error can be handled http://docs.python.org/2/library/smtplib.html
+            log.err("[E] error in sending the email to %s %s (%s)" % (receiver_addr, infotext, subject))
+            retval = False
 
-    # XXX just for avoid getting spammed when testing GLB :P
-    if not I_WANT_TO_BE_SPAMMED:
-        do_not_email(subject, text, receiver_addr)
-        return True
-
-    body = string.join(("From: GLBackend postino <%s>" % username,
-                        "To: Estimeed Receiver <%s>" % receiver_addr,
-                        "Subject: %s" % subject, text), "\r\n")
-
-    server = smtplib.SMTP(serverport)
-    server.starttls()
-    server.login(username, password)
-
-    try:
-        server.sendmail(username, [ receiver_addr ], body)
-        log.debug("Success in email %s " % tip_gus)
-        server.quit()
-        retval = True
-    except smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused:
-        # remind, other error can be handled http://docs.python.org/2/library/smtplib.html
-        log.err("[E] error in sending the email to %s %s (%s)" % (receiver_addr, infotext, subject))
-        retval = False
-
-    return retval
-
-
-# class email(Notification)
-# 
-# plugin_type = "notification"
-# plugin_name = "Twitter PM notification"
-# plugin_description = $(localized pointer)
-# 
-# def get_admin_opt():
-#   return pluginAdminDict
-
-#
-# def set_admin_opt(pluginAdminDict):
-#
-# def get_receiver_opt(pluginDataDict):
-
-# def set_receiver_opt():
-#   return pluginDataDict
-#
-# def do_notify():
-#
-# def get_log():
+        return retval
