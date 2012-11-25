c4d78d2cf80f6b1302fbd4e716d75c9b4bd52f65
vecna <vecna@delirandom.net>
2012-11-25 22:06:58 +0100

    file notification for testing

--- globaleaks/plugins/base.py
+++ globaleaks/plugins/base.py
@@ -9,11 +9,13 @@ class GLPlugingManager:
     def __init__(self):
         from globaleaks.plugins.notification.mail_plugin import MailNotification
         from globaleaks.plugins.notification.irc_plugin import IRCNotification
+        from globaleaks.plugins.notification.file_plugin import FILENotification
 
         # only here the plugin object is instanced
         self.notification_dict = {
             'email', MailNotification(),
-            'irc', IRCNotification()
+            'irc', IRCNotification(),
+            'file', FILENotification()
         }
         self.delivery_dict = {}
         self.inputfilter_dict = {}
