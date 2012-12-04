661c0bee3d8764955ace2b98fb2851397f3e8163
vecna <vecna@delirandom.net>
2012-12-04 10:33:42 +0100

    New (definitive ?) API refactoring and cleanings

--- globaleaks/rest/api.py
+++ globaleaks/rest/api.py
@@ -16,72 +16,81 @@ from globaleaks import config
 from globaleaks.handlers import node, submission, tip, admin, receiver, files
 from globaleaks.messages.base import tipGUS, receiverGUS, submissionGUS, contextGUS, profileGUS
 
-more_lax = r'(\w+)' # XXX need to be changed with regexp.submission_id | regexp.receipt_id
+more_lax = r'(\w+)' # XXX need to be changed with regexp.submission_gus | regexp.receipt_gus
 not_defined_regexp = r'(\w+)'
+receiver_token_auth = r'(\w+)' # This would cover regexp.tip_gus | regexp.welcome_token_gus
+
+# Here is mapped a path and the associated class to be invoked,
+# Three kind of Classes can be distigued:
+# * Crud (SubmissionCrud, FileCrud, etc)
+#   supports a complete CRUD (PUT, POST, DELETE, GET)
+# * Management (TipManagement, ReceiverManagement)
+#   supports operation of interaction (GET, PUT), Never Creation (POST) and maybe Delete
+# * Available
+#   supports only GET operation, returning a list of elements
 
 spec = [
     ## Node Handler ##
     #  * /node U1
-    (r'/node', node.PublicInfo),
+    (r'/node', node.InfoAvailable),
 
     ## Submission Handlers ##
-    #  * /submission/<context_GUS> U2
-    (r'/submission/' + contextGUS.regexp + '/new', submission.SubmissionRoot),
+    #  * /submission/ U2
+    (r'/submission/' + contextGUS.regexp + '/new', submission.SubmissionCrud),
 
-    #  * /submission/<GUS>/status U3
-    (r'/submission/' + submissionGUS.regexp + '/status', submission.SubmissionStatus),
+    #  * /file/ U3
+    (r'/file/', files.FileCrud),
 
-    #  * /submission/<GUS>/finalize U4
-    (r'/submission/' + submissionGUS.regexp + '/finalize', submission.SubmissionFinalize),
-
-    (r'/submission/' + submissionGUS.regexp + '/files', files.FilesHandler),
-    # https://docs.google.com/a/apps.globaleaks.org/document/d/17GXsnczhI8LgTNj438oWPRbsoz_Hs3TTSnK7NzY86S4/edit?pli=1
+    # * /statistics/ U4
+    (r'/statistics/', node.StatsAvailable),
 
     ## Tip Handlers ##
     #  * /tip/<tip_GUS>/ T1
-    (r'/tip/' + more_lax, tip.TipRoot),
-
-    #  * /tip/<tip_GUS>/comment T2
-    (r'/tip/' + more_lax + '/comment', tip.TipComment),
-
-    #  * /tip/<tip_GUS>/files T3
-    (r'/tip/' + more_lax +  '/files', tip.TipFiles),
-
-    #  * /tip/<tip_GUS>/finalize T4
-    (r'/tip/' + more_lax + '/finalize', tip.TipFinalize),
-    # but would be just receipt_id, just receipt_id is not more coherent
-
-    #  * /tip/<tip_GUS>/download T5
-    (r'/tip/' + tipGUS.regexp + '/download', tip.TipDownload),
+    (r'/tip/' + more_lax, tip.TipManagement),
 
     ## Receiver Handlers ##
-    #  * /reciever/<tip_GUS>/management R1
-    (r'/receiver/' + tipGUS.regexp + '/management', receiver.ReceiverManagement),
+    #  * /reciever/<receiver_token_auth>/management R1
+    (r'/receiver/' + receiver_token_auth + '/management', receiver.ReceiverManagement),
 
-    #  * /receiver/<tip_GUS>/plugin/<profile_GUS>/<ReceiverConf_numeric_ID> R2
-    (r'/receiver/' + tipGUS.regexp + '/plugin/' + profileGUS.regexp + '/' + 'r(\d+)', receiver.ReceiverPluginConf),
+    #  * /receiver/<receiver_token_auth>/pluginprofiles R2
+    (r'/receiver/' + tipGUS.regexp + '/pluginprofiles', receiver.ProfilesAvailable),
+
+    #  * /receiver/<tip_GUS>/plugin R3
+    (r'/receiver/' + tipGUS.regexp + '/plugin/', receiver.PluginCrud),
 
     ## Admin Handlers ##
     #  * /admin/node A1
-    (r'/admin/node', admin.AdminNode),
+    (r'/admin/node', admin.NodeManagement),
 
-    #  * /admin/contexts A2
-    (r'/admin/contexts/' + contextGUS.regexp, admin.AdminContexts),
+    #  * /admin/contexts/ A2
+    (r'/admin/contexts/', admin.ContextsAvailable),
 
-    #  * /admin/receivers/<receiver_GUS> A3
-    (r'/admin/receivers/' + receiverGUS.regexp, admin.AdminReceivers),
+    #  * /admin/context/ A3
+    (r'/admin/context/', admin.ContextCrud),
 
-    #  * /admin/plugins/<profile_GUS>/ A4
-    (r'/admin/plugins/' + profileGUS.regexp, admin.AdminPlugin),
+    #  * /admin/receivers/ A4
+    (r'/admin/receivers/', admin.ReceiversAvailable),
 
-    #  * /admin/overview A5
-    (r'/admin/overview/' + not_defined_regexp, admin.AdminOverView),
+    #  * /admin/receiver/ A5
+    (r'/admin/receiver/', admin.ReceiverCrud),
 
-    #  * /admin/tasks/ A6
-    (r'/admin/tasks/' + not_defined_regexp, admin.AdminTasks),
+    #  * /admin/plugins/ A6
+    (r'/admin/plugins/', admin.PluginsAvailable),
+
+    #  * /admin/profile/ A7
+    (r'/admin/profile/', admin.ProfilesCrud),
+
+    #  * /admin/statistics/ A8
+    (r'/admin/statistics/', admin.StatisticsAvailable),
+
+    #  * /admin/overview A9
+    (r'/admin/overview/' + not_defined_regexp, admin.EntryAvailable),
+
+    #  * /admin/tasks/ AA
+    (r'/admin/tasks/' + not_defined_regexp, admin.TaskManagement),
 
     ## Main Web app ##
     # * /
-    (r"/(.*)", StaticFileHandler, {'path': config.main.glclient_path})
+    (r'/(.*)', StaticFileHandler, {'path': config.main.glclient_path})
 ]
 
