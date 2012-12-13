ffecd716623a1f6c700271acee9e594b0cf98c14
vecna <vecna@delirandom.net>
2012-12-13 22:46:59 +0100

    supports of message for statistics in rest/*.py

--- globaleaks/rest/errors.py
+++ globaleaks/rest/errors.py
@@ -5,18 +5,36 @@
 #
 # Here shall go all the error messages that a GLBackend can generate.
 
+
+
 class GLTypeError(Exception):
 
     error_message = "GLTypesError not set"
     error_code = 0
     http_status = 500 # generic Server error
 
+
 class InvalidInputFormat(GLTypeError):
+    """
+    The expected format described in the REST specification is not
+    respected by the data body in the HTTP request.
+    """
 
     def __init__(self):
-        GLTypeError.error_message = "Invalid Input Format"
-        GLTypeError.error_code = 100 # need to be resumed the table and come back in use them
-        GLTypeError.http_status = 406 # Bad Request
+        GLTypeError.error_message = "invalid input format"
+        GLTypeError.error_code = 10
+        GLTypeError.http_status = 406 # Not Acceptable
+
+class StatsNotCollectedError(GLTypeError):
+    """
+    Statistics can be disabled by administrator, both for
+    public and admin statistics.
+    """
+
+    def __init__(self):
+        GLTypeError.error_message = "statistics disabled"
+        GLTypeError.error_code = 11
+        GLTypeError.http_status = 500 # Internal Server Error
 
 
 # This is the struct containing the errors
@@ -29,21 +47,18 @@ def errorMessage(http_error_code=500, error_dict={}):
                 'error_message': error_dict.get('string')}
     return response
 
-from cyclone.util import ObjectDict as OD
 
-GLErrorCode = OD()
-
-# All
-GLErrorCode.malformed_requst = {'code': 0, 'message': 'Malformed Request'}
-
-# U2,U3,U4
-GLErrorCode.invalid_sID = {'code': 1, 'message': 'submission ID invalid'}
-
-# U3
-GLErrorCode.invalid_receiver = {'code': 2, 'message': 'selected receiver ID is invalid in the context'}
-
-# U4
-GLErrorCode.incomplete_fields = {'code': 3, 'message': 'fields requirement not respected'}
-GLErrorCode.incomplete_upload = {'code': 4, 'message': 'An upload appears not yet completed'}
-
-# U5 -- TODO, file upload interface
+# from cyclone.util import ObjectDict as OD
+# GLErrorCode = OD()
+# # All
+# GLErrorCode.malformed_requst = {'code': 0, 'message': 'Malformed Request'}
+#
+# # U2,U3,U4
+# GLErrorCode.invalid_sID = {'code': 1, 'message': 'submission ID invalid'}
+#
+# # U3
+# GLErrorCode.invalid_receiver = {'code': 2, 'message': 'selected receiver ID is invalid in the context'}
+#
+# # U4
+# GLErrorCode.incomplete_fields = {'code': 3, 'message': 'fields requirement not respected'}
+# GLErrorCode.incomplete_upload = {'code': 4, 'message': 'An upload appears not yet completed'}
--- globaleaks/rest/responses.py
+++ globaleaks/rest/responses.py
@@ -5,6 +5,95 @@
 #   This file contain the definition of all the answer struct perfomed by GLB,
 #   and are used to make output validation, sanitization, and operations
 
+
+from globaleaks.rest.base import GLTypes
+
+class publicStatsElement(GLTypes):
+    """
+    Public statistic, just information that would be useful to the
+    public, to make well know if a GL Node has live inside or dead.
+
+    tip_accesses is the sum of wb_accesses and receiver_accesses
+    from the adminStatsElement
+    """
+    specification = {
+        'completed_submission' : int,
+        'tip_accesses' : int
+    }
+
+class publicStatsList(GLTypes):
+
+    specification = {
+        'public_stats' : [ publicStatsElement ]
+    }
+
+class adminStatsElement(GLTypes):
+    """
+    Admin Stats, information useful to the admin, for understand
+    They need to be implemented and specified, too :((
+    """
+
+    specification = {
+        'started_submission' : int,
+        'completed_submission' : int,
+        'receiver_accesses' : int,
+        'wb_accesses' : int,
+        'refused_receiver_auth' : int,
+        'refused_wb_auth' : int,
+        'comments' : int
+    }
+
+class adminStatsList(GLTypes):
+
+    specification = {
+        'admin_stats' : [ adminStatsElement ]
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 adminNodeDesc
 
