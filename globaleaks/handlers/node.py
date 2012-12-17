b91714d92d562c806393daac80310e9f11f2fbcc
vecna <vecna@delirandom.net>
2012-12-17 11:21:04 +0100

    Last API review in hackathon

--- globaleaks/handlers/node.py
+++ globaleaks/handlers/node.py
@@ -36,9 +36,6 @@ class InfoAvailable(BaseHandler):
             nodeinfo = Node()
             node_description_dicts = yield nodeinfo.get_public_info()
 
-            context_view = Context()
-            public_context_view = yield context_view.public_get_all()
-            node_description_dicts.update({"contexts": public_context_view})
 
             # Remind: this is no more an aggregate answer, a client need to perform
             # a GET /contexts to retrive list of contexts.
@@ -51,6 +48,45 @@ class InfoAvailable(BaseHandler):
             self.write({'error_message': e.error_message, 'error_code' : e.error_code})
 
         self.finish()
+
+
+class ContextsAvailable(BaseHandler):
+    """
+    U5
+    """
+
+    @asynchronous
+    @inlineCallbacks
+    def get(self, *uriargs):
+        """
+        Parameters: None
+        Response:
+        Errors:
+        """
+
+        log.debug("[D] %s %s " % (__file__, __name__), "Class Node", "get", uriargs)
+
+        try:
+            context_view = Context()
+            public_context_view = yield context_view.public_get_all()
+            node_description_dicts.update({"contexts": public_context_view})
+
+            # todo output filter
+        except KeyError: # TODO the error returned by the in
+
+class ReceiversAvailable(BaseHandler):
+    """
+    U6
+    """
+
+    @asynchronous
+    @inlineCallbacks
+    def get(self, *uriargs):
+        """
+        Parameters: None
+        Response:
+        Errors:
+        """
 
 
 class StatsAvailable(BaseHandler):
