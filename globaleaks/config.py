682d35a95e48d8590ba2024ae11798dd6be26685
vecna <vecna@delirandom.net>
2013-01-14 09:42:02 +0100

    random GUS generation unrandomized in TESTING mode

--- globaleaks/config.py
+++ globaleaks/config.py
@@ -17,7 +17,7 @@ from storm.zope.zstorm import ZStorm
 from storm.tracer import debug
 import sys
 #Storm DB dump:
-debug(True, sys.stdout)
+debug(False, sys.stdout)
 
 class ConfigError(Exception):
     pass
