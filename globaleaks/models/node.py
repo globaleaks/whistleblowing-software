f6388c597304b3f2b8534b1fc9e8633c48be1457
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2013-01-14 17:00:11 +0100

    test_node.py partially implemented node model refactored global gl variables wrapped inside a global singleton

--- globaleaks/models/node.py
+++ globaleaks/models/node.py
@@ -23,7 +23,7 @@ __all__ = [ 'Node' ]
 class Node(TXModel):
     """
     This table has only one instance, has the "id", but would not exists a second element
-    of this table. This table act, more or less, like the configuration file of the previous
+    of this table. This table acts, more or less, like the configuration file of the previous
     GlobaLeaks release (and some of the GL 0.1 details are specified in Context)
 
     This table represent the System-wide settings
@@ -39,21 +39,18 @@ class Node(TXModel):
     hidden_service = Unicode()
     email = Unicode()
     languages = Pickle()
-
     creation_time = DateTime()
-
     public_stats_update_time = Int()
     private_stats_update_time = Int()
 
     @transact
-    def configure_node(self, input_block):
+    def configure(self, input_dict):
         """
-        @param input_block: its a totally unmaintainable dict
+        @param input_dict: input dictionary
         @return: None
         """
 
-        store = self.getStore('configure_node')
-
+        store = self.getStore()
         try:
             node_data = store.find(Node, 1 == Node.id).one()
         except NotOneError:
@@ -61,22 +58,17 @@ class Node(TXModel):
         if node_data is None:
             raise NodeNotFound
 
-        node_data.description = input_block['description']
-        node_data.name = input_block['name']
-        node_data.public_site = input_block['public_site']
-        node_data.hidden_service = input_block['hidden_service']
-        node_data.public_stats_update_time = int(input_block['public_stats_update_time'])
-        node_data.private_stats_update_time = int(input_block['private_stats_update_time'])
-        node_data.email = input_block['email']
-        node_data.languages = input_block['languages']
+        cls_info = get_cls_info(Node)
+        for name in cls_info.attributes.iterkeys():
+            if name in input_dict:
+                setattr(node_data, name, input_dict[name])
 
         log.msg("Updated node main configuration")
 
     @transact
     def get_public_info(self):
 
-        store = self.getStore('get_public_info')
-
+        store = self.getStore()
         try:
             node_data = store.find(Node, 1 == Node.id).one()
         except NotOneError:
@@ -101,14 +93,9 @@ class Node(TXModel):
     @transact
     def get_admin_info(self):
 
-        store = self.getStore('get_admin_info')
+        check_single_entry();
 
-        try:
-            node_data = store.find(Node, 1 == Node.id).one()
-        except NotOneError:
-            raise NodeNotFound
-        if node_data is None:
-            raise NodeNotFound
+        store = self.getStore()
 
         # this unmaintainable crap need to be removed in the future,
         # and the dict/output generation shall not be scattered
@@ -124,43 +111,3 @@ class Node(TXModel):
             }
 
         return retAdminDict
-
-    @transact
-    def initialize_node(self):
-        """
-        @return: True | False
-        This function is called only one time in a node life, and initialize
-        the table. the configure_node run edit of this row (id = 1)
-        """
-        store = self.getStore('first node init')
-
-        onlyNode = Node()
-
-        onlyNode.id = 1
-        onlyNode.name = u"Please, set me: name/title"
-        onlyNode.description = u"Please, set me: description"
-        onlyNode.hidden_service = u"Please, set me: hidden service"
-        onlyNode.public_site = u"Please, set me: public site"
-        onlyNode.email = u"email@dumnmy.net"
-        onlyNode.private_stats_update_time = 30 # minutes
-        onlyNode.public_stats_update_time = 120 # minutes
-        onlyNode.languages = [ { "code" : "it" , "name": "Italiano"}, { "code" : "en" , "name" : "English" }]
-
-        store.add(onlyNode)
-
-    @transact
-    def only_one(self):
-        """
-        @rtype : bool
-        @return: True or False
-        check if the table in Node is only one
-        """
-
-        store = self.getStore('only_one')
-        nodenum = store.find(Node).count()
-
-        if 1 == nodenum:
-            return True
-        else:
-            print "Unexpected status (exception made for first start), node configured", nodenum
-            return False
