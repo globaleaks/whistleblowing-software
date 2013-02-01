68306fd653b909705c0298ad5e7808a8cf915a1e
vecna <vecna@delirandom.net>
2013-02-01 22:52:53 +0100

    implemented File for #46

--- globaleaks/handlers/files.py
+++ globaleaks/handlers/files.py
@@ -8,16 +8,14 @@
 
 from __future__ import with_statement
 
-import json, os, time
+import json, os
 from twisted.internet.defer import inlineCallbacks
 from cyclone.web import HTTPError, asynchronous
 from globaleaks.handlers.base import BaseHandler
 from globaleaks.utils import log
 from globaleaks.config import config
-from globaleaks.models.submission import Submission
-from globaleaks.models.externaltip import File
-from globaleaks.models.submission import Submission
-from globaleaks.rest.errors import SubmissionGusNotFound, SubmissionConcluded, InvalidInputFormat
+from globaleaks.transactors.fileoperations import FileOperations
+from globaleaks.rest.errors import SubmissionGusNotFound, InvalidInputFormat
 
 __all__ = ['Download', 'FileInstance']
 
@@ -49,49 +47,7 @@ class FileInstance(BaseHandler):
         if not self.acceptedFileType(file['type']):
             raise HTTPError(406, "File of unsupported type")
 
-    def saveFile(self, data, filelocation):
-        """
-        XXX This is currently blocking. MUST be refactored to not be blocking
-        otherwise we loose...
-        """
-        with open(filelocation, 'a+') as f:
-            f.write(data)
 
-    def process_file(self, file, submission_gus, file_gus):
-
-        result = {}
-        result['file_gus'] = file_gus
-        result['name'] = file['filename']
-        result['type'] = file['content_type']
-        result['size'] = len(file['body'])
-
-        # XXX verify this token what's is it
-        result['token'] = submission_gus
-
-        file_location = self.getFileLocation(file_gus)
-
-        log.debug("Saving file to %s" % file_location)
-        self.saveFile(file['body'], file_location)
-        return result
-
-    def getFileLocation(self, file_gus):
-        """
-        Ovewrite me with your own function to generate the location of where
-        the file should be stored.
-        """
-        if not os.path.isdir(config.advanced.submissions_dir):
-            log.debug("%s does not exist. Creating it." % config.advanced.submissions_dir)
-            os.mkdir(config.advanced.submissions_dir)
-
-        the_submission_dir = config.advanced.submissions_dir
-
-        # this happen only at the first execution
-        if not os.path.isdir(the_submission_dir):
-            os.mkdir(the_submission_dir)
-
-        location = os.path.join(the_submission_dir, file_gus)
-
-        return location
 
     @asynchronous
     @inlineCallbacks
@@ -107,11 +63,10 @@ class FileInstance(BaseHandler):
             but in our GL-API-Style design could. At the moment the client
             do not plan to use them.
         """
-        file_iface = File()
 
-        filelist = yield file_iface.get_all_by_submission(submission_gus)
+        answer = yield FileOperations().get_files(submission_gus)
 
-        self.write(json.dumps(filelist))
+        self.write(json.dumps(answer['data']))
         self.set_status(200)
 
     @asynchronous
@@ -126,48 +81,17 @@ class FileInstance(BaseHandler):
         POST in fileHandlers need to be refactored-engineered
         """
 
-        submission_iface = Submission()
         try:
+            print "fileMessageValidation need to be implemented", self.request
+            # TODO ^^^^^^^^^^^^^^^^^^^^^
 
-            submission_desc = yield submission_iface.get_single(submission_gus)
-
-            if submission_desc['finalize']:
-                raise SubmissionConcluded
-
-            results = []
-
-            # XXX will this ever be bigger than 1?
-            file_array, files = self.request.files.popitem()
-            for file in files:
-                start_time = time.time()
-
-                file_request = { 'filename' : file.get('filename'),
-                                 'content_type' : file.get('content_type'),
-                                 'file_size' : len(file['body']),
-                                 'submission_gus' : submission_gus,
-                                 'context_gus' : submission_desc['context_gus'],
-                                 'description' : ''
-                        }
-
-                print "file_request", file_request, "\n"
-
-                file_iface = File()
-                file_desc = yield file_iface.new(file_request)
-
-                log.debug("Created file from %s with file_gus %s" % (file_request['filename'], file_desc['file_gus'] ))
-
-                result = self.process_file(file, submission_gus, file_desc['file_gus'])
-                result['elapsed_time'] = time.time() - start_time
-                results.append(result)
-
-
-            response = json.dumps(results, separators=(',',':'))
+            answer = yield FileOperations().new_files(submission_gus, self.request)
 
             if 'application/json' in self.request.headers.get('Accept'):
                 self.set_header('Content-Type', 'application/json')
 
-            self.set_status(200)
-            self.write(response)
+            self.write(answer['data'])
+            self.set_status(answer['code'])
 
         except InvalidInputFormat, e:
 
---
+++ globaleaks/transactors/fileoperations.py
@@ -0,0 +1,101 @@
+
+from globaleaks.transactors.base import MacroOperation
+
+from globaleaks.models.externaltip import File
+from globaleaks.models.submission import Submission
+from globaleaks.rest.errors import SubmissionConcluded
+from globaleaks.config import config
+
+from storm.twisted.transact import transact
+import time, json, os
+
+class FileOperations(MacroOperation):
+    """
+    README.md describe pattern and reasons
+    """
+
+    @transact
+    def get_files(self, submission_gus):
+
+        file_iface = File(self.getStore())
+
+        filelist = file_iface.get_all_by_submission(submission_gus)
+
+        self.returnData(filelist)
+        self.returnCode(200)
+        return self.prepareRetVals()
+
+    def _dump_file(self, file, submission_gus, file_gus):
+
+        result = {}
+        result['file_gus'] = file_gus
+        result['name'] = file['filename']
+        result['type'] = file['content_type']
+        result['size'] = len(file['body'])
+
+        # XXX verify this token what's is it
+        result['token'] = submission_gus
+
+        if not os.path.isdir(config.advanced.submissions_dir):
+            print "%s does not exist. Creating it." % config.advanced.submissions_dir
+            os.mkdir(config.advanced.submissions_dir)
+
+        the_submission_dir = config.advanced.submissions_dir
+
+        # this happen only at the first execution
+        if not os.path.isdir(the_submission_dir):
+            os.mkdir(the_submission_dir)
+
+        filelocation = os.path.join(the_submission_dir, file_gus)
+
+        print "Saving file to %s" % filelocation
+        # TODO This is currently blocking. MUST be refactored to not be blocking
+        # otherwise we loose...
+        with open(filelocation, 'a+') as f:
+            f.write(file['body'])
+
+        return result
+
+    @transact
+    def new_files(self, submission_gus, request):
+
+        store = self.getStore()
+
+        submission_iface = Submission(store)
+
+        submission_desc = submission_iface.get_single(submission_gus)
+
+        if submission_desc['finalize']:
+            raise SubmissionConcluded
+
+        results = []
+
+        file_array, files = request.files.popitem()
+        for single_file in files:
+            start_time = time.time()
+
+            file_request = { 'filename' : single_file.get('filename'),
+                             'content_type' : single_file.get('content_type'),
+                             'file_size' : len(single_file['body']),
+                             'submission_gus' : submission_gus,
+                             'context_gus' : submission_desc['context_gus'],
+                             'description' : ''
+            }
+
+            print "file_request", file_request, "\n"
+
+            file_iface = File(store)
+            file_desc = file_iface.new(file_request)
+
+            print "Created file from %s with file_gus %s" % (file_request['filename'], file_desc['file_gus'])
+
+            result = self._dump_file(single_file, submission_gus, file_desc['file_gus'])
+            result['elapsed_time'] = time.time() - start_time
+            results.append(result)
+
+        response = json.dumps(results, separators=(',',':'))
+
+        self.returnData(response)
+        self.returnCode(200)
+        return self.prepareRetVals()
+
