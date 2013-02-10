0bddfddde1031eb80b9b8e944036664696c8ad1a
Arturo Filastò <art@fuffa.org>
2013-02-10 22:09:47 +0100

    Refactoring of receivers into service
    Make it not be a table, but a div

--- app/scripts/controllers/admin/receivers.js
+++ app/scripts/controllers/admin/receivers.js
@@ -1,41 +1,16 @@
 GLClient.controller('AdminReceiversCtrl', ['$scope', 'AdminReceivers',
 function($scope, AdminReceivers) {
 
-  $scope.new_receiver = function() {
-    var receiver = new AdminReceivers;
+  $scope.new_receiver = $scope.admin.create_receiver;
 
-    receiver.name = $scope.new_receiver_name;
-
-    receiver.description = '';
-    receiver.password = null;
-
-    receiver.notification_selected = 'email';
-    receiver.notification_fields = {'mail_address': ''};
-
-    receiver.languages = [];
-
-    // Under here go default settings
-    receiver.can_postpone_expiration = true;
-    receiver.can_configure_notification = true;
-    receiver.can_configure_delivery = true;
-    receiver.can_delete_submission = true;
-
-    receiver.receiver_level = 1;
-
-    receiver.tags = [];
-    receiver.contexts =  [];
-
-    receiver.$save(function(created_receiver){
-      $scope.adminReceivers.push(created_receiver);
-    });
-  };
-
-  $scope.delete_receiver = function(receiver) {
-    var idx = _.indexOf($scope.adminReceivers, receiver);
+  $scope.delete = function(receiver) {
+    var idx = _.indexOf($scope.admin.receivers, receiver);
 
     receiver.$delete();
-    $scope.adminReceivers.splice(idx, 1);
+    $scope.admin.receivers.splice(idx, 1);
   };
+
+  $scope.$watch('form')
 
 
 }]);
