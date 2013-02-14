aedec422a72f3ec6f036fca14d3aa5fee459453c
Arturo Filastò <art@fuffa.org>
2013-02-14 21:45:45 +0100

    Add input form to allow input of the receiver tip on homepage

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,8 +1,18 @@
 'use strict';
 
-GLClient.controller('HomeCtrl', ['$scope', 'Node',
-  function($scope, Node) {
+GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication',
+                    'WhistleblowerTip',
+  function ($scope, $location, Node, Authentication, WhistleblowerTip) {
+    $scope.receipt = '';
+
     Node.get(function(node_info){
       $scope.node_info = node_info;
     });
+
+    $scope.view_tip = function(receipt) {
+      WhistleblowerTip(receipt, function(tip_id) {
+        $location.path('/status/' + tip_id);
+      });
+    };
+
 }]);
