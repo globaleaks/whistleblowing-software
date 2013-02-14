08a92a567138b42653ebc1acc1808dac473e9f9b
Michele Orru` <maker@python.it>
2013-02-14 14:16:02 +0100

    merging notification email from feature/email

--- globaleaks/db/__init__.py
+++ globaleaks/db/__init__.py
@@ -12,24 +12,8 @@ from globaleaks.settings import transact
 from globaleaks import models
 
 @transact
-def initialize_node(store, result):
+def initialize_node(store, onlyNode, result):
     nodes = store.find(models.Node)
-    if len(list(nodes)) == 0:
-        log.debug('Initializing node with new config')
-        # Initialize the node
-        onlyNode = {
-            'name':  u"Please, set me: name/title",
-            'description':  u"Please, set me: description",
-            'hidden_service':  u"Please, set me: hidden service",
-            'public_site':  u"Please, set me: public site",
-            'email':  u"email@dumnmy.net",
-            'stats_update_time':  2, # hours,
-            'languages':  [{ "code" : "it" , "name": "Italiano"},
-                           { "code" : "en" , "name" : "English" }],
-            'notification_settings': {},
-            'password': u'globaleaks',
-            'creation_date': models.now(),
-        }
         store.add(models.Node(onlyNode))
 
 def initModels():
@@ -65,6 +49,27 @@ def createTables(create_node=True):
 
     d = create_tables_transaction()
     if create_node:
-        d.addCallback(initialize_node)
+        # load notification template
+        with f as open(settings.root_path, 'globaleaks', 'db',
+                           'emailnotification_template'):
+            emailnotification_template = f.read()
+        if len(list(nodes)) == 0:
+            log.debug('Initializing node with new config')
+            # Initialize the node
+            onlyNode = {
+                'name':  u"Please, set me: name/title",
+                'description':  u"Please, set me: description",
+                'hidden_service':  u"Please, set me: hidden service",
+                'public_site':  u"Please, set me: public site",
+                'email':  u"email@dumnmy.net",
+                'stats_update_time':  2, # hours,
+                'languages':  [{ "code" : "it" , "name": "Italiano"},
+                               { "code" : "en" , "name" : "English" }],
+                'notification_settings': {},
+                'password': u'globaleaks',
+                'creation_date': models.now(),
+                'notificationmail_template': notificationmail_template,
+            }
+        d.addCallback(initialize_node, onlyNode)
     return d
 
--- globaleaks/plugins/notification/mail_plugin.py
+++ globaleaks/plugins/notification/mail_plugin.py
@@ -7,6 +7,7 @@ from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
 
 from globaleaks.utils import log
+from globaleaks.settings import transact
 from globaleaks.plugins.base import Notification
 
 
@@ -23,15 +24,21 @@ class MailNotification(Notification):
         self.receiver_fields = {'mail_address' : 'text'}
 
     def validate_admin_opt(self, pushed_af):
-        # send a mock email
-        return True
+        if self._get_SMTP(pushed_af['server'], pushed_af['port'], pushed_af['ssl'],
+                pushed_af['username'], pushed_af['password']):
+            return True
+        else:
+            return False
 
     def validate_receiver_opt(self, admin_fields, receiver_fields):
         log.debug("[%s] receiver_fields %s (with admin %s)" % ( self.__class__.__name__, receiver_fields, admin_fields))
         return True
 
-    def initialize(self, admin_fields):
-        return True
+    @transact
+    def initialize(self, store, admin_fields):
+        node = store.find(Node).one()
+        self.body = node.notificationemail_template
+        return true
 
     def _append_email(self):
         """
@@ -45,7 +52,7 @@ class MailNotification(Notification):
         if data_type == u'tip':
             return "New tip from GLBNode"
 
-        Exception("Unsupported notification_struct usage")
+        raise Exception("Unsupported notification_struct usage")
 
     def _create_email(self, data_type, data, source, dest, subject):
         """
@@ -60,37 +67,9 @@ class MailNotification(Notification):
             body += "and, by the way, the content is:\n%s\n" % data['content']
 
         if data_type == u'tip':
-            body += "In %s as been created a new Tip for you\n" % data['notification_date']
-            body += "You can access using the unique link http://dev.globaleaks.org:8082/#/status/%s\n" % data['tip_gus']
-            body += "\n"\
-            "This is an E-Mail message to notify you that someone has selected you as a valuable recipient of "\
-            "WhistleBlowing material in the form of a Globaleaks tip-off. This message has been created "\
-            "by the GlobaLeaks Node [http://dev.globaleaks.org].\nThis tip-off has been sent to you by "\
-            "an anonymous whistleblower. She/He would like it for you to"\
-            "pay special attention to the information and material contained therein. Please consider"\
-            "that whistleblowers often expose themselves to high personal risks in order to protect the public good. Therefore "\
-            "the material that they provide with this tip-off should be considered of high importance.\n\n"\
-            "Please do not forward or share this e-mail: each tip-off has a limited number of downloads and access before being "\
-            "destroyed forever, nobody (even the node administrator) can recover and expired or dead tip-off.\n\n\n"
+            body = node.fuffa%  % data['notification_date']% data['tip_gus']
 
-            body += "\n"\
-            "--------------------------------------------------\n"\
-            "GENERAL INFO\n"\
-            "--------------------------------------------------\n"\
-            "1. What is Globaleaks?\n"\
-            "GlobaLeaks is the first Open Source Whistleblowing Framework. It empowers anyone to easily setup and "\
-            "maintain their own Whistleblowing platform. It is also a collection of what are the best practices for "\
-            "people receiveiving and submitting material. GlobaLeaks works in all environments: media, activism, corporations, public agencies.\n\n"\
-            "2. Is GlobaLeaks sending me this Mail?\n"\
-            "No, this mail has been sent to you by the Node called [http://dev.globaleaks.org]. They are running the GlobaLeaks Platform, but\n"\
-            "are not directly tied to the GlobaLeaks organization. GlobaLeaks (http://www.globaleaks.org) will never be directly "\
-            "affiliated with any real world WhistleBlowing sites, GlobaLeaks will only provide software and technical support.\n"\
-            "3. Why am I receiving this?\n"\
-            "You're receiving this communication because an anonymous whistleblower has chosen you as a trustworthy contact"\
-            "for releasing confidential and/or important information that could be of utmost importance.\n\n"\
-            "For any other inquire please refer to %(sitename)s to the GlobaLeaks website at http://globaleaks.org\n\n"
 
-        body += "\n\nBest regards,\nThe email notification plugin"
 
         return string.join(("From: GLBackend postino <%s>" % source,
                             "To: Estimeed Receiver <%s>" % dest,
