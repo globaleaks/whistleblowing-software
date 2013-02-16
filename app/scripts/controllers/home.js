c5d7a1d2e687210398b0e50902544ecdfc02ea4d
vecna <vecna@delirandom.net>
2013-02-16 12:48:01 +0100

    'Blow the whistle' button show only if a Context is configured

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,12 +1,20 @@
 'use strict';
 
 GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication',
-                    'WhistleblowerTip',
-  function ($scope, $location, Node, Authentication, WhistleblowerTip) {
+                    'WhistleblowerTip', 'Contexts',
+  function ($scope, $location, Node, Authentication, WhistleblowerTip, Contexts) {
     $scope.receipt = '';
+    $scope.configured = false;
 
     Node.get(function(node_info){
       $scope.node_info = node_info;
+    });
+
+
+    Contexts.query(function(contexts){
+      if(contexts.length > 0) {
+          $scope.configured = true
+      }
     });
 
     $scope.view_tip = function(receipt) {
