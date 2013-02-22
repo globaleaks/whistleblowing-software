219f01c808a55a7605cacb2edcb3d90111c3c618
Arturo Filastò <art@fuffa.org>
2013-02-22 19:36:13 +0100

    Immediately show the logout button as soon as we have logged in

--- app/scripts/controllers/tooltip.js
+++ app/scripts/controllers/tooltip.js
@@ -1,10 +1,17 @@
 GLClient.controller('toolTipCtrl',
-  ['$scope', 'Authentication', function($scope, Authentication) {
+  ['$scope', 'Authentication', '$location',
+function($scope, Authentication, $location) {
 
   $scope.session_id = sessionStorage['session_id'];
   $scope.auth_landing_page = sessionStorage['auth_landing_page'];
   $scope.user_id = sessionStorage['user_id'];
   $scope.role = sessionStorage['role'];
+
+  $scope.$watch(function(scope){
+    return sessionStorage['session_id'];
+  }, function(newVal, oldVal){
+    $scope.session_id = sessionStorage['session_id'];
+  });
 
   $scope.logout = function() {
     Authentication.logout().then(function(){
