0a7405f26462cc8380fecd8bc788bcce597ed202
Arturo Filastò <art@fuffa.org>
2013-02-23 23:57:18 +0100

    Add support for passing role and user_id parameters to request
    This avoids having some dangerous hacks inside of our base handler

--- globaleaks/tests/test_handlers/test_admin.py
+++ globaleaks/tests/test_handlers/test_admin.py
@@ -19,8 +19,7 @@ class TestNodeInstance(helpers.TestHandler):
 
     @inlineCallbacks
     def test_put_update_node(self):
-        handler = self.request(self.dummyNode)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(self.dummyNode, role='admin')
         yield handler.put()
         del self.dummyNode['password']
         del self.dummyNode['old_password']
@@ -32,7 +31,7 @@ class TestContextsCollection(helpers.TestHandler):
     _handler = admin.ContextsCollection
 
     def test_get(self):
-        handler = self.request()
+        handler = self.request(role='admin')
         handler.request.headers['X-Session'] = 'test_admin'
         return handler.get()
 
@@ -40,8 +39,7 @@ class TestContextsCollection(helpers.TestHandler):
     def test_post(self):
         request_context = self.dummyContext
         del request_context['contexts'] # why is here !?
-        handler = self.request(request_context)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(request_context, role='admin')
         yield handler.post()
 
         request_context['context_gus'] =  self.responses[0]['context_gus']
@@ -52,8 +50,7 @@ class TestContextInstance(helpers.TestHandler):
 
     @inlineCallbacks
     def test_get(self):
-        handler = self.request()
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(role='admin')
         yield handler.get(self.dummyContext['context_gus'])
         del self.dummyContext['contexts']
         self.assertEqual(self.responses[0], self.dummyContext)
@@ -63,8 +60,7 @@ class TestContextInstance(helpers.TestHandler):
         request_context = self.dummyContext
         request_context['name'] = u'spam'
         del request_context['contexts'] # I don't know what's doing here!!?
-        handler = self.request(request_context)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(request_context, role='admin')
         yield handler.put(request_context['context_gus'])
         self.assertEqual(self.responses[0], self.dummyContext)
 
@@ -73,8 +69,7 @@ class TestReceiversCollection(helpers.TestHandler):
 
     @inlineCallbacks
     def test_get(self):
-        handler = self.request()
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(role='admin')
         yield handler.get()
 
         # XXX helpers.py.. Why self.responses is became a double array ?
@@ -85,8 +80,7 @@ class TestReceiversCollection(helpers.TestHandler):
     @inlineCallbacks
     def test_post(self):
         self.dummyReceiver['name'] = 'beppe'
-        handler = self.request(self.dummyReceiver)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(self.dummyReceiver, role='admin')
         yield handler.post()
 
         # We delete this because it's randomly generated
@@ -99,8 +93,7 @@ class TestReceiverInstance(helpers.TestHandler):
 
     @inlineCallbacks
     def test_get(self):
-        handler = self.request()
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(role='admin')
         yield handler.get(self.dummyReceiver['receiver_gus'])
         del self.dummyReceiver['contexts']
         del self.responses[0]['contexts']
@@ -111,8 +104,7 @@ class TestReceiverInstance(helpers.TestHandler):
         self.dummyReceiver['context_gus'] = ''
         del self.dummyReceiver['username']
         self.dummyReceiver['name'] = u'new name'
-        handler = self.request(self.dummyReceiver)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(self.dummyReceiver, role='admin')
         yield handler.put(self.dummyReceiver['receiver_gus'])
         self.assertEqual(self.responses[0]['name'], self.dummyReceiver['name'])
 
@@ -122,8 +114,7 @@ class TestReceiverInstance(helpers.TestHandler):
         # keep the context_gus wrong but matching eventually regexp
         import uuid
         self.dummyReceiver['contexts'] = [ unicode(uuid.uuid4()) ]
-        handler = self.request(self.dummyReceiver)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(self.dummyReceiver, role='admin')
         # I've some issue in use assertRaises with 'yield', then:
         try:
             yield handler.put(self.dummyReceiver['receiver_gus'])
@@ -133,8 +124,7 @@ class TestReceiverInstance(helpers.TestHandler):
 
     @inlineCallbacks
     def test_delete(self):
-        handler = self.request(self.dummyReceiver)
-        handler.request.headers['X-Session'] = 'test_admin'
+        handler = self.request(self.dummyReceiver, role='admin')
         try:
             yield handler.delete(self.dummyReceiver['receiver_gus'])
             self.assertTrue(True)
