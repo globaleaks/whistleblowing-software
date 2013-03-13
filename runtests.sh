0326d2d5653b7ddd27e25718565f8830f82bd0e3
vecna <vecna@delirandom.net>
2013-03-13 01:07:43 +0100

    removed runtest #67

--- runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# for some not investigated reasons, the tests need to be started one by one.
-
-testfiles=`ls globaleaks/tests/test_*.py globaleaks/tests/*/test_*.py`
-for tf in $testfiles;
-    do trial $tf
-done
