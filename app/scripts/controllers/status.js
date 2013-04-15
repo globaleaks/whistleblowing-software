2868706ced3bb804d9928618f0e041efbcaec48d
Arturo Filastò <art@fuffa.org>
2013-04-15 15:17:47 +0200

    Fix bug in file upload on status page

--- app/scripts/controllers/status.js
+++ app/scripts/controllers/status.js
@@ -1,6 +1,6 @@
 GLClient.controller('StatusCtrl',
-  ['$scope', '$routeParams', 'Tip', '$cookies',
-  function($scope, $routeParams, Tip, $cookies) {
+  ['$scope', '$rootScope', '$routeParams', 'Tip', '$cookies',
+  function($scope, $rootScope, $routeParams, Tip, $cookies) {
     $scope.tip_id = $routeParams.tip_id;
     var TipID = {tip_id: $scope.tip_id};
 
@@ -14,9 +14,9 @@ GLClient.controller('StatusCtrl',
     };
 
     if ($cookies['role'] === 'wb') {
-      $scope.whistleblower_tip_id = $cookies['tip_id'];
-      $scope.uploadedFiles = [];
-      $scope.uploadingFiles = [];
+      $rootScope.whistleblower_tip_id = $cookies['tip_id'];
+      $rootScope.uploadedFiles = [];
+      $rootScope.uploadingFiles = [];
     };
 
 }]);
