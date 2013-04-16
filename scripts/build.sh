f5e488afb5f2709558bd5a2bc773205a95a15e86
Arturo Filastò <art@fuffa.org>
2013-04-16 13:07:50 +0200

    Place into subdirectories and add support for tags

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,20 @@ GLBACKEND_GIT_REPO="https://github.com/globaleaks/GLBackend.git"
 GLCLIENT_GIT_REPO="https://github.com/globaleaks/GLClient.git"
 
 if [ "$#" -ne 2 ]; then
-  echo "Usage: ./${SCRIPTNAME} [repository path] [output dir]"
+  echo "Usage: ./${SCRIPTNAME} [repository path] [output dir] (optional) [glclient target tag or rev] [glbackend target tag]"
   echo "repository path: is the path to a copy of the GLClient and GLBackend git repositories"
   exit
 fi
 GLOBALEAKS_DIR=$1
 OUTPUT_DIR=$2
+
+if [ "$#" -eq 4 ]; then
+  GLCLIENT_TAG=$3
+  GLBACKEND_TAG=$4
+fi
+
+mkdir $OUTPUT_DIR/GLClient
+mkdir $OUTPUT_DIR/GLBackend
 
 if [ ! -d ${GLOBALEAKS_DIR}/GLBackend ]; then
   echo "[+] Cloning GLBackend in ${GLOBALEAKS_DIR}"
@@ -24,18 +32,28 @@ fi
 
 echo "[+] Updating GLBackend"
 cd ${GLOBALEAKS_DIR}/GLBackend
-git pull
-
+git pull master
 GLBACKEND_REVISION=`git rev-parse HEAD | cut -c 1-8`
+
+if test $GLBACKEND_TAG; then
+  git checkout $GLBACKEND_TAG
+  $GLBACKEND_REVISION=$GLBACKEND_TAG
+fi
+
 echo "[+] Building GLBackend"
-#python setup.py bdist sdist
+python setup.py bdist sdist
 cd $CWD
 
 echo "[+] Updating GLClient"
 cd ${GLOBALEAKS_DIR}/GLClient
-git pull
-
+git pull master
 GLCLIENT_REVISION=`git rev-parse HEAD | cut -c 1-8`
+
+if test $GLCLIENT_TAG; then
+  git checkout $GLCLIENT_TAG
+  $GLCLIENT_REVISION=$GLCLIENT_TAG
+fi
+
 echo "[+] Building GLClient"
 npm install -d
 grunt build
