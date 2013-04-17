425ef463eaa54616e83609616861a796cced8f89
Arturo Filastò <art@fuffa.org>
2013-04-17 14:02:36 +0200

    Include also interesting modules to angular-ui
    Event, If, Jq, Keypress, Showhide, Validate

--- app/scripts/vendor/angular-ui-ieshiv.js
+++ app/scripts/vendor/angular-ui-ieshiv.js
@@ -1,6 +1,6 @@
 /**
  * AngularUI - The companion suite for AngularJS
- * @version v0.4.0 - 2013-02-15
+ * @version v0.4.0 - 2013-04-06
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
