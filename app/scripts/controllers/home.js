506f51f0a3496f3eefe1fa1216a82e0fea09e345
vecna <vecna@delirandom.net>
2013-05-16 14:18:22 +0200

    moved to GLB the configuration check https://github.com/globaleaks/GlobaLeaks/issues/230

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -10,21 +10,6 @@ GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication'
       $scope.node_info = node_info;
     });
 
-    Receivers.query(function(receivers){
-      if(receivers.length > 0) {
-          $scope.receiver_configured = true;
-      }
-    });
-
-    Contexts.query(function(contexts){
-       if(contexts.length > 0) {
-          $scope.context_configured = true;
-          if($scope.receiver_configured == true) {
-              $scope.configured = true;
-          }
-       }
-    });
-
     $scope.view_tip = function(receipt) {
       WhistleblowerTip(receipt, function(tip_id) {
         $location.path('/status/' + tip_id);
