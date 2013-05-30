b525931eafd6c188d005ee6f67fc0072e98d19f7
Arturo Filastò <art@fuffa.org>
2013-05-30 12:11:30 +0200

    Make the templates a variable.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -549,3 +549,19 @@ angular.module('resourceServices', ['ngResource', 'ngCookies', 'resourceServices
     $httpProvider.responseInterceptors.push('globaleaksInterceptor');
     $httpProvider.defaults.transformRequest.push(globaleaksRequestInterceptor);
 }]);
+
+angular.module('GLClient.themes', [])
+  .factory('Templates', function() {
+      var selected_theme = 'default';
+
+      return {
+        'home': 'templates/' + selected_theme + '/views/home.html',
+        'about': 'templates/' + selected_theme + '/views/about.html',
+        'status': 'templates/' + selected_theme + '/views/status.html',
+        'submission': {
+          'form': 'templates/' + selected_theme + '/views/submission/form.html',
+          'main': 'templates/' + selected_theme + '/views/submission/main.html'
+        }
+      };
+    }
+);
