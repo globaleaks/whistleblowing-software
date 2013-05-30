f7117829d3c0226656c19dff278db517d3cb917a
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-05-30 12:52:57 +0200

    building script refactored allowing package building tests before signing

--- scripts/build-glbackend.sh
+++ scripts/build-glbackend.sh
@@ -3,28 +3,63 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPTNAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 GLBACKEND_GIT_REPO="https://github.com/globaleaks/GLBackend.git"
 GLOBALEAKS_DIR=/data/globaleaks
+OUTPUT_DIR=$GLOBALEAKS_DIR/GLBackend/glbackend_build
 
-if [ "$1" = "-h" ]; then
-  echo "Usage: ./${SCRIPTNAME} [glbackend target tag]"
-  echo "repository path: is the path to a copy of the GLClient and GLBackend git repositories"
-  exit
-fi
+usage()
+{
+cat << EOF
+usage: ./${SCRIPTNAME} options
 
-GLBACKEND_TAG=$1
+OPTIONS:
+   -h      Show this message
+   -v      To build a tagged release"
+   -n      To build a non signed package"
+   -y      To assume yes to all queries"
 
-mkdir -p $GLOBALEAKS_DIR
+EOF
+}
+
+SIGN=1
+ASSUME_YES=0
+while getopts “hv:ny” OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    v)
+      TAG=$OPTARG
+      ;;
+    n)
+      SIGN=0
+      ;;
+    y)
+      ASSUME_YES=1
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+    esac
+done
+
+mkdir -p $GLOBALEAKS_DIR $OUTPUT_DIR
 
 build_glbackend()
 {
   if [ -d ${GLOBALEAKS_DIR}/GLBackend ]; then
     echo "Directory ${GLOBALEAKS_DIR}/GLBackend already present"
     echo "The build process needs a clean git clone of GLBackend"
-    read -n1 -p "Are you sure you want delete ${GLOBALEAKS_DIR}/GLBackend? (y/n): "
-    echo
-    if [[ $REPLY != [yY] ]]; then
-      echo "Exiting ..."
-      exit
+    if [ ${ASSUME_YES} -eq 0 ]; then
+      read -n1 -p "Are you sure you want delete ${GLOBALEAKS_DIR}/GLBackend? (y/n): "
+      echo
+      if [[ $REPLY != [yY] ]]; then
+        echo "Exiting ..."
+        exit
+      fi
     fi
+    echo "Removing directory ${GLOBALEAKS_DIR}"
     rm -rf ${GLOBALEAKS_DIR}/GLBackend
   fi
 
@@ -51,8 +86,15 @@ build_glbackend()
   cd deb_dist/globaleaks-*
   rm -rf debian/
   cp -rf ${GLOBALEAKS_DIR}/GLBackend/debian debian
-  debuild
-  
+
+  if [ $SIGN -eq 1 ]; then
+    debuild
+  else
+    debuild -i -us -uc -b
+  fi
+
+  mkdir -p ${OUTPUT_DIR}
+  mv /data/globaleaks/GLBackend/dist/* ${OUTPUT_DIR}
 }
 
 build_glbackend
