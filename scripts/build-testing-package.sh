ec854ca894de454f319785b128fec4f53b4c331d
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-06-11 18:20:40 +0200

    packaging testingg scripts simplified (one script less)

--- scripts/build-testing-package.sh
+++ scripts/build-testing-package.sh
@@ -2,5 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common_inc.sh
 
+echo "[+] Setupping GLClient build environment"
+sudo add-apt-repository ppa:chris-lea/node.js -y
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+sudo npm install -g grunt-cli
+
+echo "[+] Setupping GLBackend build environment"
+sudo apt-get install python-dev build-essential python-virtualenv python-pip -y
+
 ${DIR}/build-glclient.sh
 ${DIR}/build-glbackend.sh -n
