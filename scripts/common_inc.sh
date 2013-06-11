a388f954eb0c5837bb4da440bc4b4c59bb6c6f61
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-06-11 13:45:23 +0200

    build script now run makin a copy of GLBackend and GLClient to permit multiple clean runs

--- scripts/common_inc.sh
+++ scripts/common_inc.sh
@@ -3,6 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPTNAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
 GLBACKEND_GIT_REPO="https://github.com/globaleaks/GLBackend.git"
 GLCLIENT_GIT_REPO="https://github.com/globaleaks/GLClient.git"
 
@@ -20,5 +21,11 @@ cd ${BUILD_DIR}
 
 GLBACKEND_DIR=$( readlink -m ${BUILD_DIR}/GLBackend)
 GLCLIENT_DIR=$( readlink -m ${BUILD_DIR}/GLClient)
-GLB_BUILD=$( readlink -m ${GLBACKEND_DIR}/glbackend_build)
-GLC_BUILD=$( readlink -m ${GLCLIENT_DIR}/glclient_build)
+GLBACKEND_TMP=${GLBACKEND_DIR}_tmp
+GLCLIENT_TMP=${GLCLIENT_DIR}_tmp
+GLB_BUILD=$( readlink -m ${GLBACKEND_TMP}/glbackend_build)
+GLC_BUILD=$( readlink -m ${GLCLIENT_TMP}/glclient_build)
+
+echo "Running command ${SCRIPTNAME} $*"
+echo "Build directory used: ${BUILD_DIR}"
+echo "To override this do: 'GLOBALEAKS_BUILD_ENV=/what/you/want && export GLOBALEAKS_BUILD_ENV'"
