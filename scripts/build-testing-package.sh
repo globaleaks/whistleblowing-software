0c4a07adcaff1b15b59f46171187bea14f3ea7e9
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-06-12 11:44:49 +0200

    added python-stdeb to build-testing-package.sh apt-get install

--- scripts/build-testing-package.sh
+++ scripts/build-testing-package.sh
@@ -9,7 +9,7 @@ sudo apt-get install nodejs -y
 sudo npm install -g grunt-cli
 
 echo "[+] Setupping GLBackend build environment"
-sudo apt-get install python-dev build-essential python-virtualenv python-pip -y
+sudo apt-get install python-dev build-essential python-virtualenv python-pip python-stdeb -y
 
 ${DIR}/build-glclient.sh
 ${DIR}/build-glbackend.sh -n
