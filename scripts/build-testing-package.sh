fabaf27d3c52a9631e144df4aa9e80575547a57a
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-06-13 12:11:59 +0200

    added a stateful check to bypas requirements install inside build-testing-package

--- scripts/build-testing-package.sh
+++ scripts/build-testing-package.sh
@@ -2,14 +2,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common_inc.sh
 
-echo "[+] Setupping GLClient build environment"
-sudo -i add-apt-repository ppa:chris-lea/node.js -y
-sudo -i apt-get update -y
-sudo -i apt-get install nodejs -y
-sudo -i npm install -g grunt-cli
+echo "[+] Setupping GLClient and GLBackend build environments"
 
-echo "[+] Setupping GLBackend build environment"
-sudo -i apt-get install python-dev build-essential python-virtualenv python-pip python-stdeb devscripts -y
+if [ ! -f ${DIR}/.environment_setupped ]; then
+    sudo -i add-apt-repository ppa:chris-lea/node.js -y
+    sudo -i apt-get update -y
+    sudo -i apt-get install nodejs -y
+    sudo -i npm install -g grunt-cli
+    sudo -i apt-get install python-dev build-essential python-virtualenv python-pip python-stdeb devscripts -y
+    touch ${DIR}/.environment_setupped
+fi
 
 ${DIR}/build-glclient.sh
 ${DIR}/build-glbackend.sh -n
