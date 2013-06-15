d4445bf4e866bc0dcf45e52c9c96d78c7300ce9c
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-06-15 15:55:03 +0200

    changed from tail -4 to tail -3 on building fix

--- scripts/build-glclient.sh
+++ scripts/build-glclient.sh
@@ -126,7 +126,7 @@ build_glclient()
   cp app/scripts/translations.js build/scripts/
   cat build/index.html | head -81 >> build/hack_index.html
   echo '<script src="scripts/translations.js"></script>' >> build/hack_index.html
-  tail -4 build/index.html >> build/hack_index.html
+  tail -3 build/index.html >> build/hack_index.html
   mv build/index.html build/generated_index.html
   mv build/hack_index.html build/index.html
 
