4a614af0647ba2d7a968bcd19d48ba3a858292a2
Arturo Filastò <art@fuffa.org>
2013-06-27 02:34:52 +0200

    Fix bug that occurs if mini-dinstall is called twice

--- scripts/sign-release.sh
+++ scripts/sign-release.sh
@@ -8,16 +8,8 @@ cd ${GLB_BUILD}/deb_dist
 
 echo "[+] Adding deb package to the local repository"
 dput local globaleaks*changes
-mini-dinstall --batch
 
 if [ -d ${GLCLIENT_DIR} ]; then
   echo "[+] Copying GLClient package to ${WEB_DIR}"
   cp ${GLCLIENT_DIR}/* ${WEB_DIR}
 fi
-
-# XXX why are we doing this? This seems quite hackish and it seems to be due to
-# a bug inside of debuild. Are we sure we are using debuild properly?
-# Previously builds did not require this hack to be present.
-echo "[+] Signing Release"
-gpg --default-key "${KEYID}" --detach-sign -o Release.gpg.tmp ${REPO_DIR}/unstable/Release
-mv Release.gpg.tmp ${REPO_DIR}/unstable/Release.gpg
