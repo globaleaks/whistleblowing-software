50b6a6720e7bc77bd4f5a0f15329d21d18c45766
Arturo Filastò <art@fuffa.org>
2013-06-28 16:51:26 +0200

    Implement acceptance disclaimer on "blow the whistle button"
    closes: https://github.com/globaleaks/GlobaLeaks/issues/260

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -5,6 +5,17 @@ GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication'
   function ($scope, $location, Node, Authentication, WhistleblowerTip, Contexts, Receivers) {
     $scope.receipt = '';
     $scope.configured = false;
+    $scope.anonymous = false;
+    $scope.step = 1;
+    $scope.answer = {value: null}
+    $scope.answered = false;
+    $scope.correctAnswer = false;
+    $scope.showQuestions = false;
+
+
+    if ($scope.privacy == 'high') {
+      $scope.anonymous = true;
+    }
 
     Node.get(function(node_info){
       $scope.node_info = node_info;
@@ -15,5 +26,35 @@ GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication'
         $location.path('/status/' + tip_id);
       });
     };
+
+    $scope.checkAnswer = function() {
+      if (!$scope.answer.value)
+        return;
+      if ($scope.answer.value == 'b') {
+        $scope.correctAnswer = true;
+      }
+      $scope.answered = true;
+      $scope.step = 3;
+    }
+
+    $scope.goToSubmission = function() {
+      if ($scope.anonymous || $scope.correctAnswer) {
+        $location.path("/submission");
+      } else {
+        $scope.showQuestions = true; 
+      }
+      
+    }
+    
+    $scope.goToStep = function(idx) {
+      $scope.step = idx; 
+    }
+
+    $scope.goToStart = function() {
+      $scope.answered = false;
+      $scope.correctAnswer = false;
+      $scope.goToStep(1);
+    }
+
 
 }]);
