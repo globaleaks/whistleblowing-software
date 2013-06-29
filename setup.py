c9c2cfc1a5c17efca01a8632b67d2c6a6027291e
vecna <vecna@delirandom.net>
2013-06-29 02:09:48 +0200

    BUMP TO... ** 2.23 **, ZOMG!!

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def download_glclient():
 
 def verify_glclient():
     print "[+] Checking GLClient hash..."
-    glclient_hash = "dc668d8f85a4bd0df688ebf884089366272d56739316372512ca2c4b"
+    glclient_hash = "41d066d0e9abc421150091e8e838e117ed9b02e5274304469f177780"
     with open('glclient.zip') as f:
         h = hashlib.sha224(f.read()).hexdigest()
         if not h == glclient_hash:
