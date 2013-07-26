globaleaks/whistleblowing-software

commit 7b5c24ad5c79ca1b9fd182a4164653d17a60b752

completed and tested key expiration scheduler, part of https://github.com/globaleaks/GlobaLeaks/issues/187

globaleaks/utils.py
@@ -225,7 +225,8 @@ def sendmail(authentication_username, authentication_password, from_address,
     @param authentication_secret: account password
     @param from_address: the from address field of the email
     @param to_address: the to address field of the email
-    @param message_file: the message content
+    @param message_file: the message content,
+        string and unicode are converted in StringIO
     @param smtp_host: the smtp host
     @param smtp_port: the smtp port
     @param event: the event description, needed to keep track of failure/success
@@ -263,6 +264,14 @@ def sendmail(authentication_username, authentication_password, from_address,
     esmtp_deferred = Deferred()
     esmtp_deferred.addErrback(handle_error, event)
     esmtp_deferred.addCallback(result_deferred.callback)
+
+    if isinstance(message_file, unicode) or isinstance(message_file, str):
+        message_file = StringIO(str(message_file))
+    elif isinstance(message_file, StringIO):
+        pass
+    else:
+        log.err("Invalid usage of 'sendmail' function")
+        raise AssertionError("message wrong type: %s" % type(message_file))
 
     factory = ESMTPSenderFactory(
         authentication_username,

globaleaks/jobs/gpgexpire_sched.py
@@ -6,20 +6,29 @@
 # Implements a periodic checks to verify if keys configured by receiver are going
 # to expire in short time, if so, send a warning email to the recipient.
 # It's execute once per day.
-import os
-import sys
+import datetime
 
 from twisted.internet.defer import inlineCallbacks
 
 from globaleaks.jobs.base import GLJob
-from globaleaks.models import InternalFile, InternalTip, ReceiverTip,\
-    ReceiverFile, Receiver
+from globaleaks.models import Receiver
 from globaleaks.settings import transact, GLSetting
-from globaleaks.utils import get_file_checksum, log, pretty_date_time
+from globaleaks.utils import sendmail, log
 from globaleaks.security import get_expirations
-from globaleaks.handlers.admin import admin_serialize_receiver
 
 __all__ = ['GPGExpireCheck']
+
+untranslated_template ="""
+This is an untranslated message from a GlobaLeaks node.
+The PGP/GPG key configured by you: %s
+
+Please extend their validity and update online, or upload a new
+key.
+
+When the key expire, if you've sets encrypted notification, they 
+would not be send anymore at all.
+"""
+
 
 @transact
 def check_expiration_date(store):
@@ -39,15 +48,82 @@ def check_expiration_date(store):
 
             keytrack.update({sr.gpg_key_fingerprint : sr.username })
 
+    if not keytrack:
+        log.debug("PGP/GPG key expiration check: no keys configured in this node")
+        return (dict({}), dict({}), dict({}) )
+
     dates = get_expirations(keylist)
 
-    print dates
+    today_dt = datetime.date.today()
+    lowurgency = datetime.timedelta(weeks=2)
+    highurgency = datetime.timedelta(days=3)
 
-def GPGEXpireCheck(GLJob):
+    # the return values
+    expiring_keys_3d = {}
+    expiring_keys_2w = {}
+    expired_keys = {}
+
+    for keyid, sincepoch in dates.iteritems():
+
+        expiration_dt = datetime.datetime.utcfromtimestamp(int(sincepoch)).date()
+
+        # simply, all the keys here are expired
+        if expiration_dt < today_dt:
+            continue
+
+        key_timetolife = (expiration_dt - today_dt)
+
+        if key_timetolife < highurgency:
+
+            expiring_keys_3d.update({ keytrack[keyid]: sincepoch})
+        elif key_timetolife < lowurgency:
+            expiring_keys_2w.update({ keytrack[keyid]: sincepoch})
+        else:
+            expired_keys.update({ keytrack[keyid]: sincepoch })
+
+    return (expiring_keys_2w, expiring_keys_3d, expired_keys)
+
+
+class GPGExpireCheck(GLJob):
 
     @inlineCallbacks
     def operation(self):
 
-        (two_weeks, three_days, gone) = yield self.check_expiration_date()
+        try:
+            (two_weeks, three_days, gone) = yield check_expiration_date()
 
+            messages = dict({})
+
+            for username, sincepoch in two_weeks.iteritems():
+                messages.update({ username : untranslated_template % "expire in two weeks" })
+
+            for username, sincepoch in three_days.iteritems():
+                messages.update({ username : untranslated_template % "expire in three days" })
+
+            for username, sincepoch in gone.iteritems():
+                messages.update({ username : untranslated_template % "it's already expired" })
+
+            for recipient, message in messages.iteritems():
+
+                mail_building = []
+
+                mail_building.append("From: %s" % GLSetting.memory_copy.notif_username)
+                mail_building.append("To: %s" % recipient)
+                mail_building.append("Subject: GPG expiration date is incoming")
+                mail_building.append("Content-Type: text/plain; charset=ISO-8859-1")
+                mail_building.append("Content-Transfer-Encoding: 8bit\n")
+                mail_building.append(message)
+
+                sendmail(GLSetting.memory_copy.notif_username,
+                         GLSetting.memory_copy.notif_password,
+                         GLSetting.memory_copy.notif_username,
+                         recipient,
+                         "\n".join(mail_building),
+                         GLSetting.memory_copy.notif_server,
+                         GLSetting.memory_copy.notif_port,
+                         GLSetting.memory_copy.notif_security)
+
+
+        except Exception as excep:
+            print "Error in key scheduler whatever"
 

globaleaks/rest/errors.py
@@ -348,3 +348,12 @@ class InvalidReceiptRegexp(GLException):
     error_code = 43
     status_code = 406
     reason = "The receipt regexp is an invalid regexp"
+
+class GPGKeyIDNotUnique(GLException):
+    """
+    A GPG key id need to be unique in the node
+    Remind: not yet used
+    """
+    error_code = 44
+    status_code =  406
+    reason = "GPG Key ID it's already used by another receiver"

globaleaks/runner.py
@@ -87,8 +87,8 @@ def start_asynchronous():
     GLAsynchronous.add_interval_job(clean_sched.operation,
         hours=GLSetting.cleaning_hours_delta)
 
-    expiration_sched = gpgexpire_sched.GPGEXpireCheck()
-    expiration_sched.force_execution(GLAsynchronous, seconds=40)
+    expiration_sched = gpgexpire_sched.GPGExpireCheck()
+    expiration_sched.force_execution(GLAsynchronous, seconds=1)
     GLAsynchronous.add_interval_job(expiration_sched.operation, hours=23)
 
     #stats_sched = statistics_sched.APSStatistics()

globaleaks/tests/test_gpg.py
@@ -8,7 +8,7 @@ from globaleaks.tests import helpers
 from globaleaks.rest import errors
 from globaleaks.security import GLBGPG, gpg_options_parse, get_expirations
 from globaleaks.handlers import receiver
-from globaleaks.handlers.admin import admin_serialize_receiver
+from globaleaks.handlers.admin import admin_serialize_receiver, create_receiver
 from globaleaks.settings import GLSetting, transact
 from globaleaks.tests.helpers import MockDict
 from globaleaks.models import Receiver
@@ -41,7 +41,6 @@ class TestReceiverSetKey(helpers.TestHandler):
         "gpg_key_info": None, "gpg_key_fingerprint": None,
         'gpg_key_status': Receiver._gpg_types[0], # Disabled
         "gpg_enable_notification": False,  "gpg_enable_files": False,
-
         'name' : "irrelevant",
         'password' : "",
         'old_password': "",
@@ -168,7 +167,6 @@ class TestReceiverSetKey(helpers.TestHandler):
                 whole = f.read()
             self.assertEqual(encrypted_file_size, len(whole))
 
-
     @inlineCallbacks
     def test_expired_key_error(self):
 
@@ -190,30 +188,55 @@ class TestReceiverSetKey(helpers.TestHandler):
         self.assertTrue(gpob.validate_key(DeveloperKey.__doc__))
         self.assertRaises(errors.GPGKeyInvalid, gpob.encrypt_message, body)
 
+#    @inlineCallbacks
+#    def test_invalid_duplicated_key(self):
+#
+#        self.receiver_only_update = dict(MockDict().dummyReceiver)
+#        self.receiver_only_update['gpg_key_armor'] = unicode(DeveloperKey.__doc__)
+#        self.receiver_only_update['gpg_key_status'] = None # Test, this field is ignored and set
+#        self.receiver_only_update['gpg_key_remove'] = False
+#        handler = self.request(self.receiver_only_update, role='receiver', user_id=self.dummyReceiver['receiver_gus'])
+#        yield handler.put()
+#        self.assertEqual(self.responses[0]['gpg_key_fingerprint'],
+#            u'341F1A8CE2B4F4F4174D7C21B842093DC6765430')
+#        self.assertEqual(self.responses[0]['gpg_key_status'], Receiver._gpg_types[1])
+#
+#        # second receiver creation!
+#        new_receiver = dict(MockDict().dummyReceiver)
+#        new_receiver['name'] = new_receiver['username'] = \
+#            new_receiver['notification_fields']['mail_address'] = "quercia@nana.ptg"
+#        new_receiver_output = yield create_receiver(new_receiver)
+#
+#        self.assertGreater(new_receiver_output['receiver_gus'], 10)
+#        self.assertNotEqual(self.dummyReceiver['receiver_gus'], new_receiver_output['receiver_gus'])
+#
+#        handler = self.request(self.receiver_only_update, role='receiver', user_id=new_receiver_output['receiver_gus'])
+#        try:
+#            yield handler.put()
+#            self.assertTrue(False)
+#        except
+#        except Exception as excep:
 
-    @inlineCallbacks
+
     def test_expiration_checks(self):
 
         keylist = [ HermesGlobaleaksKey.__doc__, DeveloperKey.__doc__, ExpiredKey.__doc__ ]
 
         expiration_list = get_expirations(keylist)
 
+
+        today_dt = datetime.date.today()
+
         for keyid, sincepoch in expiration_list.iteritems():
-            expiration_t = datetime.datetime.utcfromtimestamp(int(sincepoch))
-            print expiration_t
-            print datetime.date.today()
-            lowurgency = datetime.timedelta(weeks=2)
-            highurgengy =datetime.timedelta(days=3)
-            today1 = datetime.date.today() + lowurgency
-            today2 = datetime.date.today() + highurgency
 
-            if today1 < lowurgency:
-                print keyid, "low"
-            if today2 < highurgeny:
-                print keyid, "high"
+            expiration_dt = datetime.datetime.utcfromtimestamp(int(sincepoch)).date()
 
-        self.assertFalse(True)
+            # simply, all the keys here are expired
+            if expiration_dt < today_dt:
+                continue
 
+            self.assertTrue(False)
+        self.assertTrue(True)
 
 
 

commit cbeca6bf2cb682ccb9274e7c1a2c3a14be280c82

fixed some issues introduced in commit https://github.com/globaleaks/GLBackend/commit/7b5c24ad5c79ca1b9fd182a4164653d17a60b752

globaleaks/jobs/notification_sched.py
@@ -235,7 +235,7 @@ class APSNotification(GLJob):
                 # check if the receiver has the Comment notification enabled or not
                 if not receiver.comment_notification:
                     log.debug("Receiver %s has comment notification disabled: skipped [source: %s]" % (
-                        receiver.username, comment.author))
+                        receiver.user.username, comment.author))
                     continue
 
                 event = Event(type=u'comment', trigger='Comment',

globaleaks/utils.py
@@ -14,12 +14,13 @@ import sys
 import time
 import traceback
 
+import cStringIO
+import StringIO
+
 from OpenSSL import SSL
+
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from txsocksx.client import SOCKS5ClientEndpoint
-
-from StringIO import StringIO
-
 from twisted.internet import reactor, protocol, error
 from twisted.internet.defer import Deferred
 from twisted.mail.smtp import ESMTPSenderFactory, SMTPClient
@@ -296,10 +297,10 @@ def sendmail(authentication_username, authentication_password, from_address,
     esmtp_deferred.addErrback(handle_error, event)
     esmtp_deferred.addCallback(result_deferred.callback)
 
-    if isinstance(message_file, unicode) or isinstance(message_file, str):
-        message_file = StringIO(str(message_file))
-    elif isinstance(message_file, StringIO):
-        pass
+    if isinstance(message_file, cStringIO.InputType):
+       pass
+    elif isinstance(message_file, unicode) or isinstance(message_file, str):
+        message_file = StringIO.StringIO(str(message_file))
     else:
         log.err("Invalid usage of 'sendmail' function")
         raise AssertionError("message wrong type: %s" % type(message_file))
@@ -389,7 +390,7 @@ def mail_exception(etype, value, tback):
     if type(info_string) == unicode:
         info_string = info_string.encode(encoding='utf-8', errors='ignore')
 
-    message = StringIO(info_string)
+    message = StringIO.StringIO(info_string)
 
     log.err(error_message)
     log.err(traceinfo)