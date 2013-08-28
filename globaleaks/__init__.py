5782f1082597296bf74c1e3d50b48e91f7787814
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-08-28 12:40:45 +0200

    removed update script 7 as we kept db version 6

--- globaleaks/__init__.py
+++ globaleaks/__init__.py
@@ -4,7 +4,7 @@
 # instantiated only once and be common to pieces of GLBackend code.
 
 __version__ = '2.24.9'
-DATABASE_VERSION = 7
+DATABASE_VERSION = 6
 
 # Add here by hand the languages supported!
 # copy paste format from 'grunt makeTranslations'
