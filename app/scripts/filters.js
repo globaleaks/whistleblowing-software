bfe15d613060410db2eaf50e2868fa0d454204d9
Giovanni evilaliv3 Pellerano <giovanni.pellerano@evilaliv3.org>
2013-11-04 13:29:35 +0100

    misc style corrections

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -19,4 +19,11 @@ angular.module('GLClientFilters', []).
         return input;
       }
     }
+}).
+  filter('partition', function() {
+    var part = function(arr, size) {
+      if ( 0 === arr.length ) return [];
+      return [ arr.slice( 0, size ) ].concat( part( arr.slice( size ), size) );
+    };
+    return part;
 });
