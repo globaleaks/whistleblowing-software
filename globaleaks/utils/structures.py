3d4a556688ef937d79d3bea205712bd0d748324c
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2013-11-19 11:18:03 +0100

    upgraded Twisted to 13.2.0 and txsocsx to 1.13.0.1

--- globaleaks/utils/structures.py
+++ globaleaks/utils/structures.py
@@ -61,9 +61,6 @@ class Fields:
         # admin var, in order to keep track of the real existing keys
         existing_keys = []
 
-        # XXX admin_order would be removed when presentation_order
-        # is correctly implemented in GLClient
-        # https://github.com/globaleaks/GlobaLeaks/issues/700
         for admin_order, field_desc in enumerate(admin_data):
 
             check_type = field_desc['type']
@@ -91,8 +88,6 @@ class Fields:
             # init localization track
             self._localization[language][key].update({'name' : field_desc['name']})
             self._localization[language][key].update({'hint' : field_desc['hint']})
-
-            # assign presentation order
 
             del self._fields[key]['name']
             del self._fields[key]['hint']
