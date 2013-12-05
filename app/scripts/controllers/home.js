72d2047bc1d6d8e64d196d379637916b8e28a9b0
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2013-12-05 16:05:05 +0100

    addigned css ids to html elements in order to allow easy customization

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -17,10 +17,6 @@ GLClient.controller('HomeCtrl', ['$scope', '$location', 'Node', 'Authentication'
       $scope.anonymous = true;
     }
 
-    Node.get(function(node_info){
-      $scope.node_info = node_info;
-    });
-
     $scope.view_tip = function(receipt) {
       WhistleblowerTip(receipt, function(tip_id) {
         $location.path('/status/' + tip_id);
