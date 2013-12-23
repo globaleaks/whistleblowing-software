eaa0404bae131d1850cb9fb347262e228578e4f7
Giovanni `evilaliv3` Pellerano <giovanni.pellerano@evilaliv3.org>
2013-12-23 15:07:22 +0100

    various fixes related to https://github.com/globaleaks/GlobaLeaks/issues/740

--- globaleaks/handlers/receiver.py
+++ globaleaks/handlers/receiver.py
@@ -42,7 +42,7 @@ def receiver_serialize_receiver(receiver, language=GLSetting.memory_copy.default
         "message_notification" : receiver.message_notification,
         "mail_address": receiver.mail_address,
         "failed_login": receiver.user.failed_login_count,
-        "contexts": []
+        "contexts": [],
     }
 
     mo = Rosetta()
