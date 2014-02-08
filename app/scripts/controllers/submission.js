cc3211fda99c022adf88a982e3680b4c25b3ac8d
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-02-08 15:30:22 +0100

    added skeleton for Setup Wizard

--- app/scripts/controllers/submission.js
+++ app/scripts/controllers/submission.js
@@ -132,3 +132,63 @@ GLClient.controller('SubmissionCtrl',
       $rootScope.invalidForm = $scope.submissionForm.$invalid;
     }, true);
 }])
+
+GLClient.controller('SubmissionStepsCtrl', ['$scope', function($scope) {
+
+  $scope.selection = $scope.steps[0];
+
+  $scope.getCurrentStepIndex = function(){
+    // Get the index of the current step given selectio
+    return _.indexOf($scope.steps, $scope.selection);
+  };
+
+  // Go to a defined step index
+  $scope.goToStep = function(index) {
+    if ( $scope.uploading )
+      return;
+
+    if ( !_.isUndefined($scope.steps[index]) )
+    {
+      $scope.selection = $scope.steps[index];
+    }
+  };
+
+  $scope.hasNextStep = function(){
+    var stepIndex = $scope.getCurrentStepIndex();
+    var nextStep = stepIndex + 1;
+    // Return true if there is a next step, false if not
+    return !_.isUndefined($scope.steps[nextStep]);
+  };
+
+  $scope.hasPreviousStep = function(){
+    var stepIndex = $scope.getCurrentStepIndex();
+    var previousStep = stepIndex - 1;
+    // Return true if there is a next step, false if not
+    return !_.isUndefined($scope.steps[previousStep]);
+  };
+
+  $scope.incrementStep = function() {
+    if ( $scope.uploading )
+      return;
+
+    if ( $scope.hasNextStep() )
+    {
+      var stepIndex = $scope.getCurrentStepIndex();
+      var nextStep = stepIndex + 1;
+      $scope.selection = $scope.steps[nextStep];
+    }
+  };
+
+  $scope.decrementStep = function() {
+    if ( $scope.uploading )
+      return;
+
+    if ( $scope.hasPreviousStep() )
+    {
+      var stepIndex = $scope.getCurrentStepIndex();
+      var previousStep = stepIndex - 1;
+      $scope.selection = $scope.steps[previousStep];
+    }
+  };
+
+}]);
--- app/scripts/controllers/wizard.js
+++ app/scripts/controllers/wizard.js
@@ -1,60 +1,9 @@
-GLClient.controller('WizardCtrl',
-  ['$scope', function($scope) {
+GLClient.controller('WizardCtrl', ['$scope', 'Node',
+  function($scope, Node) {
 
-  $scope.selection = $scope.steps[0];
+    Node.get(function(node){
+      $scope.node = node;
+    });
 
-  $scope.getCurrentStepIndex = function(){
-    // Get the index of the current step given selectio
-    return _.indexOf($scope.steps, $scope.selection);
-  };
-
-  // Go to a defined step index
-  $scope.goToStep = function(index) {
-    if ( $scope.uploading )
-      return;
-
-    if ( !_.isUndefined($scope.steps[index]) )
-    {
-      $scope.selection = $scope.steps[index];
-    }
-  };
-
-  $scope.hasNextStep = function(){
-    var stepIndex = $scope.getCurrentStepIndex();
-    var nextStep = stepIndex + 1;
-    // Return true if there is a next step, false if not
-    return !_.isUndefined($scope.steps[nextStep]);
-  };
-
-  $scope.hasPreviousStep = function(){
-    var stepIndex = $scope.getCurrentStepIndex();
-    var previousStep = stepIndex - 1;
-    // Return true if there is a next step, false if not
-    return !_.isUndefined($scope.steps[previousStep]);
-  };
-
-  $scope.incrementStep = function() {
-    if ( $scope.uploading )
-      return;
-
-    if ( $scope.hasNextStep() )
-    {
-      var stepIndex = $scope.getCurrentStepIndex();
-      var nextStep = stepIndex + 1;
-      $scope.selection = $scope.steps[nextStep];
-    }
-  };
-
-  $scope.decrementStep = function() {
-    if ( $scope.uploading )
-      return;
-
-    if ( $scope.hasPreviousStep() )
-    {
-      var stepIndex = $scope.getCurrentStepIndex();
-      var previousStep = stepIndex - 1;
-      $scope.selection = $scope.steps[previousStep];
-    }
-  };
-
-}]);
+  }
+]);
