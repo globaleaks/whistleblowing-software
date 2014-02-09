deee4bb4d380c2cc311d4eb10b34c092a8312c61
Arturo Filastò <art@fuffa.org>
2014-02-09 22:18:41 +0100

    Fix blind bugs.

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-- echo "USE mysql;\nUPDATE user SET password=PASSWORD('globaleaks') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
-- mysql -e 'create database globaleaks;'
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('globaleaks') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+mysql -e 'create database globaleaks;'
