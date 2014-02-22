2eacf28c3314ceafddce254cd734f1153c12502d
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-02-22 21:05:10 +0100

    minor riorganization of build scripts

--- scripts/build-testing-package.sh
+++ scripts/build-testing-package.sh
@@ -39,17 +39,6 @@ do
     esac
 done
 
-echo "[+] Setupping GLClient and GLBackend build environments"
-
-if [ ! -f ${DIR}/.environment_setupped ]; then
-    sudo -i add-apt-repository ppa:chris-lea/node.js -y
-    sudo -i apt-get update -y
-    sudo -i apt-get install nodejs -y
-    sudo -i npm install -g grunt-cli bower
-    sudo -i apt-get install python-dev build-essential python-virtualenv python-pip python-stdeb devscripts -y
-    touch ${DIR}/.environment_setupped
-fi
-
 if [ $AUTOYES -eq 1 ]; then
   OPTS="-y"
 else
