ea0b6d45004f0dd80cdb33018d080aaea7ccff96
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-03-01 11:19:17 +0100

    APScheduler simply replaced by twisted reactor

--- globaleaks/jobs/base.py
+++ globaleaks/jobs/base.py
@@ -4,27 +4,9 @@
 #
 # Base class for implement the scheduled tasks
 
-from datetime import datetime
-from globaleaks.utils.utility import log, utc_future_date, pretty_date_time
+from twisted.internet import task
 
-class GLJob:
+class GLJob(task.LoopingCall):
 
-    def force_execution(self, aps=None, seconds=1):
-        """
-        @aps: Advanced Python Scheduler object
-        seconds: number of seconds to await before operation start
-
-        force execution do not execute immidiatly self.operation(),
-        because we want be sure that is a thread start by APScheduler
-        """
-        plan_exec = utc_future_date(hours=0, seconds=seconds)
-        plan_exec += (datetime.now() - datetime.utcnow())
-
-        try:
-            aps.add_date_job(self.operation, plan_exec)
-        except ValueError as exc:
-            log.err("Failing in force schedule execution of %s planned at %s" %
-                      (self.__class__.__name__, pretty_date_time(plan_exec)))
-
-        log.debug("Forced execution of %s at %s" %
-                  (self.__class__.__name__, pretty_date_time(plan_exec)))
+    def __init__(self):
+        task.LoopingCall.__init__(self, self.operation)
