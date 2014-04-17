60d9922fbec796b5cb83dccbb0508a6223beb204
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2014-04-17 18:47:57 +0200

    added unit tests for file collections: zip, tar, targz, tarbz2, antani

--- globaleaks/tests/test_submission.py
+++ globaleaks/tests/test_submission.py
@@ -1,6 +1,11 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
+
+import os
 import re
+
+import tarfile
+import StringIO
 
 from twisted.internet.defer import inlineCallbacks
 
@@ -11,8 +16,12 @@ from globaleaks import models
 from globaleaks.jobs import delivery_sched
 from globaleaks.handlers import authentication, submission, wbtip
 from globaleaks.handlers.admin import create_context, update_context, create_receiver, get_receiver_list
+from globaleaks.handlers.receiver import get_receiver_tip_list
+from globaleaks.handlers.files import download_all_files, serialize_receiver_file
+from globaleaks.handlers.collection import get_compression_opts, CollectionStreamer
 from globaleaks.rest import errors
 from globaleaks.models import InternalTip
+from globaleaks.utils.zipstream import ZipStream, ZIP_STORED, ZIP_DEFLATED
 
 @transact_ro
 def collect_ifile_as_wb_without_wbtip(store, internaltip_id):
@@ -23,11 +32,10 @@ def collect_ifile_as_wb_without_wbtip(store, internaltip_id):
         file_list.append(wbtip.wb_serialize_file(internalfile))
     return file_list
 
-
 class TestSubmission(helpers.TestGLWithPopulatedDB):
 
     @inlineCallbacks
-    def test_create_submission(self):
+    def test_001_create_submission(self):
         submission_desc = dict(self.dummySubmission)
         submission_desc['finalize'] = False
         del submission_desc['id']
@@ -37,7 +45,24 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.assertEqual(status['mark'], u'submission')
 
     @inlineCallbacks
-    def test_create_submission_attach_files_finalize_and_access_wbtip(self):
+    def test_002_fail_submission_missing_required_file(self):
+        mycopy = dict(self.dummyContext)
+        mycopy['file_required'] = True
+        del mycopy['id']
+
+        for attrname in models.Context.localized_strings:
+            mycopy[attrname] = u'⅛¡⅜⅛’ŊÑŦŊŊ’‘ª‘ª’‘ÐŊ'
+
+        context_status = yield create_context(mycopy)
+        submission_desc = dict(self.dummySubmission)
+
+        submission_desc['context_id'] = context_status['id']
+        submission_desc['finalize'] = True
+        submission_desc['wb_fields'] = helpers.fill_random_fields(mycopy)
+        yield self.assertFailure(submission.create_submission(submission_desc, finalize=True), errors.FileRequiredMissing)
+
+    @inlineCallbacks
+    def test_003_create_submission_attach_files_finalize_and_access_wbtip(self):
         submission_desc = dict(self.dummySubmission)
         submission_desc['finalize'] = True
         del submission_desc['id']
@@ -64,28 +89,9 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.assertTrue(wb_tip.has_key('fields'))
 
     @inlineCallbacks
-    def test_fail_submission_missing_required_file(self):
+    def test_004_create_receiverfiles_allow_unencrypted_true_no_keys_loaded(self):
 
-        mycopy = dict(self.dummyContext)
-        mycopy['file_required'] = True
-        del mycopy['id']
-
-        for attrname in models.Context.localized_strings:
-            mycopy[attrname] = u'⅛¡⅜⅛’ŊÑŦŊŊ’‘ª‘ª’‘ÐŊ'
-
-        context_status = yield create_context(mycopy)
-        submission_desc = dict(self.dummySubmission)
-
-        submission_desc['context_id'] = context_status['id']
-        submission_desc['finalize'] = True
-        submission_desc['wb_fields'] = helpers.fill_random_fields(mycopy)
-
-        yield self.assertFailure(submission.create_submission(submission_desc, finalize=True), errors.FileRequiredMissing)
-
-    @inlineCallbacks
-    def test_create_receiverfiles_allow_unencrypted_true_no_keys_loaded(self):
-
-        yield self.test_create_submission_attach_files_finalize_and_access_wbtip()
+        yield self.test_003_create_submission_attach_files_finalize_and_access_wbtip()
 
         # create receivertip its NEEDED to create receiverfile
         self.rt = yield delivery_sched.tip_creation()
@@ -122,11 +128,11 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.assertEqual(len(self.wbfls), 2)
 
     @inlineCallbacks
-    def test_create_receiverfiles_allow_unencrypted_false_no_keys_loaded(self):
+    def test_005_create_receiverfiles_allow_unencrypted_false_no_keys_loaded(self):
 
         GLSetting.memory_copy.allow_unencrypted = False
 
-        yield self.test_create_submission_attach_files_finalize_and_access_wbtip()
+        yield self.test_003_create_submission_attach_files_finalize_and_access_wbtip()
 
         # create receivertip its NEEDED to create receiverfile
         self.rt = yield delivery_sched.tip_creation()
@@ -135,6 +141,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.rfilesdict = yield delivery_sched.receiverfile_planning()
         # return a list of lists [ "file_id", status, "f_path", len, "receiver_desc" ]
         self.assertTrue(isinstance(self.rfilesdict, dict))
+
 
         for ifile_path, receivermap in self.rfilesdict.iteritems():
             yield delivery_sched.encrypt_where_available(receivermap)
@@ -151,7 +158,8 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.rfi = yield delivery_sched.get_receiverfile_by_itip(self.dummySubmission['id'])
         self.assertTrue(isinstance(self.rfi, list))
 
-        self.assertEqual(len(self.rfi), 2) # no rfiles are created for the receivers that have no key
+        self.assertEqual(len(self.rfi), 2)
+        # no rfiles are created for the receivers that have no key
         self.assertEqual(self.rfi[0]['mark'], u'not notified')
         self.assertEqual(self.rfi[1]['mark'], u'not notified')
         self.assertEqual(self.rfi[0]['status'], u'nokey')
@@ -164,7 +172,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         self.assertEqual(len(self.wbfls), 2)
 
     @inlineCallbacks
-    def test_submission_with_receiver_selection_allow_unencrypted_true_no_keys_loaded(self):
+    def test_007_submission_with_receiver_selection_allow_unencrypted_true_no_keys_loaded(self):
 
         yield create_receiver(self.get_new_receiver_desc("second"))
         yield create_receiver(self.get_new_receiver_desc("third"))
@@ -214,7 +222,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
 
 
     @inlineCallbacks
-    def test_submission_with_receiver_selection_allow_unencrypted_false_no_keys_loaded(self):
+    def test_008_submission_with_receiver_selection_allow_unencrypted_false_no_keys_loaded(self):
 
         GLSetting.memory_copy.allow_unencrypted = False
 
@@ -251,7 +259,76 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         yield self.assertFailure(submission.create_submission(submission_request, finalize=True), errors.SubmissionFailFields)
 
     @inlineCallbacks
-    def test_update_submission(self):
+    def test_009_file_collection_creation(self):
+
+        yield self.test_003_create_submission_attach_files_finalize_and_access_wbtip()
+
+        # we trigger the complete DeliverySchedule() in order to create rtips and rfiles
+        delivery_sched.DeliverySchedule().operation()
+
+        rtips = yield get_receiver_tip_list(self.dummyReceiver['id'])
+
+        for rtip in rtips:
+
+            expected_keys = ['access_counter',
+                             'creation_date',
+                             'expiration_date',
+                             'last_access',
+                             'expressed_pertinence',
+                             'id',
+                             'comments_number',
+                             'files_number',
+                             'context_name',
+                             'preview', 'read_messages',
+                             'can_delete_submission',
+                             'postpone_superpower',
+                             'unread_messages',
+                             'your_messages']
+
+            self.assertEqual(set(rtip.keys()), set(expected_keys))
+
+            files_dict = yield download_all_files(self.dummyReceiver['id'], rtip['id'])
+
+            for filedesc in files_dict:
+                # Update all the path with the absolute path
+                filedesc['path'] = os.path.join(GLSetting.submission_path, filedesc['path'])
+
+            ##################################################################################
+            #  The following code reproduce the handler/collection.py get() function         #
+            #  and tests all the implemented compression alghoritms                          #
+            ##################################################################################
+                for compression in ['zipstored', 'zipdeflated', 'tar', 'targz', 'tarbz2']:
+                    opts = get_compression_opts(compression)
+
+                    if compression in ['zipstored', 'zipdeflated']:
+                        for data in ZipStream(files_dict, opts['compression_type']):
+                            pass # self.write(data)
+
+                    elif compression in ['tar', 'targz', 'tarbz2']:
+
+                        collectionstreamer = CollectionStreamer(None)
+
+                        # we replace collectionstreamer.write() with a pass function
+                        def nowrite(data):
+                            pass
+
+                        collectionstreamer.write = nowrite
+
+                        tar = tarfile.open("collection." + compression, 'w|'+opts['compression_type'], collectionstreamer)
+                        for f in files_dict:
+                            if 'path' in f:
+                                tar.add(f['path'], f['name'])
+
+                            elif 'buf' in f:
+                                tarinfo = tarfile.TarInfo(f['name'])
+                                tarinfo.size = len(f['buf'])
+                                tar.addfile(tarinfo, StringIO.StringIO(formatted_coll))
+
+                        tar.close()
+            ##################################################################################
+
+    @inlineCallbacks
+    def test_010_update_submission(self):
         submission_desc = dict(self.dummySubmission)
         submission_desc['finalize'] = False
         submission_desc['context_id'] = self.dummyContext['id']
@@ -273,7 +350,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
 
 
     @inlineCallbacks
-    def test_unable_to_access_finalized(self):
+    def test_011_unable_to_access_finalized(self):
         submission_desc = dict(self.dummySubmission)
         submission_desc['finalize'] = True
         submission_desc['context_id'] = self.dummyContext['id']
@@ -288,7 +365,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
 
 
     @inlineCallbacks
-    def test_fields_validator_all_fields(self):
+    def test_012_fields_validator_all_fields(self):
 
         sbmt = dict(self.dummySubmission)
 
@@ -311,7 +388,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
             self.assertTrue(False)
 
     @inlineCallbacks
-    def test_fields_fail_unexpected_presence(self):
+    def test_013_fields_fail_unexpected_presence(self):
 
         sbmt = helpers.get_dummy_submission(self.dummyContext['id'], self.dummyContext['fields'])
         sbmt['wb_fields'].update({ 'alien' : 'predator' })
@@ -319,7 +396,7 @@ class TestSubmission(helpers.TestGLWithPopulatedDB):
         yield self.assertFailure(submission.create_submission(sbmt, finalize=True), errors.SubmissionFailFields)
 
     @inlineCallbacks
-    def test_fields_fail_missing_required(self):
+    def test_014_fields_fail_missing_required(self):
 
         required_key = self.dummyContext['fields'][0]['key']
         sbmt = helpers.get_dummy_submission(self.dummyContext['id'], self.dummyContext['fields'])
