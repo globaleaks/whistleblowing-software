5df8a95fcd58b85df916b5c899a0227637d90eaa
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-05-07 21:04:27 +0200

    added unit tests for node handlers

---
+++ globaleaks/tests/handlers/test_node.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from twisted.internet.defer import inlineCallbacks
+
+import json
+
+from globaleaks.rest import requests
+from globaleaks.tests import helpers
+from globaleaks.handlers import node
+from globaleaks.settings import GLSetting
+
+class TestInfoCollection(helpers.TestHandler):
+    _handler = node.InfoCollection
+
+    @inlineCallbacks
+    def test_get(self):
+        handler = self.request({}, role='admin')
+        yield handler.get()
+
+        self.assertTrue(isinstance(self.responses, list))
+        self.assertEqual(len(self.responses), 1)
+        self.assertEqual(len(self.responses[0]), 26)
+        self._handler.validate_message(json.dumps(self.responses[0]), requests.anonNodeDesc)
+
+
+class TestAhmiaDescriptionHandler(helpers.TestHandler):
+    _handler = node.AhmiaDescriptionHandler
+
+    @inlineCallbacks
+    def test_get(self):
+        handler = self.request({}, role='admin')
+        yield handler.get()
+
+        self.assertTrue(isinstance(self.responses, list))
+        self.assertEqual(len(self.responses), 1)
+        self.assertEqual(len(self.responses[0]), 7)
+        self._handler.validate_message(json.dumps(self.responses[0]), requests.ahmiaDesc)
+
+
+class TestContextsCollection(helpers.TestHandler):
+    _handler = node.ContextsCollection
+
+    @inlineCallbacks
+    def test_get(self):
+        handler = self.request({}, role='admin')
+        yield handler.get()
+
+        self.assertTrue(isinstance(self.responses, list))
+        self.assertEqual(len(self.responses), 1)
+        self.assertEqual(len(self.responses[0]), 1)
+        self._handler.validate_message(json.dumps(self.responses[0]), requests.nodeContextCollection)
+
+
+class TestReceiversCollection(helpers.TestHandler):
+    _handler = node.ReceiversCollection
+
+    @inlineCallbacks
+    def test_get(self):
+        handler = self.request({}, role='admin')
+        yield handler.get()
+
+        self.assertTrue(isinstance(self.responses, list))
+        self.assertEqual(len(self.responses), 1)
+        self.assertEqual(len(self.responses[0]), 1)
+        self._handler.validate_message(json.dumps(self.responses[0]), requests.nodeReceiverCollection)
