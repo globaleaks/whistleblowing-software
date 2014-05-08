5f7c6c90427355e2463f95d0dbc34cf02b35d814
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-05-08 16:38:46 +0200

    fixed test_node UT

--- globaleaks/tests/handlers/test_node.py
+++ globaleaks/tests/handlers/test_node.py
@@ -18,7 +18,7 @@ class TestInfoCollection(helpers.TestHandler):
 
         self.assertTrue(isinstance(self.responses, list))
         self.assertEqual(len(self.responses), 1)
-        self.assertEqual(len(self.responses[0]), 26)
+        self.assertEqual(len(self.responses[0]), 28)
         self._handler.validate_message(json.dumps(self.responses[0]), requests.anonNodeDesc)
 
 
