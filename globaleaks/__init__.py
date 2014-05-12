45708ca98336bc64a93ab11a32b86b5a83ea1022
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-05-12 20:13:38 +0200

    bump to version 2.60.5

--- globaleaks/__init__.py
+++ globaleaks/__init__.py
@@ -5,33 +5,34 @@
 
 import operator
 
-__version__ = '2.60.4'
+__version__ = '2.60.5'
 DATABASE_VERSION = 12
 
 # Add here by hand the languages supported!
 # copy paste format from 'grunt makeTranslations'
 LANGUAGES_SUPPORTED = [
- { "code": "cs", "name": "Czech" },
  { "code": "ar", "name": "Arabic" },
- { "code": "en", "name": "English" },
  { "code": "bg", "name": "Bulgarian" },
- { "code": "nl", "name": "Dutch" },
- { "code": "hr_HR", "name": "Croatian (Croatia)" },
- { "code": "fr", "name": "French" },
- { "code": "it", "name": "Italian" },
+ { "code": "cs", "name": "Czech" },
  { "code": "de", "name": "German" },
- { "code": "nb_NO", "name": "Norwegian Bokmål (Norway)" },
- { "code": "pl", "name": "Polish" },
+ { "code": "el", "name": "Greek" },
+ { "code": "en", "name": "English" },
+ { "code": "es", "name": "Spanish" },
+ { "code": "fr", "name": "French" },
+ { "code": "hr_HR", "name": "Croatian (Croatia)" },
  { "code": "hu_HU", "name": "Hungarian (Hungary)" },
+ { "code": "it", "name": "Italian" },
+ { "code": "nb_NO", "name": "Norwegian Bokmål (Norway)" },
+ { "code": "nl", "name": "Dutch" },
+ { "code": "pl", "name": "Polish" },
  { "code": "pt_BR", "name": "Portuguese (Brazil)" },
+ { "code": "pt_PT", "name": "Portuguese (Portugal)" },
  { "code": "ru", "name": "Russian" },
+ { "code": "sk", "name": "Slovak" },
  { "code": "sr_RS", "name": "Serbian (Serbia)" },
  { "code": "sr_RS@latin", "name": "Serbian (Latin) (Serbia)" },
- { "code": "sk", "name": "Slovak" },
- { "code": "vi", "name": "Vietnamese" },
  { "code": "sv", "name": "Swedish" },
- { "code": "es", "name": "Spanish" },
- { "code": "pt_PT", "name": "Portuguese (Portugal)" },
+ { "code": "vi", "name": "Vietnamese" },
 ]
 
 # Sorting the list of dict using the key 'code'
