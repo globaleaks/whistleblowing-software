75c833478f5c91548eb680f73c74568838d00b97
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-06-26 19:44:53 +0200

    implemented UT for ahmia feature disabled/enabled

--- globaleaks/tests/handlers/test_node.py
+++ globaleaks/tests/handlers/test_node.py
@@ -5,7 +5,7 @@ import json
 
 from globaleaks.rest import requests
 from globaleaks.tests import helpers
-from globaleaks.handlers import node
+from globaleaks.handlers import node, admin
 from globaleaks.settings import GLSetting
 
 class TestInfoCollection(helpers.TestHandler):
@@ -26,10 +26,26 @@ class TestAhmiaDescriptionHandler(helpers.TestHandler):
     _handler = node.AhmiaDescriptionHandler
 
     @inlineCallbacks
-    def test_get(self):
+    def test_001_get_ahmia_disabled(self):
         handler = self.request({}, role='admin')
-        yield handler.get()
 
+        nodedict = helpers.MockDict().dummyNode
+        nodedict['ahmia'] = False
+        yield admin.update_node(nodedict)
+
+        yield handler.get()
+        self.assertTrue(isinstance(self.responses, list))
+        self.assertEqual(len(self.responses), 0)
+
+    @inlineCallbacks
+    def test_002_get_ahmia_enabled(self):
+        handler = self.request({}, role='admin')
+
+        nodedict = helpers.MockDict().dummyNode
+        nodedict['ahmia'] = True
+        yield admin.update_node(nodedict)
+
+        yield handler.get()
         self.assertTrue(isinstance(self.responses, list))
         self.assertEqual(len(self.responses), 1)
         self.assertEqual(len(self.responses[0]), 7)
