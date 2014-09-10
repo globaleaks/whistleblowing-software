37c20fccb31d1fa4e278da55501db73bb7811ae4
Michele Orrù <maker@tumbolandia.net>
2014-09-10 11:42:55 +0200

    Cleaning up BaseHandler.
    * Sort imports in basehandler.
    * Use str.endswith instead of slicing.

--- globaleaks/handlers/base.py
+++ globaleaks/handlers/base.py
@@ -1,39 +1,36 @@
 # -*- encoding: utf-8 -*-
-#
-#  base
-#  ****
-#
-# Implementation of BaseHandler, the Cyclone class RequestHandler extended with our
-# needings.
-#
+"""
+Implementation of BaseHandler, the Cyclone class RequestHandler extended with
+our needs.
+"""
 
-import httplib
-import types
 import collections
+import httplib
 import json
+import logging
 import re
 import sys
-import logging
+import types
 
-from StringIO import StringIO
 from cgi import parse_header
 from cryptography.hazmat.primitives.constant_time import bytes_eq
+from StringIO import StringIO
 
-from twisted.python.failure import Failure
-from twisted.internet.defer import inlineCallbacks
 from twisted.internet import fdesc
+from twisted.internet.defer import inlineCallbacks
+from twisted.python.failure import Failure
 
-from cyclone.web import RequestHandler, HTTPError, HTTPAuthenticationRequired, StaticFileHandler, RedirectHandler
-from cyclone.httpserver import HTTPConnection, HTTPRequest, _BadRequestException
 from cyclone import escape, httputil
 from cyclone.escape import native_str, parse_qs_bytes
+from cyclone.httpserver import HTTPConnection, HTTPRequest, _BadRequestException
+from cyclone.web import RequestHandler, HTTPError, HTTPAuthenticationRequired, StaticFileHandler, RedirectHandler
 
 from globaleaks.jobs.statistics_sched import alarm_level
+from globaleaks.rest import errors
+from globaleaks.settings import GLSetting
+from globaleaks.security import GLSecureTemporaryFile
 from globaleaks.utils.utility import log, log_remove_escapes, log_encode_html, datetime_now, deferred_sleep
 from globaleaks.utils.mailutils import mail_exception
-from globaleaks.settings import GLSetting
-from globaleaks.rest import errors
-from globaleaks.security import GLSecureTemporaryFile
 
 def validate_host(host_key):
     """
@@ -42,7 +39,7 @@ def validate_host(host_key):
     Is used by all the Web handlers inherit from Cyclone
     """
     # hidden service has not a :port
-    if len(host_key) == 22 and host_key[16:22] == '.onion':
+    if len(host_key) == 22 and host_key.endswith('.onion'):
         return True
 
     # strip eventually port
