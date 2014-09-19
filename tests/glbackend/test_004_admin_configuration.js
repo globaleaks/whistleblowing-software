d4b67cff75ac6c33b0b5139909ddc4238ebbf109
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2014-09-19 20:26:03 +0200

    implemented feature to disable private messages

--- tests/glbackend/test_004_admin_configuration.js
+++ tests/glbackend/test_004_admin_configuration.js
@@ -63,6 +63,7 @@ var context = {
   "can_delete_submission":true,
   "show_small_cards":false,
   "show_receivers":true,
+  "enable_private_messages":true,
   "submission_timetolive":1, 
   "file_max_download":3,
   "select_all_receivers":true,
