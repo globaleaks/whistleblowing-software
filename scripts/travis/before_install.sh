f68f84892955df68ada7b834a48baaff7c9a7d95
Michele Orrù <maker@tumbolandia.net>
2014-10-13 12:34:47 +0200

    Add torproject repo as sudo.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -21,7 +21,8 @@ git clone $GLCLIENT_GIT_REPO
 
 
 # Add tor repository.
-add-apt-repository "deb http://deb.torproject.org/torproject.org $(lsb_release -s -c) main" -y
+sudo add-apt-repository \
+    "deb http://deb.torproject.org/torproject.org $(lsb_release -s -c) main"
 gpg --keyserver x-hkp://pool.sks-keyservers.net --recv-keys 0x886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
