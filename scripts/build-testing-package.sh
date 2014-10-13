184409631f1237390488ec40d6ea3c146b1e0c71
Michele Orrù <maker@tumbolandia.net>
2014-10-13 12:30:05 +0200

    Change build-testing-package.sh not to have optional arguments.
    * Remove all dependencies installation in common evironment variables script
      (WTF)
    * Remove scripts/build-testing-package.sh's arguments, as they are already
      available in travis. If they were supposed to be used by a human, then we
      should provide a *standard* way for it, like a Makefile shit, and not random
      (and wrong) bash scripts.

--- scripts/build-testing-package.sh
+++ scripts/build-testing-package.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-if [ ! \( -r "$1" \) -o \( -z "$2" \) ]
-    then
-    echo "${BASH_SOURCE[0]} <client_revision> <backend_revision>"
-    exit 1
-fi
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common_inc.sh
 
-TAGC=$1
-TAGB=$2
 
-${DIR}/build-glclient.sh -v $TAGC
-${DIR}/build-glbackend.sh -v $TAGB
+${DIR}/build-glclient.sh -v $TRAVIS_BRANCH
+${DIR}/build-glbackend.sh -v $TRAVIS_BRANCH
--- scripts/common_inc.sh
+++ scripts/common_inc.sh
@@ -37,16 +37,3 @@ echo "Build directory used: ${BUILD_DIR}"
 echo "To override this do: 'GLOBALEAKS_BUILD_ENV=/what/you/want && export GLOBALEAKS_BUILD_ENV'"
 
 echo "[+] Setupping GLClient and GLBackend build environments"
-
-if [ "${TRAVIS}" == "true" ]; then
-    if [ ! -f ${DIR}/.environment_setupped ]; then
-        sudo -i apt-get update -y
-        sudo -i apt-get install python-software-properties -y
-        sudo -i add-apt-repository ppa:chris-lea/node.js -y
-        sudo -i apt-get update -y
-        sudo -i apt-get install nodejs -y
-        sudo -i npm install -g grunt-cli bower
-        sudo -i apt-get install python-dev build-essential python-virtualenv python-pip python-stdeb devscripts zip -y
-        touch ${DIR}/.environment_setupped
-    fi
-fi
