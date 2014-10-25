f4c993406ef999247c938a3293b56224712ae4cb
Michele Orrù <maker@tumbolandia.net>
2014-10-26 00:33:02 +0200

    Install nodejs, and client dependencies (via npm).

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -26,4 +26,7 @@ sudo add-apt-repository \
 gpg --keyserver x-hkp://pool.sks-keyservers.net --recv-keys 0x886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
+# Add apt repository
+sudo add-apt-repository -y ppa:chris-lea/node.js
+
 sudo apt-get update -qq
