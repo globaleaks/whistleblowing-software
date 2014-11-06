7c56406bfe4dd62d896490e5e1410f9c41ac4cb9
Arturo Filastò <art@fuffa.org>
2014-11-06 13:02:19 +0100

    Implement views for adding and editing fields from context pane

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -2,11 +2,11 @@
 // http://docs.angularjs.org/guide/dev_guide.templates.filters.creating_filters
 //
 angular.module('GLClientFilters', [])
-.filter('range', function() {
-  return function(val, range) {
-    range = parseInt(range);
-    for (var i=0; i<range; i++)
-      val.push(i);
-    return val;
-  };
+  .filter('range', function() {
+    return function(val, range) {
+      range = parseInt(range);
+      for (var i=0; i<range; i++)
+        val.push(i);
+      return val;
+    };
 });
