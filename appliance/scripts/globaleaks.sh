c645def484f7954f68b91fd06c15af0183c0a56e
Michele Orru` <maker@globaleaks.org>
2014-11-17 19:25:50 +0100

    Reach a ready-to-use vagrant globaleaks machine with vagrant.
    * Relax hypotheses on requirements.txt to run globaleaks.
      Storm 0.19 is actually not supported, and is the one we should change back as
      soon as possible.
    * Create a globaleaks.sh file to handle the installation of globaleaks.
      (Notes are inside the file.)
    
    To work on:
    * Do not assume an existing globaleaks debian file to upload?
    * Make globaleaks version a packer variable.

--- appliance/scripts/globaleaks.sh
+++ appliance/scripts/globaleaks.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+# The following script emulates the installation guide:
+# <https://github.com/globaleaks/GlobaLeaks/wiki/Installation-guide>
+set -e
+
+# XXX. python-cryptography is not available on ubuntu trusty,
+# so I am downloading it from debian jessie repositories.
+wget http://ftp.de.debian.org/debian/pool/main/p/python-cryptography/python-cryptography_0.6.1-1_amd64.deb
+dpkg -i python-cryptography* || true
+apt install -fy
+
+# XXX. Temporairly disable iptables, as it currently locks me out from ssh.
+apt install -y iptables
+mv /sbin/iptables /sbin/iptables.1
+ln -s /bin/true /sbin/iptables
+
+# install globaleaks
+dpkg -i globaleaks* || true
+apt install -fy
+
+# XXX. This should be moved in {pre,post}{inst,rm} debian scripts, or either
+# handled directly inside GlobaLeaks.
+cat <<EOF >> /etc/tor/torrc
+
+# GlobaLeaks Configuration.
+VirtualAddrNetwork 10.23.47.0/10
+AutomapHostsOnResolve 1
+TransPort 9040
+TransListenAddress 127.0.0.1
+DNSPort 5353
+DNSListenAddress 127.0.0.1
+HiddenServiceDir /var/globaleaks/torhs/
+HiddenServicePort 80 127.0.0.1:8082
+EOF
+## finally, restart tor in order to reload /etc/torrc.
+## XXX. Supposedly, if we remove this line every machine will have a different hidden service.
+## What should the default be?
+service tor restart
