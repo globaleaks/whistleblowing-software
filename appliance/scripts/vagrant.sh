216bb9e008d138e91e1cf9822ad60e4bf7add032
Michele Orru` <maker@globaleaks.org>
2014-11-19 16:13:42 +0100

    Refine packer scripts.
    * Do not download vagrant public key from random places on the internet, but use
      the one stored locally.
    * Do not keep the output directory of the vagrant build.
    * Give a proper name to the vagrant box;
    * Add a not about the use of storm 0.20 in scripts.

--- appliance/scripts/vagrant.sh
+++ appliance/scripts/vagrant.sh
@@ -8,13 +8,10 @@ chmod 0440 /etc/sudoers.d/vagrant
 usermod -a -G sudo vagrant
 
 # Installing vagrant keys
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-
-# Install NFS for Vagrant
-apt-get update
-apt-get install -y nfs-common
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+cat <<EOF > ~vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant
+EOF
+chmod 600 ~vagrant/.ssh/authorized_keys
+chown -R vagrant ~vagrant/.ssh
