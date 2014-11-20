6d49b82b18748ff2a677d89c073ad2b7d662a6cf
Michele Orru` <maker@globaleaks.org>
2014-11-20 16:17:35 +0100

    Fix errors running virtualbox script.
    We don't really need all this virtualbox-ose hack, so I'm just removing them.

--- appliance/scripts/virtualbox.sh
+++ appliance/scripts/virtualbox.sh
@@ -1,13 +1,9 @@
+# Install the VirtualBox guest additions
+
 # Without libdbus virtualbox would not start automatically after compile
 apt-get -y install --no-install-recommends libdbus-1-3
-
-# The netboot installs the VirtualBox support (old) so we have to remove it
-/etc/init.d/virtualbox-ose-guest-utils stop
-rmmod vboxguest
-aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
 aptitude -y install dkms
 
-# Install the VirtualBox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop $VBOX_ISO /mnt
