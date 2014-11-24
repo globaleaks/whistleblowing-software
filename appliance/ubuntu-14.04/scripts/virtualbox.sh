d669e92353bcee92c19c0bec4063855bbdcb8961
Michele Orru` <maker@globaleaks.org>
2014-11-24 11:21:48 +0100

    Use apt-get instead of aptitude in virtualbox.sh script.

--- appliance/ubuntu-14.04/scripts/virtualbox.sh
+++ appliance/ubuntu-14.04/scripts/virtualbox.sh
@@ -1,8 +1,11 @@
+# update machine
+apt-get update
+
 # Install the VirtualBox guest additions
 
 # Without libdbus virtualbox would not start automatically after compile
 apt-get -y install --no-install-recommends libdbus-1-3
-aptitude -y install dkms
+apt-get -y install dkms
 
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
