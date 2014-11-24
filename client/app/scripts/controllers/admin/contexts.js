1efa75ea346abfd935f015e6a57a56dd953074c5
Arturo Filastò <art@fuffa.org>
2014-11-24 18:13:54 +0100

    Improve sorting of steps in GUI.

--- client/app/scripts/controllers/admin/contexts.js
+++ client/app/scripts/controllers/admin/contexts.js
@@ -28,12 +28,6 @@ GLClient.controller('AdminContextsCtrl',
     });
   };
 
-  $scope.sortableOptions = {
-    stop: function(e, ui) {
-      $scope.update_contexts_order();
-    }
-  };
-
   $scope.reorder_contexts_alphabetically = function () {
     $scope.admin.contexts = _($scope.admin.contexts).sortBy(function (context) {
       return context.name;
@@ -82,7 +76,11 @@ GLClient.controller('AdminContextsCtrl',
 
   $scope.sortableOptions = {
     placeholder: "placeholder",
-    handle: ".handle"
+    handle: ".handle",
+    axis: 'x',
+    stop: function(e, ui) {
+      $scope.update_contexts_order();
+    }
   };
 
 }]);
@@ -91,7 +89,19 @@ GLClient.controller('AdminContextsEditorCtrl', ['$scope',
   function($scope) {
 
     $scope.editing = $scope.context.description === undefined;
-    
+
+     $scope.sortableOptions = {
+      placeholder: "placeholder",
+      handle: ".handle",
+      axis: 'x',
+      stop: function(e, ui) {
+        $scope.contextForm.$dirty = true;
+        $scope.contextForm.$pristine = false;
+        $scope.update_contexts_order();
+      }
+    };
+
+   
     $scope.toggleEditing = function () {
       $scope.editing = $scope.editing ^ 1;
     };
