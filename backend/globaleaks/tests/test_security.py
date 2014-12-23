5a1ceca26f281f2b4de6c9b2d184037aa44a4990
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2014-12-23 22:30:23 +0100

    Fix issue #1061

--- backend/globaleaks/tests/test_security.py
+++ backend/globaleaks/tests/test_security.py
@@ -80,7 +80,7 @@ class TestPasswordManagement(unittest.TestCase):
         sure_bin = scrypt.hash(dummy_password, get_salt(dummy_salt_input) )
         self.assertRaises(errors.InvalidInputFormat, hash_password, dummy_password, dummy_salt_input)
 
-    def test_change_password(self):
+    def test_change_password_fail_with_invalid_old_password(self):
         dummy_salt_input = "xxxxxxxx"
         first_pass = helpers.VALID_PASSWORD1
         second_pass = helpers.VALID_PASSWORD2
