3a97e1f6cb620d8759bd44128080fdc0dc237e52
vecna <vecna@globaleaks.org>
2015-01-07 13:01:44 +0100

    completed Ping logic, sendmail interface, polishing messages

--- backend/globaleaks/jobs/mailflush_sched.py
+++ backend/globaleaks/jobs/mailflush_sched.py
@@ -5,7 +5,7 @@
 #
 # Flush the email that has to be sent, is based on EventLog
 # database table.
-
+from globaleaks.utils.mailutils import MIME_mail_build, sendmail
 
 from globaleaks import models
 from globaleaks.handlers.admin import db_admin_serialize_node
@@ -39,200 +39,32 @@ class NotificationMail:
         self.plugin_used = plugin_used
 
     @inlineCallbacks
-    def do_every_notification(self, eventdict):
+    def do_every_notification(self, eventOD):
 
-        notify = self.plugin_used.do_notify(eventdict)
+        notify = self.plugin_used.do_notify(eventOD)
 
         if notify is None:
+            print "XXX investigate in this condition, if happen"
             yield deferred_sleep(1.0)
         else:
-            notify.addCallback(self.every_notification_succeeded, eventdict['id'])
-            notify.addErrback(self.every_notification_failed, eventdict['id'])
+            notify.addCallback(self.every_notification_succeeded, eventOD.storm_id)
+            notify.addErrback(self.every_notification_failed, eventOD.storm_id)
             yield notify
 
 
     @transact
-    def every_notification_succeeded(self, store, event_id):
-        print "success", event_id
-        pass
+    def every_notification_succeeded(self, store, result, event_id):
+
+        log.debug("Mail delivered correctly for event %s, [%s]" % (event_id, result))
+        evnt = store.find(models.EventLogs, models.EventLogs.id == event_id).one()
+        evnt.mail_sent = True
 
     @transact
-    def every_notification_failed(self, store, event_id):
-        print "fail", event_id
-        pass
+    def every_notification_failed(self, store, failure, event_id):
 
-
-
-    @transact
-    def receiverfile_notification_succeeded(self, store, result, receiverfile_id):
-        """
-        This is called when the Receiver File notification has succeeded
-        """
-        rfile = store.find(models.ReceiverFile, models.ReceiverFile.id == receiverfile_id).one()
-
-        if not rfile:
-            raise errors.FileIdNotFound
-
-        rfile.mark = models.ReceiverFile._marker[1] # 'notified'
-
-        log.debug("Email: +[Success] Notification of receiverfile %s for receiver %s" % (rfile.internalfile.name, rfile.receiver.user.username))
-
-    @transact
-    def receiverfile_notification_failed(self, store, failure, receiverfile_id):
-        """
-        This is called when the Receiver File notification has failed.
-        """
-        rfile = store.find(models.ReceiverFile, models.ReceiverFile.id == receiverfile_id).one()
-
-        if not rfile:
-            raise errors.FileIdNotFound
-
-        rfile.mark = models.ReceiverFile._marker[2] # 'unable to notify'
-
-        log.debug("Email: -[Fail] Notification of receiverfile %s for receiver %s" % (rfile.internalfile.name, rfile.receiver.user.username))
-
-    @inlineCallbacks
-    def do_receiverfile_notification(self, receiverfile_events):
-
-        for receiverfile_id, event in receiverfile_events.iteritems():
-
-            notify = self.plugin_used.do_notify(event)
-
-            if notify is None:
-                continue
-
-            notify.addCallback(self.receiverfile_notification_succeeded, receiverfile_id)
-            notify.addErrback(self.receiverfile_notification_failed, receiverfile_id)
-
-            # we need to wait on single mail send basis to not be prone to DoS
-            # and be forced to open so many outgoing connections.
-            yield notify
-
-
-    @transact_ro
-    def comment_notification_succeeded(self, store, result, comment_id):
-        """
-        This is called when the comment notification has succeeded
-        """
-        comment = store.find(models.Comment, models.Comment.id == comment_id).one()
-        # comment.mark  = ?
-        log.debug("Email: +[Success] Notification of comment from %s" % comment.author)
-
-    @transact_ro
-    def comment_notification_failed(self, store, failure, comment_id):
-        """
-        This is called when the comment notification has failed.
-        """
-        comment = store.find(models.Comment, models.Comment.id == comment_id).one()
-        log.debug("Email: -[Fail] Notification of comment from %s" % comment.author)
-
-    @inlineCallbacks
-    def do_comment_notification(self, comment_events):
-
-        for comment_id, event in comment_events.iteritems():
-
-            notify = self.plugin_used.do_notify(event)
-
-            if notify is None:
-                continue
-
-            notify.addCallback(self.comment_notification_succeeded, comment_id)
-            notify.addErrback(self.comment_notification_failed, comment_id)
-
-            # we need to wait on single mail send basis to not be prone to DoS
-            # and be forced to open so many outgoing connections.
-            yield notify
-
-    @transact
-    def tip_notification_succeeded(self, store, result, tip_id):
-        """
-        This is called when the tip notification has succeeded
-        """
-        receiver_tip = store.find(models.ReceiverTip, models.ReceiverTip.id == tip_id).one()
-
-        if not receiver_tip:
-            raise errors.TipIdNotFound
-
-        log.debug("Email: +[Success] Notification Tip receiver %s" % receiver_tip.receiver.user.username)
-        receiver_tip.mark = models.ReceiverTip._marker[1] # 'notified'
-
-    @transact
-    def tip_notification_failed(self, store, failure, tip_id):
-        """
-        This is called when the tip notification has failed.
-        """
-        receiver_tip = store.find(models.ReceiverTip, models.ReceiverTip.id == tip_id).one()
-
-        if not receiver_tip:
-            raise errors.TipIdNotFound
-
-        log.debug("Email: -[Fail] Notification Tip receiver %s" % receiver_tip.receiver.user.username)
-        receiver_tip.mark = models.ReceiverTip._marker[2] # 'unable to notify'
-
-    @inlineCallbacks
-    def do_tip_notification(self, tip_events):
-        """
-        This function fill the table events with new notification, to be display at the
-        receive login
-        """
-
-        for tip_id, event in tip_events.iteritems():
-
-            # plugin is the email, and the email notification may be disable globally,
-            # in this case, is there checked to be skipped
-            if GLSetting.notification_temporary_disable:
-                log.debug("Email notification of [%s %s] temporarly disable" % event.type, event.receiver_info)
-                continue
-
-            notify = self.plugin_used.do_notify(event)
-
-            if notify is None:
-                continue
-
-            notify.addCallback(self.tip_notification_succeeded, tip_id)
-            notify.addErrback(self.tip_notification_failed, tip_id)
-
-            # we need to wait on single mail send basis to not be prone to DoS
-            # and be forced to open so many outgoing connections.
-            yield notify
-
-
-    @transact_ro
-    def message_notification_succeeded(self, store, result, message_id):
-        """
-        This is called when the message notification has succeeded
-        """
-        message = models.Message.get(store, message_id)
-        log.debug("Email: +[Success] Notification of message for %s" %
-                  message.receivertip.receiver.user.username)
-
-    @transact_ro
-    def message_notification_failed(self, store, failure, message_id):
-        """
-        This is called when the message notification has failed.
-        """
-        message = store.find(models.Message, models.Message.id == message_id).one()
-        log.debug("Email: -[Fail] Notification of message receiver %s" %
-                  message.receivertip.receiver.user.username)
-
-    @inlineCallbacks
-    def do_message_notification(self, message_events):
-
-        for message_id, event in message_events.iteritems():
-
-            notify = self.plugin_used.do_notify(event)
-
-            if notify is None:
-                continue
-
-            notify.addCallback(self.message_notification_succeeded, message_id)
-            notify.addErrback(self.message_notification_failed, message_id)
-
-            # we need to wait on single mail send basis to not be prone to DoS
-            # and be forced to open so many outgoing connections.
-            yield notify
-
-
+        log.err("Mail deliver failure for event %s (%s)" % (event_id, failure))
+        evnt = store.find(models.EventLogs, models.EventLogs.id == event_id).one()
+        evnt.mail_sent = True
 
 
 
@@ -240,6 +72,11 @@ class NotificationMail:
 def load_complete_events(store):
     """
     _complete_ is explicit because do not serialize, but make an OD() of the description.
+
+    Note: there are a bug in this logic: is are loaded, let say, 3k mail, only 50 are flush
+    in the time window between each mailflush_sched. The next time, 2950 are put in this
+    list, and so on. is better that in this loop only the first email is taken, and then
+    the loop continue, one mail per time.
     """
 
     notification_settings = admin_serialize_notification(
@@ -249,13 +86,13 @@ def load_complete_events(store):
     node_desc = db_admin_serialize_node(store, 'en')
 
     event_list = []
-    storedevnts = store.find(EventLogs)
+    storedevnts = store.find(EventLogs, EventLogs.mail_sent == False)
 
     for stev in storedevnts:
 
         eventcomplete = OD()
 
-        # node level information are not stored in the node, but fetch now\
+        # node level information are not stored in the node, but fetch now
         eventcomplete.notification_settings = notification_settings
         eventcomplete.node_info = node_desc
 
@@ -265,14 +102,55 @@ def load_complete_events(store):
         eventcomplete.trigger_info = stev.description['trigger_info']
         eventcomplete.context_info = stev.description['context_info']
         eventcomplete.steps_info = stev.description['steps_info']
-        # TODO node
 
         eventcomplete.type = stev.description['type'] # 'Tip', 'Comment'
         eventcomplete.trigger = stev.event_reference['kind'] # 'plaintext_blah' ...
 
+        eventcomplete.storm_id = stev.id
+
         event_list.append(eventcomplete)
 
     return event_list
+
+
+def ping_mail_flush(receiver_dict):
+    """
+    This is a temporary class, ping_mail has to be implemented in the clean and testable
+    way defined in plugin/base.py and plugin/notification.py, and/or is the opportunity
+    to review these classes, at the moment is a simplify version thst just create a
+    ping email and send it via sendmail.
+    """
+
+    for receiver_mail, _data in receiver_dict.iteritems():
+
+        whinkles, receiver_name = _data
+        print "Ping", receiver_mail, whinkles, receiver_name
+
+        title = "There are %d good reason to..." % whinkles
+        body = "\nConnect exactly where you know!, our dear %s ;)\n" % receiver_name
+
+        message = MIME_mail_build(GLSetting.memory_copy.notif_source_name,
+                                  GLSetting.memory_copy.notif_source_email,
+                                  receiver_name,
+                                  receiver_mail,
+                                  title,
+                                  body)
+
+        event = OD()
+        event.type = "Ping mail for %s (%d info)" % (receiver_mail, whinkles)
+
+        return sendmail(authentication_username=GLSetting.memory_copy.notif_username,
+                        authentication_password=GLSetting.memory_copy.notif_password,
+                        from_address='whinklermail@globaleaks.org',
+                        # this has to be == notification_settings['source_email'],
+                        to_address= [ receiver_mail ],
+                        message_file=message,
+                        smtp_host=GLSetting.memory_copy.notif_server,
+                        smtp_port=GLSetting.memory_copy.notif_port,
+                        security=GLSetting.memory_copy.notif_security,
+                        event=event)
+
+
 
 
 class MailflushSchedule(GLJob):
@@ -286,12 +164,27 @@ class MailflushSchedule(GLJob):
         plugin = getattr(notification, GLSetting.notification_plugins[0])()
         notifcb = NotificationMail(plugin)
 
-
         for qe in queue_events:
-            print "I'm flushing"
-            print dir(qe)
             deferred_sleep(2)
             notifcb.do_every_notification(qe)
             deferred_sleep(2)
+
+        # TODO implement ping as an appropriate plugin
+        receiver_synthesis = {}
+        for qe in queue_events:
+
+            if not qe.receiver_info['ping_notification']:
+                continue
+
+            receiver_synthesis.setdefault(qe.receiver_info['ping_mail_address'],
+                                          [0, qe.receiver_info['name']])
+            receiver_synthesis[qe.receiver_info['ping_mail_address']][0] += 1
+
+
+        ping_mail_flush(receiver_synthesis)
+
+        # TODO implement digest as an appropriate plugin
+
+
 
 
