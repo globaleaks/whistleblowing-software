5a217c264318aaac61b978b7176cfc23d60bb681
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2015-01-15 22:38:18 +0100

    Add possibility to configure the default_homepage (currently hardcoded)

--- client/app/scripts/controllers/fileupload.js
+++ client/app/scripts/controllers/fileupload.js
@@ -13,3 +13,19 @@ GLClient.controller('WBFileUploadCtrl', ['$scope', 'Authentication', function($s
       $scope.options['maxNumberOfFiles'] = 1
   };
 }]);
+controller('FileUploadEditFileController', ['$scope', 'Authentication', function($scope, Authentication) {
+  $scope.empty_d = true;
+  $scope.empty_t = true;
+  $scope.file_desc;
+  $scope.file_title;
+  $scope.edit = function() {
+    if ($scope.file_desc) {
+      $scope.empty_d = false;
+    }
+    if ($scope.file_title) {
+      $scope.empty_t = false;
+    }
+  };
+}]).
+controller('FileDestroyController', ['$scope', 'Authentication', function($scope, Authentication) {
+}]);
