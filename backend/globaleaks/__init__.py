a2c2e67f81ed5cd1ea7f97e0bb50c978d6b6d791
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2015-01-18 14:58:51 +0100

    Bump to version 2.60.49

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -8,7 +8,7 @@ import operator
 __author__ = u'Random GlobaLeaks Developers'
 __copyright__ = u'Hermes Center for Transparency and Digital Human Rights.'
 __email__ = u'info@globaleaks.org'
-__version__ = u'2.60.47'
+__version__ = u'2.60.49'
 
 DATABASE_VERSION = 16
 
@@ -38,6 +38,7 @@ LANGUAGES_SUPPORTED = [
  { "code": "ru", "name": "Russian" },
  { "code": "sk", "name": "Slovak" },
  { "code": "sr_RS", "name": "Serbian (Serbia)" },
+ { "code": "sr_RS@latin", "name": "Serbian (Latin) (Serbia)" },
  { "code": "sv", "name": "Swedish" },
  { "code": "th", "name": "Thai" },
  { "code": "tr", "name": "Turkish" },
