1c10f6f8b79fdec5a1711d29bceab168d4315fe5
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2015-01-30 13:04:06 +0100

    Added skeleton for migrations of db version 17

---
+++ backend/globaleaks/db/update_16_17.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+"""
+  Changes
+
+
+"""
+
+from storm.locals import Int, Bool, Unicode, DateTime, JSON, ReferenceSet
+from globaleaks.db.base_updater import TableReplacer
+from globaleaks.models import Model, Field, Step
+from globaleaks.utils.utility import every_language
+
+class Replacer1617(TableReplacer):
+    pass
