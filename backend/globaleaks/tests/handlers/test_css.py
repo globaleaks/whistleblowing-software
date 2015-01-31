356e0eea2277f76df68cb171fa090647224f4cc1
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2015-01-31 21:35:35 +0100

    Implement corrections to test_css

--- backend/globaleaks/tests/handlers/test_css.py
+++ backend/globaleaks/tests/handlers/test_css.py
@@ -1,21 +1,25 @@
+from twisted.internet.defer import inlineCallbacks
+
 from globaleaks.handlers import css
 from globaleaks.tests import helpers
 
 # FIXME this tests does not really test the handler but
 # simply asses the code coverage.
 
-class TestLTRFileCSS(helpers.TestGL):
+class TestLTRFileCSS(helpers.TestHandler):
     _handler = css.LTRCSSFileHandler
 
+    @inlineCallbacks
     def test_get(self):
         handler = self.request({})
         yield handler.get()
         self.assertEqual(handler.get_status(), 200)
 
 
-class TestRTLFileCSS(helpers.TestGL):
+class TestRTLFileCSS(helpers.TestHandler):
     _handler = css.RTLCSSFileHandler
 
+    @inlineCallbacks
     def test_get(self):
         handler = self.request({})
         yield handler.get()
