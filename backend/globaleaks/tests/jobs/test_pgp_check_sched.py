cfed40b28f6700af766ddc90957f42fd61bc5777
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2015-02-01 01:03:40 +0100

    Add basic test pgp_check_schedule in order to raise code coverage

---
+++ backend/globaleaks/tests/jobs/test_pgp_check_sched.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from twisted.internet.defer import inlineCallbacks
+
+from globaleaks.tests import helpers
+from globaleaks.settings import GLSetting, transact, transact_ro
+from globaleaks.models import Receiver
+
+from globaleaks.jobs import pgp_check_sched
+
+class TestPGPCheckSchedule(helpers.TestGLWithPopulatedDB):
+
+    @inlineCallbacks
+    def test_php_check_schedule(self):
+        # FIXME: complete this unit test by performing checks
+        #        on the actions performed by the scheduler.
+        yield pgp_check_sched.PGPCheckSchedule().operation()
+
