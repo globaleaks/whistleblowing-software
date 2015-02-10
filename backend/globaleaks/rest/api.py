51b03cb2e12e2a177d94a934626c2b486eb6c511
vecna <vecna@globaleaks.org>
2015-02-09 23:14:35 -0500

    now context_id is part of the /submission REST

--- backend/globaleaks/rest/api.py
+++ backend/globaleaks/rest/api.py
@@ -12,7 +12,6 @@ from globaleaks.handlers import node, submission, rtip, wbtip, receiver, \
 from globaleaks.handlers.base import BaseStaticFileHandler, BaseRedirectHandler
 
 uuid_regexp = r'([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})'
-full_random_str = r'([a-zA-Z0-9]{42})'
 field_regexp = uuid_regexp
 token_string = r'([a-zA-Z0-9]{42})'
 
@@ -43,6 +42,7 @@ spec = [
 
     ## Submission Handlers ##
 <<<<<<< HEAD
+<<<<<<< HEAD
     (r'/submission/' + uuid_regexp, submission.SubmissionCreate),
     (r'/submission/' + uuid_regexp + '/' + token_string, submission.SubmissionInstance),
     (r'/submission/' + token_string + '/file', files.FileInstance),
@@ -51,6 +51,11 @@ spec = [
     (r'/submission/' + full_random_str, submission.SubmissionInstance),
     (r'/submission/' + uuid_regexp + '/file', files.FileInstance),
 >>>>>>> performed sumibssion with token_id
+=======
+    (r'/submission/' + uuid_regexp, submission.SubmissionCreate),
+    (r'/submission/' + uuid_regexp + '/' + token_string, submission.SubmissionInstance),
+    (r'/submission/' + token_string + '/file', files.FileInstance),
+>>>>>>> now context_id is part of the /submission REST
 
     ## Receiver Tip Handlers ##
     (r'/rtip/' + uuid_regexp, rtip.RTipInstance),
