5ed16083b6533cba925d08f04f4305b5928030d8
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-02-22 17:13:49 +0100

    Add various CSS classes to HTML templates to enrich customization possibilities (#1173)

--- client/app/scripts/controllers/submission.js
+++ client/app/scripts/controllers/submission.js
@@ -161,6 +161,14 @@ controller('SubmissionFieldCtrl', ['$scope', '$rootScope', function ($scope, $ro
     $scope.field.value = [];
   }
 
+  $scope.getClass = function(stepIndex, fieldIndex, toplevel) {
+    if (toplevel) {
+      return "submission-step" + stepIndex + "-field" + fieldIndex;
+    } else {
+      return "";
+    }
+  }
+
   var update_uploads_status = function(e, data) {
     $scope.$parent.uploading = false;
     if ($scope.field.value === "") {
