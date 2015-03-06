globaleaks/whistleblowing-software

commit 3da8f485b7c617c210bea84e55ae75537e587996

option -z now awaits a developer name #1174

backend/globaleaks/settings.py
@@ -259,6 +259,7 @@ class GLSettingsClass(object):
         self.gid = os.getgid()
         self.start_clean = False
         self.devel_mode = False
+        self.developer_name = None
         self.skip_wizard = False
         self.glc_path = None
 
@@ -345,6 +346,7 @@ class GLSettingsClass(object):
 
     def set_devel_mode(self):
         self.devel_mode = True
+        self.developer_name = unicode(self.cmdline_options.developer_name)
         self.pid_path = os.path.join(self.root_path, 'workingdir')
         self.working_path = os.path.join(self.root_path, 'workingdir')
         self.static_source = os.path.join(self.root_path, 'staticdata')
@@ -483,8 +485,9 @@ class GLSettingsClass(object):
 
         self.working_path = self.cmdline_options.working_path
 
-        if self.cmdline_options.devel_mode:
-            print "Enabling Development Mode"
+        if self.cmdline_options.developer_name:
+            print "Enabling Development Mode for %s" %\
+                    self.cmdline_options.developer_name
             self.set_devel_mode()
 
         if self.cmdline_options.skip_wizard:

backend/bin/globaleaks
@@ -178,9 +178,9 @@ GLSetting.parser.add_option("-k", "--kill", type="int",
     help="signal to send to the current globaleaks process (if exists) [default: don't kill]",
     dest="kill", default=0)
 
-GLSetting.parser.add_option("-z", "--devel-mode", action='store_true',
-    help="hack some configs and enable some additional features useful during development' [default: %default]",
-    dest="devel_mode", default=GLSetting.devel_mode)
+GLSetting.parser.add_option("-z", "--devel-mode", type='string',
+        help="hack some configs, specify your name to receive personalized exceptions' [default: %default]",
+    dest="developer_name")
 
 GLSetting.parser.add_option("-x", "--skip-wizard", action='store_true',
     help="skip globaleaks installation wizard' [default: False]",

backend/globaleaks/utils/mailutils.py
@@ -285,8 +285,11 @@ def mail_exception(etype, value, tback):
                                   GLSetting.memory_copy.notif_source_email,
                                   "Admin",
                                   GLSetting.memory_copy.exception_email,
-                                  "Subject: GLBackend Exception %s [%d]" % \
-                                    (__version__, mail_exception.mail_counter),
+                                  "Subject:%s %s [%d]" % \
+                                    (" %s's development Exception" %
+                                        GLSetting.developer_name if GLSetting.devel_mode else " GlobaLeaks Exception",
+                                     __version__,
+                                     mail_exception.mail_counter),
                                   mail_body)
 
         sendmail(authentication_username=GLSetting.memory_copy.notif_username,

commit bf510e478fce2c0b14fb77f1431057b6eee1df03

Fixed regression introduced in 3da8f485b7c617c210bea84e55ae75537e587996

backend/globaleaks/settings.py
@@ -259,7 +259,7 @@ class GLSettingsClass(object):
         self.gid = os.getgid()
         self.start_clean = False
         self.devel_mode = False
-        self.developer_name = None
+        self.developer_name = ''
         self.skip_wizard = False
         self.glc_path = None
 
@@ -346,7 +346,7 @@ class GLSettingsClass(object):
 
     def set_devel_mode(self):
         self.devel_mode = True
-        self.developer_name = unicode(self.cmdline_options.developer_name)
+        self.developer_name = unicode(self.developer_name)
         self.pid_path = os.path.join(self.root_path, 'workingdir')
         self.working_path = os.path.join(self.root_path, 'workingdir')
         self.static_source = os.path.join(self.root_path, 'staticdata')
@@ -488,6 +488,7 @@ class GLSettingsClass(object):
         if self.cmdline_options.developer_name:
             print "Enabling Development Mode for %s" %\
                     self.cmdline_options.developer_name
+            self.developer_name = unicode(self.cmdline_options.developer_name)
             self.set_devel_mode()
 
         if self.cmdline_options.skip_wizard:

backend/globaleaks/tests/jobs/test_cleaning_sched.py
@@ -27,8 +27,10 @@ class MockHandler(base.BaseHandler):
         pass
 
 class TestCleaning(helpers.TestGL):
+
+    @inlineCallbacks
     def setUp(self):
-        helpers.TestGL.setUp(self)
+        yield helpers.TestGL.setUp(self)
 
         # filled in setup
         self.context_desc = None