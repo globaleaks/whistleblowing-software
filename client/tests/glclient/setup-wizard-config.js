09dd1101968c5476cec3bea19c51369334682391
vecna <vecna@globaleaks.org>
2015-04-03 00:37:56 +0200

    experiments ongoing: delivery very frequent

--- client/tests/glclient/setup-wizard-config.js
+++ client/tests/glclient/setup-wizard-config.js
@@ -1,5 +1,6 @@
 // conf.js
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['setup-wizard.js']
+  specs: [ 'setup-wizard.js' ]
 }
+// specs: [ 'setup-wizard.js', /* 'setup-receivers.js' */ ]
