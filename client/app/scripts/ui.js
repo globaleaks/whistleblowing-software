cff48756b42d2cba62c6d48ed14db2ae44581541
origliante <noone@nowhere.org>
2015-04-03 14:23:13 +0200

    pgp key drag&drop load in receiver preferences tab3

--- client/app/scripts/ui.js
+++ client/app/scripts/ui.js
@@ -176,6 +176,92 @@ angular.module('submissionUI', []).
       }
     };
 }).
+  directive('fileDropzone', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      file: '=',
+      fileName: '='
+    },
+    link: function(scope, element, attrs) {
+      var checkSize, isTypeValid, processDragOverOrEnter, validMimeTypes;
+
+      processDragOverOrEnter = function(event) {
+        if (event != null) {
+          event.preventDefault();
+        }
+        event.dataTransfer.effectAllowed = 'copy';
+        return false;
+      };
+      validMimeTypes = attrs.fileDropzone;
+
+      checkSize = function(size) {
+        var _ref;
+        if (((_ref = attrs.maxFileSize) === (void 0) || _ref === '') || (size / 1024) / 1024 < attrs.maxFileSize) {
+          return true;
+        } else {
+          alert("File must be smaller than " + attrs.maxFileSize + " MB");
+          return false;
+        }
+      };
+
+      isTypeValid = function(type) {
+        if ((validMimeTypes === (void 0) || validMimeTypes === '') || validMimeTypes.indexOf(type) > -1) {
+          return true;
+        } else {
+          alert("Invalid file type.  File must be one of following types " + validMimeTypes);
+          return false;
+        }
+      };
+
+      isPgpKeyValid = function(keytext) {
+        var pgp_key = openpgp.key.readArmored(keytext).keys[0];
+        if (pgp_key) {
+          return true;
+        }
+        return false;
+      };
+
+      element.bind('dragover', processDragOverOrEnter);
+      element.bind('dragenter', processDragOverOrEnter);
+
+      return element.bind('drop', function(event) {
+        var file, name, reader, size, type;
+        if (event != null) {
+          event.preventDefault();
+        }
+        reader = new FileReader();
+        reader.onload = function(evt) {
+          if (checkSize(size) && isTypeValid(type)) {
+
+            /*return scope.$apply(function() {
+              scope.file = evt.target.result;
+              if (angular.isString(scope.fileName)) {
+                return scope.fileName = name;
+              }
+            });*/
+
+          }
+        };
+        reader.onloadend = function () {
+            if (isPgpKeyValid(reader.result)) {
+                var key = reader.result.replace(/^\s+|\s+$/g, "");
+                element.val(key);
+            } else {
+                alert("Invalid PGP Key.");
+            }
+        }
+
+        file = event.dataTransfer.files[0];
+        name = file.name;
+        type = file.type;
+        size = file.size;
+        reader.readAsText(file);
+        return false;
+      });
+    }
+  };
+}).
   directive('creditCard', ['$filter', function($filter){
     return {
       scope: {
