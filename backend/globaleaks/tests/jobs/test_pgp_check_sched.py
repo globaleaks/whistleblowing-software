346337993eea65daab70b518712eda43e76b5433
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-04-09 00:23:44 +0200

    Improve code coverage of test_pgp_check_sched.py

--- backend/globaleaks/tests/jobs/test_pgp_check_sched.py
+++ backend/globaleaks/tests/jobs/test_pgp_check_sched.py
@@ -7,8 +7,11 @@ from globaleaks.jobs import pgp_check_sched
 
 class TestPGPCheckSchedule(helpers.TestGLWithPopulatedDB):
 
+    encryption_scenario = 'ONE_VALID_ONE_EXPIRED'
+
     @inlineCallbacks
     def test_pgp_check_schedule(self):
+
         # FIXME: complete this unit test by performing checks
         #        on the actions performed by the scheduler.
         yield pgp_check_sched.PGPCheckSchedule().operation()
