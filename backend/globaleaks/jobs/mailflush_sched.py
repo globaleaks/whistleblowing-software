0218396ab8766ad8350820efc6f6978014b1e267
vecna <vecna@globaleaks.org>
2015-05-09 15:56:20 +0200

    implemented #798 in mail-flush logic

--- backend/globaleaks/jobs/mailflush_sched.py
+++ backend/globaleaks/jobs/mailflush_sched.py
@@ -17,8 +17,121 @@ from globaleaks.jobs.base import GLJob
 from globaleaks.settings import transact, transact_ro, GLSetting
 from globaleaks.plugins import notification
 from globaleaks.utils.mailutils import MIME_mail_build, sendmail
-from globaleaks.utils.utility import deferred_sleep, log
+from globaleaks.utils.utility import deferred_sleep, log, datetime_to_ISO8601, datetime_now
 from globaleaks.utils.templating import Templating
+from globaleaks.utils.tempobj import TempObj
+
+reactor_override = None
+
+
+class LastHourMailQueue(object):
+    """
+    This class has only a class variable, used to stock the queue of the
+    event happened on the latest minutes.
+    """
+
+    # This event queue is used by the tempObj
+    event_queue = dict()
+
+    # This list is used specifically for mail tracking
+    per_receiver_lastmails = dict()
+    _counter = 0
+
+    # This list is used to keep track of the currently suspended mail
+    receivers_in_threshold = list()
+
+    # This is the utility dict required by TempObj
+    blocked_in_queue = dict()
+
+    @staticmethod
+    def mail_number(receiver_mail):
+        if receiver_mail not in LastHourMailQueue.per_receiver_lastmails:
+            return 0
+        return LastHourMailQueue.per_receiver_lastmails[receiver_mail]
+
+    @staticmethod
+    def get_incremental_number():
+        LastHourMailQueue._counter += 1
+        return LastHourMailQueue._counter
+
+
+class ReceiverDeniedEmail(TempObj):
+
+    def __init__(self, receiver_mail, debug=False):
+
+        self.debug = debug
+        self.creation_date = datetime_now()
+        self.receiver_mail = receiver_mail
+        import random
+        # TODO something more appropriate
+        self.unique_id = random.randint(0, 0xffff)
+
+        if receiver_mail in LastHourMailQueue.receivers_in_threshold:
+            log.err("Implementation error ? Receiver %s already present" %
+                    receiver_mail)
+
+        TempObj.__init__(self,
+                         LastHourMailQueue.blocked_in_queue,
+                         self.unique_id,
+                         # seconds of validity:
+                         GLSetting.memory_copy.notification_blackhole_lasting_for,
+                         reactor_override)
+
+        log.info("Putting %s in the Not-mail-me-again queue for four hours" %
+                 self.receiver_mail)
+        LastHourMailQueue.receivers_in_threshold.append(receiver_mail)
+        self.expireCallbacks.append(self.manage_receiver_is_back)
+
+    def manage_receiver_is_back(self):
+        # Receiver return to be usable
+        log.info("Expiring email suspension for %s" % self.receiver_mail)
+        if self.receiver_mail not in LastHourMailQueue.receivers_in_threshold:
+            log.err("I'm removing something that is not present in %s" %
+                    LastHourMailQueue.receivers_in_threshold)
+        else:
+            LastHourMailQueue.receivers_in_threshold.remove(self.receiver_mail)
+
+
+class MailActivities(TempObj):
+
+    def __init__(self, receiver_mail, debug=False):
+
+        self.debug = debug
+        self.creation_date = datetime_now()
+        self.receiver_mail = receiver_mail
+        self.event_id = LastHourMailQueue.get_incremental_number()
+
+        TempObj.__init__(self,
+                         LastHourMailQueue.event_queue,
+                         self.event_id,
+                         # seconds of validity:
+                         3600,
+                         reactor_override)
+
+        LastHourMailQueue.per_receiver_lastmails.setdefault(receiver_mail, 0)
+        LastHourMailQueue.per_receiver_lastmails[receiver_mail] += 1
+
+        self.expireCallbacks.append(self.manage_mail_expiration)
+
+    def manage_mail_expiration(self):
+
+        # one hour expired!
+        if self.debug:
+            log.debug("Before the check: %s" % LastHourMailQueue.per_receiver_lastmails)
+        LastHourMailQueue.per_receiver_lastmails[self.receiver_mail] -= 1
+        if self.debug:
+            log.debug("After the check: %s" % LastHourMailQueue.per_receiver_lastmails)
+
+    def __repr__(self):
+        return self.receiver_mail
+
+    def serialize_object(self):
+        return {
+            'receiver_mail' : self.receiver_mail,
+            'id': self.id,
+            'creation_date' : datetime_to_ISO8601(self.creation_date)
+        }
+
 
 class NotificationMail:
     def __init__(self, plugin_used):
@@ -119,12 +232,22 @@ def load_complete_events(store, event_number=GLSetting.notification_limit):
     return event_list
 
 def filter_notification_event(notifque):
+    """
+    :param notifque: the current notification event queue
+    :return: a modified queue in the case some email has not to be sent
+
+    Basically performs two test:
+     1) issue #444
+     2) issue #798
+    """
+
     # Here we collect the Storm event of Files having as key the Tip
     files_event_by_tip = {}
-    # this is the return value:
-    new_filtered_list = []
+
+    _tmp_list = []
+    return_filtered_list = []
     # to be smoked Storm.id
-    to_be_skipped = []
+    storm_id_to_be_skipped = []
 
     for ne in notifque:
         if ne['trigger'] !=  u'Tip':
@@ -136,23 +259,52 @@ def filter_notification_event(notifque):
     # I'm looping two times because dict has random ordering
     for ne in notifque:
         if ne['trigger'] != u'File':
-            new_filtered_list.append(ne)
+            _tmp_list.append(ne)
             continue
 
         if ne['tip_info']['id'] in files_event_by_tip:
-            to_be_skipped.append(ne['storm_id'])
+            storm_id_to_be_skipped.append(ne['storm_id'])
         else:
-            new_filtered_list.append(ne)
+            _tmp_list.append(ne)
 
-    if len(to_be_skipped):
-        log.debug("Filtering function: Marked %d files notification to be suppressed" %
-                  len(to_be_skipped))
+    if len(storm_id_to_be_skipped):
+        log.debug("Filtering function: Marked %d Files notification to be suppressed as part of the submission" %
+                  len(storm_id_to_be_skipped))
+
+    for ne in _tmp_list:
+
+        receiver_mail = ne['receiver_info']['mail_address']
+        # It add automatically a mail in to the last hour email queue
+        MailActivities(receiver_mail)
+        for i in xrange(20):
+            MailActivities(receiver_mail)
+
+        email_sent_last_60min = LastHourMailQueue.mail_number(receiver_mail)
+
+        if receiver_mail in LastHourMailQueue.receivers_in_threshold:
+            log.debug("Receiver %s is currently suspended against new mail" %
+                      receiver_mail)
+            storm_id_to_be_skipped.append(ne['storm_id'])
+            continue
+
+        if email_sent_last_60min >= GLSetting.memory_copy.notification_threshold_per_hour:
+            log.info("Threshold reach of %d email with limit of %d for receiver %s" % (
+                email_sent_last_60min,
+                GLSetting.memory_copy.notification_threshold_per_hour,
+                receiver_mail)
+            )
+            ReceiverDeniedEmail(receiver_mail)
+            storm_id_to_be_skipped.append(ne['storm_id'])
+            continue
+
+        return_filtered_list.append(ne)
+
 
     log.debug("List of event %d after the filtering process is %d long" %
-              (len(notifque), len(new_filtered_list)))
+              (len(notifque), len(return_filtered_list)))
 
     # return the new list of event and the list of Storm.id
-    return new_filtered_list, to_be_skipped
+    return return_filtered_list, storm_id_to_be_skipped
 
 
 class MailflushSchedule(GLJob):
