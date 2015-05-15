4e6c24c4234dd3a6f088dc7c993fa0c52cbd1fa1
vecna <vecna@globaleaks.org>
2015-05-15 12:36:45 +0200

    submit button responsive to the counter status

--- backend/globaleaks/utils/token.py
+++ backend/globaleaks/utils/token.py
@@ -73,8 +73,7 @@ class Token(TempObj):
         # is a sign of a psyco life. (vecnish!)
         if GLSetting.devel_mode:
             # self.start_validity_secs = 0
-            print "XXX TODO - REMOVE - Development shit of today", __file__
-            self.start_validity_secs = 6
+            self.start_validity_secs = 4
 
         self.remaining_allowed_attempts = Token.MAXIMUM_ATTEMPTS_PER_TOKEN
 
