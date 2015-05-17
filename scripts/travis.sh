e1081b83e37ffc9745da6e6c679bd0e1c478c2b3
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-05-17 22:26:42 +0200

    Parallelize end2end testing

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -14,32 +14,39 @@ if [ "$TEST_SUITE" = "units" ]; then
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis
   $TRAVIS_BUILD_DIR/client/node_modules/mocha/bin/mocha -R list $TRAVIS_BUILD_DIR/client/tests/glbackend/test_00* --timeout 4000
 
-elif [ "$TEST_SUITE" = "end2end" ]; then
+else
 
-  declare -a capabilities=(
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"firefox\", \"platform\":\"Linux\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"firefox\", \"platform\":\"OS X 10.10\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"firefox\", \"version\":\"36.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"firefox\", \"version\":\"37.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"platform\":\"Linux\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"platform\":\"OS X 10.10\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"37.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"38.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"39.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"40.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"41.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"chrome\", \"version\":\"42.0\", \"platform\":\"Windows 8.1\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"safari\", \"platform\":\"OS X 10.10\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"internet explorer\", \"version\":\"11\", \"platform\":\"Windows 8.1\"}'"
-  )
+  if [ "$TEST_SUITE" = "end2end-01" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "platform":"Linux"}'
+  elif [ "$TEST_SUITE" = "end2end-02" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "platform":"OS X 10.10"}'
+  elif [ "$TEST_SUITE" = "end2end-03" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "version":"36.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-04" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "version":"37.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-05" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "platform":"Linux"}'
+  elif [ "$TEST_SUITE" = "end2end-06" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "platform":"OS X 10.10"}'
+  elif [ "$TEST_SUITE" = "end2end-07" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"37.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-08" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"38.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-09" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"39.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-10" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"40.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-11" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"41.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-12" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"42.0", "platform":"Windows 8.1"}'
+  elif [ "$TEST_SUITE" = "end2end-13" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"safari", "platform":"OS X 10.10"}'
+  elif [ "$TEST_SUITE" = "end2end-14" ]; then
+    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"internet explorer", "version":"11", "platform":"Windows 8.1"}'
+  fi
 
-  ## now loop through the above array
-  for i in "${capabilities[@]}"
-  do
-    eval $i
-    $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis -c -k9 --port 8080
-    sleep 1
-    grunt protractor:saucelabs
-  done
+  $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis -c -k9 --port 8080
+  grunt protractor:saucelabs
 
 fi
