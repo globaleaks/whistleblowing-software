cf027476945ff50548a346227f903ffe4dc6c2c3
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-05-18 10:50:15 +0200

    Simplify configuration/selection of tests on travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,7 +6,7 @@ set -e
 cd $TRAVIS_BUILD_DIR/client  # to test backend handlers
 bower install && grunt build
 
-if [ "$TEST_SUITE" = "units" ]; then
+if [ "$TEST_E2E" = "false" ]; then
 
   cd $TRAVIS_BUILD_DIR/backend  # to run backend tests
   coverage run setup.py test
@@ -16,35 +16,7 @@ if [ "$TEST_SUITE" = "units" ]; then
 
 else
 
-  if [ "$TEST_SUITE" = "end2end-01" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "platform":"Linux"}'
-  elif [ "$TEST_SUITE" = "end2end-02" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "platform":"OS X 10.10"}'
-  elif [ "$TEST_SUITE" = "end2end-03" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "version":"36.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-04" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"firefox", "version":"37.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-05" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "platform":"Linux"}'
-  elif [ "$TEST_SUITE" = "end2end-06" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "platform":"OS X 10.10"}'
-  elif [ "$TEST_SUITE" = "end2end-07" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"37.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-08" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"38.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-09" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"39.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-10" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"40.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-11" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"41.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-12" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"chrome", "version":"42.0", "platform":"Windows 8.1"}'
-  elif [ "$TEST_SUITE" = "end2end-13" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"safari", "platform":"OS X 10.10"}'
-  elif [ "$TEST_SUITE" = "end2end-14" ]; then
-    export SELENIUM_BROWSER_CAPABILITIES='{"browserName":"internet explorer", "version":"11", "platform":"Windows 8.1"}'
-  fi
+  echo "Protractor End2End test: $TEST_E2E"
 
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis -c -k9 --port 8080
   grunt protractor:saucelabs
