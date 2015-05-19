a5a6f635f694299cdf5c5ca695800a0aaa930380
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-05-19 23:13:34 +0200

    Implement skeleton for browserchecks tests

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,20 +3,26 @@
 set -e
 
 # build the client
-cd $TRAVIS_BUILD_DIR/client  # to test backend handlers
-bower install && grunt build
+cd $TRAVIS_BUILD_DIR/client
+bower install
+grunt build
 
-if [ "$TEST_E2E" = "false" ]; then
+if [ "$GLTEST" = "false" ]; then
 
-  cd $TRAVIS_BUILD_DIR/backend  # to run backend tests
+  cd $TRAVIS_BUILD_DIR/backend
   coverage run setup.py test
   coveralls || true
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis
-  $TRAVIS_BUILD_DIR/client/node_modules/mocha/bin/mocha -R list $TRAVIS_BUILD_DIR/client/tests/glbackend/test_00* --timeout 4000
+  $TRAVIS_BUILD_DIR/client/node_modules/mocha/bin/mocha -R list $TRAVIS_BUILD_DIR/client/tests/api/test_00* --timeout 4000
+
+else [ "$GLTEST" = "loader-only" ]; then
+
+  echo "Mocha test: $GLTEST"
+  grunt test-loader-saucelabs
 
 else
 
-  echo "Protractor End2End test: $TEST_E2E"
+  echo "Protractor End2End test: $GLTEST"
 
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis -c -k9 --port 8080
   grunt protractor:saucelabs
