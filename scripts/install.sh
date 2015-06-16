73ca8e86aada6c91cd3aed42df00c77113618e22
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-06-16 16:20:37 +0200

    Address issue #1299

--- scripts/install.sh
+++ scripts/install.sh
@@ -179,7 +179,7 @@ if [ $DISTRO == 'Ubuntu' ];then
   fi
 
   echo "Adding Ubuntu Universe repository"
-  add-apt-repository 'deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe'
+  add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 fi
 
 if [ ! -f /etc/apt/sources.list.d/globaleaks.list ]; then
