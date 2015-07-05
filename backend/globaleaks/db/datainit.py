1c88a262e307fbac9b4f5bc0bb5e7aef6f3e6d10
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-07-05 20:47:23 +0200

    Fix variable name change

--- backend/globaleaks/db/datainit.py
+++ backend/globaleaks/db/datainit.py
@@ -155,13 +155,12 @@ def db_update_memory_variables(store):
         GLSetting.memory_copy.notif_source_email = notif.source_email
         GLSetting.memory_copy.notif_uses_tor = notif.torify
 
-        if GLSetting.disable_mail_notifications:
+        if GLSetting.disable_mail_notification:
             GLSetting.memory_copy.disable_receiver_notification_emails = True
             GLSetting.memory_copy.disable_admin_notification_emails = True
         else:
             GLSetting.memory_copy.disable_receiver_notification_emails = notif.disable_receivers_notification_emails
             GLSetting.memory_copy.disable_admin_notification_emails = notif.disable_admin_notification_emails
-
 
     except Exception as e:
         raise errors.InvalidInputFormat("Cannot import memory variables: %s" % e)
