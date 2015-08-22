6fc0f6cedc2e942fbc0f91a5127eb4ff8e040d5f
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-08-23 01:35:55 +0200

    Apply minor fixes related to builded glclient

--- client/app/scripts/controllers/fileupload.js
+++ client/app/scripts/controllers/fileupload.js
@@ -2,13 +2,12 @@ GLClient.controller('WBFileUploadCtrl', ['$scope', function($scope) {
   $scope.disabled = false
 
   $scope.onFileAdded = function($event, $file, $flow) {
-    console.log($event.target.parentNode.id);
     if ($file.size > $scope.node.maximum_filesize * 1024 * 1024) {
       $file.error = true;
       $file.error_msg = "This file exceeds the maximum upload size for this server.";
       event.preventDefault();
     } else {
-      if (!$scope.field.multi_entry) {
+      if ($scope.field !== undefined && !$scope.field.multi_entry) {
         $scope.disabled = true;
       }
     }
