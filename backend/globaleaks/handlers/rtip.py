dba7aaa210dee4d53152cdabbeb49b19dbe95924
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-08-28 00:12:50 +0200

    Implement feature #1375

--- backend/globaleaks/handlers/rtip.py
+++ backend/globaleaks/handlers/rtip.py
@@ -22,7 +22,7 @@ from globaleaks.utils.utility import log, utc_future_date, datetime_now, \
 from globaleaks.utils.structures import Rosetta
 from globaleaks.settings import transact, transact_ro, GLSettings
 from globaleaks.models import Node, Notification, Comment, Message, \
-    ReceiverFile, ReceiverTip, EventLogs
+    ReceiverFile, ReceiverTip, EventLogs,  InternalTip, ArchivedSchema
 from globaleaks.rest import errors
 
 
@@ -174,6 +174,9 @@ def db_delete_itip(store, itip):
                     log.err("Unable to remove %s: %s" % (abspath, excep.strerror))
 
     store.remove(itip)
+
+    if store.find(InternalTip, InternalTip.questionnaire_hash == itip.questionnaire_hash).count() == 0:
+        store.find(ArchivedSchema, ArchivedSchema.hash == itip.questionnaire_hash).remove()
 
 
 def db_delete_rtip(store, rtip):
