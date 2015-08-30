c3a03a9de9ac35bdf053e554f1fdee48e643d4e9
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-08-30 23:21:07 +0200

    Refine implementation of #1383

--- client/app/scripts/controllers/submission.js
+++ client/app/scripts/controllers/submission.js
@@ -259,22 +259,4 @@ controller('SubmissionFieldCtrl', ['$scope', '$filter', function ($scope, $filte
     return ret;
   };
 
-}]).
-controller('ReceiptController', ['$scope', '$location', 'Authentication', 'WhistleblowerTip',
-  function($scope, $location, Authentication, WhistleblowerTip) {
-    var format_keycode = function(keycode) {
-      var ret = keycode;
-      if (keycode && keycode.length === 16) {
-        ret =  keycode.substr(0, 4) + ' ' +
-               keycode.substr(4, 4) + ' ' +
-               keycode.substr(8, 4) + ' ' +
-               keycode.substr(12, 4);
-      }
-
-      return ret;
-
-    };
-
-    $scope.keycode = format_keycode(Authentication.keycode);
-    $scope.formatted_keycode = format_keycode($scope.keycode);
 }]);
