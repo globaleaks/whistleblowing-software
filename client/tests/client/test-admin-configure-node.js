3369cc2c47610c7b6dc9f80ce9ccc440580900de
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-09-10 00:41:37 +0200

    Update protractor tests in relation to latest developement

--- client/tests/client/test-admin-configure-node.js
+++ client/tests/client/test-admin-configure-node.js
@@ -7,10 +7,6 @@ describe('adming configure node', function() {
     /// simplify the configuration in order to simplfy initial tests
     element(by.model('admin.node.disable_security_awareness_badge')).click();
 
-    // configure email used for exceptions testin
-    element(by.model('admin.node.exception_email')).clear();
-    element(by.model('admin.node.exception_email')).sendKeys('globaleaks-exceptions@mailinator.com');
-
     //enable all receivers to postpone and delete tips
     element(by.model('admin.node.can_postpone_expiration')).click();
     element(by.model('admin.node.can_delete_submission')).click();
@@ -22,7 +18,18 @@ describe('adming configure node', function() {
 
     // save settings
     element(by.css('[data-ng-click="updateNode(admin.node)"]')).click().then(function() {
-      deferred.fulfill();
+      browser.setLocation('/admin/mail');
+
+      element(by.cssContainingText("a", "Exception Notification")).click();
+
+      // configure email used for exceptions testin
+      element(by.model('admin.notification.exception_email_address')).clear();
+      element(by.model('admin.notification.exception_email_address')).sendKeys('globaleaks-exceptions@mailinator.com');
+
+      element(by.css('[data-ng-click="update(admin.notification)"]')).click().then(function() {
+        deferred.fulfill();
+      });
+
     });
 
     return deferred;
