1ec361e945d3f5cfc3e082f7af32873a5791a33d
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-09-17 00:14:05 +0200

    Add missing file client/app/scripts/crypto/proof-of-work.worker.js

---
+++ client/app/scripts/crypto/proof-of-work.worker.js
@@ -0,0 +1,31 @@
+importScripts('openpgp.worker.min.js');
+
+sha256 = window.openpgp.crypto.hash.sha256;
+
+var isAGoodPOW = function(binaryhash) {
+  if (binaryhash.charCodeAt(31) == 0) {
+    // Note: one ZERO check here, means TWO in the backend
+    verification = "";
+    for (k = 0; k < 32; k++) {
+      verification = (verification + binaryhash.charCodeAt(k).toString(16));
+    }
+    return true;
+  }
+  return false;
+}
+
+var iterateOverSHA = function(seed) {
+  for (i = 0; i < 1024; i++) {
+    var x = sha256(seed + i);
+
+    if (isAGoodPOW(x)) {
+      postMessage(i);
+    }
+  }
+  postMessage("12345");
+}
+
+
+onmessage = function(e) {
+  iterateOverSHA(e.data.pow);
+}
