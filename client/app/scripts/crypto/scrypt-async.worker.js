26f1b0dded91d1588e0289cc17108cf5cd6fce2b
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-09-17 09:22:05 +0200

    Integrate openpgp.js and scrypt-async libraries

---
+++ client/app/scripts/crypto/scrypt-async.worker.js
@@ -0,0 +1,19 @@
+importScripts("scrypt-async.min.js");
+
+onmessage = function(e) {
+  options = e.data;
+
+  return new Promise(function(resolve, reject) {
+    var callback = function(result) {
+      postMessage(result);
+    }
+
+    scrypt(options.password,
+           options.salt,
+           options.logN,
+           options.r,
+           options.dkLen,
+           callback,
+           options.encoding);
+  });
+}
