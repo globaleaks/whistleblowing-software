701e79398382e5c78efc2647a1040cb2583bbe4c
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-09-21 12:46:50 +0200

    Fix few typos inside unit tests

--- backend/globaleaks/tests/handlers/test_wbtip.py
+++ backend/globaleaks/tests/handlers/test_wbtip.py
@@ -18,7 +18,7 @@ class TestWBTipInstance(helpers.TestHandlerWithPopulatedDB):
     def test_get(self):
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'])
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'])
 
             yield handler.get()
 
@@ -34,7 +34,7 @@ class TestWBTipCommentCollection(helpers.TestHandlerWithPopulatedDB):
     def test_get(self):
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'])
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'])
 
             yield handler.get()
 
@@ -46,7 +46,7 @@ class TestWBTipCommentCollection(helpers.TestHandlerWithPopulatedDB):
 
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'], body=json.dumps(body))
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'], body=json.dumps(body))
 
             yield handler.post()
 
@@ -62,7 +62,7 @@ class TestWBTipMessageCollection(helpers.TestHandlerWithPopulatedDB):
     def test_get(self):
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'])
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'])
 
             for rcvr_id in wbtip_desc['wbtip_receivers']:
                 yield handler.get(rcvr_id)
@@ -75,7 +75,7 @@ class TestWBTipMessageCollection(helpers.TestHandlerWithPopulatedDB):
 
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'], body=json.dumps(body))
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'], body=json.dumps(body))
 
             for rcvr_id in wbtip_desc['wbtip_receivers']:
                 yield handler.post(rcvr_id)
@@ -92,6 +92,6 @@ class TestWBTipReceiversCollection(helpers.TestHandlerWithPopulatedDB):
     def test_get(self):
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
-            handler = self.request(role='wb', user_id = wbtip_desc['wbtip_id'])
+            handler = self.request(role='whistleblower', user_id = wbtip_desc['wbtip_id'])
 
             yield handler.get()
