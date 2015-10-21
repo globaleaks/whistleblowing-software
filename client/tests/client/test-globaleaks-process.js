854ed4ab53aecf1728a73d86e1516bcaf301cc8c
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-10-21 19:05:14 +0200

    Improve solution of issue https://github.com/globaleaks/GlobaLeaks/issues/1449

--- client/tests/client/test-globaleaks-process.js
+++ client/tests/client/test-globaleaks-process.js
@@ -49,14 +49,14 @@ describe('globaLeaks process', function() {
 
     element(by.id('step-0')).element(by.id('receiver-0')).click().then(function () {
       element(by.id('NextStepButton')).click().then(function () {
-        element(by.id('step-1')).element(by.id('field-0-input-0')).sendKeys(tip_text).then(function () {
+        element(by.id('step-1')).element(by.id('step-1-field-0-0-input-0')).sendKeys(tip_text).then(function () {
 
           // Currently the saucelabs file test seems to work only on linux
           if (browser.testFileUpload()) {
             browser.executeScript('angular.element(document.querySelector(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
-            element(by.id('step-1')).element(by.id('field-3')).element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
+            element(by.id('step-1')).element(by.id('step-1-field-3-0')).element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
               element(by.id('NextStepButton')).click().then(function () {
-                element(by.id('step-2')).element(by.id('field-0-input-0')).click().then(function () {
+                element(by.id('step-2')).element(by.id('step-2-field-0-0-input-0')).click().then(function () {
                   element(by.id('SubmitButton')).click().then(function() {
                     expect(browser.getLocationAbsUrl()).toContain('/receipt');
                     element(by.id('KeyCode')).getText().then(function (txt) {
@@ -69,7 +69,7 @@ describe('globaLeaks process', function() {
             });
           } else {
             element(by.id('NextStepButton')).click().then(function () {
-              element(by.id('step-2')).element(by.id('field-0-input-0')).click().then(function () {
+              element(by.id('step-2')).element(by.id('step-2-field-0-0-input-0')).click().then(function () {
                 var submit_button = element(by.id('SubmitButton'));
                 var isClickable = protractor.ExpectedConditions.elementToBeClickable(submit_button);
                 browser.wait(isClickable);
