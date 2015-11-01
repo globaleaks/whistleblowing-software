53a485d1454401b88d41b067b302ac266a5bc9f9
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-01 15:05:05 +0100

    Test each saucelabs capability in a different CI run to parallelize activities (take three)

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -9,7 +9,7 @@ if [ "$GLTEST" = "unit" ]; then
   coverage run setup.py test
   coveralls || true
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis
-  sleep 5
+  sleep 3
   $TRAVIS_BUILD_DIR/client/node_modules/mocha/bin/mocha -R list $TRAVIS_BUILD_DIR/client/tests/api/test_00* --timeout 30000
 
 elif [ "$GLTEST" = "browserchecks" ]; then
@@ -29,7 +29,6 @@ elif [[ $GLTEST =~ ^end2end-.* ]]; then
     "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"microsoftedge\", \"version\":\"20.10240\", \"platform\":\"Windows 10\"}'"
     "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"android\", \"version\": \"5.1\", \"deviceName\": \"Android Emulator\", \"platform\": \"Linux\", \"platform\":\"OS X 10.10\"}'"
     "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\": \"iphone\", \"version\": \"8.2\", \"deviceName\": \"iPhone Simulator\", \"device-orientation\": \"portrait\", \"platform\":\"OS X 10.10\" }'"
-
   )
 
   index=$(echo $GLTEST | cut -f2 -d-)
@@ -39,7 +38,8 @@ elif [[ $GLTEST =~ ^end2end-.* ]]; then
 
   echo "Testing Configuration: $capability"
   eval $capability
-  $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis -c -k9 --port 9000
+  $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis --port 9000
   sleep 3
+  grunt protractor:saucelabs
 
 fi
