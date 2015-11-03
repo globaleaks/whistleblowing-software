b5a4a537a1ca0c2ae8328b8ebeee930ea82ff0c1
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-03 16:53:22 +0100

    Change default context.show_receivers to false in order to simplify initial user experience

--- client/app/scripts/services.js
+++ client/app/scripts/services.js
@@ -610,7 +610,7 @@ angular.module('GLServices', ['ngResource']).
           context.presentation_order = 0;
           context.tip_timetolive = 15;
           context.show_context = true;
-          context.show_receivers = true;
+          context.show_receivers = false;
           context.show_receivers_in_alphabetical_order = true;
           context.select_all_receivers = false;
           context.maximum_selectable_receivers = 0;
