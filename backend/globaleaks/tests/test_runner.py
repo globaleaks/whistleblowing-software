583d4c3dda4d489a37bace1060681101d5c4f70e
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-07 10:49:36 +0100

    Humanize message shown in mail in case hidden service / tor2web server not configured

---
+++ backend/globaleaks/tests/test_runner.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from globaleaks.tests import helpers
+
+from globaleaks import runner
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.scripts._twistd_unix import ServerOptions
+
+class TestRunner(helpers.TestGL):
+    @inlineCallbacks
+    def test_runner(self):
+        # TODO: currently this test is mainly fake and is intended at list to
+        #       spot errors like typos etcetera by raising the code coverage
+
+        config = ServerOptions()
+        #runner.GLBaseRunner(config).run()
+        #return
+
+        globaleaks_runner = runner.GLBaseRunner(config)
+        #a = yield globaleaks_runner.start_globaleaks()
+        #print a
+
+        #globaleaks_runner.start_asynchronous_jobs()
+        yield globaleaks_runner.start_globaleaks()
