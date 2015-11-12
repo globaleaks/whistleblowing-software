263e9e65fc102042c3933ddfd19e304afdb8d834
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-12 08:47:42 +0100

    Fix end2end test receiver creation by reimplementing it as user creation

--- client/tests/end2end/test-admin-configure-receivers.js
+++ client/tests/end2end/test-admin-configure-receivers.js
@@ -1,12 +1,13 @@
 describe('admin add receivers', function() {
   it('should add new receivers', function() {
-    browser.setLocation('admin/receivers');
+    browser.setLocation('admin/users');
 
     var add_receiver = function(username, name, address) {
-      element(by.model('new_receiver.username')).sendKeys(username);
-      element(by.model('new_receiver.name')).sendKeys(name);
-      element(by.model('new_receiver.email')).sendKeys(address);
-      return element(by.css('[data-ng-click="add_receiver()"]')).click();
+      element(by.model('new_user.username')).sendKeys(username);
+      element(by.model('new_user.name')).sendKeys(name);
+      element(by.model('new_user.email')).sendKeys(address);
+      element(by.model('new_user.role')).sendKeys('custodian');
+      return element(by.css('[data-ng-click="add_user()"]')).click();
     };
 
     add_receiver("receiver2", "Recipient 2", "globaleaks-receiver2@mailinator.com").then(function() {
