7778ad0ecb24531ac7f88a368728b34a24417056
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-16 11:02:43 +0100

    Improve detection of compatible browser using capabilities assesment [#137]

--- client/app/scripts/browserchecks.js
+++ client/app/scripts/browserchecks.js
@@ -1,3 +1,30 @@
 var isBrowserCompatible = function() {
-  return !(bowser.msie && bowser.version <= 8);
+  if (typeof window === 'undefined') {
+    return false;
+  }
+
+ // Checks related to FileAPI compatibility
+ if(typeof File === 'undefined' ||
+    typeof Blob === 'undefined' ||
+    typeof FileList === 'undefined' ||
+    (!Blob.prototype.slice && !Blob.prototype.webkitSlice && !Blob.prototype.mozSlice)){
+
+    console.log("GlobaLeaks startup failure: missing requirements for Flow.js");
+    return false;
+  }
+
+  // Checks related to Webworker compatibility
+  if (typeof window.Worker === 'undefined') {
+    console.log("GlobaLeaks startup failure: missing Webworker support");
+    return false;
+  }
+
+  // Check related to WebCrypto compatibility
+  if (!(window.crypto && window.crypto.getRandomValues) &&
+      !(typeof window.msCrypto === 'object' && typeof window.msCrypto.getRandomValues === 'function')) {
+    console.log("GlobaLeaks startup failure: missing End2End encryption requirements");
+    return false;
+  }
+
+  return true;
 };
