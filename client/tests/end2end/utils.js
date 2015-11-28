36a47b806c1ab2002abba41d2c04051ff25ac082
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-11-28 12:23:03 +0100

    Disable proof of work mechanism during browser-testing if the browser does not support workers

--- client/tests/end2end/utils.js
+++ client/tests/end2end/utils.js
@@ -7,3 +7,15 @@ exports.waitUntilReady = function (elm, timeout) {
       return elm.isDisplayed();
    }, timeout);
 };
+
+browser.getCapabilities().then(function(s) {
+  exports.testFileUpload = function() {
+    return (s.caps_.platform === 'LINUX');
+  };
+
+  exports.isOldIE = function() {
+    browserName = s.caps_.browserName;
+    browserVersion = s.caps_.version;
+    return (browserName == 'internet explorer' && browserVersion < 11);
+  };
+});
