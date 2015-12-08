741522a2dc902201f7e8875d7e205fd1df464ced
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-12-08 12:24:15 +0100

    Update packer templates for precise/trusty/xenial/wheezy/jessie/stretch

--- appliance/scripts/globaleaks.sh
+++ appliance/scripts/globaleaks.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
 set -e
 
 # The following script emulates the installation guide:
 # <https://github.com/globaleaks/GlobaLeaks/wiki/Installation-guide>
 
-# install globaleaks
-dpkg -i globaleaks*.deb || true
-apt-get install -fy
+wget https://deb.globaleaks.org/install-globaleaks.sh
+chmod +x install-globaleaks.sh
+./install-globaleaks.sh
