bb19c35e29d82fbcd26770917220130f3d66978c
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-01-14 23:37:40 +0100

    Add end-to-end test of navigation thrrough the admin sections

--- client/tests/end2end/test-admin-configure-node.js
+++ client/tests/end2end/test-admin-configure-node.js
@@ -37,3 +37,43 @@ describe('adming configure node', function() {
     });
   });
 });
+
+describe('verify navigation of admin sections', function() {
+  // Even if not performing real checks this test at least verify to be able to perform the
+  // navigation of the admin section without triggering any exception
+
+  it('should should navigate through admin sections', function() {
+    element(by.cssContainingText("a", "General settings")).click().then(function() {
+      element(by.cssContainingText("li div", "Main configuration")).click();
+      element(by.cssContainingText("li div", "Theme customization")).click();
+      element(by.cssContainingText("li div", "Translation customization")).click();
+    });
+
+    element(by.cssContainingText("a", "User management")).click();
+    element(by.cssContainingText("a", "Recipient configuration")).click();
+    element(by.cssContainingText("a", "Context configuration")).click();
+    element(by.cssContainingText("a", "Questionnaire configuration")).click();
+
+    element(by.cssContainingText("a", "Notification settings")).click().then(function() {
+      element(by.cssContainingText("li div", "Main configuration")).click();
+      element(by.cssContainingText("li div", "Admin notification templates")).click();
+      element(by.cssContainingText("li div", "Recipient notification templates")).click();
+      element(by.cssContainingText("li div", "Exception notification")).click();
+    });
+
+    element(by.cssContainingText("a", "URL shortener")).click();
+
+    element(by.cssContainingText("a", "Advanced settings")).click().then(function() {
+      element(by.cssContainingText("li div", "Main configuration")).click();
+      element(by.cssContainingText("li div", "HTTPS settings")).click();
+      element(by.cssContainingText("li div", "Anomaly detection thresholds")).click();
+    });
+
+    element(by.cssContainingText("a", "Recent activities")).click();
+    element(by.cssContainingText("a", "System stats")).click();
+    element(by.cssContainingText("a", "Anomalies")).click();
+    element(by.cssContainingText("a", "User overview")).click();
+    element(by.cssContainingText("a", "Submission overview")).click();
+    element(by.cssContainingText("a", "File overview")).click();
+  });
+});
