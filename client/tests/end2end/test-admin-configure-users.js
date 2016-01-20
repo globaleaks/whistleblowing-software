7680f143f62bf67d08d91292560e1819e775daa0
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-01-20 10:48:26 +0100

    Correctly add protractor.promises to webdriver.controlFlow solving race conditions on unit tests
    Reference: https://stackoverflow.com/questions/26970684/understanding-protractors-use-of-promises

--- client/tests/end2end/test-admin-configure-users.js
+++ client/tests/end2end/test-admin-configure-users.js
@@ -5,22 +5,22 @@ describe('admin add users', function() {
     browser.setLocation('admin/users');
 
     var add_user = function(username, role, roleSelector, name, address) {
-      var deferred = protractor.promise.defer();
-      element(by.model('new_user.username')).sendKeys(username);
-      element(by.model('new_user.name')).sendKeys(name);
-      element(by.model('new_user.email')).sendKeys(address);
-      element(by.model('new_user.role')).element(by.xpath(".//*[text()='" + roleSelector + "']")).click();
-      element(by.css('[data-ng-click="add_user()"]')).click().then(function() {
-         utils.waitUntilReady(element(by.xpath(".//*[text()='" + name + "']")));
-         deferred.fulfill();
+      return protractor.promise.controlFlow().execute(function() {
+        var deferred = protractor.promise.defer();
+        element(by.model('new_user.username')).sendKeys(username);
+        element(by.model('new_user.name')).sendKeys(name);
+        element(by.model('new_user.email')).sendKeys(address);
+        element(by.model('new_user.role')).element(by.xpath(".//*[text()='" + roleSelector + "']")).click();
+        element(by.css('[data-ng-click="add_user()"]')).click().then(function() {
+           utils.waitUntilReady(element(by.xpath(".//*[text()='" + name + "']")));
+           deferred.fulfill();
+        });
+        return deferred.promise;
       });
-      return deferred.promise;
     };
 
-    add_user("receiver2", "receiver", "Recipient", "Recipient 2", "globaleaks-receiver2@mailinator.com").then(function() {
-      add_user("receiver3", "receiver", "Recipient", "Recipient 3", "globaleaks-receiver3@mailinator.com").then(function() {
-        add_user("custodian1", "custodian", "Custodian", "Custodian 1", "globaleaks-custodian1@mailinator.com");
-      });
-    });
+    add_user("receiver2", "receiver", "Recipient", "Recipient 2", "globaleaks-receiver2@mailinator.com");
+    add_user("receiver3", "receiver", "Recipient", "Recipient 3", "globaleaks-receiver3@mailinator.com");
+    add_user("custodian1", "custodian", "Custodian", "Custodian 1", "globaleaks-custodian1@mailinator.com");
   });
 });
--- client/tests/end2end/test-globaleaks-process.js
+++ client/tests/end2end/test-globaleaks-process.js
@@ -9,85 +9,91 @@ describe('globaLeaks process', function() {
   var receiver_password = "ACollectionOfDiplomaticHistorySince_1966_ToThe_Pr esentDay#"
 
   var login_whistleblower = function(receipt) {
-    var deferred = protractor.promise.defer();
+    return protractor.promise.controlFlow().execute(function() {
+      var deferred = protractor.promise.defer();
 
-    browser.get('/#/');
-    element(by.model('formatted_keycode')).sendKeys(receipt).then(function() {
-      element(by.css('[data-ng-click="view_tip(formatted_keycode)"]')).click().then(function() {
-        utils.waitForUrl('/status');
-        deferred.fulfill();
-      });
-    });
-
-    return deferred.promise;
-  }
-
-  var login_receiver = function(username, password) {
-    var deferred = protractor.promise.defer();
-
-    browser.get('/#/login');
-    element(by.model('loginUsername')).element(by.xpath(".//*[text()='" + username + "']")).click().then(function() {
-      element(by.model('loginPassword')).sendKeys(password).then(function() {
-        element(by.xpath('//button[contains(., "Log in")]')).click().then(function() {
-          utils.waitForUrl('/receiver/tips');
+      browser.get('/#/');
+      element(by.model('formatted_keycode')).sendKeys(receipt).then(function() {
+        element(by.css('[data-ng-click="view_tip(formatted_keycode)"]')).click().then(function() {
+          utils.waitForUrl('/status');
           deferred.fulfill();
         });
       });
-    });
 
-    return deferred.promise;
+      return deferred.promise;
+    });
+  }
+
+  var login_receiver = function(username, password) {
+    return protractor.promise.controlFlow().execute(function() {
+      var deferred = protractor.promise.defer();
+
+      browser.get('/#/login');
+      element(by.model('loginUsername')).element(by.xpath(".//*[text()='" + username + "']")).click().then(function() {
+        element(by.model('loginPassword')).sendKeys(password).then(function() {
+          element(by.xpath('//button[contains(., "Log in")]')).click().then(function() {
+            utils.waitForUrl('/receiver/tips');
+            deferred.fulfill();
+          });
+        });
+      });
+
+      return deferred.promise;
+    });
   }
 
   var perform_submission = function() {
-    var deferred = protractor.promise.defer();
+    return protractor.promise.controlFlow().execute(function() {
+      var deferred = protractor.promise.defer();
 
-    browser.get('/#/submission');
+      browser.get('/#/submission');
 
-    element(by.id('step-0')).element(by.id('receiver-0')).click().then(function () {
-      element(by.id('NextStepButton')).click().then(function () {
-        element(by.id('step-1')).element(by.id('step-1-field-0-0-input-0')).sendKeys(tip_text).then(function () {
+      element(by.id('step-0')).element(by.id('receiver-0')).click().then(function () {
+        element(by.id('NextStepButton')).click().then(function () {
+          element(by.id('step-1')).element(by.id('step-1-field-0-0-input-0')).sendKeys(tip_text).then(function () {
 
-          // Currently the saucelabs file test seems to work only on linux
-          if (utils.testFileUpload()) {
-            browser.executeScript('angular.element(document.querySelector(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
-            element(by.id('step-1')).element(by.id('step-1-field-3-0')).element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
+            // Currently the saucelabs file test seems to work only on linux
+            if (utils.testFileUpload()) {
+              browser.executeScript('angular.element(document.querySelector(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
+              element(by.id('step-1')).element(by.id('step-1-field-3-0')).element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
+                element(by.id('NextStepButton')).click().then(function () {
+                  element(by.id('step-2')).element(by.id('step-2-field-0-0-input-0')).click().then(function () {
+                    var submit_button = element(by.id('SubmitButton'));
+                    var isClickable = protractor.ExpectedConditions.elementToBeClickable(submit_button);
+                    browser.wait(isClickable);
+                    submit_button.click().then(function() {
+                      utils.waitForUrl('/receipt');
+                      element(by.id('KeyCode')).getText().then(function (txt) {
+                        receipts.unshift(txt); 
+                        deferred.fulfill();
+                      });
+                    });
+                  });
+                });
+              });
+            } else {
               element(by.id('NextStepButton')).click().then(function () {
                 element(by.id('step-2')).element(by.id('step-2-field-0-0-input-0')).click().then(function () {
                   var submit_button = element(by.id('SubmitButton'));
                   var isClickable = protractor.ExpectedConditions.elementToBeClickable(submit_button);
                   browser.wait(isClickable);
                   submit_button.click().then(function() {
-                    utils.waitForUrl('/receipt');
+                  utils.waitForUrl('/receipt');
                     element(by.id('KeyCode')).getText().then(function (txt) {
                       receipts.unshift(txt);
                       deferred.fulfill();
                     });
                   });
                 });
-               });
-            });
-          } else {
-            element(by.id('NextStepButton')).click().then(function () {
-              element(by.id('step-2')).element(by.id('step-2-field-0-0-input-0')).click().then(function () {
-                var submit_button = element(by.id('SubmitButton'));
-                var isClickable = protractor.ExpectedConditions.elementToBeClickable(submit_button);
-                browser.wait(isClickable);
-                submit_button.click().then(function() {
-                  utils.waitForUrl('/receipt');
-                  element(by.id('KeyCode')).getText().then(function (txt) {
-                    receipts.unshift(txt);
-                    deferred.fulfill();
-                  });
-                });
-               });
-            });
-          }
+              });
+            }
 
+          });
         });
       });
-    });
 
-    return deferred.promise;
+      return deferred.promise;
+    });
   }
 
   it('should redirect to /submission by clicking on the blow the whistle button', function() {
@@ -135,23 +141,21 @@ describe('globaLeaks process', function() {
   });
 
   it('Recipient should be able to access the first submission', function() {
-    login_receiver(receiver_username, receiver_password).then(function () {
-      element(by.id('tip-0')).click().then(function() {
-        expect(element(by.xpath("//*[contains(text(),'" + tip_text + "')]")).getText()).toEqual(tip_text);
-      });
+    login_receiver(receiver_username, receiver_password);
+    element(by.id('tip-0')).click().then(function() {
+      expect(element(by.xpath("//*[contains(text(),'" + tip_text + "')]")).getText()).toEqual(tip_text);
     });
   });
 
   it('Recipient should be able to leave a comment to the whistleblower', function() {
-    login_receiver(receiver_username, receiver_password).then(function () {
-      element(by.id('tip-0')).click().then(function() {
-        element(by.model('tip.newCommentContent')).sendKeys(comment);
-        element(by.id('comment-action-send')).click().then(function() {
-          element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
-            expect(c).toContain(comment);
-            element(by.id('LogoutLink')).click().then(function() {
-              utils.waitForUrl('/login');
-            });
+    login_receiver(receiver_username, receiver_password);
+    element(by.id('tip-0')).click().then(function() {
+      element(by.model('tip.newCommentContent')).sendKeys(comment);
+      element(by.id('comment-action-send')).click().then(function() {
+        element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
+          expect(c).toContain(comment);
+          element(by.id('LogoutLink')).click().then(function() {
+            utils.waitForUrl('/login');
           });
         });
       });
@@ -159,42 +163,39 @@ describe('globaLeaks process', function() {
   });
 
   it('Whistleblower should be able to read the comment from the receiver and reply', function() {
-    login_whistleblower(receipts[0]).then(function () {
-      element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
-        expect(c).toEqual(comment);
-        element(by.model('tip.newCommentContent')).sendKeys(comment_reply);
-        element(by.id('comment-action-send')).click().then(function() {
-          element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
-            expect(c).toContain(comment_reply);
-          });
+    login_whistleblower(receipts[0]);
+    element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
+      expect(c).toEqual(comment);
+      element(by.model('tip.newCommentContent')).sendKeys(comment_reply);
+      element(by.id('comment-action-send')).click().then(function() {
+        element(by.id('comment-0')).element(by.css('.preformatted')).getText().then(function(c) {
+          expect(c).toContain(comment_reply);
         });
       });
     });
   });
 
   it('Whistleblower should be able to attach a new file to the first submission', function() {
-    login_whistleblower(receipts[0]).then(function () {
-      if (utils.testFileUpload()) {
-        browser.executeScript('angular.element(document.querySelector(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
-        element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
-          // TODO: test file addition
-          element(by.id('LogoutLink')).click().then(function() {
-            utils.waitForUrl('/');
-          });
+    login_whistleblower(receipts[0]);
+    if (utils.testFileUpload()) {
+      browser.executeScript('angular.element(document.querySelector(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
+      element(by.xpath("//input[@type='file']")).sendKeys(__filename).then(function() {
+        // TODO: test file addition
+        element(by.id('LogoutLink')).click().then(function() {
+          utils.waitForUrl('/');
         });
-      }
-    });
+      });
+    }
   });
 
   it('Recipient should be able to postpone first submission from tip page', function() {
-    login_receiver(receiver_username, receiver_password).then(function () {
-      element(by.id('tip-0')).click().then(function() {
-        element(by.id('tip-action-postpone')).click().then(function () {
-          element(by.id('modal-action-ok')).click().then(function() {
-            //TODO: check postpone
-            element(by.id('LogoutLink')).click().then(function() {
-              utils.waitForUrl('/login');
-            });
+    login_receiver(receiver_username, receiver_password);
+    element(by.id('tip-0')).click().then(function() {
+      element(by.id('tip-action-postpone')).click().then(function () {
+        element(by.id('modal-action-ok')).click().then(function() {
+          //TODO: check postpone
+          element(by.id('LogoutLink')).click().then(function() {
+            utils.waitForUrl('/login');
           });
         });
       });
@@ -202,15 +203,14 @@ describe('globaLeaks process', function() {
   });
 
   it('Recipient should be able to delete first submission from tip page', function() {
-    login_receiver(receiver_username, receiver_password).then(function () {
-      element(by.id('tip-0')).click().then(function() {
-        element(by.id('tip-action-delete')).click().then(function () {
-          element(by.id('modal-action-ok')).click().then(function() {
-            utils.waitForUrl('/receiver/tips');
-            //TODO: check delete
-            element(by.id('LogoutLink')).click().then(function() {
-              utils.waitForUrl('/login');
-            });
+    login_receiver(receiver_username, receiver_password);
+    element(by.id('tip-0')).click().then(function() {
+      element(by.id('tip-action-delete')).click().then(function () {
+        element(by.id('modal-action-ok')).click().then(function() {
+          utils.waitForUrl('/receiver/tips');
+          //TODO: check delete
+          element(by.id('LogoutLink')).click().then(function() {
+            utils.waitForUrl('/login');
           });
         });
       });
@@ -218,15 +218,14 @@ describe('globaLeaks process', function() {
   });
 
   it('Recipient should be able to postpone all tips', function() {
-    login_receiver(receiver_username, receiver_password).then(function () {
-      element(by.id('tip-action-select-all')).click().then(function() {
-        element(by.id('tip-action-postpone-selected')).click().then(function () {
-          element(by.id('modal-action-ok')).click().then(function() {
-            utils.waitForUrl('/receiver/tips');
-            //TODO: check postpone
-            element(by.id('LogoutLink')).click().then(function() {
-              utils.waitForUrl('/login');
-            });
+    login_receiver(receiver_username, receiver_password);
+    element(by.id('tip-action-select-all')).click().then(function() {
+      element(by.id('tip-action-postpone-selected')).click().then(function () {
+        element(by.id('modal-action-ok')).click().then(function() {
+          utils.waitForUrl('/receiver/tips');
+          //TODO: check postpone
+          element(by.id('LogoutLink')).click().then(function() {
+            utils.waitForUrl('/login');
           });
         });
       });
