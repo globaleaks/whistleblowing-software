d43ea528d6ded7428b707d328978f4deeba7839c
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-01-20 11:51:04 +0100

    Revert commit f63152f51b91185f76c6f795f4b7c1412e3f7834 given upstream revert of angular-ui-bootstrap
    This reverts commit f63152f51b91185f76c6f795f4b7c1412e3f7834.
    
    Reference: https://github.com/angular-ui/bootstrap/commit/907c851fd0e904c17d969583cbd16460ee9907b8

--- client/tests/end2end/test-admin-configure-node.js
+++ client/tests/end2end/test-admin-configure-node.js
@@ -21,14 +21,14 @@ describe('adming configure node', function() {
     element(by.model('admin.node.enable_experimental_features')).click();
 
     // grant tor2web permissions
-    element(by.cssContainingText("li div", "HTTPS settings")).click();
+    element(by.cssContainingText("a", "HTTPS settings")).click();
     element(by.model('admin.node.tor2web_whistleblower')).click();
 
     // save settings
     element(by.css('[data-ng-click="updateNode(admin.node)"]')).click().then(function() {
       browser.setLocation('/admin/mail');
 
-      element(by.cssContainingText("li div", "Exception notification")).click();
+      element(by.cssContainingText("a", "Exception notification")).click();
 
       // configure email used for exceptions testin
       element(by.model('admin.notification.exception_email_address')).clear();
@@ -47,9 +47,9 @@ describe('verify navigation of admin sections', function() {
 
   it('should should navigate through admin sections', function() {
     element(by.cssContainingText("a", "General settings")).click().then(function() {
-      element(by.cssContainingText("li div", "Main configuration")).click();
-      element(by.cssContainingText("li div", "Theme customization")).click();
-      element(by.cssContainingText("li div", "Translation customization")).click();
+      element(by.cssContainingText("a", "Main configuration")).click();
+      element(by.cssContainingText("a", "Theme customization")).click();
+      element(by.cssContainingText("a", "Translation customization")).click();
     });
 
     element(by.cssContainingText("a", "User management")).click();
@@ -58,18 +58,18 @@ describe('verify navigation of admin sections', function() {
     element(by.cssContainingText("a", "Questionnaire configuration")).click();
 
     element(by.cssContainingText("a", "Notification settings")).click().then(function() {
-      element(by.cssContainingText("li div", "Main configuration")).click();
-      element(by.cssContainingText("li div", "Admin notification templates")).click();
-      element(by.cssContainingText("li div", "Recipient notification templates")).click();
-      element(by.cssContainingText("li div", "Exception notification")).click();
+      element(by.cssContainingText("a", "Main configuration")).click();
+      element(by.cssContainingText("a", "Admin notification templates")).click();
+      element(by.cssContainingText("a", "Recipient notification templates")).click();
+      element(by.cssContainingText("a", "Exception notification")).click();
     });
 
     element(by.cssContainingText("a", "URL shortener")).click();
 
     element(by.cssContainingText("a", "Advanced settings")).click().then(function() {
-      element(by.cssContainingText("li div", "Main configuration")).click();
-      element(by.cssContainingText("li div", "HTTPS settings")).click();
-      element(by.cssContainingText("li div", "Anomaly detection thresholds")).click();
+      element(by.cssContainingText("a", "Main configuration")).click();
+      element(by.cssContainingText("a", "HTTPS settings")).click();
+      element(by.cssContainingText("a", "Anomaly detection thresholds")).click();
     });
 
     element(by.cssContainingText("a", "Recent activities")).click();
