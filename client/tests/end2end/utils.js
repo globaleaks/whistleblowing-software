257fe053d301038983d138c8f09acf781f5f2071
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-01-31 02:57:57 +0100

    Fine configure browsers on which test the file uploader

--- client/tests/end2end/utils.js
+++ client/tests/end2end/utils.js
@@ -11,26 +11,19 @@ exports.waitUntilReady = function (elm, timeout) {
 
 browser.getCapabilities().then(function(s) {
   exports.testFileUpload = function() {
-    // Test on file upload is currently performed in all browsers;
-    return true;
+    var browserName = s.caps_.browserName.toLowerCase();
+    return (['chrome', 'firefox', 'internet explorer', 'edge'].indexOf(browserName) !== -1);
   };
 
   exports.testFileDownload = function() {
-    // Test on file download is currently performed only in chrome and safary;
-    // the reason is that except this these, the other browsers asks user interaction;
-    var browserName = s.caps_.browserName.toLowerCase();
-    return (['chrome', 'safari'].indexOf(browserName) !== -1);
+    // Test on file upload is currently performed in all browsers;
+    return true;
   };
 
   exports.isOldIE = function() {
     var browserName = s.caps_.browserName.toLowerCase();
     var browserVersion = s.caps_.version;
     return (browserName == 'internet explorer' && browserVersion < 11);
-  };
-
-  exports.isChrome = function() {
-    var browserName = s.caps_.browserName.toLowerCase();
-    return browserName == 'chrome';
   };
 });
 
