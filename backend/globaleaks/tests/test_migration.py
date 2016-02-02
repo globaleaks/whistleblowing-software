263c54dbdc1fbb0c05679e41e251d7f81e97c94b
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-02-02 23:01:59 +0100

    Make every db migration test run indipendently

--- backend/globaleaks/tests/test_migration.py
+++ backend/globaleaks/tests/test_migration.py
@@ -11,28 +11,28 @@ import shutil
 
 from twisted.trial import unittest
 
+from globaleaks import DATABASE_VERSION, FIRST_DATABASE_VERSION_SUPPORTED
+
 from globaleaks.db import check_db_files
 from globaleaks.settings import GLSettings
 from globaleaks.tests.helpers import init_glsettings_for_unit_tests
 
 
 class TestMigrationRoutines(unittest.TestCase):
-    def _test_dbs_migration(self, directory):
+    def _test(self, path, f):
+        init_glsettings_for_unit_tests()
         GLSettings.db_path = os.path.join(GLSettings.ramdisk_path, 'db_test')
-        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), directory)
-        for (path, dirs, files) in os.walk(path):
-            for f in files:
-                os.mkdir(GLSettings.db_path)
-                dbpath = os.path.join(path, f)
-                shutil.copyfile(dbpath, ('%s/%s' % (GLSettings.db_path, f)))
-                ret = check_db_files()
-                shutil.rmtree(GLSettings.db_path)
-                self.assertNotEqual(ret, -1)
+        os.mkdir(GLSettings.db_path)
+        dbpath = os.path.join(path, f)
+        shutil.copyfile(dbpath, '%s/%s' % (GLSettings.db_path, f))
+        ret = check_db_files()
+        shutil.rmtree(GLSettings.db_path)
+        self.assertNotEqual(ret, -1)
 
-    def test_migration_of_default_dbs(self):
-        init_glsettings_for_unit_tests()
-        self._test_dbs_migration('db/empty')
+def test(path, f):
+    return lambda self: self._test(path, f)
 
-    def test_migration_of_populated_dbs(self):
-        init_glsettings_for_unit_tests()
-        self._test_dbs_migration('db/populated')
+for directory in ['empty', 'populated']:
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db', directory)
+    for i in range(FIRST_DATABASE_VERSION_SUPPORTED, DATABASE_VERSION):
+        setattr(TestMigrationRoutines, "test_%s_db_migration_%d" % (directory, i), test(path, 'glbackend-%d.db' % i))
