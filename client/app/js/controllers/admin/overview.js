4a53507b57d90eb004168c7ea939cc945b0a1196
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-02-15 01:30:45 +0100

    Fix variables resolution in users overview that still show information of only recipients

--- client/app/js/controllers/admin/overview.js
+++ client/app/js/controllers/admin/overview.js
@@ -1,7 +1,7 @@
 GLClient.controller('OverviewCtrl', ['$scope', '$location', 'ReceiverOverview', 'TipOverview', 'FileOverview',
   function($scope, $location, ReceiverOverview, TipOverview, FileOverview) {
 
-      $scope.users = ReceiverOverview.query();
+      $scope.receivers = ReceiverOverview.query();
       $scope.tips = TipOverview.query();
       $scope.files = FileOverview.query();
 
