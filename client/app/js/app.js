fe751747052c9c6f74e523472cfe3d22b20f0d1f
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-02-27 00:07:32 +0100

    Reset angular digestTtl to 30 cause the issue is still happening on some forms also on angular 1.5

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -11,8 +11,8 @@ var GLClient = angular.module('GLClient', [
     'GLDirectives',
     'GLFilters'
   ]).
-  config(['$compileProvider', '$httpProvider', '$routeProvider', '$translateProvider', '$uibTooltipProvider',
-    function($compileProvider, $httpProvider, $routeProvider, $translateProvider, $uibTooltipProvider) {
+  config(['$compileProvider', '$httpProvider', '$routeProvider', '$rootScopeProvider', '$translateProvider', '$uibTooltipProvider',
+    function($compileProvider, $httpProvider, $routeProvider, $rootScopeProvider, $translateProvider, $uibTooltipProvider) {
     $compileProvider.debugInfoEnabled(false);
 
     $httpProvider.interceptors.push('globaleaksRequestInterceptor');
@@ -200,6 +200,10 @@ var GLClient = angular.module('GLClient', [
       otherwise({
         redirectTo: '/'
       });
+
+      // Raise the default digest loop limit to 30 because of the template recursion used by fields:
+      // https://github.com/angular/angular.js/issues/6440
+      $rootScopeProvider.digestTtl(30);
 
       $translateProvider.useStaticFilesLoader({
         prefix: 'l10n/',
