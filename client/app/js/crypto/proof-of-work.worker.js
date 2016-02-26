globaleaks/whistleblowing-software

commit 2f8efe5caea5587098c99eb8d652b3b5ae6203b9

Align proof of work to the api change in openpgpjs

client/app/js/crypto/proof-of-work.worker.js
@@ -1,13 +1,13 @@
 importScripts('openpgp.worker.min.js');
 
-sha256 = window.openpgp.crypto.hash.sha256;
+var sha256 = window.openpgp.crypto.hash.sha256;
 
 var iterateOverSHA = function(seed) {
   var i;
   for (i = 0; i < 1024; i++) {
     var x = sha256(seed + i);
 
-    if (x.charCodeAt(31) == 0) {
+    if (x[31] == 0) {
       postMessage(i);
     }
   }
@@ -15,6 +15,6 @@ var iterateOverSHA = function(seed) {
   postMessage("12345");
 };
 
-onmessage = function(e) {
+var onmessage = function(e) {
   iterateOverSHA(e.data.pow);
 };

commit 1de821e82025f41005aa0dbeacc502f2503085ab

Fix proof of work regression present since 2f8efe5caea5587098c99eb8d652b3b5ae6203b9 and affecting IE

client/app/js/crypto/proof-of-work.worker.js
@@ -15,6 +15,6 @@ var iterateOverSHA = function(seed) {
   postMessage("12345");
 };
 
-var onmessage = function(e) {
+onmessage = function(e) {
   iterateOverSHA(e.data.pow);
 };