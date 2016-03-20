88817e9acc88530f24f474e150da8c34b8f11132
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-03-21 00:32:00 +0100

    Update e2e tests in relation to latest ui changes

--- client/tests/end2end/test-admin-configure-users.js
+++ client/tests/end2end/test-admin-configure-users.js
@@ -4,10 +4,9 @@ describe('admin add users', function() {
   it('should add new users', function(done) {
     browser.setLocation('admin/users');
 
-    var add_user = function(username, role, roleSelector, name, address) {
+    var add_user = function(role, roleSelector, name, address) {
       return protractor.promise.controlFlow().execute(function() {
         var deferred = protractor.promise.defer();
-        element(by.model('new_user.username')).sendKeys(username);
         element(by.model('new_user.name')).sendKeys(name);
         element(by.model('new_user.email')).sendKeys(address);
         element(by.model('new_user.role')).element(by.xpath(".//*[text()='" + roleSelector + "']")).click();
@@ -19,9 +18,9 @@ describe('admin add users', function() {
       });
     };
 
-    add_user("receiver2", "receiver", "Recipient", "Recipient 2", "globaleaks-receiver2@mailinator.com");
-    add_user("receiver3", "receiver", "Recipient", "Recipient 3", "globaleaks-receiver3@mailinator.com");
-    add_user("custodian1", "custodian", "Custodian", "Custodian 1", "globaleaks-custodian1@mailinator.com");
+    add_user("receiver", "Recipient", "Recipient 2", "globaleaks-receiver2@mailinator.com");
+    add_user("receiver", "Recipient", "Recipient 3", "globaleaks-receiver3@mailinator.com");
+    add_user("custodian", "Custodian", "Custodian 1", "globaleaks-custodian1@mailinator.com");
     done();
   });
 });
