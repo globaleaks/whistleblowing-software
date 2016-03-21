96abcd669a4f00885171c4477765bdb8acfb8700
Nick Skelsey <nskelsey@gmail.com>
2016-03-21 11:25:36 -0400

    Table list export button addresses #1580
    Due to the problems with spawning multifile downloads on form submission.
    This commit adds a button for each row in the table. It is not ideal,
    but it works.

--- client/app/js/controllers/receiver.js
+++ client/app/js/controllers/receiver.js
@@ -35,20 +35,6 @@ GLClient.controller('ReceiverTipsCtrl', ['$scope',  '$http', '$route', '$locatio
     return $scope.selected_tips.indexOf(id) !== -1;
   };
 
-  $scope.tip_export_all = function () {
-    var modalInstance = $uibModal.open({
-      templateUrl: 'views/partials/tip_operation_export_selected.html',
-      controller: TipListExportCtrl,
-      resolve: {
-        selected_rtips: function () {
-          return $scope.tips.filter(function(rtip) {
-              return $scope.selected_tips.indexOf(rtip.id) > -1;
-          })
-        }
-      }
-    });
-  };
-
   $scope.tip_delete_all = function () {
     var modalInstance = $uibModal.open({
       templateUrl: 'views/partials/tip_operation_delete_selected.html',
@@ -80,48 +66,8 @@ GLClient.controller('ReceiverTipsCtrl', ['$scope',  '$http', '$route', '$locatio
   };
 }]);
 
-TipListExportCtrl = ['$scope', '$http', '$route', '$location', '$filter', '$uibModalInstance', 'Authentication', 'selected_rtips',
-                        function ($scope, $http, $route, $location, $filter, $uibModalInstance, Authentication, selected_rtips) {
-  // DANGER this is not canonical usage DANGER
-  $scope.session = Authentication.session;
-  // DANGER
-
-  $scope.cancel = function () {
-    $uibModalInstance.close();
-  };
-
-  $scope.tips = selected_rtips;
-
-  $scope.export_urls = selected_rtips.map(function(tip) {
-    return "/rtip/" + tip.id + "/export";
-  });
-
-  $scope.ok = function () {
-    $uibModalInstance.close();
-    var forms = angular.element("#ExportFormContainer form button")
-    for (var i = 0; i < $scope.export_urls.length; i++) {
-       console.log("triggering click");
-       var b = angular.element(forms[i]).triggerHandler('click');
-    }
-
-   /*// Chain the series of http post's together
-     $http({
-          method: 'POST', url: url,
-          data: {'session': $scope.session.id},
-          headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  
-      }).then(function(data) {
-        console.log("Success!", url, data);
-      }, function(fail) {
-        console.log("failure!", url, data);
-      });
-   */
-
-  }
-
-}];
-
-TipBulkOperationsCtrl = ['$scope', '$http', '$route', '$location', '$uibModalInstance', 'Authentication', 'selected_tips', 'operation', 
-                        function ($scope, $http, $route, $location, $uibModalInstance, Authentication, selected_tips, operation) {
+TipBulkOperationsCtrl = ['$scope', '$http', '$route', '$location', '$uibModalInstance', 'selected_tips', 'operation',
+                        function ($scope, $http, $route, $location, $uibModalInstance, selected_tips, operation) {
   $scope.selected_tips = selected_tips;
   $scope.operation = operation;
 
@@ -132,19 +78,16 @@ TipBulkOperationsCtrl = ['$scope', '$http', '$route', '$location', '$uibModalIns
   $scope.ok = function () {
      $uibModalInstance.close();
 
-    switch (operation) {
-      case 'postpone':
-      case 'delete':
-          return $http({method: 'PUT', url: '/rtip/operations', data:{
-            'operation': $scope.operation,
-            'rtips': $scope.selected_tips
-          }}).success(function(data, status, headers, config){
-            $scope.selected_tips = [];
-            $route.reload();
-          });
-      default:
-          return;
+    if (['postpone', 'delete'].indexOf(operation) === -1) {
+      return;
     }
+
+    return $http({method: 'PUT', url: '/rtip/operations', data:{
+      'operation': $scope.operation,
+      'rtips': $scope.selected_tips
+    }}).success(function(data, status, headers, config){
+      $scope.selected_tips = [];
+      $route.reload();
+    });
   };
 }];
-
