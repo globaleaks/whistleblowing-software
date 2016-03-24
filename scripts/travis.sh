6f94316046327783c196c5414af3a23baf37608f
Nick Skelsey <nskelsey@gmail.com>
2016-03-24 17:07:49 -0400

    Added mochaTest to Grunt. Addressing #1602

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -39,7 +39,7 @@ if [ "$GLTEST" = "unit" ]; then
 
   $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z travis --disable-mail-notification
   sleep 5
-  $TRAVIS_BUILD_DIR/client/node_modules/mocha/bin/mocha -R list $TRAVIS_BUILD_DIR/client/tests/api/test_00* --timeout 30000
+  grunt mochaTest
 
   echo "Running BrowserTesting locally collecting code coverage"
   cd $TRAVIS_BUILD_DIR/client
