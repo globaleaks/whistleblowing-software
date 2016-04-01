c0bebe71e7aaec5b818cbbc7b1904a89e9edce00
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-02 00:25:35 +0200

    Implement rtlcss #1621

--- backend/globaleaks/tests/handlers/test_css.py
+++ backend/globaleaks/tests/handlers/test_css.py
@@ -7,11 +7,15 @@ from globaleaks.tests import helpers
 # FIXME this tests does not really test the handler but
 # simply asses the code coverage.
 
-class TestFileCSS(helpers.TestHandler):
-    _handler = css.CSSFileHandler
+class TestLTRCSS(helpers.TestHandler):
+    _handler = css.LTRCSSFileHandler
 
     @inlineCallbacks
     def test_get(self):
         handler = self.request({})
         yield handler.get()
         self.assertEqual(handler.get_status(), 200)
+
+
+class TestRTLCSS(TestLTRCSS):
+    _handler = css.RTLCSSFileHandler
