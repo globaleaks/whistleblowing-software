81b7e1da7eeacdaf7db7d74841678ee107b1dda8
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-03 16:16:29 +0200

    Delete outdated filters

--- client/app/js/filters.js
+++ client/app/js/filters.js
@@ -1,39 +1,4 @@
-angular.module('GLFilters', [])
-  .filter('range', function() {
-    return function(val, range) {
-      range = parseInt(range);
-      for (var i=0; i<range; i++) {
-        val.push(i);
-      }
-      return val;
-    };
-}).
-  filter('trimTo', function() {
-    return function (value, max, wordwise, tail) {
-        if (!value) {
-          return '';
-        }
-
-        max = parseInt(max, 10);
-        if (!max) {
-          return value;
-        }
-
-        if (value.length <= max) {
-          return value;
-        }
-
-        value = value.substr(0, max);
-        if (wordwise) {
-            var lastspace = value.lastIndexOf(' ');
-            if (lastspace !== -1) {
-                value = value.substr(0, lastspace);
-            }
-        }
-
-        return value + (tail || ' …');
-    };
-}).
+angular.module('GLFilters', []).
   filter('prettyBytes', function() {
     return function (bytes) {
         var ret;
