b73483d699b5654fa62afdb77859eda7d6f16e1b
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-07 13:39:11 +0200

    Implement fixing of field attributes at migration script time

--- backend/globaleaks/db/appdata.py
+++ backend/globaleaks/db/appdata.py
@@ -5,6 +5,8 @@ import copy
 import json
 import os
 
+from storm.expr import And, Not, In
+
 from globaleaks import models
 from globaleaks.orm import transact
 from globaleaks.handlers.admin.field import db_create_field, db_update_field, db_import_fields
@@ -73,3 +75,33 @@ def db_update_appdata(store):
 @transact
 def update_appdata(store):
     return db_update_appdata(store)
+
+
+def db_fix_fields_attrs(store):
+    # Load the field attributes descriptors
+    field_attrs = {}
+    with file(GLSettings.field_attrs_file, 'r') as f:
+        json_string = f.read()
+        field_attrs = json.loads(json_string)
+
+    # Get the list of keys
+    uptodate_keys = []
+    for k1 in field_attrs.keys():
+        for k2 in field_attrs[k1].keys():
+            field_attrs[k1][k2]['name'] = k2
+            uptodate_keys.append(k2)
+
+    # Remove keys that have been removed
+    store.find(models.FieldAttr,
+               Not(In(models.FieldAttr.name, uptodate_keys))).remove()
+
+    # Add keys that have been added
+    for field in store.find(models.Field):
+        attrs = field_attrs.get(field.type, {})
+        for attr_name in attrs.keys():
+            if not store.find(models.FieldAttr,
+                              And(models.FieldAttr.field_id == field.id,
+                                  models.FieldAttr.name == attr_name)).one():
+
+                field.attrs.add(models.db_forge_obj(store, models.FieldAttr,
+                                                    attrs[attr_name]))
