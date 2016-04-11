5c76a3fd235dfa22555bcf8e069e98a8e4618364
Nick Skelsey <nskelsey@gmail.com>
2016-04-11 15:43:54 -0400

    Fix for admin authentication problem and test-admin-context refactor

--- client/tests/end2end/test-admin-delete-users.js
+++ client/tests/end2end/test-admin-delete-users.js
@@ -1,11 +1,13 @@
+var pages = require('./pages.js');
+
 describe('admin del users', function() {
-  it('should del existing users', function(done) {
+  var adminLog = new pages.adminLoginPage();
+
+  it('should del existing users', function() {
+    adminLog.login('admin', 'ACollectionOfDiplomaticHistorySince_1966_ToThe_Pr esentDay#');
     browser.setLocation('admin/users');
 
-    element.all((by.css('.actionButtonDelete'))).last().click().then(function() {
-      element(by.id('modal-action-ok')).click().then(function() {
-        done();
-      });
-    });
+    element.all((by.css('.actionButtonDelete'))).last().click();
+    element(by.id('modal-action-ok')).click();
   });
 });
