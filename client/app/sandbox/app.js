f181febaf5bcfb6a9a8016dc0a4595d7c15d9dbb
Nick Skelsey <nskelsey@gmail.com>
2016-04-16 11:19:35 -0400

    Added PGP key validation and detail extraction

--- client/app/sandbox/app.js
+++ client/app/sandbox/app.js
@@ -1,19 +1,72 @@
 angular.module('GLClient', [])
-.controller('MainCtrl', ['$scope', function($scope) {
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
   rec = { 
     pgp_key_public: "",
     email: "nskelsey@gmail.com",
   }; 
+  $http({ method: 'GET', url: '/sandbox/giovanni.pub'}).then(function(res) {
+    rec.pgp_key_public = res.data;
+  });
   $scope.receiver = rec;
 }])
-// pgppubkeyinput is an element directive that can display and validate pgp
+
+// pgppubkeyinput is an element directive that can display validated pgp
 // public keys. The attached-model attribute will be bound to the input of the
 // text area.
 .directive('pgppubkeyinput', function() {
-  
-  // Craete object from valid pubKey
-  function pgpKeyDetails(pubKey) {
-    // TODO
+
+  // makeNiceFingerPrint produces the full key fingerprint in the standard
+  // 160 bit format. See: https://tools.ietf.org/html/rfc4880#section-12.2
+  function makeNiceFingerPrint(print) {
+    if (print.length !== 40) {
+      // Do nothing, the passed params are strange.
+      return print;
+    }
+    print = print.toUpperCase();
+    var nice = print[0];
+    for (var i = 1; i < 40; i++) {
+      // Insert a space every 4th octet
+      if (i % 4 === 0) {
+        nice += " ";
+      }
+      if (i % 20 === 0) {
+        nice += " ";
+      }
+      nice += print[i];
+    }
+    return nice;
+  }
+
+  // Returns all of the userId's found in the list of uids attached to the key
+  function extractAllUids(key) {
+    var uids = [];
+    key.users.forEach(function(user) {
+      uids.push(user.userId.userid);
+    });
+    return uids;
+  }
+
+  // Create object that displays relevant key details to the user. This function
+  // returns fingerprint, key id, creation date, and expiration date.
+  function pgpKeyDetails(armoredText) {
+    var res = openpgp.key.readArmored(armoredText);
+
+    if (angular.isDefined(res.err)) {
+      console.log(res.err);
+    }
+    console.log(res.keys);
+    var key = res.keys[0];
+    
+    var niceprint = makeNiceFingerPrint(key.primaryKey.fingerprint);
+    var uids = extractAllUids(key);
+    var created = key.primaryKey.created;
+    
+    return {
+      user_info: uids,
+      fingerprint: niceprint,
+      created: created,
+      expiration: key.getExpirationTime(),
+    };
   }
 
   return {
@@ -33,14 +86,10 @@ angular.module('GLClient', [])
           return;
         }
 
-        // The GPGKey is valid. Extract its details.
+        // The PGP Key is valid. Extract its details.
         if (newVal) {
-          $scope.key_details = {
-            'username': 'e@e.com',
-            'name': 'John Doe',
-            'expires': '10-10-2020',
-          };      
-          // TODO extract key details
+          var keyTxt = $scope.keyForm.txt.$modelValue;
+          $scope.key_details = pgpKeyDetails(keyTxt);
         } else {
           // If the key has changed due to new input, dereference old key_details.
           $scope.key_details = undefined;
@@ -57,11 +106,38 @@ angular.module('GLClient', [])
 .directive('pgpPubKeyValidator', function() {
   // Checks to see if passed text is an ascii armored GPG public key.
   function validatePubKey(textInput) {
-    // TODO
-    if (textInput === 'b' || textInput === 'c') {
-      return true;
+    // Check for obvious problems
+    var s = textInput.trim();
+    if (!s.startsWith('-----')) {
+      return false;
     }
-    return false;
+
+    // Try to parse the key
+    var result;
+    try {
+      result = openpgp.key.readArmored(s);
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    
+    // Assert that the parse created no errors
+    if (angular.isDefined(result.err)) {
+      return false;
+    }
+
+    // Assert that there is only one key in the input
+    if (result.keys.length !== 1) {
+      return false;
+    }
+    
+    var key = result.keys[0];
+    // Assert that the key is a not a private key and is a public key
+    if (key.isPrivate() || !key.isPublic()) {
+      return false;
+    }
+    
+    return true;
   }
   
   // scope is the directives scope
