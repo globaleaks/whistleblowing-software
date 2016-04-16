globaleaks/whistleblowing-software

commit 580cf2cc784c8b3d36ad882b09defd7f4defcd40

Fix flickering of file upload between  'uploading' and 'complete' status
The issue was happening on files bigger thant the chunk size (1MB) because the isUploading() function of flowjs returns True only when a chunk is on fly but not when a chunk is being prepared.
The fix involved simply using the correct function for checking if the file upload (possibly involving multiple files) is completed or not: the function is the file.isComplete()

client/app/js/controllers/main.js
@@ -387,7 +387,7 @@ GLClient.controller('MainCtrl', ['$q', '$scope', '$rootScope', '$http', '$route'
 
       for (var key in uploads) {
         if (uploads.hasOwnProperty(key)) {
-          if(uploads[key].isUploading()) {
+          if(!uploads[key].isComplete()) {
             return 'uploading';
           }
 

commit 510531759ad5dca2d5662b35e92b551ab492187b

Fix regression on file upload status introduced by 580cf2cc784c8b3d36ad882b09defd7f4defcd40
For error i finally committed the wrong code; the isComplete() function does not exists on the flow object and we require to use .progress() == 1 for that.

client/app/js/controllers/main.js
@@ -387,7 +387,7 @@ GLClient.controller('MainCtrl', ['$q', '$scope', '$rootScope', '$http', '$route'
 
       for (var key in uploads) {
         if (uploads.hasOwnProperty(key)) {
-          if(!uploads[key].isComplete()) {
+          if(uploads[key].progress() != 1) {
             return 'uploading';
           }
 