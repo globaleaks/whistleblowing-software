97bd6baf586903696cf87976aa24d23757ebc168
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-24 19:15:57 +0200

    Revert "Faith No More Zipstream! Epic!"
    This reverts commit 08b686e5ad6f5c3a1465a172210ec07d40202db1.

---
+++ backend/globaleaks/tests/utils/test_zipstream.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+import os
+import StringIO
+from zipfile import ZipFile
+
+from twisted.internet.defer import inlineCallbacks
+
+from globaleaks.db.appdata import load_appdata
+from globaleaks.settings import GLSettings
+from globaleaks.tests import helpers
+from globaleaks.utils.zipstream import ZipStream
+
+
+class TestZipStream(helpers.TestGL):
+    @inlineCallbacks
+    def setUp(self):
+        yield helpers.TestGL.setUp(self)
+
+        self.files = []
+        for k in self.internationalized_text:
+            self.files.append({'name': self.internationalized_text[k].encode('utf8'),
+                               'buf': self.internationalized_text[k].encode('utf-8')})
+
+    def test_zipstream(self):
+        output = StringIO.StringIO()
+
+        for data in ZipStream(self.files):
+            output.write(data)
+
+        with ZipFile(output, 'r') as f:
+            self.assertIsNone(f.testzip())
