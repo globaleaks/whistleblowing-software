d575b41c63f93b4d4b517c8def38dc04cc314bb9
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-24 15:16:52 +0200

    Implement isolateForm directive for isolating form validation like option password changes
    https://github.com/angular/angular.js/issues/5858

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -266,4 +266,39 @@ angular.module('GLDirectives', ['GLBrowserCrypto']).
         pgpPubkeyValidator: '@',
       }
     };
+}]).
+  // isolateForm directive makes possible to isolate validation of nested forms
+  directive('isolateForm', [function () {
+    return {
+        restrict: 'A',
+        require: '?form',
+        link: function (scope, elm, attrs, ctrl) {
+            if (!ctrl) {
+                return;
+            }
+
+            // Do a copy of the controller
+            var ctrlCopy = {};
+            angular.copy(ctrl, ctrlCopy);
+
+            // Get the parent of the form
+            var parent = elm.parent().controller('form');
+            // Remove parent link to the controller
+            parent.$removeControl(ctrl);
+
+            // Replace form controller with a "isolated form"
+            var isolatedFormCtrl = {
+                $setValidity: function (validationToken, isValid, control) {
+                    ctrlCopy.$setValidity(validationToken, isValid, control);
+                    parent.$setValidity(validationToken, true, ctrl);
+                },
+                $setDirty: function () {
+                    elm.removeClass('ng-pristine').addClass('ng-dirty');
+                    ctrl.$dirty = true;
+                    ctrl.$pristine = false;
+                },
+            };
+            angular.extend(ctrl, isolatedFormCtrl);
+        }
+    };
 }]);
