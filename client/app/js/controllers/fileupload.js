0b43272d10b3075bd633e57817e541b44e839a0d
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-04-25 17:10:14 +0200

    Partially address issue #1654

--- client/app/js/controllers/fileupload.js
+++ client/app/js/controllers/fileupload.js
@@ -2,9 +2,9 @@ GLClient.controller('WBFileUploadCtrl', ['$scope', function($scope) {
   $scope.disabled = false;
 
   $scope.$on('flow::fileAdded', function (event, flow, file) {
+    $scope.error_msg = undefined;
     if (file.size > $scope.node.maximum_filesize * 1024 * 1024) {
-      file.error = true;
-      file.error_msg = "This file exceeds the maximum upload size for this server.";
+      $scope.error_msg = "This file exceeds the maximum upload size for this server.";
       event.preventDefault();
     } else {
       if ($scope.field !== undefined && !$scope.field.multi_entry) {
