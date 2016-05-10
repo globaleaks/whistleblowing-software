c6bf3460262b49989a458c45027909f70e4b125d
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-05-10 10:37:46 +0200

    Avoid to scatter download while testing on browsers that would require user interaction.

--- client/tests/end2end/test-globaleaks-process.js
+++ client/tests/end2end/test-globaleaks-process.js
@@ -266,19 +266,17 @@ describe('globaLeaks process', function() {
   });
 
   it('Recipient should be able to export the submission', function(done) {
-      login_receiver(receiver_username, receiver_password);
-      element(by.id('tip-0')).click();
-      element(by.id('tipFileName')).getText().then(function(t) {
-        expect(t).toEqual(jasmine.any(String));
-        element(by.id('tip-action-export')).click();
-        if (utils.testFileDownload()) {
-          // TODO: Verify the zips content
+      if (utils.testFileDownload()) {
+        login_receiver(receiver_username, receiver_password);
+        element(by.id('tip-0')).click();
+        element(by.id('tipFileName')).getText().then(function(t) {
+          expect(t).toEqual(jasmine.any(String));
           utils.waitForFile(t + '.zip', 30000);
           done();
-        } else {
-          done();
-        }
-      });
+        });
+      } else {
+        done();
+      }
   });
 
   it('Recipient should be able to disable and renable email notifications', function(done) {
