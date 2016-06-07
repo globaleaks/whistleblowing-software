globaleaks/whistleblowing-software

commit a264966194b0ce1c967eb8e92cb01dc2aa837395

Simplify wizard handler

backend/globaleaks/handlers/wizard.py
@@ -7,7 +7,6 @@ from globaleaks.orm import transact
 from globaleaks.handlers.base import BaseHandler
 from globaleaks.handlers.admin.context import db_create_context
 from globaleaks.handlers.admin.receiver import db_create_receiver
-from globaleaks.handlers.admin.node import db_update_node, db_admin_serialize_node
 from globaleaks.handlers.admin.user import db_create_admin_user
 from globaleaks.handlers.public import serialize_node
 from globaleaks.rest import requests
@@ -21,23 +20,17 @@ from twisted.internet.defer import inlineCallbacks
 @transact
 def wizard(store, request, language):
     try:
-        node = db_admin_serialize_node(store, language)
-        node['default_language'] = language
-        node['languages_enabled'] = [language]
-
-        # Header title of the homepage and the node presentation is
-        # initially set with the node title
-        node['header_title_homepage'] = node['name']
-        node['presentation'] = node['name']
-
-        db_update_node(store, node, True, language)
+        node = store.find(models.Node).one()
+        node.default_language = language
+        node.languages_enabled = [language]
+        node.header_title_homepage[language] = request['node']['name']
+        node.presentation[language] = request['node']['name']
+        node.wizard_done = True
 
         context = db_create_context(store, request['context'], language)
 
-        # associate the new context to the receiver
         request['receiver']['contexts'] = [context.id]
         request['receiver']['language'] = language
-
         db_create_receiver(store, request['receiver'], language)
 
         admin_dict = {
@@ -50,7 +43,7 @@ def wizard(store, request, language):
             'description': u'',
             'mail_address': request['admin']['mail_address'],
             'language': language,
-            'timezone': node['default_timezone'],
+            'timezone': node.default_timezone,
             'password_change_needed': False,
             'pgp_key_remove': False,
             'pgp_key_status': 'disabled',

backend/globaleaks/handlers/admin/node.py
@@ -94,7 +94,7 @@ def admin_serialize_node(store, language):
     return db_admin_serialize_node(store, language)
 
 
-def db_update_node(store, request, wizard_done, language):
+def db_update_node(store, request, language):
     """
     Update and serialize the node infos
 
@@ -136,9 +136,6 @@ def db_update_node(store, request, wizard_done, language):
     else:
         node.basic_auth = False
 
-    if wizard_done:
-        node.wizard_done = True
-
     node.update(request)
 
     db_refresh_memory_variables(store)
@@ -179,7 +176,7 @@ class NodeInstance(BaseHandler):
         request = self.validate_message(self.request.body,
                                         requests.AdminNodeDesc)
 
-        node_description = yield update_node(request, True, self.request.language)
+        node_description = yield update_node(request, self.request.language)
         GLApiCache.invalidate()
 
         self.set_status(202) # Updated

backend/globaleaks/tests/handlers/test_public.py
@@ -17,7 +17,7 @@ class TestAhmiaDescriptionHandler(helpers.TestHandlerWithPopulatedDB):
         nodedict = helpers.MockDict().dummyNode
         nodedict['ahmia'] = False
 
-        yield admin.node.update_node(nodedict, True, 'en')
+        yield admin.node.update_node(nodedict, 'en')
 
         yield handler.get()
 
@@ -27,7 +27,7 @@ class TestAhmiaDescriptionHandler(helpers.TestHandlerWithPopulatedDB):
 
         nodedict = helpers.MockDict().dummyNode
         nodedict['ahmia'] = True
-        yield admin.node.update_node(nodedict, True, 'en')
+        yield admin.node.update_node(nodedict, 'en')
 
         yield handler.get()
 
@@ -44,7 +44,7 @@ class TestRobotstxtHandlerHandler(helpers.TestHandlerWithPopulatedDB):
         nodedict = helpers.MockDict().dummyNode
         nodedict['allow_indexing'] = False
 
-        yield admin.node.update_node(nodedict, True, 'en')
+        yield admin.node.update_node(nodedict, 'en')
 
         yield handler.get()
 
@@ -56,7 +56,7 @@ class TestRobotstxtHandlerHandler(helpers.TestHandlerWithPopulatedDB):
 
         nodedict = helpers.MockDict().dummyNode
         nodedict['allow_indexing'] = True
-        yield admin.node.update_node(nodedict, True, 'en')
+        yield admin.node.update_node(nodedict, 'en')
 
         yield handler.get()
 

commit 0172c2fa5410c7839fcc1764312e0c85f8cc20bb

Fix small regression on wizard configuration introduced in a264966194b0ce1c967eb8e92cb01dc2aa837395

backend/globaleaks/handlers/wizard.py
@@ -23,6 +23,8 @@ def wizard(store, request, language):
         node = store.find(models.Node).one()
         node.default_language = language
         node.languages_enabled = [language]
+        node.name = request['node']['name']
+        node.description[language] = request['node']['name']
         node.header_title_homepage[language] = request['node']['name']
         node.presentation[language] = request['node']['name']
         node.wizard_done = True