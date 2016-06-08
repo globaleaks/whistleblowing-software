4be785cc37e3e6f23cd99b7433dd4f50a68177f3
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-06-08 15:38:41 +0200

    Fix e2e tests in relation to changes to the wizard

--- client/tests/end2end/test-admin-perform-wizard.js
+++ client/tests/end2end/test-admin-perform-wizard.js
@@ -8,20 +8,20 @@ describe('globaLeaks setup wizard', function() {
     element(by.id('ButtonNext1')).click();
       
     // Fill out the form
-    element(by.model('admin.node.name')).sendKeys('E2E Test Instance');
-    element(by.model('admin.node.description')).sendKeys('This instance is for E2E testing');
-    element(by.model('admin_mail_address')).sendKeys('globaleaks-admin@mailinator.com');
-    element(by.model('admin_password')).sendKeys(utils.vars['user_password']);
+    element(by.model('wizard.node.name')).sendKeys('E2E Test Instance');
+    element(by.model('wizard.node.description')).sendKeys('This instance is for E2E testing');
+    element(by.model('wizard.admin.mail_address')).sendKeys('globaleaks-admin@mailinator.com');
+    element(by.model('wizard.admin.password')).sendKeys(utils.vars['user_password']);
     element(by.model('admin_check_password')).sendKeys(utils.vars['user_password']);
 
-    element(by.model('receiver.name')).sendKeys('Recipient 1');
-    element(by.model('receiver.mail_address')).sendKeys('globaleaks-receiver1@mailinator.com');
+    element(by.model('wizard.receiver.name')).sendKeys('Recipient 1');
+    element(by.model('wizard.receiver.mail_address')).sendKeys('globaleaks-receiver1@mailinator.com');
       
     // Disable encryption for receiver
-    element(by.model('admin.node.allow_unencrypted')).click();
+    element(by.model('wizard.node.allow_unencrypted')).click();
     element(by.css('[data-ng-click="ok()"]')).click();
  
-    element(by.model('context.name')).sendKeys('Context 1');
+    element(by.model('wizard.context.name')).sendKeys('Context 1');
       
     // Complete the form
     element.all(by.id('ButtonNext2')).click();
