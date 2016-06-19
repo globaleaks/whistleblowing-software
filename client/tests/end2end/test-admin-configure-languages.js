eeb496f66aa2166917e53265eb82a05e400837f3
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-06-19 23:20:54 +0200

    Add test of language selector to the wizard and complete the wizard in english

--- client/tests/end2end/test-admin-configure-languages.js
+++ client/tests/end2end/test-admin-configure-languages.js
@@ -1,5 +1,3 @@
-var utils = require('./utils.js');
-
 describe('adming configure languages', function() {
   it('should configure languages', function() {
     browser.setLocation('admin/content');
@@ -14,12 +12,12 @@ describe('adming configure languages', function() {
 
     element(by.model('GLTranslate.indirect.appLanguage')).element(by.xpath(".//*[text()='Italiano']")).click();
 
-    utils.waitUntilReady(element(by.cssContainingText("a", "Impostazioni generali")));
     expect(browser.isElementPresent(element(by.cssContainingText("a", "General settings")))).toBe(false);
+    expect(browser.isElementPresent(element(by.cssContainingText("a", "Impostazioni generali")))).toBe(true);
 
     element(by.model('GLTranslate.indirect.appLanguage')).element(by.xpath(".//*[text()='English']")).click();
 
-    utils.waitUntilReady(element(by.cssContainingText("a", "General settings")));
     expect(browser.isElementPresent(element(by.cssContainingText("a", "Impostazioni generali")))).toBe(false);
+    expect(browser.isElementPresent(element(by.cssContainingText("a", "General settings")))).toBe(true);
   });
 });
