3a1a39ab701a01e506f228b64ab7081b40949225
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-08-27 09:41:33 +0200

    Fix default initialization of contexts in relation of recipients selection
    Past implementation since we disabled the recipeitn selection by default, was causing the user to
    have to manually set the "select all recipients by default" option causing the first configuration to be less usable.

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -667,7 +667,7 @@ factory("Access", ["$q", "Authentication", function ($q, Authentication) {
       context.show_recipients_details = false;
       context.allow_recipients_selection = false;
       context.show_receivers_in_alphabetical_order = true;
-      context.select_all_receivers = false;
+      context.select_all_receivers = true;
       context.maximum_selectable_receivers = 0;
       context.show_small_receiver_cards = false;
       context.enable_comments = true;
