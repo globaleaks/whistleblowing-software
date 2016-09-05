d86b778d819dcf7e99f5b907a3b7103726d7c744
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-05 23:39:47 +0200

    Implement unit test for custodian (admin) password change

--- client/tests/end2end/test-custodian-first-login.js
+++ client/tests/end2end/test-custodian-first-login.js
@@ -1,5 +1,7 @@
 var utils = require('./utils.js');
 
+var temporary_password = "typ0drome@absurd.org";
+
 describe('receiver first login', function() {
   it('should redirect to /firstlogin upon successful authentication', function() {
     utils.login_custodian('Custodian1', utils.vars['default_password'], '/#/custodian', true);
@@ -7,13 +9,21 @@ describe('receiver first login', function() {
 
   it('should be able to change password from the default one', function() {
     element(by.model('preferences.old_password')).sendKeys(utils.vars['default_password']);
-    element(by.model('preferences.password')).sendKeys(utils.vars['user_password']);
-    element(by.model('preferences.check_password')).sendKeys(utils.vars['user_password']);
+    element(by.model('preferences.password')).sendKeys(temporary_password);
+    element(by.model('preferences.check_password')).sendKeys(temporary_password);
     element(by.css('[data-ng-click="pass_save()"]')).click();
     utils.waitForUrl('/custodian/identityaccessrequests');
   });
 
   it('should be able to login with the new password', function() {
-    utils.login_custodian();
+    utils.login_custodian('Custodian1', temporary_password, '/#/custodian', false);
+  });
+
+  it('should be able to change password accessing the user preferences', function() {
+    element(by.cssContainingText("a", "Password configuration")).click();
+    element(by.model('preferences.old_password')).sendKeys(temporary_password);
+    element(by.model('preferences.password')).sendKeys(utils.vars['user_password']);
+    element(by.model('preferences.check_password')).sendKeys(utils.vars['user_password']);
+    element(by.css('[data-ng-click="pass_save()"]')).click();
   });
 });
