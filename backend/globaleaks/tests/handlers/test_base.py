70d7058e66e0fc73a692f5bc129d6d502916feb8
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-05 16:54:36 +0200

    Add tests for all conditions of presence/absence of languages headers

--- backend/globaleaks/tests/handlers/test_base.py
+++ backend/globaleaks/tests/handlers/test_base.py
@@ -50,6 +50,45 @@ class TestBaseHandler(helpers.TestHandlerWithPopulatedDB):
         date2 = GLSessions.get(session.id).getTime()
         self.assertEqual(date1 + FUTURE, date2)
 
+    @inlineCallbacks
+    def test_get_with_no_language_header(self):
+        handler = self.request({})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'en')
+
+    @inlineCallbacks
+    def test_get_with_gl_language_header(self):
+        handler = self.request({}, headers={'GL-Language': 'it'})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'it')
+
+    @inlineCallbacks
+    def test_get_with_accept_language_header(self):
+        handler = self.request({}, headers={'Accept-Language': 'ar;q=0.8,it;q=0.6'})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'ar')
+
+    @inlineCallbacks
+    def test_get_with_gl_language_header_and_accept_language_header_1(self):
+        handler = self.request({}, headers={'GL-Language': 'en',
+                                            'Accept-Language': 'en-US,en;q=0.8,it;q=0.6'})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'en')
+
+    @inlineCallbacks
+    def test_get_with_gl_language_header_and_accept_language_header_2(self):
+        handler = self.request({}, headers={'GL-Language': 'antani',
+                                            'Accept-Language': 'en-US,en;it;q=0.6'})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'en')
+
+    @inlineCallbacks
+    def test_get_with_gl_language_header_and_accept_language_header_3(self):
+        handler = self.request({}, headers={'GL-Language': 'antani',
+                                            'Accept-Language': 'antani1,antani2;q=0.8,antani3;q=0.6'})
+        yield handler.get_unauthenticated()
+        self.assertEqual(handler.request.language, 'en')
+
     def test_validate_jmessage_valid(self):
         dummy_message = {'spam': 'ham', 'firstd': {3: 4}, 'fields': "CIAOCIAO", 'nest': [{1: 2, 3: 4}]}
         dummy_message_template = {'spam': str, 'firstd': dict, 'fields': '\w+', 'nest': [dict]}
