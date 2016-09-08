f00c0be6900d4afee611071028d1685c99c6c44e
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-08 21:53:34 +0200

    Add minimal implementation of test of configuration of admin custom files

---
+++ client/tests/end2end/test-admin-custom-file.js
@@ -0,0 +1,28 @@
+var utils = require('./utils.js');
+
+describe('Admin upload custom file', function() {
+  it('should upload a file and the file should be available for download and deletion', function() {
+    utils.login_admin();
+    browser.setLocation('admin/content');
+    element(by.cssContainingText("a", "Theme customization")).click();
+
+    var customFile = utils.makeTestFilePath('antani.js');
+
+    browser.executeScript('angular.element(document.querySelectorAll(\'input[type="file"]\')).attr("style", "opacity:0; visibility: visible;");');
+    element(by.css("div.uploadfile.file-custom")).element(by.css("input")).sendKeys(customFile);
+
+    element(by.cssContainingText("a", "Theme customization")).click();
+
+    utils.waitUntilPresent(element(by.cssContainingText("td", "antani.js")));
+
+    if (utils.testFileDownload() && utils.verifyFileDownload()) {
+      element(by.css("div.uploadfile.file-custom")).element(by.cssContainingText("span", "Download"))
+      .click().then(function() {
+        var actualFile = utils.makeSavedFilePath('antani.js');
+        utils.TestFileEquality(customFile, actualFile);
+      });
+    }
+
+    element(by.id("fileList")).element(by.cssContainingText("span", "Delete")).click();
+  });
+});
