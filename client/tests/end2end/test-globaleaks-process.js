4fabd3ca52bd1608a6cd0929396ad28c482d9870
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-09 18:31:55 +0200

    During tests get sure that a modal is open before trying to interact with it.

--- client/tests/end2end/test-globaleaks-process.js
+++ client/tests/end2end/test-globaleaks-process.js
@@ -17,7 +17,7 @@ describe('globaLeaks process', function() {
   var perform_submission = function() {
     browser.get('/#/');
     element(by.cssContainingText("button", "Blow the whistle")).click();
-    browser.driver.switchTo().activeElement();
+    utils.waitUntilPresent(element(by.cssContainingText("div.modal-title", "Warning! You are not anonymous.")));
     element(by.id("answer-2")).click();
     element(by.cssContainingText("a", "Proceed to submission")).click();
 
