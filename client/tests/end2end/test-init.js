c69943f3e95e74d702540184a238f6c50cad7aef
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-12 21:38:39 +0200

    Set selenium-webdriver as devDependency for compatibility with both npm2 and npm3

--- client/tests/end2end/test-init.js
+++ client/tests/end2end/test-init.js
@@ -1,6 +1,6 @@
 describe('init', function() {
   it('should load the app', function() {
-    var remote = require('protractor/node_modules/selenium-webdriver/remote');
+    var remote = require('selenium-webdriver/remote');
     browser.driver.setFileDetector(new remote.FileDetector());
 
     browser.driver.manage().window().maximize();
