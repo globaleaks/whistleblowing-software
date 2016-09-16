973e06bbe948b012d308819e58c7b336f1128c47
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-16 19:41:33 +0200

    Change name of function from update_table to update_defaults

--- backend/globaleaks/models/config.py
+++ backend/globaleaks/models/config.py
@@ -235,7 +235,7 @@ def is_cfg_valid(store):
 
     return True
 
-def update_table(store):
+def update_defaults(store):
     if not is_cfg_valid(store):
         log.info("This update will change system configuration")
 
