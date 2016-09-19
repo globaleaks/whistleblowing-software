085fc8df11d42b7c602d3c07b870f0c9fed38276
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-09-19 11:29:19 +0200

    Remove try/catch condition handling impossible situation globaleaks/rest/apicache.py

--- backend/globaleaks/rest/apicache.py
+++ backend/globaleaks/rest/apicache.py
@@ -1,7 +1,5 @@
 from twisted.internet.defer import inlineCallbacks, returnValue
 
-from globaleaks.utils.utility import log
-
 
 class GLApiCache(object):
     memory_cache_dict = {}
@@ -9,32 +7,22 @@ class GLApiCache(object):
     @classmethod
     @inlineCallbacks
     def get(cls, resource_name, language, function, *args, **kwargs):
-        try:
-            if resource_name in cls.memory_cache_dict \
-                    and language in cls.memory_cache_dict[resource_name]:
-                returnValue(cls.memory_cache_dict[resource_name][language])
+        if resource_name in cls.memory_cache_dict \
+                and language in cls.memory_cache_dict[resource_name]:
+            returnValue(cls.memory_cache_dict[resource_name][language])
 
-            value = yield function(*args, **kwargs)
-            if resource_name not in cls.memory_cache_dict:
-                cls.memory_cache_dict[resource_name] = {}
-            cls.memory_cache_dict[resource_name][language] = value
-            returnValue(value)
-        except KeyError:
-            log.debug("KeyError exception while operating on the cache; probable race")
-            returnValue(None)
+        value = yield function(*args, **kwargs)
+        if resource_name not in cls.memory_cache_dict:
+            cls.memory_cache_dict[resource_name] = {}
+        cls.memory_cache_dict[resource_name][language] = value
+        returnValue(value)
 
     @classmethod
     def set(cls, resource_name, language, value):
-        try:
-            if resource_name not in GLApiCache.memory_cache_dict:
-                cls.memory_cache_dict[resource_name] = {}
+        if resource_name not in GLApiCache.memory_cache_dict:
+            cls.memory_cache_dict[resource_name] = {}
 
-            cls.memory_cache_dict[resource_name][language] = value
-        except KeyError:
-            log.debug("KeyError exception while operating on the cache; probable race")
-            returnValue(None)
-
-    @classmethod
+        cls.memory_cache_dict[resource_name][language] = value
     def invalidate(cls, resource_name=None):
         """
         When a function has an update, all the language need to be
