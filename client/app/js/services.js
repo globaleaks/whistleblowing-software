globaleaks/whistleblowing-software

commit f9c54c7120c49ef36d86ee209bf7726145fcd29d

Use explicit displayLogin function
This moves the use of the session object out of an angular digest.

client/app/js/services.js
@@ -932,6 +932,10 @@ factory("Access", ["$q", "Authentication", function ($q, Authentication) {
                 !this.isSubmissionPage());
       },
 
+      showUserStatusBox: function() {
+        return angular.isDefined($rootScope.session);
+      },
+
       showPrivacyBadge: function() {
         return (!$rootScope.embedded &&
                 !$rootScope.node.disable_privacy_badge &&

client/app/views/partials/user.html
@@ -4,8 +4,8 @@
       <select class="form-control" data-ng-model="GLTranslate.indirect.appLanguage" ng-change="GLTranslate.setLang(); reload();" data-ng-options="item.code as item.name for item in languages_enabled_selector"></select>
     </div>
   </div>
-  <div id="LoginStatusBox" class="row" data-ng-if="Utils.showLoginForm() || session">
-    <div data-ng-show="session" class="pull-right">
+  <div id="LoginStatusBox" class="row" data-ng-if="Utils.showUserStatusBox()">
+    <div class="pull-right">
       <span id="LoginRoleText">
         <label data-ng-if="session.role === 'whistleblower'" data-translate>Whistleblower</label>
         <span data-ng-if="session.role !== 'whistleblower'" data-ng-switch="session.role">
@@ -26,7 +26,9 @@
         </span>
       </span>
     </div>
-    <div data-ng-if="Utils.showLoginForm() && !session" class="pull-right">
+  </div>
+  <div id="LoginStatusBox" class="row" data-ng-if="Utils.showLoginForm()">
+    <div class="pull-right">
       <div>{{::node.whistleblowing_receipt_prompt}}</div>
       <div data-ng-include="'views/partials/wb_login.html'"></div>
       <div class="row LoginErrorBox" data-ng-show="errors[0].code == 29">

commit 8dc2e509fcedc0cce9f98ccedfdbc5bc5ce1c272

Fix regression introduced in f9c54c7

client/app/css/main.css
@@ -130,7 +130,7 @@ table {
   margin-bottom: 0.8em;
 }
 
-#LoginStatusBox {
+#LoginStatusBox, #LoginFormBox {
   color: #000;
   padding: 1em;
   background-color: #FFF;

client/app/js/services.js
@@ -906,7 +906,11 @@ factory("Access", ["$q", "Authentication", function ($q, Authentication) {
 
       isWhistleblowerPage: function() {
         var path = $location.path();
-        return this.isSubmissionPage() || path === '/status';
+        return (path === '/' ||
+                path === '/start' ||
+                path === '/submission' ||
+                path === '/receipt' ||
+                path === '/status');
       },
 
       isSubmissionPage: function() {
@@ -927,9 +931,8 @@ factory("Access", ["$q", "Authentication", function ($q, Authentication) {
       },
 
       showLoginForm: function () {
-        return (!this.isHomepage() &&
-                !this.isLoginPage() &&
-                !this.isSubmissionPage());
+        var path = $location.path();
+        return path === '/submission';
       },
 
       showUserStatusBox: function() {

client/app/views/partials/user.html
@@ -27,7 +27,7 @@
       </span>
     </div>
   </div>
-  <div id="LoginStatusBox" class="row" data-ng-if="Utils.showLoginForm()">
+  <div id="LoginFormBox" class="row" data-ng-if="Utils.showLoginForm()">
     <div class="pull-right">
       <div>{{::node.whistleblowing_receipt_prompt}}</div>
       <div data-ng-include="'views/partials/wb_login.html'"></div>