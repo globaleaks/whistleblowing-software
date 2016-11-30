1d5d13d764f61400b5ae646e8543003b15bb09f0
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-11-30 17:38:08 +0100

    Apply review over commit 60d178cf8907d9462cf91c8649eda671678aa9b5

--- appliance/scripts/globaleaks-deb.sh
+++ appliance/scripts/globaleaks-deb.sh
@@ -2,8 +2,8 @@
 
 cat > /etc/default/globaleaks <<EOL
 LOGLEVEL='DEBUG'
-NETWORK_SANDBOXING=0                                                                                    
-APPARMOR_SANDBOXING=0                                                                                
+NETWORK_SANDBOXING=0
+APPARMOR_SANDBOXING=0
 HOSTS_LIST=127.0.0.1,localhost,192.168.33.10,0.0.0.0
 ALLOWED_DST_IP=192.168.33.10
 EOL
--- appliance/scripts/globaleaks.sh
+++ appliance/scripts/globaleaks.sh
@@ -13,9 +13,4 @@ cat > /etc/default/globaleaks <<EOL
 LOGLEVEL='DEBUG'
 NETWORK_SANDBOXING=0
 APPLICATION_SANDBOXING=0
-HOSTS_LIST=127.0.0.1,localhost,192.168.33.10,0.0.0.0
-ALLOWED_DST_IP=192.168.33.10
 EOL
-
-
-config.vm.network "private_network", ip: "192.168.33.10"
