e3a3d950d37efe78d32ad6c744aba67cb66141be
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2016-12-09 09:17:15 +0100

    Comment packer lines for automatic setup

--- appliance/scripts/globaleaks.sh
+++ appliance/scripts/globaleaks.sh
@@ -2,15 +2,7 @@
 
 set -e
 
-# The following script emulates the installation guide:
-# <https://github.com/globaleaks/GlobaLeaks/wiki/Installation-guide>
-
-wget https://deb.globaleaks.org/install-globaleaks.sh
-chmod +x install-globaleaks.sh
-yes | ./install-globaleaks.sh
-
-cat > /etc/default/globaleaks <<EOL
-LOGLEVEL='DEBUG'
-NETWORK_SANDBOXING=0
-APPLICATION_SANDBOXING=0
-EOL
+# Decomment to run the official install script
+# wget https://deb.globaleaks.org/install-globaleaks.sh
+# chmod +x install-globaleaks.sh
+#./install-globaleaks.sh
