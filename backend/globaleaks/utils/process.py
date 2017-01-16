f03832a9370d26c8fee23c16147b7098de716d56
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-01-16 11:42:11 +0100

    Add process utilities for setting process title and pdeathsigh

---
+++ backend/globaleaks/utils/process.py
@@ -0,0 +1,13 @@
+import ctypes
+
+def set_proctitle(title):
+    libc = ctypes.cdll.LoadLibrary('libc.so.6')
+    buff = ctypes.create_string_buffer(len(title) + 1)
+    buff.value = title
+    libc.prctl(15, ctypes.byref(buff), 0, 0, 0)
+
+def set_pdeathsig(sig):
+    PR_SET_PDEATHSIG = 1
+    libc = ctypes.cdll.LoadLibrary('libc.so.6')
+    libc.prctl.argtypes = (ctypes.c_int, ctypes.c_ulong, ctypes.c_ulong,
+                           ctypes.c_ulong, ctypes.c_ulong)
