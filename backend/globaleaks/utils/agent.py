e488ad28a0f46ac6baf1d03cc868862b9d1523c2
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-05 23:50:55 +0100

    Implement downloadPageSecurely

--- backend/globaleaks/utils/agent.py
+++ backend/globaleaks/utils/agent.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from twisted.internet.defer import fail
-from twisted.web.client import getPage
+from twisted.web.client import getPage, downloadPage
 
 try:
     from twisted.web.client import URI
@@ -9,6 +9,7 @@ except ImportError:
     from twisted.web.client import _URI as URI
 
 from globaleaks.utils import tls
+
 
 def getPageSecurely(url):
     try:
@@ -18,6 +19,19 @@ def getPageSecurely(url):
             return fail()
 
         return getPage(url, tls.TLSClientContextFactory(parsed_url.host))
+
+    except:
+        return fail()
+
+
+def downloadPageSecurely(url, file):
+    try:
+        parsed_url=URI.fromBytes(url)
+
+        if parsed_url.scheme != 'https':
+            return fail()
+
+        return downloadPage(url, file, tls.TLSClientContextFactory(parsed_url.host))
 
     except:
         return fail()
