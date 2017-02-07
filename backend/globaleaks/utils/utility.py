157943e0d2c01c5d0483698929d13b02a2d33285
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-07 14:39:46 +0100

    Fix import of datetime

--- backend/globaleaks/utils/utility.py
+++ backend/globaleaks/utils/utility.py
@@ -260,7 +260,7 @@ def datetime_null():
     """
     @return: a utc datetime object representing a null date
     """
-    return datetime.datetime(1970, 1, 1, 0, 0)
+    return datetime(1970, 1, 1, 0, 0)
 
 
 def datetime_now():
@@ -274,7 +274,7 @@ def datetime_never():
     """
     @return: a utc datetime object representing the 1st January 3000
     """
-    return datetime.datetime(3000, 1, 1, 0, 0)
+    return datetime(3000, 1, 1, 0, 0)
 
 
 def get_expiration(days):
