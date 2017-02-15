16d2627900749aa321820d3b4e8f9250e7bdbfe4
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-15 11:06:02 +0100

    Package workers inside workers directory

--- backend/globaleaks/workers/https_worker.py
+++ backend/globaleaks/workers/https_worker.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import os
 import signal
 from datetime import datetime
@@ -24,16 +23,15 @@ signal.signal(signal.SIGUSR1, SigRespond)
 
 
 import json
-import socket
-import sys
 from urlparse import urlparse
 
-from twisted.internet import reactor, ssl, protocol, defer
-from twisted.protocols import tls
+from twisted.internet import reactor
 
+# TODO: make this conditional and abstract it with a worker class
 # When this executable is not within the systems standard path, the globaleaks
 # module must add it to sys path manually. Hence the following line.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from globaleaks.utils.process import set_pdeathsig
 from globaleaks.utils.sock import listen_tls_on_sock
@@ -64,7 +62,7 @@ def config_wait(file_desc):
     return cfg
 
 
-def setup_tls_proxy(cfg):
+def setup_https_proxy(cfg):
     proxy_url = 'http://' + cfg['proxy_ip'] + ':' + str(cfg['proxy_port'])
     res = urlparse(proxy_url)
     if not res.hostname in ['127.0.0.1', 'localhost']:
@@ -93,12 +91,11 @@ def setup_tls_proxy(cfg):
         log("TLS proxy listening on %s" % port)
 
 
-if __name__ == '__main__':
-    try:
-        cfg = config_wait(42)
-        setup_tls_proxy(cfg)
-    except Exception as e:
-        log("setup failed with %s" % e)
-        raise e
+try:
+    cfg = config_wait(42)
+    setup_https_proxy(cfg)
+except Exception as e:
+    log("setup failed with %s" % e)
+    raise e
 
-    reactor.run()
+reactor.run()
