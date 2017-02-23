56a899f97ca6600305063be0466c44c1503ffcf7
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-23 17:05:12 +0100

    Move exception handling from the job to the exit set object

--- backend/globaleaks/utils/tor_exit_set.py
+++ backend/globaleaks/utils/tor_exit_set.py
@@ -3,17 +3,28 @@
 import re
 
 from globaleaks.utils import agent
+from globaleaks.utils.utility import log
 
 class TorExitSet(set):
+    """Set that keep the list of tor exit nodes ip using check.torproject.org"""
     def processData(self, data):
+        log.debug('Fetching exit nodes\'s ip list')
+
         self.clear()
 
         for ip in re.findall( r'ExitAddress ([^ ]*) ', data):
             self.add(ip)
 
+        log.debug('Retrieved: %d exit nodes\' ip' % len(self))
+
     def update(self):
+        def errback(fail):
+             log.err('Exit relay fetch failed: %s' % fail.value)
+
         pageFetchedDeferred = agent.getPageSecurely('https://check.torproject.org/exit-addresses')
         pageFetchedDeferred.addCallback(self.processData)
+        pageFetchedDeferred.addErrback(errback)
+
         return pageFetchedDeferred
 
 def should_redirect_tor(tor_addr, exit_relay_set, request):
