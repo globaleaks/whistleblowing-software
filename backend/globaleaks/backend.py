ce1ce3f266d9b6ecb9c950ee373bd3439c8a10ab
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-24 22:01:38 +0100

    Improve packaging of GlobaLeaks backend removing race conditions at startup

--- backend/globaleaks/backend.py
+++ backend/globaleaks/backend.py
@@ -11,33 +11,102 @@
 import os
 
 from twisted.application import internet, service
-from twisted.python import log, logfile
+from twisted.internet import reactor, defer
+from twisted.python import log as txlog, logfile as txlogfile
 
+from globaleaks.db import init_db, clean_untracked_files, \
+    refresh_memory_variables
+from globaleaks.jobs import jobs_list
+from globaleaks.jobs.base import GLJobsMonitor
 from globaleaks.rest import api
 from globaleaks.settings import GLSettings
-from globaleaks.utils.utility import GLLogObserver
+from globaleaks.utils.utility import log, GLLogObserver
+from globaleaks.utils.sock import listen_tcp_on_sock, reserve_port_for_ifaces
+from globaleaks.workers.supervisor import ProcessSupervisor
 
 # this import seems unused but it is required in order to load the mocks
 import globaleaks.mocks.cyclone_mocks
 
-application = service.Application('GLBackend')
 
-if not GLSettings.nodaemon and GLSettings.logfile:
-    name = os.path.basename(GLSettings.logfile)
-    directory = os.path.dirname(GLSettings.logfile)
+class GLService(service.Service):
+    def startService(self):
+        if not GLSettings.nodaemon and GLSettings.logfile:
+            name = os.path.basename(GLSettings.logfile)
+            directory = os.path.dirname(GLSettings.logfile)
 
-    gl_logfile = logfile.LogFile(name, directory,
-                              rotateLength=GLSettings.log_file_size,
-                              maxRotatedFiles=GLSettings.num_log_files)
+            gl_logfile = txlogfile.LogFile(name, directory,
+                                         rotateLength=GLSettings.log_file_size,
+                                         maxRotatedFiles=GLSettings.num_log_files)
 
-    application.setComponent(log.ILogObserver, GLLogObserver(gl_logfile).emit)
+            application.setComponent(txlog.ILogObserver, GLLogObserver(gl_logfile).emit)
 
-api_factory = api.get_api_factory()
+        reactor.callLater(0, self.deferred_start)
 
-for ip in GLSettings.bind_addresses:
-    for port in GLSettings.bind_ports:
+    @defer.inlineCallbacks
+    def _deferred_start(self):
+        mask = 0
+        if GLSettings.devel_mode:
+            mask = 9000
+
+        GLSettings.http_socks = []
+        for port in GLSettings.bind_ports:
+            try:
+                port = port+mask if port < 1024 else port
+                http_socks, fails = reserve_port_for_ifaces(GLSettings.bind_addresses, port)
+            except Exception as err:
+                log.err('Failed to listen on %s:%d due to: %s' % (ip, port, err))
+
+            else:
+                GLSettings.http_socks += http_socks
+
+        GLSettings.https_socks, fails = reserve_port_for_ifaces(GLSettings.bind_addresses, 443+mask)
+        for addr, err in fails:
+            log.err("Could not reserve socket for %s because %s!" % (addr, err))
+
+        GLSettings.fix_file_permissions()
+        GLSettings.drop_privileges()
+        GLSettings.check_directories()
+
+        GLSettings.orm_tp.start()
+        reactor.addSystemEventTrigger('after', 'shutdown', GLSettings.orm_tp.stop)
+
+        if GLSettings.initialize_db:
+            yield init_db()
+
+        yield clean_untracked_files()
+
+        yield refresh_memory_variables()
+
+        GLSettings.api_factory = api.get_api_factory()
+
+        for sock in GLSettings.http_socks:
+            listen_tcp_on_sock(reactor, sock.fileno(), GLSettings.api_factory)
+
+        GLSettings.state.process_supervisor = ProcessSupervisor(GLSettings.https_socks,
+                                                                '127.0.0.1',
+                                                                GLSettings.bind_port)
+
+        yield GLSettings.state.process_supervisor.maybe_launch_https_workers()
+
+        GLSettings.jobs = []
+        for job in jobs_list:
+            j = job()
+            GLSettings.jobs.append(j)
+            j.schedule()
+
+        GLSettings.jobs_monitor = GLJobsMonitor(GLSettings.jobs)
+        GLSettings.jobs_monitor.schedule()
+
+    @defer.inlineCallbacks
+    def deferred_start(self):
         try:
-            GLBackendAPI = internet.TCPServer(port, api_factory, interface=ip)
-            GLBackendAPI.setServiceParent(application)
-        except Exception as err:
-            log.err('Failed to listen on %s:%d due to: %s' % (ip, port, err))
+            yield self._deferred_start()
+
+        except Exception as excep:
+            log.err("ERROR: Cannot start GlobaLeaks; please manually check the error.")
+            log.err("EXCEPTION: %s" % excep)
+            reactor.stop()
+
+application = service.Application('GLBackend')
+service = GLService()
+service.setServiceParent(application)
