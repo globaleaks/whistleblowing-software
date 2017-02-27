9c064743cec895eea41edeff000094334345134d
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-02-28 00:11:29 +0100

    Remove isolate-form directive not necessary anymore and faulty due to angular copy on controller

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -317,43 +317,7 @@ directive('stopccp', function(){
     }
   };
 }).
-// isolateForm directive makes possible to isolate validation of nested forms
-directive('isolateForm', [function () {
-  return {
-    restrict: 'A',
-    require: '?form',
-    link: function (scope, elm, attrs, ctrl) {
-      if (!ctrl) {
-        return;
-      }
-
-      // Do a copy of the controller
-      var ctrlCopy = {};
-      angular.copy(ctrl, ctrlCopy);
-
-      // Get the parent of the form
-      var parent = elm.parent().controller('form');
-      // Remove parent link to the controller
-      parent.$removeControl(ctrl);
-
-      // Replace form controller with a "isolated form"
-      var isolatedFormCtrl = {
-        $setValidity: function (validationToken, isValid, control) {
-          ctrlCopy.$setValidity(validationToken, isValid, control);
-          parent.$setValidity(validationToken, true, ctrl);
-        },
-        $setDirty: function () {
-          elm.removeClass('ng-pristine').addClass('ng-dirty');
-          ctrl.$dirty = true;
-          ctrl.$pristine = false;
-        },
-      };
-
-      angular.extend(ctrl, isolatedFormCtrl);
-    }
-  };
-}])
-.directive('singleClick', [function() {
+directive('singleClick', [function() {
   return {
     restrict: 'A',
     link: function(scope, elm) {
