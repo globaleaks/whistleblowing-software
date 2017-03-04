dc6135ea005b5b64e5bafc69e5b3199f33d699d2
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-03-04 14:09:51 +0100

    Add wait on test-submission-ui.js

--- client/tests/end2end/test-submission-ui.js
+++ client/tests/end2end/test-submission-ui.js
@@ -4,11 +4,13 @@ describe('submission error elements', function() {
   it('submission should be blocked until recipients are selected', function() {
     browser.get('/#/submission');
 
+    utils.waitUntilPresent(by.id('submissionForm'));
+
     element(by.id('step-0-link')).click();
 
     element(by.id('step-0')).element(by.id('step-0-field-0-0-input-0')).sendKeys('err-panel test');
-    var errPanel = by.id('SubmissionErrors');
-    utils.waitUntilPresent(errPanel);
+
+    utils.waitUntilPresent(by.id('SubmissionErrors'));
 
     // Navigation should take us back to the Recipient selection page
     element(by.css('#SubmissionErrors span.err-link')).click();
