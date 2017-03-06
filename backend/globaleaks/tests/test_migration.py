70ac455c537ec43f629df4e378a8e4780ec838ae
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-03-06 07:55:03 +0100

    Run db tests on all db versions

--- backend/globaleaks/tests/test_migration.py
+++ backend/globaleaks/tests/test_migration.py
@@ -51,7 +51,7 @@ def test(path, f):
 
 for directory in ['populated']:
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db', directory)
-    for i in range(FIRST_DATABASE_VERSION_SUPPORTED, DATABASE_VERSION, 3):
+    for i in range(FIRST_DATABASE_VERSION_SUPPORTED, DATABASE_VERSION):
         setattr(TestMigrationRoutines, "test_%s_db_migration_%d" % (directory, i), test(path, 'glbackend-%d.db' % i))
 
 
