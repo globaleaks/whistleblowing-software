e0405b2c4d88653a11090f9bba914af48b4b1b60
nskelsey <nskelsey@gmail.com>
2017-03-31 17:19:00 +0200

    Improve comments and document remaining TODOs for onion service mig

--- backend/globaleaks/db/migrations/update_36/__init__.py
+++ backend/globaleaks/db/migrations/update_36/__init__.py
@@ -14,11 +14,12 @@ class MigrationScript(MigrationBase):
     def migrate_hs_dir(self):
         '''Imports the contents of the tor_hs directory into the config table
 
-        NOTE the function does not delete the torhs dir
+        NOTE the function does not delete the torhs dir, but instead leaves it 
+        on disk to ensure that the operator does not lose their HS key.
         '''
         priv_fact = PrivateFactory(self.store_new)
 
-	tor_dir = '/var/globaleaks/torhs'
+        tor_dir = '/var/globaleaks/torhs'
         pk_path = os.path.join(tor_dir, 'private_key')
         hn_path = os.path.join(tor_dir, 'hostname')
         if os.path.exists(pk_path) and os.path.exists(hn_path):
@@ -26,17 +27,20 @@ class MigrationScript(MigrationBase):
                r = f.read()
                if not r.startswith('-----BEGIN RSA PRIVATE KEY-----\n'):
                    raise Exception('%s does not have the right format!')
+               # Clean and convert the pem encoded key read into the format
+               # expected by the ADD_ONION tor control protocol.
+               # TODO assert the key passes deeper validation
                priv_key = 'RSA1024:' + ''.join(r.strip().split('\n')[1:-1])
-               #TODO assert more insteresting things about priv key
             add_raw_config(self.store_new, u'private', u'tor_onion_priv_key', True, priv_key)
 
             with open(hn_path, 'r') as f:
                 hostname = f.read().strip()
+               # TODO assert that the hostname corresponds with the priv_key
                 if not re.match(r'[A-Za-z0-9]{16}\.onion', hostname):
                     raise Exception('The hostname format does not match')
             add_raw_config(self.store_new, u'private', u'tor_onion_hostname', True, hostname)
         else:
-           raise Exception('The path %s does not exist; Tor will not work on this instance' % hn_path)
+           raise Exception('The structure of %s is incorrect. Cannot load onion service keys' % tor_dir)
 
         self.entries_count['Config'] += 2
 
@@ -59,6 +63,6 @@ class MigrationScript(MigrationBase):
         add_raw_config(self.store_new, u'node', u'reachable_via_web', False, False)
         self.entries_count['Config'] += 1
 
-	self.migrate_hs_dir()
+        self.migrate_hs_dir()
 
         self.store_new.commit()
