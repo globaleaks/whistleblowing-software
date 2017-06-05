ca1ee58fdbcc80ebc62862462452b3e0cd71cd2c
nskelsey <nskelsey@gmail.com>
2017-06-05 18:44:46 +0200

    Add test for tor exit set scheduler

---
+++ backend/globaleaks/tests/jobs/test_tor_exit_set.py
@@ -0,0 +1,20 @@
+from twisted.internet.defer import inlineCallbacks
+
+from globaleaks.settings import GLSettings
+from globaleaks.jobs.exit_nodes_refresh_sched import ExitNodesRefreshSchedule
+from globaleaks.tests import helpers
+
+class TestExitNodesRefresh(helpers.TestGL):
+
+    @inlineCallbacks
+    def test_refresh_works(self):
+        # NOTE this test will fail without an internet connection
+        self.assertEqual(len(GLSettings.state.tor_exit_set), 0)
+
+        GLSettings.memory_copy.anonymize_outgoing_connections = False
+
+        # TODO mocking the check-status endpoint will help us detect changes 
+        # to the exit set structure
+        res = yield ExitNodesRefreshSchedule()._operation()
+
+        self.assertTrue(len(GLSettings.state.tor_exit_set) > 700)
