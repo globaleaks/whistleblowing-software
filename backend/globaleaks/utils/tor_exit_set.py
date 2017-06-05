4728921b92eda1033b08a4d12374eade9876225d
nskelsey <nskelsey@gmail.com>
2017-06-05 18:44:46 +0200

    Add test for tor exit set scheduler

--- backend/globaleaks/utils/tor_exit_set.py
+++ backend/globaleaks/utils/tor_exit_set.py
@@ -5,6 +5,7 @@ import re
 from globaleaks.utils.agent import get_page
 from globaleaks.utils.utility import log
 
+EXIT_ADDR_URL = 'https://check.torproject.org/exit-addresses'
 
 class TorExitSet(set):
     """Set that keep the list of tor exit nodes ip using check.torproject.org"""
@@ -22,7 +23,7 @@ class TorExitSet(set):
         def errback(fail):
              log.err('Exit relay fetch failed: %s' % fail.value)
 
-        pageFetchedDeferred = get_page(agent, 'https://check.torproject.org/exit-addresses')
+        pageFetchedDeferred = get_page(agent, EXIT_ADDR_URL)
         pageFetchedDeferred.addCallback(self.processData)
         pageFetchedDeferred.addErrback(errback)
 
