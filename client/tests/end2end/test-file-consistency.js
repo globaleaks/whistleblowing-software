38335e7157b7225497a8328474ea34847933480e
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-06-21 11:24:01 +0200

    Improve test of file-consistency.js trying to remove a race-condition

--- client/tests/end2end/test-file-consistency.js
+++ client/tests/end2end/test-file-consistency.js
@@ -206,8 +206,9 @@ describe('Test file upload/download consistency', function() {
     browser.gl.utils.login_whistleblower(receipt);
     // Choose the first file which should be f1_info
     element(by.css('#AttachedWBFile #wbfile-0 div.download-button')).click();
-    browser.gl.utils.waitUntilPresent(by.css('#modal-action-ok'));
-    element(by.css('#modal-action-ok')).element(by.cssContainingText("span", "Download")).click();
-    f1_info.waitForDownload();
+    browser.gl.utils.waitUntilPresent(by.css('#modal-action-ok')).then(function() {
+      element(by.css('#modal-action-ok')).element(by.cssContainingText("span", "Download")).click();
+      f1_info.waitForDownload();
+    });
   });
 });
