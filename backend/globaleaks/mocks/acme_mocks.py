c5ba77ebc336b0727b80e6ff4627c5ede86c92e3
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-06-27 12:31:00 +0200

    Backport patch  https://github.com/certbot/certbot/pull/3990 to python acme 0.4.1

--- backend/globaleaks/mocks/acme_mocks.py
+++ backend/globaleaks/mocks/acme_mocks.py
@@ -3,7 +3,6 @@
 # This mock is required in order to make python-acmne 0.4.1 compatible with:
 # -
 # - https://github.com/certbot/certbot/issues/4877
-
 from acme import messages, util
 
 def mock_Directory__init__(self, jobj):
@@ -12,3 +11,7 @@ def mock_Directory__init__(self, jobj):
 
 messages.Directory.__init__ = mock_Directory__init__
 
+# Backport patch https://github.com/certbot/certbot/pull/3990
+if sys.version_info < (2, 7, 9):  # pragma: no cover
+    import requests, urllib3
+    requests.packages.urllib3.contrib.pyopenssl.inject_into_urllib3 = urllib3.contrib.pyopenssl.inject_into_urllib3
