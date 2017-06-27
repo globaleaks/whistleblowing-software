76ed2d3d74f2f8d1a8c68f05bf47e23776ecbdf9
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-06-27 12:39:35 +0200

    Fix import issue

--- backend/globaleaks/mocks/acme_mocks.py
+++ backend/globaleaks/mocks/acme_mocks.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8
 
 # This mock is required in order to make python-acmne 0.4.1 compatible with:
-# -
+# - https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417
 # - https://github.com/certbot/certbot/issues/4877
 from acme import messages, util
 
@@ -12,6 +12,8 @@ def mock_Directory__init__(self, jobj):
 messages.Directory.__init__ = mock_Directory__init__
 
 # Backport patch https://github.com/certbot/certbot/pull/3990
+import sys
+
 if sys.version_info < (2, 7, 9):  # pragma: no cover
     import requests, urllib3
     requests.packages.urllib3.contrib.pyopenssl.inject_into_urllib3 = urllib3.contrib.pyopenssl.inject_into_urllib3
