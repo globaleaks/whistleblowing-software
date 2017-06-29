b853dbb82e67f47485794b38cf4214b16cf7e1f6
nskelsey <nskelsey@gmail.com>
2017-06-29 11:13:17 +0200

    Allocate https_sock in bind_remote_ports
    Move interface print out of backend.py

--- backend/globaleaks/settings.py
+++ backend/globaleaks/settings.py
@@ -536,6 +536,26 @@ class GLSettingsClass(object):
         else:
             return get_web_agent()
 
+    def print_listening_interfaces(self):
+        print("GlobaLeaks is now running and accessible at the following urls:")
+
+        for port in self.bind_local_ports:
+            print("- [LOCAL HTTP]\t--> http://127.0.0.1:%d%s" % (port, self.api_prefix))
+
+        if self.memory_copy.reachable_via_web:
+            hostname = self.memory_copy.hostname if self.memory_copy.hostname != '' else '0.0.0.0'
+            if self.devel_mode:
+                print("- [DEVEL HTTP]\t--> http://localhost:8080")
+                if self.memory_copy.private.https_enabled:
+                    print("- [DEVEL HTTPS]\t--> https://localhost:8443")
+            else:
+                print("- [REMOTE HTTP]\t--> http://%s%s" % (hostname, self.api_prefix))
+                if self.memory_copy.private.https_enabled:
+                    print("- [REMOTE HTTPS]\t--> https://%s%s" % (hostname, self.api_prefix))
+
+        if self.memory_copy.onionservice != '':
+            print("- [REMOTE Tor]:\t--> http://%s%s" % (self.memory_copy.onionservice, self.api_prefix))
+
 
 # GLSettings is a singleton class exported once
 GLSettings = GLSettingsClass()
