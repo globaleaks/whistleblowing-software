globaleaks/whistleblowing-software

commit 2b94a9c0206a6d7e01c3f95a6426e89c1751b063

Extend json resources validation to be recursive based on the existing REST API definition

backend/globaleaks/handlers/base.py
@@ -372,6 +372,10 @@ class BaseHandler(object):
                     raise errors.InvalidInputFormat("Key (%s) double validation failure" % key)
                 success_check += 1
 
+                if isinstance(message_template[key], dict) or isinstance(message_template[key], list):
+                    if (message_template[key]):
+                        BaseHandler.validate_jmessage(jmessage[key], message_template[key])
+
             if success_check == len(message_template.keys()) * 2:
                 return True
             else:

backend/globaleaks/handlers/public.py
@@ -22,10 +22,17 @@ def db_prepare_contexts_serialization(store, contexts):
     data = {'imgs': {}, 'receivers': {}}
 
     contexts_ids = []
+    img_ids = []
+
     for c in contexts:
         data['imgs'][c.id] = None
         data['receivers'][c.id] = []
         contexts_ids.append(c.id)
+        if c.img_id is not None:
+            img_ids.append(c.img_id)
+
+    for o in store.find(models.File, In(models.File.id, img_ids)):
+        data['imgs'][o.id] = o.data
 
     for o in store.find(models.ReceiverContext, In(models.ReceiverContext.context_id, contexts_ids)):
         data['receivers'][o.context_id].append(o.receiver_id)
@@ -37,6 +44,8 @@ def db_prepare_receivers_serialization(store, receivers):
     data = {'users': {}, 'imgs': {}, 'contexts': {}}
 
     receivers_ids = []
+    img_ids = []
+
     for r in receivers:
         data['imgs'][r.id] = None
         data['contexts'][r.id] = []
@@ -44,6 +53,10 @@ def db_prepare_receivers_serialization(store, receivers):
 
     for o in store.find(models.User, In(models.User.id, receivers_ids)):
         data['users'][o.id] = o
+        img_ids.append(o.img_id)
+
+    for o in store.find(models.File, In(models.File.id, img_ids)):
+        data['imgs'][o.id] = o.data
 
     for o in store.find(models.ReceiverContext, In(models.ReceiverContext.receiver_id, receivers_ids)):
         data['contexts'][o.receiver_id].append(o.context_id)
@@ -145,6 +158,11 @@ def serialize_context(store, context, language, data=None):
     @return: a dict describing the contexts available for submission,
         (e.g. checks if almost one receiver is associated)
     """
+    if (context.img_id is not None and context.img_id in data['imgs']):
+        img = data['imgs'][context.img_id]
+    else:
+        img = ''
+
     ret_dict = {
         'id': context.id,
         'presentation_order': context.presentation_order,
@@ -164,7 +182,7 @@ def serialize_context(store, context, language, data=None):
         'show_receivers_in_alphabetical_order': context.show_receivers_in_alphabetical_order,
         'questionnaire_id': context.questionnaire_id,
         'receivers': data['receivers'][context.id],
-        'picture': data['imgs'][context.id]
+        'picture': img
     }
 
     return get_localized_values(ret_dict, context, context.localized_keys, language)
@@ -324,6 +342,11 @@ def serialize_receiver(store, receiver, language, data=None):
 
     user = data['users'][receiver.id]
 
+    if (user.img_id is not None and user.img_id in data['imgs']):
+        img = data['imgs'][user.img_id]
+    else:
+        img = ''
+
     ret_dict = {
         'id': receiver.id,
         'name': user.public_name,
@@ -332,6 +355,9 @@ def serialize_receiver(store, receiver, language, data=None):
         'configuration': receiver.configuration,
         'presentation_order': receiver.presentation_order,
         'contexts': data['contexts'][receiver.id],
+        'can_delete_submission': receiver.can_delete_submission,
+        'can_postpone_expiration': receiver.can_postpone_expiration,
+        'can_grant_permissions': receiver.can_grant_permissions,
         'picture': data['imgs'][receiver.id]
     }
 

backend/globaleaks/models/config.py
@@ -108,12 +108,6 @@ class NodeFactory(ConfigFactory):
         'default_password',
         'default_timezone',
 
-        'can_postpone_expiration',
-        'can_delete_submission',
-        'can_grant_permissions',
-
-        'allow_indexing',
-
         'threshold_free_disk_megabytes_high',
         'threshold_free_disk_megabytes_medium',
         'threshold_free_disk_megabytes_low',

backend/globaleaks/rest/requests.py
@@ -488,7 +488,10 @@ ReceiverDesc = {
     'description': unicode,
     'presentation_order': int,
     'id': uuid_regexp,
-    'state': user_states_regexp
+    'state': user_states_regexp,
+    'can_delete_submission': bool,
+    'can_postpone_expiration': bool,
+    'can_grant_permissions': bool
 }
 
 ReceiverCollectionDesc = [ReceiverDesc]
@@ -510,7 +513,8 @@ ContextDesc = {
     'enable_messages': bool,
     'enable_two_way_messages': bool,
     'enable_attachments': bool,
-    'show_receivers_in_alphabetical_order': bool
+    'show_receivers_in_alphabetical_order': bool,
+    'picture': unicode
 }
 
 ContextCollectionDesc = [ContextDesc]

backend/globaleaks/tests/handlers/test_public.py
@@ -17,5 +17,4 @@ class TestPublicResource(helpers.TestHandlerWithPopulatedDB):
         handler = self.request()
         response = yield handler.get()
 
-        resp_desc = self.ss_serial_desc(config.NodeFactory.public_node, requests.PublicResourcesDesc)
-        self._handler.validate_message(json.dumps(response), resp_desc)
+        self._handler.validate_message(json.dumps(response), requests.PublicResourcesDesc)

commit 41ac5ae8e036035a80283abde9bed7b567587305

Fix issue #1986
Commit https://github.com/globaleaks/GlobaLeaks/commit/2b94a9c0206a6d7e01c3f95a6426e89c1751b063 introduced the retesting necessary to validate the solution

backend/globaleaks/handlers/base.py
@@ -290,6 +290,10 @@ class BaseHandler(object):
 
     @staticmethod
     def validate_type(value, type):
+        if value is None:
+            log.err("-- Invalid python_type, in [%s] expected %s" % (value, type))
+            return False
+
         # if it's callable, than assumes is a primitive class
         if callable(type):
             retval = BaseHandler.validate_python_type(value, type)

backend/globaleaks/handlers/public.py
@@ -354,11 +354,11 @@ def serialize_receiver(store, receiver, language, data=None):
         'state': user.state,
         'configuration': receiver.configuration,
         'presentation_order': receiver.presentation_order,
-        'contexts': data['contexts'][receiver.id],
         'can_delete_submission': receiver.can_delete_submission,
         'can_postpone_expiration': receiver.can_postpone_expiration,
         'can_grant_permissions': receiver.can_grant_permissions,
-        'picture': data['imgs'][receiver.id]
+        'picture': data['imgs'][receiver.id],
+        'contexts': data['contexts'][receiver.id]
     }
 
     # description and eventually other localized strings should be taken from user model