e58e988282008811f46bac33e71b911a5a37ae6c
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-07-04 13:45:01 +0200

    Fix serialization of recipient's picture

--- backend/globaleaks/handlers/public.py
+++ backend/globaleaks/handlers/public.py
@@ -357,7 +357,7 @@ def serialize_receiver(store, receiver, language, data=None):
         'can_delete_submission': receiver.can_delete_submission,
         'can_postpone_expiration': receiver.can_postpone_expiration,
         'can_grant_permissions': receiver.can_grant_permissions,
-        'picture': data['imgs'][receiver.id],
+        'picture': img,
         'contexts': data['contexts'][receiver.id]
     }
 
