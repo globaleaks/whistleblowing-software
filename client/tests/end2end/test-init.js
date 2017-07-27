8366145cfb4f9e7802b421babbb44b06f6c5c064
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-07-27 12:16:28 +0200

    Fix issue on Chrome headless configuration in place since Chrome 59 and now failing since 60

--- client/tests/end2end/test-init.js
+++ client/tests/end2end/test-init.js
@@ -3,10 +3,6 @@ describe('init', function() {
     var remote = require('selenium-webdriver/remote');
     browser.driver.setFileDetector(new remote.FileDetector());
 
-    if (!browser.gl.utils.isMobile()) {
-      browser.driver.manage().window().maximize();
-    }
-
     browser.get('/');
   });
 });
