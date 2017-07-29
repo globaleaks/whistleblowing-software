d1c49b9ce9df408a56ae3c97531920f7abfa6b23
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-07-29 10:09:56 +0200

    Open Anonimity modal also on homepage as the user click blow the whistle like in past

--- client/app/js/controllers/home.js
+++ client/app/js/controllers/home.js
@@ -1,10 +1,3 @@
 GLClient.controller('HomeCtrl', ['$scope', function ($scope) {
   $scope.keycode = '';
 }]);
-
-GLClient.controller('ModalCtrl', ['$scope', '$uibModalInstance',
-                    function($scope, $uibModalInstance) {
-  $scope.ok = function () {
-    $uibModalInstance.close();
-  };
-}]);
