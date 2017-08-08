68a2029eca8b4050bf759ec37da58c29befa0c29
nskelsey <nskelsey@gmail.com>
2017-08-08 16:46:34 +0200

    Update E2E perform-wizard test for #2039

--- client/tests/end2end/test-admin-perform-wizard.js
+++ client/tests/end2end/test-admin-perform-wizard.js
@@ -15,24 +15,28 @@ describe('globaLeaks setup wizard', function() {
 
     element.all(by.id('ButtonNext')).get(1).click();
 
+    element(by.id('profile-1')).click();
+
+    element.all(by.id('ButtonNext')).get(2).click();
+
     element(by.model('wizard.admin.mail_address')).sendKeys('globaleaks-admin@mailinator.com');
     element(by.model('wizard.admin.password')).sendKeys(browser.gl.utils.vars['user_password']);
     element(by.model('admin_check_password')).sendKeys(browser.gl.utils.vars['user_password']);
 
-    element.all(by.id('ButtonNext')).get(2).click();
+    element.all(by.id('ButtonNext')).get(3).click();
 
     element(by.model('wizard.context.name')).sendKeys('Context 1');
 
-    element.all(by.id('ButtonNext')).get(3).click();
+    element.all(by.id('ButtonNext')).get(4).click();
 
     element(by.model('wizard.receiver.name')).sendKeys('Recipient1');
     element(by.model('wizard.receiver.mail_address')).sendKeys('globaleaks-receiver1@mailinator.com');
 
-    element.all(by.id('ButtonNext')).get(4).click();
+    element.all(by.id('ButtonNext')).get(5).click();
 
     expect(element(by.css('.congratulations')).isPresent()).toBe(true);
 
-    element.all(by.id('ButtonNext')).get(5).click();
+    element.all(by.id('ButtonNext')).get(6).click();
 
     browser.gl.utils.waitForUrl('/admin/home');
   });
