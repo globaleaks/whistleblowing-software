fe0f6764f21dbec59cfd5f54a25feb8536e06a3b
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-08-08 17:18:33 +0200

    Simplify unuseful code

--- backend/globaleaks/handlers/wizard.py
+++ backend/globaleaks/handlers/wizard.py
@@ -26,55 +26,50 @@ def wizard(store, request, language):
         log.err("DANGER: Wizard already initialized!")
         raise errors.ForbiddenOperation
 
-    try:
-        node._query_group()
+    node._query_group()
 
-        node.set_val('name', request['node']['name'])
-        node.set_val('default_language', language)
-        node.set_val('wizard_done', True)
+    node.set_val('name', request['node']['name'])
+    node.set_val('default_language', language)
+    node.set_val('wizard_done', True)
 
-        if GLSettings.memory_copy.onionservice is not None:
-            node.set_val('onionservice', GLSettings.memory_copy.onionservice)
+    if GLSettings.memory_copy.onionservice is not None:
+        node.set_val('onionservice', GLSettings.memory_copy.onionservice)
 
-        node_l10n = NodeL10NFactory(store)
+    node_l10n = NodeL10NFactory(store)
 
-        node_l10n.set_val('description', language, request['node']['description'])
-        node_l10n.set_val('header_title_homepage', language, request['node']['name'])
+    node_l10n.set_val('description', language, request['node']['description'])
+    node_l10n.set_val('header_title_homepage', language, request['node']['name'])
 
-        context = db_create_context(store, request['context'], language)
+    context = db_create_context(store, request['context'], language)
 
-        langs_to_drop = EnabledLanguage.list(store)
-        langs_to_drop.remove(language)
-        if len(langs_to_drop):
-            EnabledLanguage.remove_old_langs(store, langs_to_drop)
+    langs_to_drop = EnabledLanguage.list(store)
+    langs_to_drop.remove(language)
+    if len(langs_to_drop):
+        EnabledLanguage.remove_old_langs(store, langs_to_drop)
 
-        request['receiver']['contexts'] = [context.id]
-        request['receiver']['language'] = language
-        db_create_receiver(store, request['receiver'], language)
+    request['receiver']['contexts'] = [context.id]
+    request['receiver']['language'] = language
+    db_create_receiver(store, request['receiver'], language)
 
-        admin_dict = {
-            'username': u'admin',
-            'password': request['admin']['password'],
-            'role': u'admin',
-            'state': u'enabled',
-            'deletable': False,
-            'name': u'Admin',
-            'public_name': u'Admin',
-            'description': u'',
-            'mail_address': request['admin']['mail_address'],
-            'language': language,
-            'password_change_needed': False,
-            'pgp_key_remove': False,
-            'pgp_key_fingerprint': '',
-            'pgp_key_public': '',
-            'pgp_key_expiration': datetime_null()
-        }
+    admin_dict = {
+        'username': u'admin',
+        'password': request['admin']['password'],
+        'role': u'admin',
+        'state': u'enabled',
+        'deletable': False,
+        'name': u'Admin',
+        'public_name': u'Admin',
+        'description': u'',
+        'mail_address': request['admin']['mail_address'],
+        'language': language,
+        'password_change_needed': False,
+        'pgp_key_remove': False,
+        'pgp_key_fingerprint': '',
+        'pgp_key_public': '',
+        'pgp_key_expiration': datetime_null()
+    }
 
-        db_create_admin_user(store, admin_dict, language)
-
-    except Exception as excep:
-        log.err("Failed wizard initialization %s" % excep)
-        raise excep
+    db_create_admin_user(store, admin_dict, language)
 
 
 class Wizard(BaseHandler):
