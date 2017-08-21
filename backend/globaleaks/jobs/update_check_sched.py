edf96e083f342df550b68d87a94a133dc5226cf7
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-08-21 11:08:09 +0200

    Fix update_check_sched filtering packages != globaleaks

--- backend/globaleaks/jobs/update_check_sched.py
+++ backend/globaleaks/jobs/update_check_sched.py
@@ -28,7 +28,7 @@ class UpdateCheckJob(LoopingJob):
         try:
             log.debug('Fetching latest GlobaLeaks version from repository')
             packages_file = yield self.fetch_packages_file()
-            versions = [p['Version'] for p in deb822.Deb822.iter_paragraphs(packages_file)]
+            versions = [p['Version'] for p in deb822.Deb822.iter_paragraphs(packages_file) if p['Package'] == 'globaleaks']
             versions.sort(key=V)
             GLSettings.appstate.latest_version = versions[-1]
             log.debug('The newest version in the repository is: %s', GLSettings.appstate.latest_version)
