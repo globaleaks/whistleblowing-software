globaleaks/whistleblowing-software

commit 026a47dececaa1d9bceb775b2c929bb1eac3bf14

Create fileChangeInput directive
Some notes:
- data-text-label determines the displayed msg.
- class can be used like its a btn because its must be a label
- the handling function is passed in via the attribute

client/app/js/directives.js
@@ -271,17 +271,18 @@ directive('pgpPubkeyDisplay', ['pgp', 'glbcKeyLib', function(pgp, glbcKeyLib) {
       keyStr: '=keyStr',
 
     },
-    controller: ['$scope', function($scope) {
-      $scope.$watch('keyStr', function(newVal) {
+    link: function(scope) {
+      scope.$watch('keyStr', function(newVal) {
         if (newVal === "") {
           return;
         }
-        $scope.is_valid_key = glbcKeyLib.validPublicKey(newVal);
-        if ($scope.is_valid_key) {
-          $scope.key_details = pgpKeyDetails(newVal);
+        scope.is_valid_key = glbcKeyLib.validPublicKey(newVal);
+        if (scope.is_valid_key) {
+          scope.key_details = pgpKeyDetails(newVal);
         }
       });
-  }]};
+    },
+  };
 }]).
 // pgpPubkeyValidator binds to text-areas to provide input validation on user
 // input PGP public keys. Note that the directive attaches itself to the
@@ -339,19 +340,21 @@ directive('wbfile', [function() {
     templateUrl: 'views/partials/wbfile.html',
   };
 }]).
-directive('fileChange', function() {
+directive('fileChangeInput', function() {
   return {
     restrict: 'A',
+    templateUrl: 'views/partials/file_change_input.html',
     scope: {
-      handler: '&',
+      fileChangeInput: '&',
+      labelText: '@',
     },
-    link: function (scope, element) {
-      element.on('change', function (event) {
-      scope.$apply(function(){
-        scope.handler({files: event.target.files});
+    link: function (scope, iElement) {
+      iElement.find('input').on('change', function (event) {
+        scope.$apply(function(){
+          scope.fileChangeInput({file: event.target.files[0]});
+        });
       });
-    });
-    }
+    },
   };
 }).
 directive('requiredAsterisk', function() {

client/app/css/main.css
@@ -419,3 +419,7 @@ html[dir="rtl"] #UserBox  {
   padding: 15px;
   margin-bottom: 10px;
 }
+
+textarea.form-control {
+  resize: vertical;
+}

client/app/js/services.js
@@ -1014,7 +1014,26 @@ factory('AdminUtils', ['AdminContextResource', 'AdminQuestionnaireResource', 'Ad
         reader.readAsText(file);
 
         return deferred.promise;
-      }
+      },
+
+      readFileAsJson: function (file) {
+        return this.readFileAsText(file).then(function(txt) {
+          try {
+            return JSON.parse(txt);
+          } catch (excep) {
+            return $q.reject(excep);
+          }
+        });
+      },
+
+      displayErrorMsg: function(reason) {
+        var error = {
+          'message': 'local-failure',
+          'arguments': [reason],
+          'code': 10,
+        };
+        $rootScope.errors.push(error);
+      },
     }
 }]).
   factory('fieldUtilities', ['$filter', 'CONSTANTS', function($filter, CONSTANTS) {

client/app/views/partials/file_change_input.html
@@ -1,0 +1,5 @@
+<span class="file-upload">
+  <i class="glyphicon glyphicon-plus-sign"></i>
+  <span>{{ labelText | translate }}</span>
+  <input type="file"/>
+</span>

commit 3ba17ca5b3e9ecc0ce805bb3d8b1fd0e3a03aaea

Fix regression on HTTPS configuration introduced in 026a47dececaa1d9bceb775b2c929bb1eac3bf14

client/app/js/directives.js
@@ -343,7 +343,7 @@ directive('wbfile', [function() {
 directive('filePickerInput', function() {
   return {
     restrict: 'A',
-    templateUrl: 'views/partials/textarea_file_input.html',
+    templateUrl: 'views/partials/file_picker_input.html',
     scope: {
       filePickerInput: '&',
       labelText: '@',

client/app/views/admin/network/https_files.html
@@ -12,12 +12,17 @@
       </span>
       <span class="pull-right">
         <span class="file-upload" data-ng-if="!tls_config.files.priv_key.set">
-          <label for="keyUpload" class="btn btn-xs btn-primary" data-translate>Upload</label>
-          <input id="keyUpload" type="file" file-change handler="postFile(files[0], file_resources.priv_key)" />
+          <label id="keyUpload" file-picker-input="postFile(file, file_resources.priv_key)" data-label-text="Upload" class="btn btn-xs btn-primary"></label>
         </span>
-        <button id="pkGen" class="btn btn-xs btn-success" data-ng-if="!tls_config.files.priv_key.set" data-ng-click="gen_priv_key()" data-translate>Generate</button>
+        <button id="pkGen" class="btn btn-xs btn-success" data-ng-if="!tls_config.files.priv_key.set" data-ng-click="gen_priv_key()">
+            <i class="glyphicon glyphicon-cog"></i>
+          <span data-translate>Generate</span>
+        </button>
         <span data-ng-if="tls_config.files.priv_key.set">
-          <button id="deleteKey" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.priv_key)" data-translate>Delete</button>
+          <button id="deleteKey" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.priv_key)">
+            <i class="glyphicon glyphicon-remove"></i>
+            <span data-translate>Delete</span>
+          </button>
         </span>
       </span>
     </div>
@@ -42,8 +47,14 @@
       </span>
       <span data-ng-if="tls_config.files.csr.set">
         <span class="pull-right">
-          <button id="downloadCsr" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.csr)" data-translate>Download</button>
-          <button id="deleteCsr" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.csr)" data-translate>Delete</button>
+          <button id="downloadCsr" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.csr)">
+            <i class="glyphicon glyphicon-save-file"></i>
+            <span data-translate>Download</span>
+          </button>
+          <button id="deleteCsr" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.csr)">
+            <i class="glyphicon glyphicon-remove"></i>
+            <span data-translate>Delete</span>
+          </button>
         </span>
       </span>
     </div>
@@ -62,12 +73,17 @@
       </span>
       <span class="pull-right">
         <span class="file-upload" data-ng-if="!tls_config.files.cert.set">
-          <label for="certUpload" class="btn btn-xs btn-primary" data-ng-translate>Upload</label>
-          <input id="certUpload" type="file" file-change handler="postFile(files[0], file_resources.cert)"  />
+          <label id="certUpload" file-picker-input="postFile(file, file_resources.cert)" data-label-text="Upload" class="btn btn-xs btn-primary"></label>
         </span>
         <span data-ng-if="tls_config.files.cert.set">
-          <button id="downloadCert" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.cert)" data-translate>Download</button>
-          <button id="deleteCert" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.cert)" data-translate>Delete</button>
+          <button id="downloadCert" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.cert)">
+            <i class="glyphicon glyphicon-save-file"></i>
+            <span data-translate>Download</span>
+          </button>
+          <button id="deleteCert" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.cert)">
+            <i class="glyphicon glyphicon-remove"></i>
+            <span data-translate>Delete</span>
+          </button>
         </span>
       </span>
     </div>
@@ -86,12 +102,17 @@
       </span>
       <div class="pull-right">
         <span class="file-upload" data-ng-if="!tls_config.files.chain.set">
-          <label for="chainUpload" class="btn btn-xs btn-primary" data-translate>Upload</label>
-          <input id="chainUpload" type="file" file-change handler="postFile(files[0], file_resources.chain)" />
+          <label id="chainUpload" file-picker-input="postFile(file, file_resources.chain)" data-label-text="Upload" class="btn btn-xs btn-primary"></label>
         </span>
         <span data-ng-if="tls_config.files.chain.set">
-          <button id="downloadChain" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.chain)" data-translate>Download</button>
-          <button id="deleteChain" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.chain)" data-translate>Delete</button>
+          <button id="downloadChain" class="btn btn-xs btn-primary" data-ng-click="downloadFile(file_resources.chain)">
+            <i class="glyphicon glyphicon-save-file"></i>
+            <span data-translate>Download</span>
+          </button>
+          <button id="deleteChain" class="btn btn-xs btn-danger" data-ng-click="deleteFile(file_resources.chain)">
+            <i class="glyphicon glyphicon-remove"></i>
+            <span data-translate>Delete</span>
+          </button>
         </span>
       </div>
     </div>

client/app/views/partials/file_picker_input.html
@@ -1,5 +1,5 @@
 <span class="file-upload">
-  <i class="glyphicon glyphicon-plus-sign"></i>
+  <i class="glyphicon glyphicon-open-file"></i>
   <span>{{labelText | translate}}</span>
   <input type="file"/>
 </span>