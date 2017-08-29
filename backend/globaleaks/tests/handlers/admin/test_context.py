24a77852a72ae4befb9fd7fe4e3f7fd98fa8e020
nskelsey <nskelsey@gmail.com>
2017-08-29 19:18:33 +0200

    Implement TestInstanceHandler and TestCollectionHandler
    the new base class tests would allow to uniform and simplify tests for the common handlers.
    The classes are now experimented just on the tests for Contexts and Questionnaires.

--- backend/globaleaks/tests/handlers/admin/test_context.py
+++ backend/globaleaks/tests/handlers/admin/test_context.py
@@ -5,57 +5,23 @@ from globaleaks.rest import errors
 from globaleaks.tests import helpers
 from twisted.internet.defer import inlineCallbacks
 
-# special guest:
-
-stuff = u"³²¼½¬¼³²"
-
-
-class TestContextsCollection(helpers.TestHandler):
+class TestContextsCollection(helpers.TestCollectionHandler):
     _handler = context.ContextsCollection
-
-    def test_get(self):
-        handler = self.request(role='admin')
-        return handler.get()
-
-    @inlineCallbacks
-    def test_post(self):
-        for attrname in Context.localized_keys:
-            self.dummyContext[attrname] = stuff
-
-        handler = self.request(self.dummyContext, role='admin')
-        response = yield handler.post()
-
-        self.dummyContext['id'] = response['id']
-        self.assertEqual(response['description'], stuff)
+    _test_desc = {
+      'model': Context,
+      'create': context.create_context,
+      'data': {
+          'tip_timetolive': 100
+      }
+    }
 
 
-class TestContextInstance(helpers.TestHandlerWithPopulatedDB):
+class TestContextInstance(helpers.TestInstanceHandler):
     _handler = context.ContextInstance
-
-    @inlineCallbacks
-    def test_put(self):
-        for attrname in Context.localized_keys:
-            self.dummyContext[attrname] = stuff
-
-        handler = self.request(self.dummyContext, role='admin')
-        response = yield handler.put(self.dummyContext['id'])
-        self.assertEqual(response['description'], stuff)
-
-    @inlineCallbacks
-    def test_update_context_timetolive(self):
-        self.dummyContext['tip_timetolive'] = 100
-
-        for attrname in Context.localized_keys:
-            self.dummyContext[attrname] = stuff
-
-        handler = self.request(self.dummyContext, role='admin')
-        response = yield handler.put(self.dummyContext['id'])
-
-        self.assertEqual(response['tip_timetolive'], self.dummyContext['tip_timetolive'])
-
-    @inlineCallbacks
-    def test_delete(self):
-        handler = self.request(self.dummyContext, role='admin')
-        yield handler.delete(self.dummyContext['id'])
-        yield self.assertFailure(handler.delete(self.dummyContext['id']),
-                                 errors.ContextIdNotFound)
+    _test_desc = {
+      'model': Context,
+      'create': context.create_context,
+      'data': {
+          'tip_timetolive': 100
+      }
+    }
