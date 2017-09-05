067a4367ceb5cf50a7bac490ac3d1a5ce4b4a1b4
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-09-05 09:20:32 +0200

    Raise coverage of objectdict util

---
+++ backend/globaleaks/tests/utils/test_objectdict.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from twisted.trial import unittest
+from globaleaks.utils.objectdict import ObjectDict
+
+class TestUtility(unittest.TestCase):
+
+    def test_object_dict(self):
+        od = ObjectDict()
+        self.assertRaises(AttributeError, getattr, od, 'something')
+        od['foo'] = 'bar'
+        self.assertEqual(od['foo'], 'bar')
+        self.assertEqual(od.foo, 'bar')
+        od.key = 'value'
+        self.assertEqual(od['key'], 'value')
