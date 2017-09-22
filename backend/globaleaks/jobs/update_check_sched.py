4289fe12054749807ac0dc1347b616adca68553a
nskelsey <nskelsey@gmail.com>
2017-09-22 15:45:20 +0200

    Uniformly handle outgoing network failures in LoopingJobs
    These changes resolve #984 and provide a structure for handling other
    errors which may be thrown by the socks port during normal execution.

--- backend/globaleaks/jobs/update_check_sched.py
+++ backend/globaleaks/jobs/update_check_sched.py
@@ -2,17 +2,17 @@
 from distutils.version import LooseVersion as V  # pylint: disable=no-name-in-module,import-error
 
 from debian import deb822
-from globaleaks.jobs.base import LoopingJob
+from globaleaks.jobs.base import ExternNetLoopingJob
 from globaleaks.settings import GLSettings
 from globaleaks.utils.agent import get_page
 from globaleaks.utils.utility import log
 from twisted.internet.defer import inlineCallbacks
-from twisted.internet.error import ConnectionRefusedError
+
 
 DEB_PACKAGE_URL = 'https://deb.globaleaks.org/xenial/Packages'
 
 
-class UpdateCheckJob(LoopingJob):
+class UpdateCheckJob(ExternNetLoopingJob):
     name = "Update Check"
     interval = 60*60*24
     threaded = False
@@ -28,9 +28,3 @@ class UpdateCheckJob(LoopingJob):
         versions.sort(key=V)
         GLSettings.appstate.latest_version = versions[-1]
         log.debug('The newest version in the repository is: %s', GLSettings.appstate.latest_version)
-
-    def on_error(self, excep):
-        if isinstance(excep, ConnectionRefusedError):
-            log.err('New version check failed: %s', excep)
-        else:
-            super(UpdateCheckJob, self).on_error(excep)
