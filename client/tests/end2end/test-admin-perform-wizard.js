db4b03776f49e5f2c22555a5cb82d7e93fd55934
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-12-09 01:12:02 +0100

    Fix html elements with the same id

--- client/tests/end2end/test-admin-perform-wizard.js
+++ client/tests/end2end/test-admin-perform-wizard.js
@@ -8,35 +8,35 @@ describe('globaLeaks setup wizard', function() {
     element(by.model('GLTranslate.indirect.appLanguage')).element(by.xpath(".//*[text()='English']")).click();
     browser.gl.utils.waitUntilPresent(by.cssContainingText("form", "Welcome to GlobaLeaks!"));
 
-    element.all(by.id('ButtonNext')).get(0).click();
+    element.all(by.css('.ButtonNext')).get(0).click();
 
     element(by.model('wizard.node_name')).sendKeys('E2E Test Instance');
 
-    element.all(by.id('ButtonNext')).get(1).click();
+    element.all(by.css('.ButtonNext')).get(1).click();
 
     element(by.id('profile-0')).click();
 
-    element.all(by.id('ButtonNext')).get(2).click();
+    element.all(by.css('.ButtonNext')).get(2).click();
 
     element(by.model('wizard.admin_name')).sendKeys('Admin');
     element(by.model('wizard.admin_mail_address')).sendKeys('globaleaks-admin@mailinator.com');
     element(by.model('wizard.admin_password')).sendKeys(browser.gl.utils.vars['user_password']);
     element(by.model('admin_check_password')).sendKeys(browser.gl.utils.vars['user_password']);
 
-    element.all(by.id('ButtonNext')).get(3).click();
+    element.all(by.css('.ButtonNext')).get(3).click();
 
     element(by.model('wizard.receiver_name')).sendKeys('Recipient1');
     element(by.model('wizard.receiver_mail_address')).sendKeys('globaleaks-receiver1@mailinator.com');
 
-    element.all(by.id('ButtonNext')).get(4).click();
+    element.all(by.css('.ButtonNext')).get(4).click();
 
     element.all(by.css('.tos-agreement')).click();
 
-    element.all(by.id('ButtonNext')).get(5).click();
+    element.all(by.css('.ButtonNext')).get(5).click();
 
     expect(element(by.css('.congratulations')).isPresent()).toBe(true);
 
-    element.all(by.id('ButtonNext')).get(6).click();
+    element.all(by.css('.ButtonNext')).get(6).click();
 
     browser.gl.utils.waitForUrl('/admin/home');
   });
