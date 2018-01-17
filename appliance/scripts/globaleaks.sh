86879af0e0992c737da87710abde0aaa9df287b5
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-01-17 14:50:29 +0100

    Delete outdated appliance scripts

--- appliance/scripts/globaleaks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Decomment to run the official install script
-# wget https://deb.globaleaks.org/install-globaleaks.sh
-# chmod +x install-globaleaks.sh
-#./install-globaleaks.sh
--- appliance/scripts/zerodisk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
