e794a113ab519e531c64001807e8a3bea19b11b2
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-01-23 22:59:24 +0100

    Improve code coverage of test_wizard and test_orm

--- backend/globaleaks/tests/handlers/test_wizard.py
+++ backend/globaleaks/tests/handlers/test_wizard.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-import os
-
 from globaleaks.handlers import wizard
-from globaleaks.models.profiles import load_profile
-from globaleaks.orm import transact
 from globaleaks.rest import errors
-from globaleaks.settings import Settings
 from globaleaks.tests import helpers
 from twisted.internet.defer import inlineCallbacks
 
@@ -18,24 +13,6 @@ class TestWizard(helpers.TestHandler):
         handler = self.request(self.dummyWizard)
         yield handler.post()
 
-    @inlineCallbacks
-    def test_fail_after_first_post(self):
-        handler = self.request(self.dummyWizard)
-        yield handler.post()
-
+        # should fail if the wizard has been already completed
         handler = self.request(self.dummyWizard)
         yield self.assertFailure(handler.post(), errors.ForbiddenOperation)
-
-
-class TestProfileLoad(helpers.TestHandler):
-
-    @inlineCallbacks
-    def setUp(self):
-        yield helpers.TestHandler.setUp(self)
-        self.old_path = Settings.client_path
-        Settings.client_path = os.path.join(helpers.DATA_DIR, '..')
-
-    @inlineCallbacks
-    def tearDown(self):
-        Settings.client_path = self.old_path
-        yield helpers.TestHandler.tearDown(self)
