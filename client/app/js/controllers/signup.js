581bf538cb89c8a2fd908ba2d31b1c59427754c9
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-02-02 18:42:44 +0100

    Add missing signup.js controller

---
+++ client/app/js/controllers/signup.js
@@ -0,0 +1,50 @@
+    GLClient.controller('SignupCtrl', ['$scope', '$location', '$route', '$http', 'Authentication', 'CONSTANTS',
+                    function($scope, $location, $route, $http, Authentication, CONSTANTS) {
+
+  $scope.email_regexp = CONSTANTS.email_regexp;
+
+  $scope.hostname = $location.search().hostname || '';
+
+  $scope.step = 1;
+  $scope.signup = {
+    'subdomain': '',
+    'name': '',
+    'surname': '',
+    'email': '',
+    'use_case': '',
+    'use_case_other': ''
+  };
+
+  var completed = false;
+
+  $scope.complete = function() {
+    if (completed) {
+        return;
+    }
+
+    completed = true;
+
+    $http.post('signup', $scope.signup).then(function() {
+      $scope.step += 1;
+    });
+  };
+}]).
+controller('SignupActivationCtrl', ['$scope', '$location', '$route', '$http', '$window', 'CONSTANTS',
+                    function($scope, $location, $route, $http, $window, CONSTANTS) {
+  var token = $location.search().token;
+  $scope.login_url = '';
+
+  if (token) {
+    $http.get('signup/' + token).then(function(response) {
+      $scope.data = response.data;
+    });
+  }
+
+  $scope.goToAdminInterface = function() {
+    $window.open($scope.data.admin_login_url, '_blank');
+  };
+
+  $scope.goToRecipientInterface = function() {
+    $window.open($scope.data.recipient_login_url, '_blank');
+  };
+}]);
