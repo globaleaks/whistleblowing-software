bd08cc4f7f4d4750263133efd390f32da0b92443
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-02-16 18:57:08 +0100

    Improve testing of short url handler

--- backend/globaleaks/tests/handlers/test_shorturl.py
+++ backend/globaleaks/tests/handlers/test_shorturl.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
+from twisted.internet.defer import inlineCallbacks
 from globaleaks.handlers import shorturl
 from globaleaks.handlers.admin import shorturl as admin_shorturl
+from globaleaks.rest import errors
 from globaleaks.tests import helpers
-from twisted.internet.defer import inlineCallbacks
 
 
-class TestShortUrlInstance(helpers.TestHandlerWithPopulatedDB):
-    _handler = shorturl.ShortUrlInstance
-
-    @inlineCallbacks
-    def test_get_existent_shorturl(self):
-        shorturl_desc = self.get_dummy_shorturl()
-        shorturl_desc = yield admin_shorturl.create_shorturl(1, shorturl_desc)
-
-        handler = self.request(role='admin')
-        yield handler.get(shorturl_desc['shorturl'])
+class TestShortURLCollection(helpers.TestHandlerWithPopulatedDB):
+    _handler = shorturl.ShortURL
 
     @inlineCallbacks
-    def test_get_nonexistent_shorturl(self):
+    def test_get(self):
         handler = self.request(role='admin')
-        yield handler.get(u'/s/nonexistent')
+        self.assertFailure(handler.get(u'/u/unexistent'), errors.ResourceNotFound)
+
+        for i in range(3):
+            req = self.get_dummy_shorturl(str(i))
+            yield admin_shorturl.create_shorturl(1, req)
+            handler = self.request(role='admin')
+            response = yield handler.get(unicode(req['shorturl']))
+            self.assertEqual(handler.request.responseHeaders.getRawHeaders('location')[0], req['longurl'])
