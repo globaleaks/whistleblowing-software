5167520d086416e1b91dd9146abfd27e0ae58dec
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-02-23 15:38:19 +0100

    Add packaging for Debian Stretch 8.10 (#2171)

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,12 +12,12 @@ usage() {
   echo " -h"
   echo -e " -t tagname (build specific release/branch)"
   echo -e " -l (Use local repository & enviroment)"
-  echo -e " -d distribution (available: trusty, xenial, wheezy, jessie)"
+  echo -e " -d distribution (available: trusty, xenial, wheezy, jessie, stretch)"
   echo -e " -n (do not sign)"
   echo -e " -p (push on repository)"
 }
 
-TARGETS="trusty xenial wheezy jessie"
+TARGETS="trusty xenial wheezy jessie stretch"
 DISTRIBUTION="trusty"
 TAG="master"
 LOCAL_ENV=0
