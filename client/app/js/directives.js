globaleaks/whistleblowing-software

commit 4e97c09debc3cb3c3b4670154af8caccbee30707

Correctly handle async validation for PGP keys
Fixes #2218 due to multiple changes.

OpenPGP.js 3.0.2 is required to resolve issues with some PGP keys that have
errorously reported expired.

python-gnupg 0.4.1 allows for newer versions of GnuPG to work successfully.

glbcKeyLib.validPublicKey was modified to properly handle some operations
that are async in openpgpjs; the directive and controllers were modified to
become asyncValidators.

Signed-off-by: Michael Casadevall <michael@casadevall.pro>

client/app/js/directives.js
@@ -259,10 +259,11 @@ directive('pgpPubkeyDisplay', ['pgp', 'glbcKeyLib', function(pgp, glbcKeyLib) {
         if (newVal === "") {
           return;
         }
-        scope.is_valid_key = glbcKeyLib.validPublicKey(newVal);
-        if (scope.is_valid_key) {
-          scope.key_details = pgpKeyDetails(newVal);
-        }
+        glbcKeyLib.validPublicKey(newVal).then(result => {
+          if (result) {
+            scope.key_details = pgpKeyDetails(newVal);
+          }
+        });
       });
     },
   };
@@ -272,7 +273,7 @@ directive('pgpPubkeyDisplay', ['pgp', 'glbcKeyLib', function(pgp, glbcKeyLib) {
 // containing form's ngModelController NOT the ngModel bound to the value of the
 // text-area itself. If the key word 'canBeEmpty' the pgp key validator is disabled
 // when the textarea's input is empty.
-directive('pgpPubkeyValidator', ['glbcKeyLib', function(glbcKeyLib) {
+directive('pgpPubkeyValidator', ['$q', 'glbcKeyLib', function($q, glbcKeyLib) {
   // scope is the directives scope
   // elem is a jqlite reference to the bound element
   // attrs is the list of directives on the element
@@ -282,14 +283,16 @@ directive('pgpPubkeyValidator', ['glbcKeyLib', function(glbcKeyLib) {
     scope.canBeEmpty = scope.pgpPubkeyValidator === 'canBeEmpty';
 
     // modelValue is the models value, viewVal is displayed on the page.
-    ngModel.$validators.pgpPubKeyValidator = function(modelVal) {
+    ngModel.$asyncValidators.pgpPubKeyValidator = function(modelVal) {
       // Check for obvious problems.
+      var defer = $q.defer();
+
       if (typeof modelVal !== 'string') {
         modelVal = '';
       }
 
       if (scope.canBeEmpty && modelVal === '') {
-        return true;
+        return $q.reject();
       }
 
       return glbcKeyLib.validPublicKey(modelVal);

client/app/js/controllers/preferences.js
@@ -1,5 +1,5 @@
-GLClient.controller('PreferencesCtrl', ['$scope', '$rootScope', 'Utils', 'CONSTANTS',
-  function($scope, $rootScope, Utils, CONSTANTS) {
+GLClient.controller('PreferencesCtrl', ['$scope', '$q', '$rootScope', 'Utils', 'CONSTANTS',
+  function($scope, $q, $rootScope, Utils, CONSTANTS) {
     $scope.tabs = [
       {
         title: "Preferences",

client/app/js/crypto/main.js
@@ -71,13 +71,15 @@ angular.module('GLBrowserCrypto', [])
        * @return {Bool}
        */
       validPublicKey: function(textInput) {
+        var defer = $q.defer();
+
         if (typeof textInput !== 'string') {
-          return false;
+          return defer.reject();
         }
 
         var s = textInput.trim();
         if (s.substr(0, 5) !== '-----') {
-          return false;
+          return defer.reject();
         }
 
         // Try to parse the key.
@@ -85,32 +87,36 @@ angular.module('GLBrowserCrypto', [])
         try {
           result = pgp.key.readArmored(s);
         } catch (err) {
-          return false;
+          return defer.reject();
         }
 
         // Assert that the parse created no errors.
         if (angular.isDefined(result.err)) {
-          return false;
+          return defer.reject();
         }
 
         // Assert that there is only one key in the input.
         if (result.keys.length !== 1) {
-          return false;
+          return defer.reject();
         }
 
         var key = result.keys[0];
 
         // Assert that the key type is not private and the public flag is set.
         if (!key.isPublic() || key.isPrivate()) {
-          return false;
+          return defer.reject();
         }
 
         // Verify expiration, revocation, and self sigs.
-        if (key.verifyPrimaryKey() !== pgp.enums.keyStatus.valid) {
-          return false;
-        }
+        key.verifyPrimaryKey().then(keyStatus => {
+          if (keyStatus === pgp.enums.keyStatus.valid) {
+            defer.resolve(true);
+          } else {
+            defer.reject();
+          }
+        })
 
-        return true;
+        return defer.promise;
       },
     };
 }]);

commit 0800ea1def367226f56c7eb7ebe7ece94b6a2141

Fix minor regression on key validation preventing to save users with empty key
Regression introducted in commit: https://github.com/globaleaks/GlobaLeaks/commit/4e97c09debc3cb3c3b4670154af8caccbee30707

client/app/js/directives.js
@@ -300,7 +300,7 @@ directive('pgpPubkeyValidator', ['$q', 'glbcKeyLib', function($q, glbcKeyLib) {
       }
 
       if (scope.canBeEmpty && modelVal === '') {
-        return $q.reject();
+        return $q.resolve(true);
       }
 
       return glbcKeyLib.validPublicKey(modelVal);