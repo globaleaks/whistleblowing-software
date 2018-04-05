419124c4c4497fb95d73b46c527d089dd70f2c00
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-04-05 23:30:48 +0200

    Implement skeleton for db migration 40

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -11,7 +11,7 @@ __email__ = u'info@globaleaks.org'
 __version__ = u'3.0.19'
 __license__ = u'AGPL-3.0'
 
-DATABASE_VERSION = 39
+DATABASE_VERSION = 40
 FIRST_DATABASE_VERSION_SUPPORTED = 24
 
 # Add new languages as they are supported here! To do this retrieve the name of
