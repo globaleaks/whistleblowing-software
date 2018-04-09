bb26b5f30e9007d64e529b2efb450e252df7e6a2
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-04-09 12:08:34 +0200

    Fix migrations of instances where usernames have been used duplicated

--- backend/globaleaks/db/migrations/update_40/__init__.py
+++ backend/globaleaks/db/migrations/update_40/__init__.py
@@ -31,3 +31,22 @@ class MigrationScript(MigrationBase):
                     setattr(new_obj, key, getattr(old_obj, key))
 
             self.session_new.add(new_obj)
+
+    def migrate_User(self):
+        usernames = {}
+        old_objs = self.session_old.query(self.model_from['User'])
+        for old_obj in old_objs:
+            new_obj = self.model_to['User']()
+            for key in [c.key for c in new_obj.__table__.columns]:
+                setattr(new_obj, key, getattr(old_obj, key))
+
+            if new_obj.tid not in usernames:
+                usernames[new_obj.tid] = {}
+
+            if new_obj.username in usernames[new_obj.tid]:
+                new_obj.username = new_obj.id
+
+            usernames[new_obj.tid][new_obj.username] = True
+
+            self.session_new.add(new_obj)
+            self.session_new.commit()
