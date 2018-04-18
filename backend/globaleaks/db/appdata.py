4a12171d3b0e20474d6c513b0757221f32752906
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-04-18 14:18:16 +0200

    Fix migration default fields attributes in relation to wb_identity

--- backend/globaleaks/db/appdata.py
+++ backend/globaleaks/db/appdata.py
@@ -59,30 +59,28 @@ def db_fix_fields_attrs(session):
     """
     field_attrs = read_json_file(Settings.field_attrs_file)
 
-    special_lst = [u'whistleblower_identity']
-
     std_lst = [u'inputbox', u'textarea', u'multichoice', u'checkbox', u'tos', u'date']
 
     for field_type, attrs_dict in field_attrs.items():
         attrs_to_keep_for_type = attrs_dict.keys()
         if field_type in std_lst:
             # Ensure that the standard field attrs do not have extra attr rows
-            res = session.query(models.FieldAttr).filter(not_(models.FieldAttr.name.in_(attrs_to_keep_for_type)),
-                                                       models.FieldAttr.field_id == models.Field.id,
-                                                       models.Field.type == field_type,
-                                                       not_(models.Field.id.in_(special_lst)))
+            _filter = not_(models.FieldAttr.name.in_(attrs_to_keep_for_type)), \
+                      models.FieldAttr.field_id == models.Field.id, \
+                      models.Field.type == field_type, \
+                      models.Field.template_id == None
         else:
             # Look for dropped attrs in non-standard field_groups like whistleblower_identity
-            res = session.query(models.FieldAttr).filter(not_(models.FieldAttr.name.in_(attrs_to_keep_for_type)),
-                                                       models.FieldAttr.field_id == models.Field.id,
-                                                       models.Field.id == field_type)
+            _filter = not_(models.FieldAttr.name.in_(attrs_to_keep_for_type)), \
+                      models.FieldAttr.field_id == models.Field.id, \
+                      models.Field.template_id == field_type
 
-        for r in res:
-            session.delete(r)
+        for res in session.query(models.FieldAttr).filter(*_filter):
+            session.delete(res)
 
     # Add keys to the db that have been added to field_attrs
     for field in session.query(models.Field):
-        type = field.type if field.template_id not in special_lst else field.template_id
+        type = field.type if field.template_id is None else field.template_id
         attrs = field_attrs.get(type, {})
         db_add_field_attrs(session, field.id, attrs)
 
