f1aa7e2285f522bd05a3cf76ceb4e5f41af8db86
Michael Casadevall <michael@casadevall.pro>
2018-04-23 01:26:13 -0400

    Fix lint errors and clean up some code
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/utils/httpsproxy.py
+++ backend/globaleaks/utils/httpsproxy.py
@@ -2,10 +2,7 @@
 import io
 import sys
 
-if sys.version[0] == '2':
-    import urlparse
-else:
-    from urllib.parse import urlparse
+from six.moves import urllib
 
 from twisted.internet import reactor, protocol, defer
 from twisted.internet.protocol import connectionDone
@@ -93,7 +90,8 @@ class HTTPStreamProxyRequest(http.Request):
             self.reset_buffer()
 
     def process(self):
-        proxy_url = bytes(urlparse.urljoin(self.channel.proxy_url, self.uri))
+        joined_url = urllib.parse.urljoin(self.channel.proxy_url, self.uri)
+        proxy_url = bytes(urllib.parse.urlparse(joined_url))
 
         hdrs = self.requestHeaders
         hdrs.setRawHeaders('GL-Forwarded-For', [self.getClientIP()])
