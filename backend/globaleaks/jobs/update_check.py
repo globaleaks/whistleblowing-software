7496d9d898ced258a94b1a9d361c7e4ccf12807c
Michael Casadevall <michael@casadevall.pro>
2018-04-23 02:19:27 +0000

    Fix futurize change that was non-sensical
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/jobs/update_check.py
+++ backend/globaleaks/jobs/update_check.py
@@ -50,8 +50,6 @@ class UpdateCheck(NetLoopingJob):
 
     @inlineCallbacks
     def operation(self):
-        yield 1
-        return
         log.debug('Fetching latest GlobaLeaks version from repository')
         packages_file = yield self.fetch_packages_file()
         versions = [p['Version'] for p in deb822.Deb822.iter_paragraphs(packages_file) if p['Package'] == 'globaleaks']
