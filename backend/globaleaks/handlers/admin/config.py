7793558840d604d4ca6d6efab414b4a9bcbf3215
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-04-27 19:41:39 +0200

    Add some fixes (maybe?) for py3 compatibility

--- backend/globaleaks/handlers/admin/config.py
+++ backend/globaleaks/handlers/admin/config.py
@@ -19,7 +19,7 @@ def check_hostname(session, tid, input_hostname):
     Ensure the hostname does not collide across tenants or include an origin
     that it shouldn't.
     """
-    if input_hostname == u'':
+    if input_hostname == '':
         raise errors.InputValidationError('Hostname cannot be empty')
 
     root_hostname = ConfigFactory(session, 1, 'node').get_val(u'hostname')
@@ -28,8 +28,9 @@ def check_hostname(session, tid, input_hostname):
     if tid != 1 and root_hostname != '':
         forbidden_endings.append(root_hostname)
 
-    if reduce(lambda b, v: b or input_hostname.endswith(v), forbidden_endings, False):
-        raise errors.InputValidationError('Hostname contains a forbidden origin')
+    for v in forbidden_endings:
+        if input_hostname.endswith(v):
+            raise errors.InputValidationError('Hostname contains a forbidden origin')
 
     existing_hostnames = {h.get_v() for h in session.query(Config) \
                                                     .filter(Config.tid != tid,
@@ -65,11 +66,11 @@ class AdminConfigHandler(OperationHandler):
         url = urlunsplit(('http', req_args['value'], 'robots.txt', None, None)).encode()
 
         try:
-            resp = yield net_agent.request('GET', url)
+            resp = yield net_agent.request(b'GET', url)
             body = yield readBody(resp)
 
-            server_h = resp.headers.getRawHeaders('Server', [None])[-1].lower()
-            if not body.startswith('User-agent: *') or server_h != 'globaleaks':
+            server_h = resp.headers.getRawHeaders(b'Server', [None])[-1].lower()
+            if not body.startswith(b'User-agent: *') or server_h != b'globaleaks':
                 raise EnvironmentError('Response unexpected')
 
         except Exception as e:
