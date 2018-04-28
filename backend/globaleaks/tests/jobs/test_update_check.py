11b4743a894aa00ccdda971c0f60eca819087b82
Michael Casadevall <michael@casadevall.pro>
2018-04-28 06:02:23 -0400

    Correct update check and ensure test case passes
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/tests/jobs/test_update_check.py
+++ backend/globaleaks/tests/jobs/test_update_check.py
@@ -9,24 +9,24 @@ from globaleaks.orm import transact
 from globaleaks.state import State
 from globaleaks.tests import helpers
 
-packages="Package: globaleaks\n" \
-         "Version: 0.0.1\n" \
-         "Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
-         "Package: globaleaks\n" \
-         "Version: 1.0.0\n" \
-         "Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
-         "Package: globaleaks\n" \
-         "Version: 1.2.3\n" \
-         "Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
-         "Package: globaleaks\n" \
-         "Version: 2.0.666\n" \
-         "Filename: xenial/globaleaks_2.0.9_all.deb\n\n" \
-         "Package: globaleaks\n" \
-         "Version: 2.0.1337\n" \
-         "Filename: xenial/globaleaks_2.0.100_all.deb\n\n" \
-         "Package: tor2web\n" \
-         "Version: 31337\n" \
-         "Filename: xenial/tor2web_31337_all.deb\n\n"
+packages=b"Package: globaleaks\n" \
+         b"Version: 0.0.1\n" \
+         b"Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
+         b"Package: globaleaks\n" \
+         b"Version: 1.0.0\n" \
+         b"Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
+         b"Package: globaleaks\n" \
+         b"Version: 1.2.3\n" \
+         b"Filename: xenial/globaleaks_1.0.0_all.deb\n\n" \
+         b"Package: globaleaks\n" \
+         b"Version: 2.0.666\n" \
+         b"Filename: xenial/globaleaks_2.0.9_all.deb\n\n" \
+         b"Package: globaleaks\n" \
+         b"Version: 2.0.1337\n" \
+         b"Filename: xenial/globaleaks_2.0.100_all.deb\n\n" \
+         b"Package: tor2web\n" \
+         b"Version: 31337\n" \
+         b"Filename: xenial/tor2web_31337_all.deb\n\n"
 
 
 @transact
