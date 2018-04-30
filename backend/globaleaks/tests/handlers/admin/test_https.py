9be3508ae626360a5c6fec5fe78ec39d68d4e289
Michael Casadevall <michael@casadevall.pro>
2018-04-30 05:41:33 -0400

    Disable test that dials out to LetsEncrypt so that internet access is not required for GL tests
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/tests/handlers/admin/test_https.py
+++ backend/globaleaks/tests/handlers/admin/test_https.py
@@ -195,18 +195,18 @@ class TestCSRHandler(helpers.TestHandler):
 class TestAcmeHandler(helpers.TestHandler):
     _handler = https.AcmeHandler
 
-    @inlineCallbacks
-    def test_post(self):
-        hostname = 'gl.dl.localhost.com'
-        State.tenant_cache[1].hostname = hostname
-        valid_setup = test_tls.get_valid_setup()
-        yield https.PrivKeyFileRes.create_file(1, valid_setup['key'])
-
-        handler = self.request(role='admin')
-        resp = yield handler.post()
-
-        current_le_tos = 'https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf'
-        self.assertEqual(resp['terms_of_service'], current_le_tos)
+#    @inlineCallbacks
+#    def test_post(self):
+#        hostname = 'gl.dl.localhost.com'
+#        State.tenant_cache[1].hostname = hostname
+#        valid_setup = test_tls.get_valid_setup()
+#        yield https.PrivKeyFileRes.create_file(1, valid_setup['key'])
+#
+#        handler = self.request(role='admin')
+#        resp = yield handler.post()
+#
+#        current_le_tos = 'https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf'
+#        self.assertEqual(resp['terms_of_service'], current_le_tos)
 
     @inlineCallbacks
     def test_put(self):
