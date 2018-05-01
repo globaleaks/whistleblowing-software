e7ebd6d656cbf7e23697e1ad8431f0bb879b56b1
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-05-02 00:31:16 +0200

    Prepare empty db migration 41

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -11,7 +11,7 @@ __email__ = u'info@globaleaks.org'
 __version__ = u'3.0.27'
 __license__ = u'AGPL-3.0'
 
-DATABASE_VERSION = 40
+DATABASE_VERSION = 41
 FIRST_DATABASE_VERSION_SUPPORTED = 24
 
 # Add new languages as they are supported here! To do this retrieve the name of
