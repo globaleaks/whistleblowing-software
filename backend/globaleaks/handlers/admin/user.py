80591c75f2a6f019f299bf5059bfccd18cf0ad2b
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-05-02 00:16:50 +0200

    Fix bug related to overlapping usernames

--- backend/globaleaks/handlers/admin/user.py
+++ backend/globaleaks/handlers/admin/user.py
@@ -99,9 +99,6 @@ def db_create_user(session, state, tid, request, language):
         'mail_address': request['mail_address']
     })
 
-    if not request['username']:
-        user.username = user.id
-
     if request['password']:
         password = request['password']
     elif user.role == 'receiver':
@@ -119,6 +116,9 @@ def db_create_user(session, state, tid, request, language):
     session.add(user)
 
     session.flush()
+
+    if not request['username']:
+        user.username = user.id
 
     return user
 
