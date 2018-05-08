754b02284089a027e8ddb24fec4331964d779eb4
Michael Casadevall <michael@casadevall.pro>
2018-05-08 05:47:14 -0400

    Reaching a stopping point for now. Started coding model reads but they're not working just yet. Once implemented, email change is more or less done \o/
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/handlers/email_validation.py
+++ backend/globaleaks/handlers/email_validation.py
@@ -2,6 +2,7 @@
 #
 # Validates the token for email changes
 
+import globaleaks
 from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
 from globaleaks.orm import transact
@@ -19,8 +20,19 @@ def generate_email_change_token(session, user, new_email):
 
     return validation_token
 
+@transact
+def get_email_change_token(session, validation_token):
+    return models.db_get(session, models.EmailValidations, models.EmailValidations == validation_token)
+
+
 class EmailValidation(BaseHandler):
     check_roles = '*'
 
     def get(self, validation_token):
-        return self.redirect("/#/email/validation/success")
+        print(validation_token)
+        try:
+            token = get_email_change_token(validation_token)
+            return self.redirect("/#/email/validation/success")
+        except globaleaks.rest.errors.ModelNotFound:
+            return self.redirect("/#/email/validation/failure")
+
