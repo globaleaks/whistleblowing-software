8e58da4165452ba0ffb9bafe77064c73e800049e
Michael Casadevall <michael@casadevall.pro>
2018-05-08 05:32:36 -0400

    Successfully get email generation tokens to generate. One per user upon change
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/handlers/email_validation.py
+++ backend/globaleaks/handlers/email_validation.py
@@ -2,7 +2,22 @@
 #
 # Validates the token for email changes
 
+from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
+from globaleaks.orm import transact
+from globaleaks.utils.security import generateRandomKey
+
+def generate_email_change_token(session, user, new_email):
+    validation_token = generateRandomKey(32)
+
+    validation_obj = models.EmailValidations()
+    validation_obj.user_id = user.id
+    validation_obj.new_email = new_email
+    validation_obj.validation_token = validation_token
+    session.merge(validation_obj)
+    session.flush()
+
+    return validation_token
 
 class EmailValidation(BaseHandler):
     check_roles = '*'
