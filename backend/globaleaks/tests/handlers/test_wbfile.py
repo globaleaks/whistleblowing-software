1f64dc4b9330dffa1e3c2650b0aa9d7e46348ba0
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-05-09 12:51:27 +0200

    Fix bug in secure deletion of files loaded by recipients

--- backend/globaleaks/tests/handlers/test_wbfile.py
+++ backend/globaleaks/tests/handlers/test_wbfile.py
@@ -43,7 +43,6 @@ class TestWBFileWorkFlow(helpers.TestHandlerWithPopulatedDB):
 
                 yield self.test_model_count(models.SecureFileDelete, len(deleted_wbfiles_ids))
 
-
         # check that the files are effectively gone from the db
         rtips_desc = yield self.get_rtips()
         for rtip_desc in rtips_desc:
