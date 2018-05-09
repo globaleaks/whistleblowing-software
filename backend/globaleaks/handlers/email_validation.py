061e10e4f8b3923a5a6e1c0d35c064c510bd6ede
Michael Casadevall <michael@casadevall.pro>
2018-05-09 02:48:06 -0400

    Commit WIP work
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/handlers/email_validation.py
+++ backend/globaleaks/handlers/email_validation.py
@@ -2,9 +2,11 @@
 #
 # Validates the token for email changes
 
-import globaleaks
+from twisted.internet.defer import inlineCallbacks
+
 from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
+from globaleaks.handlers.user import user_serialize_user
 from globaleaks.orm import transact
 from globaleaks.utils.security import generateRandomKey
 
@@ -22,17 +24,45 @@ def generate_email_change_token(session, user, new_email):
 
 @transact
 def get_email_change_token(session, validation_token):
-    return models.db_get(session, models.EmailValidations, models.EmailValidations == validation_token)
+    return db_get_email_change_token(session, validation_token)
 
+@transact
+def delete_email_change_token(session, validation_token):
+    return db_delete_email_change_token(session, validation_token)
+
+def db_get_email_change_token(session, validation_token):
+    token = session.query(models.EmailValidations).filter(
+        models.EmailValidations.validation_token == validation_token
+    ).first()
+
+    return token
+
+def db_delete_email_change_token(session, validation_token):
+    session.query(models.EmailValidations).filter(
+        models.EmailValidations.validation_token == validation_token
+    ).delete()
+    session.flush()
+
+@transact
+def change_user_email(session, user_id, email):
+    from globaleaks.handlers.admin.user import get_user
+
+    user = get_user
+    print(user)
+    user.mail_address = email
+    session.flush()
 
 class EmailValidation(BaseHandler):
     check_roles = '*'
 
+    @inlineCallbacks
     def get(self, validation_token):
-        print(validation_token)
-        try:
-            token = get_email_change_token(validation_token)
+        token = yield get_email_change_token(validation_token)
+        print(token.user_id)
+        if token is not None:
+            yield change_user_email(token.user_id, token.new_email)
+            #yield db_delete_email_change_token(validation_token)
             return self.redirect("/#/email/validation/success")
-        except globaleaks.rest.errors.ModelNotFound:
+        else:
             return self.redirect("/#/email/validation/failure")
 
