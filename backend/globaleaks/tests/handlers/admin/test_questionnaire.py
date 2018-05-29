2fa317bea46db0ba43319ba57c8ee384eb9cfd8f
Michael Casadevall <michael@casadevall.pro>
2018-05-29 11:59:32 -0400

    Implement possibility to clone an existing questionnaire
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- backend/globaleaks/tests/handlers/admin/test_questionnaire.py
+++ backend/globaleaks/tests/handlers/admin/test_questionnaire.py
@@ -6,6 +6,8 @@ from sqlalchemy.exc import IntegrityError
 from twisted.python import failure
 from twisted.internet.defer import inlineCallbacks
 
+from globaleaks import models
+from globaleaks.orm import transact
 from globaleaks.handlers.admin import questionnaire
 from globaleaks.models import Questionnaire
 from globaleaks.rest import errors
@@ -16,11 +18,11 @@ from globaleaks.utils.utility import read_json_file
 class TestQuestionnairesCollection(helpers.TestCollectionHandler):
     _handler = questionnaire.QuestionnairesCollection
     _test_desc = {
-      'model': Questionnaire,
-      'create': questionnaire.create_questionnaire,
-      'data': {
-        'name': 'test'
-      }
+        'model': Questionnaire,
+        'create': questionnaire.create_questionnaire,
+        'data': {
+            'name': 'test'
+        }
     }
 
     @inlineCallbacks
@@ -44,9 +46,50 @@ class TestQuestionnairesCollection(helpers.TestCollectionHandler):
 class TestQuestionnaireInstance(helpers.TestInstanceHandler):
     _handler = questionnaire.QuestionnaireInstance
     _test_desc = {
-      'model': Questionnaire,
-      'create': questionnaire.create_questionnaire,
-      'data': {
-        'name': 'test'
-      }
+        'model': Questionnaire,
+        'create': questionnaire.create_questionnaire,
+        'data': {
+            'name': 'test'
+        }
     }
+
+
+class TestQuestionnareDuplication(helpers.TestHandlerWithPopulatedDB):
+    _handler = questionnaire.QuestionnareDuplication
+
+    @transact
+    def get_questionnare_count(self, session):
+        '''Gets a count of the questionnaires'''
+        return session.query(models.Questionnaire).count()
+
+    @transact
+    def get_new_questionnare(self, session):
+        '''Returns first questionnare ID'''
+        questionnare_obj = session.query(models.Questionnaire).filter(
+            models.Questionnaire.id != 'default').first()
+        session.expunge(questionnare_obj)
+        return questionnare_obj
+
+    @inlineCallbacks
+    def setUp(self):
+        yield helpers.TestHandlerWithPopulatedDB.setUp(self)
+
+    @inlineCallbacks
+    def test_duplicate_questionnaire(self):
+        # Sanity check our base behavior
+        questionnaire_count = yield self.get_questionnare_count()
+        self.assertEqual(questionnaire_count, 1)
+
+        data_request = {
+            'questionnaire_id': 'default',
+            'new_name': 'Duplicated Default'
+        }
+
+        handler = self.request(data_request, role='admin')
+        response = yield handler.post()
+
+        questionnaire_count = yield self.get_questionnare_count()
+        self.assertEqual(questionnaire_count, 2)
+
+        new_questionnare = yield self.get_new_questionnare()
+        self.assertEqual(new_questionnare.name, 'Duplicated Default')
