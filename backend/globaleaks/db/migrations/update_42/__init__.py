aa19a5c5750db84692315ce7f05a2d0ac5689226
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-06-16 16:58:47 +0200

    Refactor whistleblower identify field to support #2314

--- backend/globaleaks/db/migrations/update_42/__init__.py
+++ backend/globaleaks/db/migrations/update_42/__init__.py
@@ -2,4 +2,14 @@
 from globaleaks.db.migrations.update import MigrationBase
 
 class MigrationScript(MigrationBase):
-    pass
+    def migrate_FieldAttr(self):
+        old_objs = self.session_old.query(self.model_from['FieldAttr'])
+        for old_obj in old_objs:
+            new_obj = self.model_to['FieldAttr']()
+            for key in [c.key for c in new_obj.__table__.columns]:
+                setattr(new_obj, key, getattr(old_obj, key))
+
+            if old_obj.name == 'text_of_confirmation_question_upon_negative_answer':
+                setattr(new_obj, 'name', 'text_shown_upon_negative_answer')
+
+            self.session_new.add(new_obj)
