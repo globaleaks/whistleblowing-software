b261770967602667f2dc50f1ab5aa04e3e066905
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-07-01 11:12:09 +0200

    Apply review of pull request #2343

--- backend/globaleaks/handlers/password_reset.py
+++ backend/globaleaks/handlers/password_reset.py
@@ -1,41 +1,23 @@
 # -*- coding: utf-8 -*-
 #
 # Validates the token for password reset changes
-
 from datetime import datetime, timedelta
-
 from sqlalchemy import or_
-
 from twisted.internet.defer import inlineCallbacks
 
 from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
-from globaleaks.handlers.operation import OperationHandler
-from globaleaks.handlers.user import user_serialize_user
-
 from globaleaks.models import get_auth_token
 from globaleaks.orm import transact
-from globaleaks.rest import requests, errors
+from globaleaks.rest import requests
 from globaleaks.state import State
 from globaleaks.utils.utility import datetime_now
 from globaleaks.utils.security import generateRandomKey
 
-from globaleaks.utils.security import hash_password, generateRandomKey
-
 
 @transact
 def validate_password_reset(session, state, tid, reset_token):
-    '''transact version of db_validate_address_change'''
-    return db_validate_password_reset(session, state, tid, reset_token)
-
-
-def db_validate_password_reset(session, state, tid, reset_token):
     '''Retrieves a user given a password reset validation token'''
-    from globaleaks.handlers.admin.notification import db_get_notification
-    from globaleaks.handlers.admin.node import db_admin_serialize_node
-    from globaleaks.handlers.admin.user import get_user
-    from globaleaks.handlers.user import user_serialize_user
-
     user = session.query(models.User).filter(
         models.User.reset_password_token == reset_token,
         models.User.reset_password_date >= datetime.now() - timedelta(hours=72)
@@ -53,8 +35,11 @@ def db_validate_password_reset(session, state, tid, reset_token):
     return user.auth_token
 
 
-def db_generate_password_reset_token(session, state, tid, username_or_email, allow_admins=False):
-    '''Generates a reset token against the backend, then send email to validate it'''
+def db_generate_password_reset_token(session, state, tid, username_or_email, allow_admin_reset = False):
+    from globaleaks.handlers.admin.notification import db_get_notification
+    from globaleaks.handlers.admin.node import db_admin_serialize_node
+    from globaleaks.handlers.user import user_serialize_user
+
     users = session.query(models.User).filter(
       or_(models.User.username == username_or_email,
           models.User.mail_address == username_or_email),
@@ -62,45 +47,30 @@ def db_generate_password_reset_token(session, state, tid, username_or_email, all
     ).distinct()
 
     for user in users:
-        do_password_reset_for_user(
-            session, state, tid, user, allow_admins=allow_admins
-        )
+        if not allow_admin_reset and user.role == u'admin':
+            continue
 
-def do_password_reset_for_user(session, state, tid, user, allow_admins=False):
-    from globaleaks.handlers.admin.notification import db_get_notification
-    from globaleaks.handlers.admin.node import db_admin_serialize_node
-    from globaleaks.handlers.admin.user import get_user
-    from globaleaks.handlers.user import user_serialize_user
+        user.reset_password_token = generateRandomKey(32)
+        user.reset_password_date = datetime_now()
 
-    if user.role == u'admin' and allow_admins is False:
-        return
+        user_desc = user_serialize_user(session, user, user.language)
 
-    user.reset_password_token = generateRandomKey(32)
-    user.reset_password_date = datetime_now()
+        template_vars = {
+            'type': 'password_reset_validation',
+            'user': user_desc,
+            'reset_token': user.reset_password_token,
+            'node': db_admin_serialize_node(session, 1, user.language),
+            'notification': db_get_notification(session, tid, user.language)
+        }
 
-    user_desc = user_serialize_user(session, user, user.language)
-
-    template_vars = {
-        'type': 'password_reset_validation',
-        'user': user_desc,
-        'reset_token': user.reset_password_token,
-        'node': db_admin_serialize_node(session, 1, user.language),
-        'notification': db_get_notification(session, tid, user.language)
-    }
-
-    state.format_and_send_mail(session, tid, user_desc, template_vars)
+        state.format_and_send_mail(session, tid, user_desc, template_vars)
 
 
 @transact
-def generate_password_reset_token(session, state, tid, username_or_email, allow_admins=False):
-    '''transact version of db_generate_password_reset_token'''
-    return db_generate_password_reset_token(session, state, tid, username_or_email, allow_admins=allow_admins)
+def generate_password_reset_token(session, state, tid, username_or_email, allow_admin_reset = False):
+    '''Generates a reset token against the backend, then send email to validate it'''
+    return db_generate_password_reset_token(session, state, tid, username_or_email, allow_admin_reset)
 
-@transact
-def admin_reset_user_pw(session, state, tid, user_id, allow_admins=False):
-    user = session.query(models.User).filter(
-        models.User.id == user_id).first()
-    do_password_reset_for_user(session, state, tid, user, allow_admins=True)
 
 class PasswordResetHandler(BaseHandler):
     check_roles = 'unauthenticated'
@@ -108,6 +78,9 @@ class PasswordResetHandler(BaseHandler):
 
     @inlineCallbacks
     def get(self, reset_token):
+        if State.tenant_cache[self.request.tid]['enable_password_reset'] is False:
+            return
+
         auth_token = yield validate_password_reset(self.state,
                                                    self.request.tid,
                                                    reset_token)
@@ -125,5 +98,4 @@ class PasswordResetHandler(BaseHandler):
 
         return generate_password_reset_token(self.state,
                                              self.request.tid,
-                                             request['username_or_email'],
-                                             allow_admins=True)
+                                             request['username_or_email'])
