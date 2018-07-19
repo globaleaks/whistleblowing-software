b58866ef3b33c35709451b35404f2493620a7a05
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-07-19 13:38:59 +0200

    Add missing templates required by whistleblowing.it signup

--- client/app/js/controllers/signup.js
+++ client/app/js/controllers/signup.js
@@ -1,8 +1,10 @@
     GLClient.controller('SignupCtrl', ['$scope', '$location', '$route', '$http', 'Authentication', 'CONSTANTS', 'DATA_COUNTRIES_ITALY_REGIONS', 'DATA_COUNTRIES_ITALY_PROVINCES', 'DATA_COUNTRIES_ITALY_CITIES',
                     function($scope, $location, $route, $http, Authentication, CONSTANTS, DATA_COUNTRIES_ITALY_REGIONS, DATA_COUNTRIES_ITALY_PROVINCES, DATA_COUNTRIES_ITALY_CITIES) {
-  $scope.data_countries_italy_regions = DATA_COUNTRIES_ITALY_REGIONS.query();
-  $scope.data_countries_italy_provinces = DATA_COUNTRIES_ITALY_PROVINCES.query();
-  $scope.data_countries_italy_cities = DATA_COUNTRIES_ITALY_CITIES.query();
+  if ($scope.node.signup_mode === 'whistleblowing.it') {
+    $scope.data_countries_italy_regions = DATA_COUNTRIES_ITALY_REGIONS.query();
+    $scope.data_countries_italy_provinces = DATA_COUNTRIES_ITALY_PROVINCES.query();
+    $scope.data_countries_italy_cities = DATA_COUNTRIES_ITALY_CITIES.query();
+  }
 
   $scope.email_regexp = CONSTANTS.email_regexp;
 
