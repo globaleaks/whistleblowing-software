3de59a0b690478d89c9d12ca956b8459604ab5ef
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-08-09 09:59:18 +0200

    Make it possible to custimize every mail template

--- client/app/js/controllers/admin/main.js
+++ client/app/js/controllers/admin/main.js
@@ -215,7 +215,63 @@ controller('AdminShorturlEditCtrl', ['$scope', 'AdminShorturlResource',
     };
 }]).
 controller('AdminMailCtrl', ['$scope', '$http', 'Utils', 'AdminNotificationResource',
-  function($scope, $http, Utils, AdminNotificationResource){
+  function($scope, $http, Utils, AdminNotificationResource) {
+
+  $scope.text_templates = [
+    'activation_mail_template',
+    'activation_mail_title',
+    'admin_anomaly_activities',
+    'admin_anomaly_disk_high',
+    'admin_anomaly_disk_low',
+    'admin_anomaly_mail_template',
+    'admin_anomaly_mail_title',
+    'admin_pgp_alert_mail_template',
+    'admin_pgp_alert_mail_title',
+    'admin_signup_alert_mail_template',
+    'admin_signup_alert_mail_title',
+    'admin_test_mail_template',
+    'admin_test_mail_title',
+    'comment_mail_template',
+    'comment_mail_title',
+    'email_validation_mail_template',
+    'email_validation_mail_title',
+    'export_message_recipient',
+    'export_message_whistleblower',
+    'export_template',
+    'file_mail_template',
+    'file_mail_title',
+    'https_certificate_expiration_mail_template',
+    'https_certificate_expiration_mail_title',
+    'https_certificate_renewal_failure_mail_template',
+    'https_certificate_renewal_failure_mail_title',
+    'identity_access_authorized_mail_template',
+    'identity_access_authorized_mail_title',
+    'identity_access_denied_mail_template',
+    'identity_access_denied_mail_title',
+    'identity_access_request_mail_template',
+    'identity_access_request_mail_title',
+    'identity_provided_mail_template',
+    'identity_provided_mail_title',
+    'message_mail_template',
+    'message_mail_title',
+    'password_reset_complete_mail_template',
+    'password_reset_complete_mail_title',
+    'password_reset_validation_mail_template',
+    'password_reset_validation_mail_title',
+    'pgp_alert_mail_template',
+    'pgp_alert_mail_title',
+    'receiver_notification_limit_reached_mail_template',
+    'receiver_notification_limit_reached_mail_title',
+    'signup_mail_template',
+    'signup_mail_title',
+    'software_update_available_mail_template',
+    'software_update_available_mail_title',
+    'tip_expiration_summary_mail_template',
+    'tip_expiration_summary_mail_title',
+    'tip_mail_template',
+    'tip_mail_title',
+    'user_credentials'
+  ];
 
   $scope.tabs = [
     {
@@ -229,7 +285,7 @@ controller('AdminMailCtrl', ['$scope', '$http', 'Utils', 'AdminNotificationResou
   ];
 
   var sendTestMail = function() {
-    $http({
+    return $http({
       method: 'POST',
       url: '/admin/notification/mail',
     });
@@ -237,7 +293,7 @@ controller('AdminMailCtrl', ['$scope', '$http', 'Utils', 'AdminNotificationResou
 
   $scope.updateThenTestMail = function() {
     AdminNotificationResource.update($scope.admin.notification)
-    .$promise.then(function() { sendTestMail(); }, function() { });
+    .$promise.then(function() { return sendTestMail(); }, function() { });
   };
 }]).
 controller('AdminReviewModalCtrl', ['$scope', '$uibModalInstance', 'targetFunc',
