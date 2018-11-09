globaleaks/whistleblowing-software

commit f638b3138afb50cfab7ff46387b0e00676236234

Fix issue on Xenial-> Bionic updated systems
In doubt, always overwrite globaleaks.list with the updated one according to install script.
See the thread on Forum https://forum.globaleaks.org/t/update-problem/291/26 where the bug happened on a Xenial machine that has been updated to Bionic

scripts/install.sh
@@ -303,7 +303,7 @@ if [ -d /globaleaks/deb ]; then
   DO "apt-get -y --allow-unauthenticated install globaleaks"
   DO "/etc/init.d/globaleaks restart"
 else
-  if [ ! -f /etc/apt/sources.list.d/globaleaks.list ]; then
+    echo "Updating GlobaLeaks apt source.list in /etc/apt/sources.list.d/globaleaks.list ..."
     echo "deb http://deb.globaleaks.org $DISTRO_CODENAME/" > /etc/apt/sources.list.d/globaleaks.list
   fi
   DO "apt-get update -y"

commit 1bedb811152910cd1525193319205ce1fc8001ea

Fix syntax error introduced in f638b3138afb50cfab7ff46387b0e00676236234

scripts/install.sh
@@ -303,9 +303,8 @@ if [ -d /globaleaks/deb ]; then
   DO "apt-get -y --allow-unauthenticated install globaleaks"
   DO "/etc/init.d/globaleaks restart"
 else
-    echo "Updating GlobaLeaks apt source.list in /etc/apt/sources.list.d/globaleaks.list ..."
-    echo "deb http://deb.globaleaks.org $DISTRO_CODENAME/" > /etc/apt/sources.list.d/globaleaks.list
-  fi
+  echo "Updating GlobaLeaks apt source.list in /etc/apt/sources.list.d/globaleaks.list ..."
+  echo "deb http://deb.globaleaks.org $DISTRO_CODENAME/" > /etc/apt/sources.list.d/globaleaks.list
   DO "apt-get update -y"
   DO "apt-get install globaleaks -y"
 fi