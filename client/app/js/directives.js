6bb108de5abffba39c0b934295ecc2a0e72aae75
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-11-14 16:14:13 +0100

    Make it possible to add empy options to dropdown selections

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -263,4 +263,17 @@ directive('isolateClick', function() {
       });
     }
  };
+}).
+directive('emptyOption', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        if (value === null) {
+          value = '';
+        }
+        return value;
+     });
+   }
+  }
 });
