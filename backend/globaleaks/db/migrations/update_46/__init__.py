efcc1a7f67304d370c00a37870df2735e710d92f
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-12-05 08:48:31 +0100

    Fix migration 46 in relation to old db versions missing ip filtering variables

--- backend/globaleaks/db/migrations/update_46/__init__.py
+++ backend/globaleaks/db/migrations/update_46/__init__.py
@@ -7,10 +7,13 @@ class MigrationScript(MigrationBase):
         tenants = self.session_new.query(self.model_from['Tenant'])
         for t in tenants:
             m = self.model_from['Config']
-            a = self.session_new.query(m.value).filter(m.tid == t.id, m.var_name == u'ip_filter_authenticated_enable').one()[0]
-            b = self.session_new.query(m.value).filter(m.tid == t.id, m.var_name == u'ip_filter_authenticated').one()[0]
+            a = self.session_new.query(m.value).filter(m.tid == t.id, m.var_name == u'ip_filter_authenticated_enable').one_or_none()
+            b = self.session_new.query(m.value).filter(m.tid == t.id, m.var_name == u'ip_filter_authenticated').one_or_none()
+
+            if a is None or b is None:
+                continue
 
             for c in ['admin', 'custodian', 'receiver']:
-                self.session_new.add(self.model_to['Config'](t.id, u'ip_filter_' + c + '_enable', a))
-                self.session_new.add(self.model_to['Config'](t.id, u'ip_filter_' + c, b))
+                self.session_new.add(self.model_to['Config'](t.id, u'ip_filter_' + c + '_enable', a[0]))
+                self.session_new.add(self.model_to['Config'](t.id, u'ip_filter_' + c, b[0]))
                 self.entries_count['Config'] += 2
