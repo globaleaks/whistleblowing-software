cb55015d2232749e68fd6a5a6da90cba931b06c1
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-12-06 15:18:56 +0100

    Implement migrations for the scoring system

--- backend/globaleaks/db/migrations/update_46/__init__.py
+++ backend/globaleaks/db/migrations/update_46/__init__.py
@@ -3,6 +3,18 @@ from globaleaks.db.migrations.update import MigrationBase
 
 
 class MigrationScript(MigrationBase):
+    def migrate_FieldOption(self):
+        old_objs = self.session_old.query(self.model_from['FieldOption'])
+        for old_obj in old_objs:
+            new_obj = self.model_to['FieldOption']()
+            for key in [c.key for c in new_obj.__table__.columns]:
+                setattr(new_obj, key, getattr(old_obj, key))
+
+            if new_obj.score_points != 0:
+                new_obj.score_type = 1
+
+            self.session_new.add(new_obj)
+
     def epilogue(self):
         tenants = self.session_new.query(self.model_from['Tenant'])
         for t in tenants:
