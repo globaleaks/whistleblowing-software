106bdc8c064d39fd3addafc39108bb708fb80d0a
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-12-28 23:14:57 +0100

    Add tar utils

---
+++ backend/globaleaks/utils/tar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+#  Tar utilities
+import os
+import tarfile
+
+def tardir(dst, src):
+   with tarfile.open(dst, "w:gz") as tf:
+       tf.dereference = True
+
+       def exclude(x):
+           if x.startswith(dst):
+               return True
+
+           return False
+
+       tf.add(src, arcname=os.path.basename(src), exclude=exclude)
