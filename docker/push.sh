b14e7d148abd770984f39a207fc06502ebb9081b
Giovanni Toraldo <me@gionn.net>
2018-12-31 14:33:54 +0100

    Adjust pushing image for tags and devel branch

--- docker/push.sh
+++ docker/push.sh
@@ -3,11 +3,11 @@
 # Login on Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Always push latest tag when building master
-docker push "$DOCKER_IMAGE_NAME:latest"
-
-# Push release tag if current build
+# Push release tag or devel branch
 if [ -n "$TRAVIS_TAG" ]; then
   docker tag "$DOCKER_IMAGE_NAME:latest" "$DOCKER_IMAGE_NAME:$TRAVIS_TAG"
   docker push "$DOCKER_IMAGE_NAME:$TRAVIS_TAG"
+elif [ "$TRAVIS_BRANCH" == "devel" ]; then
+  docker tag "$DOCKER_IMAGE_NAME:latest" "$DOCKER_IMAGE_NAME:$TRAVIS_BRANCH"
+  docker push "$DOCKER_IMAGE_NAME:$TRAVIS_BRANCH"
 fi
