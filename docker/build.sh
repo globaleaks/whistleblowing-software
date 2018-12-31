19cd6a4288956adc0418a3c408500fb57763cd1f
Giovanni Toraldo <me@gionn.net>
2018-12-31 15:52:57 +0100

    Run and test built image

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,24 @@
 #!/bin/bash -e
+
+# Build image
 docker build -t "$DOCKER_IMAGE_NAME:latest" .
+
+# Run container on background
+CONTAINER_ID=$(docker run --rm -d -p '8080:80' -p '443:443' "$DOCKER_IMAGE_NAME:latest")
+
+# Wait for server ready
+retry=0
+while ! curl -sS -I localhost:8080; do
+  retry=$((retry + 1))
+  if [ $retry -eq 10 ]; then
+    echo 'Unable to reach container port'
+    exit 1
+  fi
+  sleep 1;
+done
+
+# Expect html page
+curl -sS --compressed localhost:8080 | grep 'start_globaleaks'
+
+# Cleanup
+docker rm -f "$CONTAINER_ID"
