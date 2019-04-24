ed4f80bf1e5621db0331f3506946aeb7ab10b64d
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-04-24 15:24:55 +0200

    Revise code related to receipt_salt fixes

--- backend/globaleaks/db/migrations/fixes.py
+++ backend/globaleaks/db/migrations/fixes.py
@@ -10,10 +10,7 @@ from globaleaks.models import Config, SubmissionStatus, User
 def db_fix_salt(session):
     items = session.query(Config).filter(Config.var_name == u'receipt_salt')
     for item in items:
-        try:
-            if len(base64.b64decode(item.value)) != 16:
-                item.value = base64.b64encode(os.urandom(16)).decode()
-        except:
+        if len(item.value) != 24:
             item.value = base64.b64encode(os.urandom(16)).decode()
 
 
