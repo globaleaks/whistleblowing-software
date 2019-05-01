1a93e0d48fa2f5cf42c798b43820964ae8211e95
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-05-01 15:05:46 +0200

    Update browser tests in relation to latest UI revision

--- client/tests/test-admin-custom-file.js
+++ client/tests/test-admin-custom-file.js
@@ -6,18 +6,18 @@ describe("Admin upload custom file", function() {
 
     browser.setLocation("admin/content");
 
-    browser.gl.utils.waitUntilPresent(by.cssContainingText("a", "Theme customization"));
+    browser.gl.utils.waitUntilPresent(by.cssContainingText("a", "Files"));
 
-    element(by.cssContainingText("a", "Theme customization")).click();
+    element(by.cssContainingText("a", "Files")).click();
 
     var customFile = browser.gl.utils.makeTestFilePath("nyancat.pdf");
 
     browser.executeScript("angular.element(document.querySelectorAll('input[type=\"file\"]')).attr(\"style\", \"visibility: visible\")");
-    element(by.css("div.uploadfile.file-custom")).element(by.css("input")).sendKeys(customFile);
+    element(by.css("span.file-custom")).element(by.css("input")).sendKeys(customFile);
 
     browser.gl.utils.waitUntilPresent(by.cssContainingText("label", "Project name"));
 
-    element(by.cssContainingText("a", "Theme customization")).click();
+    element(by.cssContainingText("a", "Files")).click();
 
     element(by.id("fileList")).element(by.cssContainingText("span", "Delete")).click();
   });
