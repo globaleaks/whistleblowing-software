3604453d63615fb0b17641cee2607dd2063ce5c9
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-05-07 07:49:50 +0200

    Update test-admin-configure-node.js in relation to latest development

--- client/tests/test-admin-configure-node.js
+++ client/tests/test-admin-configure-node.js
@@ -1,7 +1,7 @@
 describe("admin configure node", function() {
   it("should configure node en internalization", function() {
     browser.setLocation("admin/content");
-    element(by.model("GLTranslate.indirect.appLanguage")).element(by.xpath(".//*[text()='English']")).click();
+    element(by.model("GLTranslate.state.language")).element(by.xpath(".//*[text()='English']")).click();
     expect(element(by.model("admin.node.header_title_homepage")).clear().sendKeys("TEXT1_EN"));
     expect(element(by.model("admin.node.presentation")).clear().sendKeys("TEXT2_EN"));
 
@@ -10,13 +10,13 @@ describe("admin configure node", function() {
 
   it("should configure node it internalization", function() {
     browser.setLocation("admin/content");
-    element(by.model("GLTranslate.indirect.appLanguage")).element(by.xpath(".//*[text()='Italiano']")).click();
+    element(by.model("GLTranslate.state.language")).element(by.xpath(".//*[text()='Italiano']")).click();
     expect(element(by.model("admin.node.header_title_homepage")).clear().sendKeys("TEXT1_IT"));
     expect(element(by.model("admin.node.presentation")).clear().sendKeys("TEXT2_IT"));
 
     element.all(by.cssContainingText("button", "Salva")).get(0).click();
 
-    element(by.model("GLTranslate.indirect.appLanguage")).element(by.xpath(".//*[text()='English']")).click();
+    element(by.model("GLTranslate.state.language")).element(by.xpath(".//*[text()='English']")).click();
   });
 
   it("should configure node advanced settings", function() {
