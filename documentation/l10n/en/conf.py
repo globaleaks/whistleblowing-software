364fbacd66ce33a2a79c677268ee60584c26872c
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-05-19 21:40:19 +0200

    Update texts of the frontpage of the documentation

--- documentation/l10n/en/conf.py
+++ documentation/l10n/en/conf.py
@@ -222,7 +222,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'GlobaLeaks.tex', u'GlobaLeaks Documentation',
-     u'Random GlobaLeaks Contributors', 'manual'),
+     u'Hermes Center for Transparency and Digital Human Rights', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
