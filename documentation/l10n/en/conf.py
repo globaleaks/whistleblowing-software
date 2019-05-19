267022aa03c43b3ac1899bd2125a69848736f5da
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-05-19 13:02:39 +0200

    Reorganize existing images in their respective directory

--- documentation/l10n/en/conf.py
+++ documentation/l10n/en/conf.py
@@ -126,7 +126,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'imgs/144x144.png'
+html_logo = 'logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
