4d911253bd444c184ac640b79cebb320ea643e2f
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-06-18 19:10:57 +0200

    Upon redirect configuration update the tenant state

--- backend/globaleaks/handlers/admin/redirect.py
+++ backend/globaleaks/handlers/admin/redirect.py
@@ -4,10 +4,13 @@
 #   *****
 # Implementation of the URL redirect handlers
 #
+from twisted.internet.defer import inlineCallbacks
+
 from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
 from globaleaks.orm import transact
 from globaleaks.rest import requests
+from globaleaks.state import State
 
 
 def serialize_redirect(redirect):
@@ -23,6 +26,16 @@ def get_redirect_list(session, tid):
     return [serialize_redirect(redirect) for redirect in session.query(models.Redirect).filter(models.Redirect.tid == tid)]
 
 
+@inlineCallbacks
+def update_redirect_state(tid):
+    State.tenant_cache[tid]['redirects'] = {}
+
+    redirects = yield get_redirect_list(tid)
+
+    for redirect in redirects:
+        State.tenant_cache[tid]['redirects'][redirect['path1']] = redirect['path2']
+
+
 @transact
 def create(session, tid, request):
     request['tid'] = tid
@@ -33,7 +46,6 @@ def create(session, tid, request):
 class RedirectCollection(BaseHandler):
     check_roles = 'admin'
     cache_resource = True
-    invalidate_cache = True
 
     def get(self):
         """
@@ -47,15 +59,20 @@ class RedirectCollection(BaseHandler):
         """
         request = self.validate_message(self.request.content.read(), requests.AdminRedirectDesc)
 
-        return create(self.request.tid, request)
+        redirect = create(self.request.tid, request)
+
+        yield update_redirects_state(self.request.tid)
+
+        return redirect
 
 
 class RedirectInstance(BaseHandler):
     check_roles = 'admin'
-    invalidate_cache = True
 
     def delete(self, redirect_id):
         """
         Delete the specified redirect.
         """
-        return models.delete(models.Redirect, models.Redirect.tid == self.request.tid, models.Redirect.id == redirect_id)
+        models.delete(models.Redirect, models.Redirect.tid == self.request.tid, models.Redirect.id == redirect_id)
+
+        yield update_redirects_state(self.request.tid)
