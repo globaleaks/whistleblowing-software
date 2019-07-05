316cf437b081f79a34f7197174c6e8b51d2d94ee
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-07-05 19:32:52 +0200

    Fix issue #2612

--- backend/globaleaks/handlers/submission.py
+++ backend/globaleaks/handlers/submission.py
@@ -31,6 +31,11 @@ def decrypt_tip(user_key, tip_prv_key, tip):
         if k in tip['data'] and tip['data'][k]['encrypted'] and tip['data'][k]['value']:
             tip['data'][k]['value'] = json.loads(GCE.asymmetric_decrypt(tip_key, base64.b64decode(tip['data'][k]['value'].encode())).decode())
 
+        # Fix for issue: https://github.com/globaleaks/GlobaLeaks/issues/2612
+        # The bug is due to the fact that the data was initially saved as an array of one entry
+        if k == 'whistleblower_identity' and isinstance(tip['data'][k]['value'], list):
+            tip['data'][k]['value'] = tip['data'][k]['value'][0]
+
     for x in tip['comments'] + tip['messages']:
         x['content'] = GCE.asymmetric_decrypt(tip_key, base64.b64decode(x['content'].encode())).decode()
 
