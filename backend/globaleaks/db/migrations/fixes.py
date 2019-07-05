f75725433bcbb9ff47ce656eed548cbce2320f13
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-07-05 20:19:46 +0200

    Revise fix for #2612

--- backend/globaleaks/db/migrations/fixes.py
+++ backend/globaleaks/db/migrations/fixes.py
@@ -4,7 +4,17 @@ import os
 
 from sqlalchemy.sql.expression import func
 
-from globaleaks.models import Config, SubmissionStatus, User
+from globaleaks.models import Config, InternalTipData, SubmissionStatus, User
+
+
+def db_fix_tip_data(session):
+    # Fix for issue: https://github.com/globaleaks/GlobaLeaks/issues/2612
+    # The bug is due to the fact that the data was initially saved as an array of one entry
+    for data in session.query(InternalTipData).filter(InternalTipData.key == 'whistleblower_identity',
+                                                      InternalTipData.encrypted == False):
+        if isinstance(data.value, list):
+            data.value = data.value[0]
+
 
 def db_fix_statuses(session):
     items = session.query(SubmissionStatus).filter(SubmissionStatus.system_usage == u'open')
@@ -23,3 +33,4 @@ def db_fix_users(session):
 def db_fix(session):
     db_fix_statuses(session)
     db_fix_users(session)
+    db_fix_tip_data(session)
