c442df6abc5b49f79cf9596c7ff85b3d74974227
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-07-09 13:30:28 +0200

    Fix regression #2629

--- backend/globaleaks/models/config.py
+++ backend/globaleaks/models/config.py
@@ -92,8 +92,8 @@ class ConfigL10NFactory(object):
 
             for cfg in self.get_all(group, lang):
                 old_keys.append(cfg.var_name)
-                if (cfg.update_date == null or reset) and cfg.var_name in data:
-                    cfg.value = data[cfg.var_name][lang]
+                if (cfg.update_date == null or reset) and cfg.var_name in data['templates']:
+                    cfg.value = data['templates'][cfg.var_name][lang]
 
             ConfigL10NFactory.initialize(self, list(set(ConfigL10NFilters[group]) - set(old_keys)), lang, data)
 
