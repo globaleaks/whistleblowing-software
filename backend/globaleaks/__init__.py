78075ef9de964e8a0f69955c3a18b95a781cbe5f
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-07-14 19:47:42 +0200

    Add empty db migration 50

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -10,7 +10,7 @@ __copyright__ = u'2011-2019 - Hermes Center for Transparency and Digital Human R
 __version__ = u'3.9.15'
 __license__ = u'AGPL-3.0'
 
-DATABASE_VERSION = 49
+DATABASE_VERSION = 50
 FIRST_DATABASE_VERSION_SUPPORTED = 24
 
 # Add new languages as they are supported here! To do this retrieve the name of
