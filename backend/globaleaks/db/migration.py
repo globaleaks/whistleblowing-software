3e9baddc9e34695cd5bfd8d2110a30b3d2ba9833
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-08-13 09:07:08 +0200

    Remove debugging output preventing migration to fail
    Possibly related to #2661

--- backend/globaleaks/db/migration.py
+++ backend/globaleaks/db/migration.py
@@ -285,9 +285,6 @@ def perform_migration(version):
         if os.path.exists(path):
             shutil.rmtree(path)
 
-    except Exception as e:
-        print(e)
-
     finally:
         # Always cleanup the temporary directory used for the migration
         for f in os.listdir(tmpdir):
