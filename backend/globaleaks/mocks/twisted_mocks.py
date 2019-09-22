eeff2b3df5a6bfc3e69af34e81e13e3730613c23
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-09-22 20:04:24 +0200

    Disable Twisted logging in H2Connection and HTTPChannel class

--- backend/globaleaks/mocks/twisted_mocks.py
+++ backend/globaleaks/mocks/twisted_mocks.py
@@ -3,7 +3,7 @@ from io import BytesIO as StringIO
 
 from twisted.logger import ILogObserver, Logger
 from twisted.python import log
-from twisted.web.http import Request
+from twisted.web.http import HTTPChannel, Request
 
 from zope.interface import implementer
 
@@ -21,5 +21,16 @@ def mock_Request_gotLength(self, length):
 
 log.msg = log.info = log.err = mock_log
 
-Request._log = Logger(observer=NullObserver())
+null_logger = Logger(observer=NullObserver())
+
+Request._log = null_logger
 Request.gotLength = mock_Request_gotLength
+
+HTTPChannel._log = null_logger
+
+try:
+    from twisted.web._http2 import H2Connection
+except ImportError:
+    pass
+else:
+    H2Connection._log = null_logger
