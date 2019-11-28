1f3365e4876f0aa0d37a1d821699b5928bc3279e
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2019-11-28 16:43:28 +0100

    Update migration script in relation to changes for feature #2727

--- backend/globaleaks/db/migration.py
+++ backend/globaleaks/db/migration.py
@@ -46,7 +46,7 @@ from globaleaks.db.migrations.update_48 import Field_v_47, FieldOption_v_47
 from globaleaks.db.migrations.update_49 import InternalTip_v_48
 from globaleaks.db.migrations.update_50 import SubmissionStatus_v_49, SubmissionSubStatus_v_49, User_v_49
 from globaleaks.db.migrations.update_51 import Field_v_50, InternalFile_v_50, User_v_50
-from globaleaks.db.migrations.update_52 import SubmissionStatus_v_51
+from globaleaks.db.migrations.update_52 import InternalTip_v_51, SubmissionStatus_v_51
 
 from globaleaks.orm import get_engine, get_session, make_db_uri
 from globaleaks.models import config, Base
@@ -79,7 +79,7 @@ migration_mapping = OrderedDict([
     ('File', [-1, -1, -1, -1, -1, -1, -1, File_v_38, 0, 0, 0, 0, 0, 0, 0, models._File, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     ('IdentityAccessRequest', [IdentityAccessRequest_v_38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, models._IdentityAccessRequest, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     ('InternalFile', [InternalFile_v_25, 0, InternalFile_v_38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, InternalFile_v_40, 0, InternalFile_v_45, 0, 0, 0, 0, InternalFile_v_50, 0, 0, 0, InternalFile_v_50, models._InternalFile, 0]),
-    ('InternalTip', [InternalTip_v_32, 0, 0, 0, 0, 0, 0, 0, 0, InternalTip_v_34, 0, InternalTip_v_38, 0, 0, 0, InternalTip_v_40, 0, InternalTip_v_41, InternalTip_v_42, InternalTip_v_44, 0, InternalTip_v_45, InternalTip_v_46, InternalTip_v_48, 0, models._InternalTip, 0, 0, 0]),
+    ('InternalTip', [InternalTip_v_32, 0, 0, 0, 0, 0, 0, 0, 0, InternalTip_v_34, 0, InternalTip_v_38, 0, 0, 0, InternalTip_v_40, 0, InternalTip_v_41, InternalTip_v_42, InternalTip_v_44, 0, InternalTip_v_45, InternalTip_v_46, InternalTip_v_48, 0, InternalTip_v_51, 0, 0, models._InternalTip]),
     ('InternalTipAnswers', [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, models._InternalTipAnswers, 0, 0, 0, 0, 0, 0, 0]),
     ('InternalTipData', [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, models._InternalTipData, 0, 0, 0, 0, 0, 0, 0]),
     ('Mail', [-1, -1, Mail_v_38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, models._Mail, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
--- backend/globaleaks/db/migrations/update_52/__init__.py
+++ backend/globaleaks/db/migrations/update_52/__init__.py
@@ -2,12 +2,37 @@
 from globaleaks.db.migrations.update import MigrationBase as MigrationScript
 from globaleaks.models import Model
 from globaleaks.models.properties import *
-from globaleaks.utils.utility import datetime_now, datetime_null
+from globaleaks.utils.utility import datetime_now, datetime_never
+
+
+class InternalTip_v_51(Model):
+    __tablename__ = 'internaltip'
+    id = Column(UnicodeText(36), primary_key=True, default=uuid4)
+    tid = Column(Integer, default=1, nullable=False)
+    creation_date = Column(DateTime, default=datetime_now, nullable=False)
+    update_date = Column(DateTime, default=datetime_now, nullable=False)
+    context_id = Column(UnicodeText(36), nullable=False)
+    preview = Column(JSON, default=dict, nullable=False)
+    progressive = Column(Integer, default=0, nullable=False)
+    https = Column(Boolean, default=False, nullable=False)
+    mobile = Column(Boolean, default=False, nullable=False)
+    total_score = Column(Integer, default=0, nullable=False)
+    expiration_date = Column(DateTime, default=datetime_never, nullable=False)
+    enable_two_way_comments = Column(Boolean, default=True, nullable=False)
+    enable_two_way_messages = Column(Boolean, default=True, nullable=False)
+    enable_attachments = Column(Boolean, default=True, nullable=False)
+    enable_whistleblower_identity = Column(Boolean, default=False, nullable=False)
+    additional_questionnaire_id = Column(UnicodeText(36))
+    wb_last_access = Column(DateTime, default=datetime_now, nullable=False)
+    wb_access_counter = Column(Integer, default=0, nullable=False)
+    status = Column(UnicodeText(36), nullable=True)
+    substatus = Column(UnicodeText(36), nullable=True)
+    crypto_tip_pub_key = Column(LargeBinary(32), default=b'', nullable=False)
+    binary_keys = ['crypto_tip_pub_key']
 
 
 class SubmissionStatus_v_51(Model):
     __tablename__ = 'submissionstatus'
-
     id = Column(UnicodeText(36), primary_key=True, default=uuid4)
     tid = Column(Integer, primary_key=True, default=1, nullable=False)
     label = Column(JSON, default=dict, nullable=False)
