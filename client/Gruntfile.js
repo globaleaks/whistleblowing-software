globaleaks/whistleblowing-software

commit 0cf61e9248b4ee65848fd72a992832a25b871f9d

Deprecate qrious in favour of angular-qrcode
The requirements for this change is the bug described at:
- https://github.com/neocotic/qrious/issues/120
- https://bugzilla.mozilla.org/show_bug.cgi?id=1446472
- https://trac.torproject.org/projects/tor/ticket/27543

client/Gruntfile.js
@@ -47,7 +47,6 @@ module.exports = function(grunt) {
     copy: {
       sources: {
         files: [
-          { dest: "app/lib/css/", cwd: ".", src: ["node_modules/angular/angular-csp.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/ui-bootstrap4/dist/ui-bootstrap-csp.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/bootstrap/dist/css/bootstrap.min.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css"], expand: true, flatten: true },
@@ -62,6 +61,7 @@ module.exports = function(grunt) {
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-dynamic-locale/tmhDynamicLocale.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-file-saver/dist/angular-file-saver.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-filter/dist/angular-filter.min.js"], expand: true, flatten: true },
+          { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-qrcode/angular-qrcode.js"], expand: true, flatten: true},
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-resource/angular-resource.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-route/angular-route.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-sanitize/angular-sanitize.min.js"], expand: true, flatten: true },
@@ -70,9 +70,9 @@ module.exports = function(grunt) {
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/d3/dist/d3.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/file-saver/FileSaver.min.js"], expand: true, flatten: true },
+          { dest: "app/lib/js/", cwd: ".", src: ["node_modules/qrcode-generator/qrcode.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/stacktrace-js/dist/stacktrace.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/topojson/dist/topojson.min.js"], expand: true, flatten: true},
-          { dest: "app/lib/js/", cwd: ".", src: ["node_modules/qrious/dist/qrious.min.js"], expand: true, flatten: true},
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/ui-bootstrap4/dist/ui-bootstrap-tpls.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/ui-select/dist/select.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/locale", cwd: ".", src: ["node_modules/angular-i18n/angular-locale*"], expand: true, flatten: true },

client/app/index.html
@@ -71,11 +71,12 @@
     <footer id="FooterBox" data-ng-if="started && !embedded" data-ng-include="'views/partials/footer.html'"></footer>
 
     <!-- build:js js/scripts.js -->
-    <script src="lib/js/qrious.min.js"></script>
+    <script src="lib/js/qrcode.js"></script>
     <script src="lib/js/angular.min.js"></script>
     <script src="lib/js/angular-aria.min.js"></script>
     <script src="lib/js/angular-file-saver.min.js"></script>
     <script src="lib/js/angular-filter.min.js"></script>
+    <script src="lib/js/angular-qrcode.js"></script>
     <script src="lib/js/angular-resource.min.js"></script>
     <script src="lib/js/angular-route.min.js"></script>
     <script src="lib/js/angular-sanitize.min.js"></script>

client/app/js/app.js
@@ -10,6 +10,7 @@ var GLClient = angular.module("GLClient", [
     "ui.select",
     "tmh.dynamicLocale",
     "flow",
+    "monospaced.qrcode",
     "pascalprecht.translate",
     "ngSanitize",
     "ngFileSaver",

client/app/js/controllers/preferences.js
@@ -19,6 +19,7 @@ GLClient.controller("PreferencesCtrl", ["$scope", "$rootScope", "$q", "$http", "
     $scope.editingName = false;
     $scope.editingEmail = false;
     $scope.showEncryptionKey = false;
+    $scope.qrcode_string = '';
 
     $scope.toggleNameEditing = function () {
       $scope.editingName = !$scope.editingName;
@@ -54,12 +55,7 @@ GLClient.controller("PreferencesCtrl", ["$scope", "$rootScope", "$q", "$http", "
           "args": {}
         }}).then(function(data){
           $scope.two_factor_secret = data.data;
-          var qr = new QRious({
-            value: "otpauth://totp/GlobaLeaks?secret=" + $scope.two_factor_secret,
-            size: "240"
-          });
-
-          $scope.two_factor_qrcode = qr.toDataURL();
+          $scope.qrcode_string = "otpauth://totp/GlobaLeaks?secret=" + $scope.two_factor_secret
 
           $scope.Utils.openConfirmableModalDialog("views/partials/enable_2fa_modal.html", {}, $scope).then(function (result) {
             return $http({method: "PUT", url: "user/operations", data:{

client/app/views/partials/enable_2fa.html
@@ -17,7 +17,7 @@
       </div>
       <br />
       <div class="text-center">
-        <img data-ng-src="{{two_factor_qrcode}}" />
+        <qrcode data-ng-if="qrcode_string" data="{{qrcode_string}}" version="2" error-correction-level="Q" size="240" color="#000" background="#FFF"></qrcode>
         <div class="small text-bold">{{two_factor_secret}}</div>
       </div>
     </div>

commit 90f19d4cc2f1cded11e6d82605cb2d23ebb1c047

Fix client build regression introduced in 0cf61e9248b4ee65848fd72a992832a25b871f9d

client/Gruntfile.js
@@ -47,6 +47,7 @@ module.exports = function(grunt) {
     copy: {
       sources: {
         files: [
+          { dest: "app/lib/css/", cwd: ".", src: ["node_modules/angular/angular-csp.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/ui-bootstrap4/dist/ui-bootstrap-csp.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/bootstrap/dist/css/bootstrap.min.css"], expand: true, flatten: true },
           { dest: "app/lib/css/", cwd: ".", src: ["node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css"], expand: true, flatten: true },
@@ -67,7 +68,6 @@ module.exports = function(grunt) {
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-sanitize/angular-sanitize.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-translate/dist/angular-translate.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js"], expand: true, flatten: true },
-          { dest: "app/lib/js/", cwd: ".", src: ["node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/d3/dist/d3.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/file-saver/FileSaver.min.js"], expand: true, flatten: true },
           { dest: "app/lib/js/", cwd: ".", src: ["node_modules/qrcode-generator/qrcode.js"], expand: true, flatten: true },