02b0cb08cf0e8486170848d73f7ae97519a412a1
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-01-22 11:49:42 +0100

    Add test for admin assignment of escrow key

---
+++ client/tests/test-admin-assign-escrow-key.js
@@ -0,0 +1,19 @@
+describe("key escrow assignment and revocation", function() {
+  it("should assign escrow key to Admin 2", async function() {
+    await browser.gl.utils.login_admin();
+
+    for (var i = 0; i<2; i++) {
+      await browser.setLocation("admin/users");
+
+      var user = { name: "Admin2" };
+      var path = "//form[contains(.,\"" + user.name + "\")]";
+
+      var editUsrForm = element(by.xpath(path));
+
+      await editUsrForm.element(by.cssContainingText("button", "Edit")).click();
+
+      // Toggle key escrow
+      await element(by.model("user.escrow")).click();
+    }
+  });
+});
