5c16d45756abcdef9f1d6a1aeb80993fe2aaa760
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-01-22 10:49:49 +0100

    Reorganize tests of users first login

--- client/tests/test-users-first-login.js
+++ client/tests/test-users-first-login.js
@@ -1,7 +1,5 @@
 var receiver = new browser.gl.pages.receiver();
-
 var path = require("path");
-
 var pgp_key_path = path.resolve("../backend/globaleaks/tests/data/gpg/VALID_PGP_KEY1_PUB");
 
 describe("Recipient1 first login", function() {
@@ -36,12 +34,17 @@ describe("Recipient2 first login", function() {
     await element(by.css("[data-ng-click=\"save()\"]")).click();
     await browser.gl.utils.waitForUrl("/receiver/home");
   });
+});
 
-  it("should be able to login with the new password", async function() {
-    await browser.gl.utils.login_receiver();
+describe("Custodian first login", function() {
+  it("should redirect to /firstlogin upon successful authentication", async function() {
+    await browser.gl.utils.login_custodian("Custodian1", "globaleaks123!", "/#/login", true);
   });
 
-  it("should be able to load his/her public PGP key", async function() {
-    await receiver.addPublicKey(pgp_key_path);
+  it("should be able to change password from the default one", async function() {
+    await element(by.model("preferences.password")).sendKeys(browser.gl.utils.vars["user_password"]);
+    await element(by.model("preferences.check_password")).sendKeys(browser.gl.utils.vars["user_password"]);
+    await element(by.css("[data-ng-click=\"save()\"]")).click();
+    await browser.gl.utils.waitForUrl("/custodian/home");
   });
 });
