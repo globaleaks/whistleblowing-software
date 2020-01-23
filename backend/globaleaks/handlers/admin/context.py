1eca756dc7a10f6f1632f2ed8d36a318cea7072f
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-01-23 16:52:01 +0100

    Remove unnecessary documentation line

--- backend/globaleaks/handlers/admin/context.py
+++ backend/globaleaks/handlers/admin/context.py
@@ -81,7 +81,6 @@ def db_associate_context_receivers(session, context, receiver_ids):
     :param session: An ORM session
     :param context: The context on which associate the specified receivers
     :param receiver_ids: A list of receivers ids to be associated to the context
-    :return:
     """
     session.query(models.ReceiverContext).filter(models.ReceiverContext.context_id == context.id).delete(synchronize_session='fetch')
 
