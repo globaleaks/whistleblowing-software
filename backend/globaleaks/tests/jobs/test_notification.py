bae05d1b5a3e9bd3c0f9eefc53b64cb002d692b4
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-02-25 11:26:52 +0100

    Update notification tests in relation to latest development

--- backend/globaleaks/tests/jobs/test_notification.py
+++ backend/globaleaks/tests/jobs/test_notification.py
@@ -14,36 +14,13 @@ class TestNotification(helpers.TestGLWithPopulatedDB):
         yield self.perform_full_submission_actions()
 
     @inlineCallbacks
-    def test_notification_success(self):
+    def test_notification(self):
         yield self.test_model_count(models.Mail, 2)
 
         yield Delivery().run()
 
         notification = Notification()
         notification.skip_sleep = True
-        yield notification.run()
-
-        yield self.test_model_count(models.Mail, 0)
-
-    @inlineCallbacks
-    def test_notification_failure(self):
-        yield self.test_model_count(models.Mail, 2)
-
-        yield Delivery().run()
-
-        notification = Notification()
-        notification.skip_sleep = True
-
-        def sendmail_failure(_):
-            # simulate the failure just returning with no action
-            return succeed(None)
-
-        notification.sendmail = sendmail_failure
-
-        for _ in range(10):
-            yield notification.run()
-            yield self.test_model_count(models.Mail, 2 + 24)
-
         yield notification.run()
 
         yield self.test_model_count(models.Mail, 0)
