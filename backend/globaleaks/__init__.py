4e9aa80eb26f84b9a0fec5325d22cb179ce3f1a6
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-03-12 19:14:33 +0100

    Bump to version 4.0.3

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -7,7 +7,7 @@ import operator
 __author__ = 'Random GlobaLeaks Developers'
 __email__ = 'info@globaleaks.org'
 __copyright__ = '2011-2020 - Hermes Center for Transparency and Digital Human Rights - GlobaLeaks Project'
-__version__ = '4.0.2'
+__version__ = '4.0.3'
 __license__ = 'AGPL-3.0'
 
 DATABASE_VERSION = 52
