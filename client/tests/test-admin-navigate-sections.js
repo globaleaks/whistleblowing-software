cbfdbc18bd5f1fae2152cc673ee80d3f8169d640
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-05-11 21:06:03 +0200

    Update e2e tests

--- client/tests/test-admin-navigate-sections.js
+++ client/tests/test-admin-navigate-sections.js
@@ -6,43 +6,43 @@ describe("verify navigation of admin sections", function() {
     await browser.gl.utils.login_admin();
 
     await element.all(by.cssContainingText("a", "Home")).first().click();
-    await element(by.cssContainingText("ul li a", "Changelog")).click();
-    await element(by.cssContainingText("ul li a", "License")).click();
+    await element.all(by.cssContainingText("a", "Changelog")).first().click();
+    await element.all(by.cssContainingText("a", "License")).first().click();
 
-    await element(by.cssContainingText("a", "Site settings")).click();
-    await element(by.cssContainingText("ul li a", "Main configuration")).click();
-    await element(by.cssContainingText("ul li a", "Theme customization")).click();
-    await element(by.cssContainingText("ul li a", "Languages")).click();
-    await element(by.cssContainingText("ul li a", "Text customization")).click();
+    await element.all(by.cssContainingText("a", "Site settings")).first().click();
+    await element.all(by.cssContainingText("a", "Main configuration")).first().click();
+    await element.all(by.cssContainingText("a", "Theme customization")).first().click();
+    await element.all(by.cssContainingText("a", "Languages")).first().click();
+    await element.all(by.cssContainingText("a", "Text customization")).first().click();
 
-    await element(by.cssContainingText("a", "Users")).click();
-    await element(by.cssContainingText("a", "Questionnaires")).click();
-    await element(by.cssContainingText("a", "Contexts")).click();
-    await element(by.cssContainingText("a", "Case management")).click();
+    await element.all(by.cssContainingText("a", "Users")).first().click();
+    await element.all(by.cssContainingText("a", "Questionnaires")).first().click();
+    await element.all(by.cssContainingText("a", "Contexts")).first().click();
+    await element.all(by.cssContainingText("a", "Case management")).first().click();
 
-    await element(by.cssContainingText("a", "Notification settings")).click();
-    await element(by.cssContainingText("ul li a", "Main configuration")).click();
-    await element(by.cssContainingText("ul li a", "Notification templates")).click();
+    await element.all(by.cssContainingText("a", "Notification settings")).first().click();
+    await element.all(by.cssContainingText("a", "Main configuration")).first().click();
+    await element.all(by.cssContainingText("a", "Notification templates")).first().click();
 
-    await element(by.cssContainingText("a", "Network settings")).click();
-    await element(by.cssContainingText("ul li a", "Tor")).click();
-    await element(by.cssContainingText("ul li a", "HTTPS")).click();
-    await element(by.cssContainingText("ul li a", "Access control")).click();
-    await element(by.cssContainingText("ul li a", "URL redirects")).click();
+    await element.all(by.cssContainingText("a", "Network settings")).first().click();
+    await element.all(by.cssContainingText("a", "Tor")).first().click();
+    await element.all(by.cssContainingText("a", "HTTPS")).first().click();
+    await element.all(by.cssContainingText("a", "Access control")).first().click();
+    await element.all(by.cssContainingText("a", "URL redirects")).first().click();
 
-    await element(by.cssContainingText("a", "Sites management")).click();
-    await element(by.cssContainingText("ul li a", "Main configuration")).click();
-    await element.all(by.cssContainingText("ul li a", "Sites")).get(1).click();
+    await element.all(by.cssContainingText("a", "Sites management")).first().click();
+    await element.all(by.cssContainingText("a", "Main configuration")).first().click();
+    await element.all(by.cssContainingText("a", "Sites")).first().click();
 
-    await element(by.cssContainingText("a", "Advanced settings")).click();
-    await element(by.cssContainingText("ul li a", "Main configuration")).click();
-    await element(by.cssContainingText("ul li a", "Anomaly detection thresholds")).click();
+    await element.all(by.cssContainingText("a", "Advanced settings")).first().click();
+    await element.all(by.cssContainingText("a", "Main configuration")).first().click();
+    await element.all(by.cssContainingText("a", "Anomaly detection thresholds")).first().click();
 
-    await element(by.cssContainingText("a", "Audit log")).click();
-    await element(by.cssContainingText("ul li a", "Stats")).click();
-    await element(by.cssContainingText("ul li a", "Activities")).click();
-    await element.all(by.cssContainingText("ul li a", "Users")).get(1).click();
-    await element(by.cssContainingText("ul li a", "Anomalies")).click();
-    await element(by.cssContainingText("ul li a", "Scheduled jobs")).click();
+    await element.all(by.cssContainingText("a", "Audit log")).first().click();
+    await element.all(by.cssContainingText("a", "Stats")).first().click();
+    await element.all(by.cssContainingText("a", "Activities")).first().click();
+    await element.all(by.cssContainingText("a", "Users")).get(1).click();
+    await element.all(by.cssContainingText("a", "Anomalies")).first().click();
+    await element.all(by.cssContainingText("a", "Scheduled jobs")).first().click();
   });
 });
