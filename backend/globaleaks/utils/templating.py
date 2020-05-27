f82d3345a23b452933589eca65354ad1d9fb6e28
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-05-27 15:36:18 +0200

    Fix export in relation to multichoice questions

--- backend/globaleaks/utils/templating.py
+++ backend/globaleaks/utils/templating.py
@@ -236,7 +236,7 @@ class TipKeyword(UserNodeKeyword, ContextKeyword):
                 for option in field['options']:
                     if k == option.get('id', '') and v is True:
                         output += indent(indent_n) + option['label'] + '\n'
-        elif field_type in ['selectbox']:
+        elif field_type in ['multichoice', 'selectbox']:
             for option in field['options']:
                 if entry.get('value', '') == option['id']:
                     output += indent(indent_n) + option['label'] + '\n'
