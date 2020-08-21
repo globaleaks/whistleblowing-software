e6a1761ae0d019c526373059bec1f7058b6ab721
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-08-21 18:42:06 +0200

    Move client constants to separated constants.js file

---
+++ client/app/js/constants.js
@@ -0,0 +1,10 @@
+GL.constant("CONSTANTS", {
+   /* The email regexp restricts email addresses to less than 400 chars. See #1215 */
+   "email_regexp": /^([\w+-.]){0,100}[\w]{1,100}@([\w+-.]){0,100}[\w]{1,100}$/,
+   "number_regexp": /^\d+$/,
+   "phonenumber_regexp": /^[+]?[ \d]+$/,
+   "hostname_regexp": /^[a-z0-9-.]+$|^$/,
+   "https_regexp": /^https:\/\/([a-z0-9-]+)\.(.*)$|^$/,
+   "uuid_regexp": /^([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/,
+   "domain_list_regexp": /^((([a-z0-9.]+,)*[a-z0-9.]+)|)$/
+});
