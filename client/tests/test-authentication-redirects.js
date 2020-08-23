d7d5a080eae7a0aa659de1b5f4b89a76e4839aba
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-08-23 11:50:56 +0200

    [ci] Add tests for /login and /admin redirects

--- client/tests/test-authentication-redirects.js
+++ client/tests/test-authentication-redirects.js
@@ -1,4 +1,14 @@
 describe("perform rediects on authenticated pages", function() {
+  it("test redirect to login page using /login url", async function() {
+    await browser.get("/login");
+    await browser.gl.utils.waitForUrl("/login");
+  });
+
+  it("test redirect to login page using /admin url", async function() {
+    await browser.get("/admin");
+    await browser.gl.utils.waitForUrl("/admin");
+  });
+
   it("test rtip redirect to login page", async function() {
     await browser.get("/#/status/2f0535eb-9710-47e5-8082-5f882d4ec770");
 
@@ -9,11 +19,5 @@ describe("perform rediects on authenticated pages", function() {
     await browser.get("/#/admin/advanced");
 
     await browser.gl.utils.waitForUrl("/login?src=%2Fadmin%2Fadvanced");
-  });
-
-  it("test custodian redirect to login page", async function() {
-    await browser.get("/#/custodian/identityaccessrequests");
-
-    await browser.gl.utils.waitForUrl("/login?src=%2Fcustodian%2Fidentityaccessrequests");
   });
 });
