ee66daab705853538321f1d36385d87584b4c968
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-10-13 12:16:47 +0200

    Update end to end tests in relation to latest development

--- client/tests/test-admin-first-login.js
+++ client/tests/test-admin-first-login.js
@@ -6,7 +6,7 @@ describe("admin login", function() {
   it("should be retrieve the account recovery key", async function() {
     await element(by.cssContainingText("a", "Preferences")).click();
     await browser.gl.utils.waitForUrl("/admin/preferences");
-    await element(by.cssContainingText("span", "Account recovery key")).click();
+    await element(by.cssContainingText("button", "Account recovery key")).click();
     await element(by.cssContainingText("button", "Close")).click();
   });
 });
