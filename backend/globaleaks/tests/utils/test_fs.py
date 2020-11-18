23b5c0c61a042b31ff3270410bb282f1f87ef3f3
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2020-11-18 23:19:25 +0100

    Add basic unit test for secure overwrite

--- backend/globaleaks/tests/utils/test_fs.py
+++ backend/globaleaks/tests/utils/test_fs.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8
+import glob
 import os
 
 from globaleaks.rest import errors
 from globaleaks.settings import Settings
 from globaleaks.tests import helpers
-from globaleaks.utils.fs import directory_traversal_check
+from globaleaks.utils import fs
 
 
-class TestFilesystemAccess(helpers.TestGL):
+class TestFilesystemUtilities(helpers.TestGL):
     def test_directory_traversal_failure_on_relative_trusted_path_must_fail(self):
-        self.assertRaises(Exception, directory_traversal_check, 'invalid/relative/trusted/path', "valid.txt")
+        self.assertRaises(Exception, fs.directory_traversal_check, 'invalid/relative/trusted/path', "valid.txt")
 
     def test_directory_traversal_check_blocked(self):
-        self.assertRaises(errors.DirectoryTraversalError, directory_traversal_check, Settings.files_path,
+        self.assertRaises(errors.DirectoryTraversalError, fs.directory_traversal_check, Settings.files_path,
                           "/etc/passwd")
 
     def test_directory_traversal_check_allowed(self):
         valid_access = os.path.join(Settings.files_path, "valid.txt")
-        directory_traversal_check(Settings.files_path, valid_access)
+        fs.directory_traversal_check(Settings.files_path, valid_access)
+
+    def test_overwrite_and_remove(self):
+        path = os.path.join(Settings.working_path, "antani.txt")
+
+        f = open(path, "wb")
+        f.seek((10 * 1024 * 1024) -1 )
+        f.write(b"\0")
+        f.close()
+
+        self.assertTrue(os.path.isfile(path))
+
+        fs.overwrite_and_remove(path, 10)
+
+        self.assertFalse(os.path.isfile(path))
