12f94871d4860d6b9922724f1b5fbf225b33af2c
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2021-03-15 15:23:47 +0100

    Revise implementation of Permission-Policy header

--- backend/globaleaks/rest/api.py
+++ backend/globaleaks/rest/api.py
@@ -479,13 +479,11 @@ class APIResourceWrapper(Resource):
             request.setHeader(b'Content-Security-Policy', csp)
 
         # Disable features that could be used to deanonymize the user
-        request.setHeader(b'Permissions-Policy', b"camera=('none') "
-                                                 b"display-capture=('none') "
-                                                 b"document-domain=('none') "
-                                                 b"fullscreen=('none') "
-                                                 b"geolocation=('none') "
-                                                 b"microphone=('none') "
-                                                 b"speaker=('none')")
+        request.setHeader(b'Permissions-Policy', b"camera=(),"
+                                                 b"document-domain=(),"
+                                                 b"fullscreen=(),"
+                                                 b"geolocation=(),"
+                                                 b"microphone=()")
 
         # Prevent old browsers not supporting CSP frame-ancestors directive to includes the platform within an iframe
         request.setHeader(b'X-Frame-Options', b'deny')
