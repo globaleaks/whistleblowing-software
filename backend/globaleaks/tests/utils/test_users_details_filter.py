3b9b7af95e94ba61a97bdee0256799b053320304
sunilpateliit <suniliit1821@gmail.com>
2021-04-02 17:00:50 +0530

    Added unit test file for filtering users sensitive information.

---
+++ backend/globaleaks/tests/utils/test_users_details_filter.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+import json
+from globaleaks.utils.users_details_filter import UserDetailsFilter
+
+
+class TestLogUtilities(unittest.TestCase):
+    def test_user_details_filter_removes_bgp_formatted_string(self):
+        mail_body = '''some text before pgp string 
+        -----BEGIN PGP PUBLIC KEY BLOCK-----
+        Comment: Alice's revocation certificate
+        Comment: https://www.ietf.org/id/draft-bre-openpgp-samples-01.html
+        iHgEIBYIACAWIQTrhbtfozp14V6UTmPyMVUMT0fjjgUCXaWkOwIdAAAKCRDyMVUM
+        T0fjjoBlAQDA9ukZFKRFGCooVcVoDVmxTaHLUXlIg9TPh2f7zzI9KgD/SLNXUOaH
+        O6TozOS7C9lwIHwwdHdAxgf5BzuhLT9iuAM==Tm8h
+        -----END PGP PUBLIC KEY BLOCK-----
+        some text after pgp string'''
+
+        user_details_filter = UserDetailsFilter(mail_body)
+        expected_formatted_string = 'some text before pgp string \n        *****\n        some text after pgp string'
+        self.assertEqual(user_details_filter.filtered_string(), expected_formatted_string)
+
+    def test_remove_session_id_details(self):
+        session = {"session_id" : 12324}
+        mail_body = json.dumps(session)
+        user_details_filter = UserDetailsFilter(mail_body)
+        expected_formatted_string = '{"***:***}'
+        self.assertEqual(user_details_filter.filtered_string(), expected_formatted_string)
+
+        session = OrderedDict([("session_id", 12324), ("user_id", 12345)])
+        mail_body = json.dumps(session)
+        user_details_filter = UserDetailsFilter(mail_body)
+        expected_formatted_string = '{"***":***, "user_id": 12345}'
+        self.assertEqual(user_details_filter.filtered_string(), expected_formatted_string)
+
+    def test_remove_uuid_details(self):
+        mail_body = "123e4567-e89b-12d3-a456-426614174000"
+        user_details_filter = UserDetailsFilter(mail_body)
+        self.assertEqual(user_details_filter.filtered_string(), 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
+
+    def test_remove_user_email_information(self):
+        mail_body = "abcd@defg.com"
+        user_details_filter = UserDetailsFilter(mail_body)
+        self.assertEqual(user_details_filter.filtered_string(), '~~~~~~@~~~~~~')
--- backend/globaleaks/utils/users_details_filter.py
+++ backend/globaleaks/utils/users_details_filter.py
@@ -2,75 +2,50 @@
 import os
 import re
 import sys
-import traceback
-from log import log
 
 class UserDetailsFilter:
     def __init__(self, text):
         self._text = text
-    
+
     def remove_bgp_formatted_string(self):
-        try:
-            # remove public key block
-            self._text = re.sub(r'-----BEGIN PGP PUBLIC KEY BLOCK-----.*-----END PGP PUBLIC KEY BLOCK-----',\
-                "*****", self._text, 0, re.DOTALL)
+        # remove public key block
+        self._text = re.sub(r'-----BEGIN PGP PUBLIC KEY BLOCK-----.*-----END PGP PUBLIC KEY BLOCK-----',
+            "*****", self._text, 0, re.DOTALL)
 
-            # remove private key block
-            self._text = re.sub(r'-----BEGIN PGP PRIVATE KEY BLOCK-----.*-----END PGP PRIVATE KEY BLOCK-----',\
-                "*****", self._text, 0, re.DOTALL)
+        # remove private key block
+        self._text = re.sub(r'-----BEGIN PGP PRIVATE KEY BLOCK-----.*-----END PGP PRIVATE KEY BLOCK-----',
+            "*****", self._text, 0, re.DOTALL)
 
-        except Exception as e:
-            log.err('Unable to replace the bgp formatted string %s .: %s', self._text, exception)
-            log.err('Verbose exception traceback:')
-            etype, value, tback = sys.exc_info()
-            log.err('\n'.join(traceback.format_exception(etype, value, tback)))
 
     def remove_session_id_details(self):
-        try:
-            # remove session id details
-            # here the assumption is that json output session details
-            # may look something like this:
-            #   {...., "session_id": .... } or  {...., "session_id": .... , .... } 
+        # remove session id details
+        # here the assumption is that json output session details
+        # may look something like this:
+        #   {...., "session_id": .... } or  {...., "session_id": .... , .... }
 
-            self._text = re.sub(r'session_id":.*,', "*****", self._text, 0, re.DOTALL)
-            self._text = re.sub(r'session_id":.*}', "*****", self._text, 0, re.DOTALL)                        
-        
-        except Exception as e:
-            log.err('Unable to replace the session_id string %s .: %s', self._text, exception)
-            log.err('Verbose exception traceback:')
-            etype, value, tback = sys.exc_info()
-            log.err('\n'.join(traceback.format_exception(etype, value, tback)))
+        self._text = re.sub(r'"session_id":.*,', '"***":***,', self._text, 0, re.DOTALL)
+        self._text = re.sub(r'"session_id":.*}', '"***":***}', self._text, 0, re.DOTALL)
+
 
     def remove_uuid_details(self):
-        try:
-            self._text = re.sub(r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}',\
-                "*****", self._text, 0, re.DOTALL)
-
-        except Exception as e:
-            log.err('Unable to replace the uuid string %s .: %s', self._text, exception)
-            log.err('Verbose exception traceback:')
-            etype, value, tback = sys.exc_info()
-            log.err('\n'.join(traceback.format_exception(etype, value, tback)))
+        # Removes the uuid4 details
+        self._text = re.sub(r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}',
+            "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", self._text, 0, re.DOTALL)
 
     def remove_user_email_information(self):
-        try:
-            self._text = re.sub(r'([\w+-.]){0,100}[\w]{1,100}@([\w+-.]){0,100}.[\w]{1,100}',\
-                "*****", self._text, 0, re.DOTALL)
-
-        except Exception as e:
-            log.err('Unable to replace the email id string %s .: %s', self._text, exception)
-            log.err('Verbose exception traceback:')
-            etype, value, tback = sys.exc_info()
-            log.err('\n'.join(traceback.format_exception(etype, value, tback)))
-
+        # removes the email address details
+        self._text = re.sub(r'([\w+-.]){0,100}[\w]{1,100}@([\w+-.]){0,100}.[\w]{1,100}',
+            "~~~~~~@~~~~~~", self._text, 0, re.DOTALL)
 
     def remove_user_sensitive_information(self):
+        # removes the user sensitive informations
         self.remove_bgp_formatted_string()
         self.remove_session_id_details()
         self.remove_user_email_information()
         self.remove_uuid_details()
-    
+
     def filtered_string(self):
+        # returns a filtered string after removing all the user sensitive information
         self.remove_user_sensitive_information()
         return self._text
 
