fdde7a4280fa92323008c40c6f1711155ea4883b
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2021-07-28 13:43:25 +0200

    Revise build script in relation to build of specific tags

--- scripts/build.sh
+++ scripts/build.sh
@@ -80,8 +80,7 @@ mkdir -p $BUILDSRC && cd $BUILDSRC
 if [ $LOCAL_ENV -eq 1 ]; then
   git clone --branch="$TAG" --depth=1 file://$(pwd)/../../../GlobaLeaks .
 else
-  git clone --depth=1 https://github.com/globaleaks/GlobaLeaks.git .
-  git checkout $TAG
+  git clone --branch="$TAG" --depth=1 https://github.com/globaleaks/GlobaLeaks.git .
 fi
 
 cd client && npm install -d && ./node_modules/grunt/bin/grunt build
