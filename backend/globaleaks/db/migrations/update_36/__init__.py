1156b2486f6b07cf039bab20a67bf7cc72fcc217
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2021-08-09 10:24:46 +0200

    Remove duplicated files and link to single file

--- backend/globaleaks/db/migrations/update_36/__init__.py
+++ backend/globaleaks/db/migrations/update_36/__init__.py
@@ -1,2 +1,1 @@
-# -*- coding: utf-8
-from globaleaks.db.migrations.update import MigrationBase as MigrationScript
+../empty.py
--- backend/globaleaks/db/migrations/update_44/__init__.py
+++ backend/globaleaks/db/migrations/update_44/__init__.py
@@ -1,2 +1,1 @@
-# -*- coding: UTF-8
-from globaleaks.db.migrations.update import MigrationBase as MigrationScript
+../empty.py
