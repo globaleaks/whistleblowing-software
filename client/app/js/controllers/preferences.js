fab3df2e79a70d934b0f57354f1bc0fe39795bbe
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2021-11-18 09:52:08 +0100

    Remove unused duplicated query to user preferences

--- client/app/js/controllers/preferences.js
+++ client/app/js/controllers/preferences.js
@@ -1,5 +1,5 @@
-GL.controller("PreferencesCtrl", ["$scope", "$q", "$http", "$location", "$window", "$uibModal", "UserPreferences",
-  function($scope, $q, $http, $location, $window, $uibModal, UserPreferences) {
+GL.controller("PreferencesCtrl", ["$scope", "$q", "$http", "$location", "$window", "$uibModal",
+  function($scope, $q, $http, $location, $window, $uibModal) {
     $scope.tabs = [
       {
         title: "Preferences",
@@ -10,10 +10,6 @@ GL.controller("PreferencesCtrl", ["$scope", "$q", "$http", "$location", "$window
         template: "views/partials/preferences/tab2.html"
       }
     ];
-
-    UserPreferences.get().$promise.then(function(preferences) {
-      $scope.preferences = preferences;
-    });
 
     $scope.editingName = false;
     $scope.editingPublicName = false;
