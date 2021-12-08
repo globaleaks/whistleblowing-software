6db37720ce73c2d531b7bf799545b71ff54b28bb
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2021-12-08 17:04:20 +0100

    Fix loading of receipt page when context id is provided within the uri

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -512,6 +512,7 @@ var GL = angular.module("GL", [
     };
 
     $rootScope.setPage = function(page) {
+      $location.path("/");
       $rootScope.page = page;
       $rootScope.Utils.set_title();
     };
