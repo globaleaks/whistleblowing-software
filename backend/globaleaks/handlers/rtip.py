globaleaks/whistleblowing-software

commit d3b194eeb7b5689145d6eb9a8505a0a3d531115a

Revise postpone feature in relation to #3166

backend/globaleaks/handlers/rtip.py
@@ -349,7 +349,7 @@ def db_postpone_expiration(session, itip, expiration_date):
     context = session.query(models.Context).filter(models.Context.id == itip.context_id).one()
 
     if context.tip_timetolive > 0:
-        max_expiration_date = get_expiration(context.tip_timetolive * 2)
+        max_expiration_date = get_expiration(Math.max(365, context.tip_timetolive * 2))
     else:
         max_expiration_date = datetime_never()
 

client/app/js/controllers/tip.js
@@ -283,7 +283,7 @@ GL.controller("TipCtrl",
               expiration_date: $scope.Utils.getPostponeDate($scope.contexts_by_id[$scope.tip.context_id].tip_timetolive),
               dateOptions: {
                 minDate: new Date($scope.tip.expiration_date),
-                maxDate: $scope.Utils.getPostponeDate($scope.contexts_by_id[$scope.tip.context_id].tip_timetolive * 2)
+                maxDate: $scope.Utils.getPostponeDate(Math.max(365, $scope.contexts_by_id[$scope.tip.context_id].tip_timetolive * 2))
               },
               opened: false,
               Utils: $scope.Utils

commit 44fe35ca1d732eb56290d9cfddaaabe39b471817

Fix regression on report postpone feature introduced in d3b194eeb7

backend/globaleaks/handlers/rtip.py
@@ -349,7 +349,7 @@ def db_postpone_expiration(session, itip, expiration_date):
     context = session.query(models.Context).filter(models.Context.id == itip.context_id).one()
 
     if context.tip_timetolive > 0:
-        max_expiration_date = get_expiration(Math.max(365, context.tip_timetolive * 2))
+        max_expiration_date = get_expiration(max(365, context.tip_timetolive * 2))
     else:
         max_expiration_date = datetime_never()
 