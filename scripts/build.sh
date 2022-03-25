d4a4acd01ca486e742d8c81171fee6eb62581740
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-03-26 00:04:03 +0100

    Reorder distro order in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TARGETS="bullseye buster focal bionic"
+TARGETS="bionic bullseye buster focal"
 DISTRIBUTION="bullseye"
 TAG="main"
 LOCAL_ENV=0
@@ -15,7 +15,7 @@ usage() {
   echo " -h"
   echo -e " -t tagname (build specific release/branch)"
   echo -e " -l (Use local repository & enviroment)"
-  echo -e " -d distribution (available: bullseye, buster, focal, bionic)"
+  echo -e " -d distribution (available: bionic, bullseye, buster, focal)"
   echo -e " -n (do not sign)"
   echo -e " -p (push on repository)"
 }
