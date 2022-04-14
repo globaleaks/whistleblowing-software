4f5c365e12c7af174b87bd820a727d74a324e51c
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-04-14 21:42:05 +0200

    Fix compatibility with both SQLAlchemy 1.3 and 1.4.3

--- backend/globaleaks/utils/json.py
+++ backend/globaleaks/utils/json.py
@@ -2,12 +2,19 @@
 import datetime
 import json
 
+try:
+    from sqlalchemy.engine import row
+except:
+    row = None
+
 from globaleaks.utils.utility import datetime_to_ISO8601
 
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, datetime.datetime):
+        if row and isinstance(obj, row.Row):
+            return dict(obj)
+        elif isinstance(obj, datetime.datetime):
             return datetime_to_ISO8601(obj)
         else:
             return super().default(obj)
