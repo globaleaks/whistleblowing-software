e89a3d32284f0575cb24d0deacf49362c4edf02d
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-04-16 11:51:47 +0200

    Bump to version 4.9.2

--- backend/globaleaks/__init__.py
+++ backend/globaleaks/__init__.py
@@ -7,7 +7,7 @@ import operator
 __author__ = 'GlobaLeaks'
 __email__ = 'info@globaleaks.org'
 __copyright__ = '2011-2022 - GlobaLeaks'
-__version__ = '4.9.1'
+__version__ = '4.9.2'
 __license__ = 'AGPL-3.0'
 
 DATABASE_VERSION = 62
