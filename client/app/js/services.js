globaleaks/whistleblowing-software

commit 7cb5aa5b035abd8c2fc178e7cab48d16b1d4d47f

Extend proof of work protection to every not authenticated request != GET, OPTIONS

client/app/js/services.js
@@ -12,8 +12,8 @@ GL.factory("GLResource", ["$resource", function($resource) {
   };
 }]).
 factory("Authentication",
-  ["$filter", "$http", "$location", "$window", "$rootScope", "GLTranslate", "TokenResource",
-  function($filter, $http, $location, $window, $rootScope, GLTranslate, TokenResource) {
+  ["$filter", "$http", "$location", "$window", "$rootScope", "GLTranslate",
+  function($filter, $http, $location, $window, $rootScope, GLTranslate) {
     function Session(){
       var self = this;
 
@@ -100,21 +100,19 @@ factory("Authentication",
           }
         };
 
-        return new TokenResource().$get().then(function(token) {
-          var promise;
-          if (authtoken) {
-            promise = $http.post("api/tokenauth?token=" + token.id, {"authtoken": authtoken});
+        var promise;
+        if (authtoken) {
+          promise = $http.post("api/tokenauth", {"authtoken": authtoken});
+        } else {
+          if (username === "whistleblower") {
+            password = password.replace(/\D/g,"");
+            promise = $http.post("api/receiptauth", {"receipt": password});
           } else {
-            if (username === "whistleblower") {
-              password = password.replace(/\D/g,"");
-              promise = $http.post("api/receiptauth?token=" + token.id, {"receipt": password});
-            } else {
-              promise = $http.post("api/authentication?token=" + token.id, {"tid": tid, "username": username, "password": password, "authcode": authcode});
-            }
+            promise = $http.post("api/authentication", {"tid": tid, "username": username, "password": password, "authcode": authcode});
           }
+        }
 
-          return promise.then(success_fn, failure_fn);
-        });
+        return promise.then(success_fn, failure_fn);
       };
 
       self.deleteSession = function() {
@@ -221,9 +219,7 @@ factory("TokenResource", ["GLResource", "glbcProofOfWork", function(GLResource, 
           var token = response.resource;
           return glbcProofOfWork.proofOfWork(token.id).then(function(result) {
             token.answer = result;
-            return token.$update().then(function(token) {
-              return token;
-            });
+            return token;
           });
         }
       }
@@ -245,7 +241,7 @@ factory("DATA_COUNTRIES_ITALY_PROVINCES", ["$resource", function($resource) {
 factory("DATA_COUNTRIES_ITALY_CITIES", ["$resource", function($resource) {
   return $resource("data/countries/it/comuni.json");
 }]).
-factory("Submission", ["$q", "$location", "$rootScope", "Authentication", "GLResource", "SubmissionResource", "TokenResource",
+factory("Submission", ["$q", "$location", "$rootScope", "Authentication", "GLResource", "SubmissionResource",
     function($q, $location, $rootScope, Authentication, GLResource, SubmissionResource) {
 
   return function(fn) {
@@ -358,28 +354,22 @@ factory("RTipCommentResource", ["GLResource", function(GLResource) {
 factory("RTipMessageResource", ["GLResource", function(GLResource) {
   return new GLResource("api/rtips/:id/messages", {id: "@id"});
 }]).
-factory("RTipDownloadRFile", ["$http", "$window", "TokenResource", function($http, $window, TokenResource) {
+factory("RTipDownloadRFile", ["Utils", function(Utils) {
   return function(file) {
-    return new TokenResource().$get().then(function(token) {
-      $window.open("api/rfile/" + file.id + "?token=" + token.id);
-    });
+    Utils.download("api/rfile/" + file.id);
   };
 }]).
 factory("RTipWBFileResource", ["GLResource", function(GLResource) {
   return new GLResource("api/wbfile/:id", {id: "@id"});
 }]).
-factory("RTipDownloadWBFile", ["$http", "$window", "TokenResource", function($http, $window, TokenResource) {
+factory("RTipDownloadWBFile", ["Utils", function(Utils) {
   return function(file) {
-    return new TokenResource().$get().then(function(token) {
-      $window.open("api/wbfile/" + file.id + "?token=" + token.id);
-    });
+    Utils.download("api/wbfile/" + file.id);
   };
 }]).
-factory("RTipExport", ["$http", "$window", "TokenResource", function($http, $window, TokenResource) {
+factory("RTipExport", ["Utils", function(Utils) {
   return function(tip) {
-    return new TokenResource().$get().then(function(token) {
-      $window.open("api/rtips/" + tip.id + "/export?token=" + token.id);
-    });
+    Utils.download("api/rtips/" + tip.id + "/export");
   };
 }]).
 factory("RTip", ["$rootScope", "$http", "RTipResource", "RTipMessageResource", "RTipCommentResource",
@@ -444,11 +434,9 @@ factory("WBTipCommentResource", ["GLResource", function(GLResource) {
 factory("WBTipMessageResource", ["GLResource", function(GLResource) {
   return new GLResource("api/wbtip/messages/:id", {id: "@id"});
 }]).
-factory("WBTipDownloadFile", ["$http", "$window", "TokenResource", function($http, $window, TokenResource) {
+factory("WBTipDownloadFile", ["Utils", function(Utils) {
   return function(file) {
-    return new TokenResource().$get().then(function(token) {
-      $window.open("api/wbtip/wbfile/" + file.id + "?token=" + token.id);
-    });
+    Utils.downoad("api/wbtip/wbfile/" + file.id);
   };
 }]).
 factory("WBTip", ["$rootScope", "WBTipResource", "WBTipCommentResource", "WBTipMessageResource",
@@ -1052,6 +1040,12 @@ factory("Utils", ["$rootScope", "$http", "$q", "$location", "$filter", "$uibModa
       };
     },
 
+    download: function(url) {
+      return new TokenResource().$get().then(function(token) {
+        $window.open(url + "?token=" + token.id);
+      });
+    },
+
     getSubmissionStatusText: function(status, substatus, submission_statuses) {
       var text;
       for (var i = 0; i < submission_statuses.length; i++) {
@@ -1077,9 +1071,7 @@ factory("Utils", ["$rootScope", "$http", "$q", "$location", "$filter", "$uibModa
     },
 
     submitSupportRequest: function(data) {
-      return new TokenResource().$get().then(function(token) {
-        return $http({method: "POST", url: "api/support?token=" + token.id, data:{"mail_address": data.mail_address, "text": data.text, "url": $location.absUrl()}});
-      });
+      return $http({method: "POST", url: "api/support", data:{"mail_address": data.mail_address, "text": data.text, "url": $location.absUrl()}});
     },
 
     print: function() {
@@ -1150,9 +1142,7 @@ factory("Utils", ["$rootScope", "$http", "$q", "$location", "$filter", "$uibModa
         return s.replace(uuid4RE, uuid4Empt).replace(emailRE, emailEmpt);
       }
 
-      return new TokenResource().$get().then(function(token) {
-        return $http.post("api/exception?token=" + token.id, scrub(exception));
-      });
+      return $http.post("api/exception", scrub(exception));
     }
   };
 }]).

backend/globaleaks/handlers/authentication.py
@@ -146,7 +146,6 @@ class AuthenticationHandler(BaseHandler):
     Login handler for admins and recipents and custodians
     """
     check_roles = 'any'
-    require_token = True
     uniform_answer_time = True
 
     @inlineCallbacks
@@ -183,7 +182,6 @@ class TokenAuthHandler(BaseHandler):
     Login handler for token based authentication
     """
     check_roles = 'any'
-    require_token = True
     uniform_answer_time = True
 
     @inlineCallbacks
@@ -209,7 +207,6 @@ class ReceiptAuthHandler(BaseHandler):
     Receipt handler used by whistleblowers
     """
     check_roles = 'any'
-    require_token = True
     uniform_answer_time = True
 
     @inlineCallbacks

backend/globaleaks/handlers/base.py
@@ -97,15 +97,12 @@ class BaseHandler(object):
         # Check session header
         session_id = self.request.headers.get(b'x-session')
 
-        # Check token GET argument:
-        if b'token' in self.request.args:
-            try:
-                token_id = self.request.args[b'token'][0].decode()
-                self.token = self.state.tokens.validate(token_id)
-                if self.token.session is not None:
-                    session_id = self.token.session.id.encode()
-            except:
-                pass
+        # Check token header
+        token = self.request.headers.get(b'x-token')
+        if token:
+            self.token = self.state.tokens.validate(token)
+            if self.token.session is not None:
+                session_id = self.token.session.id.encode()
 
         if session_id is None:
             return

backend/globaleaks/handlers/export.py
@@ -116,7 +116,6 @@ def prepare_tip_export(cc, tip_export):
 
 class ExportHandler(BaseHandler):
     check_roles = 'receiver'
-    require_token = True
     handler_exec_time_threshold = 3600
 
     @inlineCallbacks

backend/globaleaks/handlers/token.py
@@ -6,11 +6,7 @@ from globaleaks.rest import errors, requests
 from globaleaks.state import State
 
 
-def generate_token(tid, session):
-    return State.tokens.new(tid, session).serialize()
-
-
-class TokenCreate(BaseHandler):
+class TokenHandler(BaseHandler):
     """
     This class implement the handler for requesting a token.
     """
@@ -21,22 +17,4 @@ class TokenCreate(BaseHandler):
         This API create a Token, a temporary memory only object able to
         keep track and limit user actions.
         """
-        return generate_token(self.request.tid, self.session)
-
-
-class TokenInstance(BaseHandler):
-    """
-    This class implements the handler for updating a token (e.g.: solving a captcha)
-    """
-    check_roles = 'any'
-
-    def put(self, token_id):
-        request = self.validate_request(self.request.content.read(), requests.TokenAnswerDesc)
-
-        token = self.state.tokens.get(token_id)
-        if token is None or self.request.tid != token.tid:
-            raise errors.InvalidAuthentication
-
-        token.update(request['answer'])
-
-        return token.serialize()
+        return State.tokens.new(self.request.tid, self.session).serialize()

backend/globaleaks/rest/api.py
@@ -76,8 +76,7 @@ api_spec = [
     (r'/api/user/operations', user.UserOperationHandler),
 
     # Token Handlers
-    (r'/api/token', token.TokenCreate),
-    (r'/api/token/' + requests.token_regexp, token.TokenInstance),
+    (r'/api/token', token.TokenHandler),
 
     # Submission Handlers
     (r'/api/submission', submission.SubmissionInstance),

backend/globaleaks/rest/decorators.py
@@ -34,7 +34,7 @@ def decorator_rate_limit(f):
 
 def decorator_require_token(f):
     def wrapper(self, *args, **kwargs):
-        if self.require_token and not self.token:
+        if not self.session and not self.request.uri.startswith(b"/api/token") and not self.token:
             raise errors.InternalServerError("TokenFailure: Missing or invalid token")
 
         return f(self, *args, **kwargs)
@@ -124,12 +124,10 @@ def decorate_method(h, method):
             if h.refresh_connection_endpoints:
                 f = decorator_refresh_connection_endpoints(f)
 
-    if h.require_token:
-        f = decorator_require_token(f)
-
     f = decorator_authentication(f, value)
 
-    if method in ['post', 'put', 'delete']:
+    if method not in ['get', 'options'] or h.require_token:
+        f = decorator_require_token(f)
         f = decorator_rate_limit(f)
 
     setattr(h, method, f)

backend/globaleaks/tests/handlers/test_token.py
@@ -6,8 +6,8 @@ from globaleaks.jobs import anomalies
 from globaleaks.tests import helpers
 
 
-class Test_TokenCreate(helpers.TestHandlerWithPopulatedDB):
-    _handler = token.TokenCreate
+class Test_Token(helpers.TestHandlerWithPopulatedDB):
+    _handler = token.TokenHandler
 
     def assert_default_token_values(self, token):
         self.assertEqual(token['ttl'], 60)
@@ -23,41 +23,3 @@ class Test_TokenCreate(helpers.TestHandlerWithPopulatedDB):
         response = yield handler.post()
 
         self.assert_default_token_values(response)
-
-
-class Test_TokenInstance(helpers.TestHandlerWithPopulatedDB):
-    _handler = token.TokenInstance
-
-    @inlineCallbacks
-    def test_put_right_answer(self):
-        self.pollute_events()
-        yield anomalies.Anomalies().run()
-
-        token = self.getToken()
-
-        request_payload = token.serialize()
-        request_payload['answer'] = token.answer
-
-        handler = self.request(request_payload)
-
-        yield handler.put(token.id)
-
-        token.tokenlist.validate(token.id)
-
-        self.assertTrue(token.solved)
-
-    @inlineCallbacks
-    def test_put_wrong_answer(self):
-        self.pollute_events()
-        yield anomalies.Anomalies().run()
-
-        token = self.getToken()
-
-        request_payload = token.serialize()
-        request_payload['answer'] = 0
-
-        handler = self.request(request_payload)
-
-        yield handler.put(token.id)
-
-        self.assertRaises(Exception, token.tokenlist.validate, token.id)

commit a0a04ed45a1f66d78e3d609653d8e53be6fb6d5e

Fix regression on download of files uploaded by recipients introduced in 4.9.1
Regression introduced with commit: 7cb5aa5b035abd8c2fc178e7cab48d16b1d4d47f

client/app/js/services.js
@@ -436,7 +436,7 @@ factory("WBTipMessageResource", ["GLResource", function(GLResource) {
 }]).
 factory("WBTipDownloadFile", ["Utils", function(Utils) {
   return function(file) {
-    Utils.downoad("api/wbtip/wbfile/" + file.id);
+    Utils.download("api/wbtip/wbfile/" + file.id);
   };
 }]).
 factory("WBTip", ["$rootScope", "WBTipResource", "WBTipCommentResource", "WBTipMessageResource",