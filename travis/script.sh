61fe4c94329bf2facba1cd097ad4634cf337ff34
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-04-24 12:26:41 +0200

    Remove code related to testing on saucelabs

--- travis/script.sh
+++ travis/script.sh
@@ -107,27 +107,4 @@ elif [ "$GLTEST" = "build_and_install" ]; then
   sudo su -c 'echo "builduser ALL=NOPASSWD: ALL" >> "$chroot"/etc/sudoers'
   sudo chroot "$chroot" chown builduser -R /build
   sudo chroot "$chroot" su - builduser /bin/bash -c '/build/GlobaLeaks/travis/build_and_install.sh'
-elif [[ $GLTEST =~ ^end2end-.* ]]; then
-  echo "Running Browsertesting on Saucelabs"
-
-  declare -a capabilities=(
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"Chrome\", \"version\":\"latest\", \"platform\":\"macOS 10.15\", \"maxDuration\":\"7200\", \"commandTimeout\":\"600\", \"idleTimeout\":\"270\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"Firefox\", \"version\":\"latest\", \"platform\":\"macOS 10.15\", \"maxDuration\":\"7200\", \"commandTimeout\":\"600\", \"idleTimeout\":\"270\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"MicrosoftEdge\", \"version\":\"latest\", \"platform\":\"Windows 10\", \"maxDuration\":\"7200\", \"commandTimeout\":\"600\", \"idleTimeout\":\"270\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"Internet Explorer\", \"version\":\"latest\", \"platform\":\"Windows 10\", \"maxDuration\":\"7200\", \"commandTimeout\":\"600\", \"idleTimeout\":\"270\"}'"
-    "export SELENIUM_BROWSER_CAPABILITIES='{\"browserName\":\"Chrome\", \"platformName\":\"Android\", \"platformVersion\": \"11.0\", \"deviceName\": \"Android GoogleAPI Emulator\", \"deviceOrientation\": \"portrait\", \"maxDuration\":\"7200\", \"commandTimeout\":\"600\", \"idleTimeout\":\"270\"}'"
-  )
-
-  testkey=$(echo $GLTEST | cut -f2 -d-)
-
-  ## now loop through the above array
-  capability=${capabilities[${testkey}]}
-
-  echo "Testing Configuration: ${testkey}"
-  setupDependencies 1
-  eval $capability
-  $TRAVIS_BUILD_DIR/backend/bin/globaleaks -z -d
-  sleep 3
-  cd $TRAVIS_BUILD_DIR/client
-  node_modules/protractor/bin/protractor tests/protractor-sauce.config.js
 fi
