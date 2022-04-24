globaleaks/whistleblowing-software

commit a4aa89a8a46abe439099f752e7df33e428ac7d6a

Remove outdated and unused client code

client/app/js/controllers/login.js
@@ -10,9 +10,7 @@ GL.controller("LoginCtrl", ["$scope", "$location", function($scope, $location) {
     $location.path($scope.Authentication.session.homepage);
   }
 
-  if ($location.path() === "/receipt") {
-    $scope.login_template = "views/login/receipt.html";
-  } else if ($location.path() === "/login" && $scope.public.node.simplified_login) {
+  if ($scope.public.node.simplified_login) {
     $scope.login_template = "views/login/simplified.html";
   } else {
     $scope.login_template = "views/login/default.html";

commit 0031dd775a762df4724a115c6f3be6147ed59d68

Fix regression on simplified login introduced in 4.9.0
Fix regression introduced in commit a4aa89a8a46abe439099f752e7df33e428ac7d6a

client/app/js/controllers/login.js
@@ -10,7 +10,7 @@ GL.controller("LoginCtrl", ["$scope", "$location", function($scope, $location) {
     $location.path($scope.Authentication.session.homepage);
   }
 
-  if ($scope.public.node.simplified_login) {
+  if ($location.path() === "/login" && $scope.public.node.simplified_login) {
     $scope.login_template = "views/login/simplified.html";
   } else {
     $scope.login_template = "views/login/default.html";