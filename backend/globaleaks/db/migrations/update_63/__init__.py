8218659e6a0207b59ea0e05a8ac56f10b32dac22
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-07-24 17:34:04 +0200

    Implement migration script in relation to tenant modes refactoring

--- backend/globaleaks/db/migrations/update_63/__init__.py
+++ backend/globaleaks/db/migrations/update_63/__init__.py
@@ -8,6 +8,21 @@ from globaleaks.db.migrations.update import MigrationBase
 
 
 class MigrationScript(MigrationBase):
+    def migrate_Config(self):
+        for old_obj in self.session_old.query(self.model_from['Config']):
+            new_obj = self.model_to['Config']()
+            for key in new_obj.__mapper__.column_attrs.keys():
+                setattr(new_obj, key, getattr(old_obj, key))
+
+            if old_obj.var_name == 'mode':
+                if old_obj.value == 'whistleblowing.it':
+                    new_obj.value = 'wbpa'
+
+                elif old_obj.value == 'eat':
+                    new_obj.value = 'default'
+
+            self.session_new.add(new_obj)
+
     def migrate_File(self):
         for old_obj in self.session_old.query(self.model_from['File']):
             if old_obj.name == 'script':
