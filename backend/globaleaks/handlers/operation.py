bb8da2f2e0e06b2eafbd7bee108e1a042991aa0f
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2022-07-25 13:52:53 +0200

    Reorganize code related to confirmation checks on operations

--- backend/globaleaks/handlers/operation.py
+++ backend/globaleaks/handlers/operation.py
@@ -7,13 +7,15 @@ class OperationHandler(BaseHandler):
     """
     Base handler for implementing handlers for executing platform configuration
     """
+    require_confirmation = []
+
     def operation_descriptors(self):
         raise NotImplementedError
 
     def put(self, *args, **kwargs):
         request = self.validate_request(self.request.content.read(), requests.OpsDesc)
 
-        if request['operation'] in ['get_recovery_key', 'disable_2fa']:
+        if request['operation'] in self.require_confirmation:
             self.check_confirmation()
 
         func = self.operation_descriptors().get(request['operation'], None)
