globaleaks/whistleblowing-software

commit 0750098ef936452904f666bcb5aefc24fcfade84

Make it possible to filter receiver tips by date args (updated_after, updated_before)

backend/globaleaks/handlers/receiver/__init__.py
@@ -4,6 +4,8 @@
 import base64
 import json
 
+from datetime import datetime
+
 from sqlalchemy.sql.expression import distinct, func
 
 from globaleaks import models
@@ -16,7 +18,7 @@ from globaleaks.utils.crypto import GCE
 
 
 @transact
-def get_receivertips(session, tid, receiver_id, user_key, language):
+def get_receivertips(session, tid, receiver_id, user_key, language, args={}):
     """
     Return list of submissions received by the specified receiver
 
@@ -31,6 +33,9 @@ def get_receivertips(session, tid, receiver_id, user_key, language):
       'questionnaires': {},
       'rtips': []
     }
+
+    updated_after = datetime.fromtimestamp(int(args.get(b'updated_after', [b'1660801500'])[0]))
+    updated_before = datetime.fromtimestamp(int(args.get(b'updated_before', [b'32503680000'])[0]))
 
     messages_by_rtip = {}
     comments_by_itip = {}
@@ -69,6 +74,8 @@ def get_receivertips(session, tid, receiver_id, user_key, language):
                                                   models.InternalTipAnswers,
                                                   models.ArchivedSchema) \
                                            .filter(models.ReceiverTip.receiver_id == receiver_id,
+                                                   models.InternalTip.update_date >= updated_after,
+                                                   models.InternalTip.update_date <= updated_before,
                                                    models.InternalTip.id == models.ReceiverTip.internaltip_id,
                                                    models.InternalTipAnswers.internaltip_id == models.ReceiverTip.internaltip_id,
                                                    models.InternalTipAnswers.questionnaire_hash == models.ArchivedSchema.hash) \
@@ -164,7 +171,8 @@ class TipsCollection(BaseHandler):
         return get_receivertips(self.request.tid,
                                 self.session.user_id,
                                 self.session.cc,
-                                self.request.language)
+                                self.request.language,
+                                self.request.args)
 
 
 class Operations(BaseHandler):

commit a28cbe8b7654c924873c71505f3bbee787aff776

Fix regression in relation to old reports introduced in 0750098ef936452904f666bcb5aefc24fcfade84

backend/globaleaks/handlers/receiver/__init__.py
@@ -34,7 +34,7 @@ def get_receivertips(session, tid, receiver_id, user_key, language, args={}):
       'rtips': []
     }
 
-    updated_after = datetime.fromtimestamp(int(args.get(b'updated_after', [b'1660801500'])[0]))
+    updated_after = datetime.fromtimestamp(int(args.get(b'updated_after', [b'0'])[0]))
     updated_before = datetime.fromtimestamp(int(args.get(b'updated_before', [b'32503680000'])[0]))
 
     messages_by_rtip = {}