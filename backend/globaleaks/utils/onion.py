134a8ad9025ec10f24f12f164958576a62c0b6e6
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-02-01 18:44:26 +0100

    Fix re-generation of onion services in relation to txtorcon libraries changes

--- backend/globaleaks/utils/onion.py
+++ backend/globaleaks/utils/onion.py
@@ -37,4 +37,4 @@ def generate_onion_service_v3():
     onionAddressBytes = b''.join([public_bytes, checksum, bytes([0x03])])
     onionAddress = base64.b32encode(onionAddressBytes).lower().decode('utf-8')
 
-    return onionAddress + '.onion', b'ED25519-V3:' + base64.b64encode(expandSK(private_bytes))
+    return onionAddress + '.onion', 'ED25519-V3:' + base64.b64encode(expandSK(private_bytes)).decode('utf-8')
