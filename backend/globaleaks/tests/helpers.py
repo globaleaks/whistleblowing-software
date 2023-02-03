059df3ff3d1034ad5a42089b886b998b3d094172
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-02-03 22:39:32 +0530

    Add backend unit tests for custom_support_url

--- backend/globaleaks/tests/helpers.py
+++ backend/globaleaks/tests/helpers.py
@@ -358,7 +358,8 @@ class MockDict:
             'adminonly': False,
             'basic_auth': False,
             'basic_auth_username': '',
-            'basic_auth_password': ''
+            'basic_auth_password': '',
+            'custom_support_url': '',
         }
 
         self.dummyNetwork = {
