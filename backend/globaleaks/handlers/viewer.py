eac3263a0f83ef99f004c81a245ef66c40e922b5
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-03-09 17:11:26 +0100

    Implement sandbox for FileViewer

---
+++ backend/globaleaks/handlers/viewer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Handler exposing viewer application
+from globaleaks.handlers.staticfile import StaticFileHandler
+
+class ViewerHandler(StaticFileHandler):
+    def __init__(self, state, request):
+        StaticFileHandler.__init__(self, state, request)
+
+        if not state.settings.disable_csp:
+            request.setHeader(b'Content-Security-Policy',
+                              b"base-uri 'none';"
+                              b"default-src 'none';"
+                              b"form-action 'none';"
+                              b"frame-ancestors 'self';"
+                              b"img-src blob:;"
+                              b"media-src blob:;"
+                              b"script-src 'self';")
+
+        request.setHeader(b'Access-Control-Allow-Origin', b'null')
