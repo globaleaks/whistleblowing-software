76f56e172ba26d5b5cbce6c42a66d8fa802c62d7
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-03-13 18:13:16 +0100

    Add view support for files of type PDF and TXT

--- client/app/viewer/script.js
+++ client/app/viewer/script.js
@@ -1,14 +1,122 @@
-function receiveMessage (evt) {
-  var url = URL.createObjectURL(evt.data.blob);
+/* eslint-disable no-undef */
+var mediaViewer = document.getElementById("media-viewer");
+var pdfViewer = document.getElementById("pdf-viewer");
+var pdfCanvas = document.getElementById("pdf-canvas");
+var pdfControlNext = document.getElementById("next-page-btn");
+var pdfControlPrev = document.getElementById("prev-page-btn");
+var pdfControlPage = document.getElementById("page-number");
+var pdfControlPageCount = document.getElementById("page-count");
+var pdfDoc = null;
+var pageNum = 1;
+var pageCount = 0;
 
-  if (evt.data.tag === "audio" ) {
-    document.body.innerHTML = "<audio id=\"viewer\" src=\"" + url + "\" controls /></audio>";
-  } else if (evt.data.tag === "image" ) {
-    document.body.innerHTML = "<img id=\"viewer\" src=\"" + url + "\" />";
-  } else if (evt.data.tag === "video" ) {
-    document.body.innerHTML = "<video id=\"viewer\" src=\"" + url + "\" controls /></video>";
+function receiveMessage(evt) {
+  var url = URL.createObjectURL(evt.data.blob);
+  if (evt.data.tag === "pdf") {
+    pdfViewer.style.display = "block";
+    mediaViewer.style.display = "none";
+    createPdfViewer(url);
+  } else {
+    pdfViewer.style.display = "none";
+    mediaViewer.style.display = "block";
+    if (evt.data.tag === "audio") {
+      mediaViewer.innerHTML =
+        "<audio id=\"viewer\" src=\"' + url + '\" controls /></audio>";
+    } else if (evt.data.tag === "image") {
+      mediaViewer.innerHTML =
+        "<img width=\"100%\" id=\"viewer\" src=\"" + url + "\" />";
+    } else if (evt.data.tag === "video") {
+      mediaViewer.innerHTML =
+        "<video id=\"viewer\" src=\"" + url + "\" controls /></video>";
+    } else if (evt.data.tag === "txt"){
+      evt.data.blob.text().then(function(text){
+        mediaViewer.innerHTML =
+        "<pre id=\"txt-viewer\">" + text + "</pre>";
+      });
+    }
   }
 }
 
-window.addEventListener("message", receiveMessage, {once: true});
-window.parent.postMessage("ready", "*");
+function createPdfViewer(url) {
+  pdfjsLib.getDocument(url).promise.then(function (pdfDoc_) {
+    pdfDoc = pdfDoc_;
+    pageCount = pdfDoc.numPages;
+    pdfControlPageCount.innerHTML = pageCount;
+    pdfControlPage.innerText = 0;
+    renderPage(pageNum);
+  });
+
+  pdfControlNext.addEventListener("click", pdfNextPage);
+  pdfControlPrev.addEventListener("click", pdfPrevPage);
+}
+
+function renderPage(num) {
+  pdfDoc.getPage(num).then(function (page) {
+    // find scale to fit page in canvas
+    var scale = pdfCanvas.clientWidth / page.getViewport({ scale: 1.0 }).width;
+    var viewport = page.getViewport({ scale: scale});
+    var canvas = pdfCanvas;
+    var context = canvas.getContext("2d");
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    var renderTask = page.render(renderContext);
+    renderTask.promise.then(function () {
+      // updating label
+      pdfControlPage.innerText = pageNum;
+      // disable and enable control buttons
+      if (pageNum <= 1) {
+        pdfControlPrev.disabled = true;
+      }
+      if (pageNum >= pageCount) {
+        pdfControlNext.disabled = true;
+      }
+      if (pageNum > 1) {
+        pdfControlPrev.disabled = false;
+      }
+      if (pageNum < pageCount) {
+        pdfControlNext.disabled = false;
+      }
+    });
+  });
+}
+
+function pdfNextPage() {
+  if (pageNum >= pageCount) {
+    return;
+  }
+  pageNum++;
+  pdfControlPage.value = pageNum;
+  renderPage(pageNum);
+}
+
+function pdfPrevPage() {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  pdfControlPage.value = pageNum;
+  renderPage(pageNum);
+}
+
+window.addEventListener(
+  "load",
+  function () {
+    window.parent.postMessage("ready", "*");
+    window.addEventListener("message", receiveMessage, { once: true });
+  },
+  true
+);
+
+window.addEventListener(
+  "unload",
+  function () {
+    pdfControlNext.removeEventListener("click", pdfNextPage);
+    pdfControlPrev.removeEventListener("click", pdfPrevPage);
+  },
+  true
+);
