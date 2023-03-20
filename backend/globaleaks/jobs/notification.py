globaleaks/whistleblowing-software

commit cf708b294029d827b3c69ff60d0c7a13b9f9ca74

Remove redundant serializations from mail notifications

backend/globaleaks/jobs/notification.py
@@ -2,17 +2,17 @@
 # Implement the notification of new submissions
 import copy
 
+import itertools
+
 from datetime import timedelta
 
 from sqlalchemy import and_, or_
 from twisted.internet import defer
 
 from globaleaks import models
-from globaleaks.handlers.admin.context import admin_serialize_context
 from globaleaks.handlers.admin.node import db_admin_serialize_node
 from globaleaks.handlers.admin.notification import db_get_notification
-from globaleaks.handlers.public import db_get_submission_statuses
-from globaleaks.handlers.rtip import serialize_rtip, serialize_message, serialize_comment
+from globaleaks.handlers.rtip import serialize_rtip
 from globaleaks.handlers.user import user_serialize_user
 from globaleaks.jobs.job import LoopingJob
 from globaleaks.orm import db_del, transact, tw
@@ -20,22 +20,6 @@ from globaleaks.utils.log import log
 from globaleaks.utils.pgp import PGPContext
 from globaleaks.utils.templating import Templating
 from globaleaks.utils.utility import datetime_now, deferred_sleep
-
-
-trigger_template_map = {
-    'Comment': 'tip_update',
-    'Message': 'tip_update',
-    'ReceiverFile': 'tip_update',
-    'ReceiverTip': 'tip'
-}
-
-
-trigger_model_map = {
-    'Comment': models.Comment,
-    'Message': models.Message,
-    'ReceiverFile': models.ReceiverFile,
-    'ReceiverTip': models.ReceiverTip
-}
 
 
 def gen_cache_key(*args):
@@ -61,107 +45,6 @@ class MailGenerator(object):
 
         return self.cache[cache_key]
 
-    def serialize_obj(self, session, key, obj, tid, language):
-        obj_id = obj.id
-
-        cache_key = gen_cache_key(key, tid, obj_id, language)
-        cache_obj = None
-
-        if cache_key not in self.cache:
-            if key == 'user':
-                cache_obj = user_serialize_user(session, obj, language)
-            elif key == 'context':
-                cache_obj = admin_serialize_context(session, obj, language)
-            elif key == 'tip':
-                itip = session.query(models.InternalTip).filter(models.InternalTip.id == obj.internaltip_id).one()
-                cache_obj = serialize_rtip(session, obj, itip, language)
-            elif key == 'message':
-                cache_obj = serialize_message(session, obj)
-            elif key == 'comment':
-                cache_obj = serialize_comment(session, obj)
-            elif key == 'file':
-                cache_obj = models.serializers.serialize_ifile(session, obj)
-
-            self.cache[cache_key] = cache_obj
-
-        return self.cache[cache_key]
-
-    def process_ReceiverTip(self, session, rtip, data):
-        user, context = session.query(models.User, models.Context) \
-                               .filter(models.User.id == rtip.receiver_id,
-                                       models.InternalTip.id == rtip.internaltip_id,
-                                       models.Context.id == models.InternalTip.context_id).one()
-
-        tid = context.tid
-
-        data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-        data['tip'] = self.serialize_obj(session, 'tip', rtip, tid, user.language)
-        data['context'] = self.serialize_obj(session, 'context', context, tid, user.language)
-
-        self.process_mail_creation(session, tid, data)
-
-    def process_Message(self, session, message, data):
-        # if the message was created by a receiver do not generate mails
-        if message.type == "receiver":
-            return
-
-        user, context, rtip = session.query(models.User, models.Context, models.ReceiverTip) \
-                                     .filter(models.User.id == models.ReceiverTip.receiver_id,
-                                             models.ReceiverTip.id == message.receivertip_id,
-                                             models.Context.id == models.InternalTip.context_id,
-                                             models.InternalTip.id == models.ReceiverTip.internaltip_id).one()
-
-        tid = context.tid
-
-        data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-        data['tip'] = self.serialize_obj(session, 'tip', rtip, tid, user.language)
-        data['context'] = self.serialize_obj(session, 'context', context, tid, user.language)
-
-        self.process_mail_creation(session, tid, data)
-
-    def process_Comment(self, session, comment, data):
-        for user, context, rtip in session.query(models.User, models.Context, models.ReceiverTip) \
-                                          .filter(models.User.id == models.ReceiverTip.receiver_id,
-                                                  models.ReceiverTip.internaltip_id == comment.internaltip_id,
-                                                  models.Context.id == models.InternalTip.context_id,
-                                                  models.InternalTip.id == comment.internaltip_id,
-                                                  models.ReceiverTip.internaltip_id == comment.internaltip_id,
-                                                  models.ReceiverTip.receiver_id != comment.author_id):
-            tid = context.tid
-
-            umsg = copy.deepcopy(data)
-            umsg['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-            umsg['tip'] = self.serialize_obj(session, 'tip', rtip, tid, user.language)
-            umsg['context'] = self.serialize_obj(session, 'context', context, tid, user.language)
-
-            self.process_mail_creation(session, tid, umsg)
-
-    def process_ReceiverFile(self, session, rfile, data):
-        user, context, rtip, ifile = session.query(models.User, models.Context, models.ReceiverTip, models.InternalFile) \
-                                            .filter(models.User.id == models.ReceiverTip.receiver_id,
-                                                    models.InternalFile.id == rfile.internalfile_id,
-                                                    models.InternalFile.submission.is_(False),
-                                                    models.InternalTip.id == models.InternalFile.internaltip_id,
-                                                    models.ReceiverTip.id == rfile.receivertip_id,
-                                                    models.Context.id == models.InternalTip.context_id).one()
-
-        tid = context.tid
-
-        data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-        data['tip'] = self.serialize_obj(session, 'tip', rtip, tid, user.language)
-        data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-        data['context'] = self.serialize_obj(session, 'context', context, tid, user.language)
-
-        self.process_mail_creation(session, tid, data)
-
-    def process_UnreadTips(self, session, user, data):
-        tid = user.tid
-
-        data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
-
-        self.process_mail_creation(session, tid, data)
-
-
     def process_mail_creation(self, session, tid, data):
         user_id = data['user']['id']
         language = data['user']['language']
@@ -172,8 +55,6 @@ class MailGenerator(object):
             return
 
         data['node'] = self.serialize_config(session, 'node', tid, language)
-
-        data['submission_statuses'] = db_get_submission_statuses(session, tid, language)
 
         if data['node']['mode'] == 'default':
             data['notification'] = self.serialize_config(session, 'notification', tid, language)
@@ -198,6 +79,7 @@ class MailGenerator(object):
     @transact
     def generate(self, session):
         now = datetime_now()
+        rtips_ids = {}
         silent_tids = []
 
         reminder_time = self.state.tenant_cache[1].unread_reminder_time if 1 in self.state.tenant_cache else 7
@@ -206,37 +88,51 @@ class MailGenerator(object):
             if cache_item.notification and cache_item.notification.disable_receiver_notification_emails:
                 silent_tids.append(tid)
 
-        if silent_tids:
-            for x in session.query(models.ReceiverTip).filter(models.ReceiverTip.internaltip_id == models.InternalTip.id,
-                                                              models.InternalTip.tid.in_(silent_tids)):
-                x.new = False
+        results1 = session.query(models.User, models.ReceiverTip, models.InternalTip, models.ReceiverTip) \
+                          .filter(models.User.id == models.ReceiverTip.receiver_id,
+                                  models.InternalTip.id == models.ReceiverTip.internaltip_id,
+                                  models.ReceiverTip.new.is_(True))
 
-            for x in session.query(models.Comment).filter(models.Comment.internaltip_id == models.InternalTip.id,
-                                                          models.InternalTip.tid.in_(silent_tids)):
-                x.new = False
+        results2 = session.query(models.User, models.ReceiverTip, models.InternalTip, models.Comment) \
+                                 .filter(models.User.id == models.ReceiverTip.receiver_id,
+                                         models.ReceiverTip.internaltip_id == models.Comment.internaltip_id,
+                                         models.Comment.type == 'whistleblower',
+                                         models.InternalTip.id == models.ReceiverTip.internaltip_id,
+                                         models.Comment.new.is_(True))
 
-            for x in session.query(models.Message).filter(models.Message.receivertip_id == models.ReceiverTip.id,
-                                                          models.ReceiverTip.internaltip_id == models.InternalTip.id,
-                                                          models.InternalTip.tid.in_(silent_tids)):
-                x.new = False
+        results3 = session.query(models.User, models.ReceiverTip, models.InternalTip, models.Message) \
+                            .filter(models.User.id == models.ReceiverTip.receiver_id,
+                                    models.ReceiverTip.id == models.Message.receivertip_id,
+                                    models.InternalTip.id == models.ReceiverTip.internaltip_id,
+                                    models.Message.new.is_(True))
 
-            for x in session.query(models.ReceiverFile).filter(models.ReceiverFile.receivertip_id == models.ReceiverTip.id,
-                                                               models.ReceiverTip.internaltip_id == models.InternalTip.id,
-                                                               models.InternalTip.tid.in_(silent_tids)):
-                x.new = False
+        results4 = session.query(models.User, models.ReceiverTip, models.InternalTip, models.ReceiverFile) \
+                          .filter(models.User.id == models.ReceiverTip.receiver_id,
+                                    models.ReceiverTip.id == models.ReceiverFile.receivertip_id,
+                                    models.InternalTip.id == models.ReceiverTip.internaltip_id,
+                                    models.InternalFile.id == models.ReceiverFile.internalfile_id,
+                                    models.InternalFile.submission.is_(False),
+                                    models.ReceiverFile.new.is_(True))
 
-        for trigger in ['ReceiverTip', 'Comment', 'Message', 'ReceiverFile']:
-            model = trigger_model_map[trigger]
+        for user, rtip, itip, obj in itertools.chain(results1, results2, results3, results4):
+            tid = user.tid
 
-            for element in session.query(model).filter(model.new.is_(True)):
-                data = {'type': trigger_template_map[trigger]}
+            obj.new = False
 
-                try:
-                    getattr(self, 'process_%s' % trigger)(session, element, data)
-                except Exception as e:
-                    log.err("Unhandled exception during mail generation: %s", e)
-                finally:
-                    element.new = False
+            if rtips_ids.get(rtip.id, False) or tid in silent_tids:
+                continue
+
+            rtips_ids[rtip.id] = True
+
+            if isinstance(obj, models.ReceiverTip):
+                data = {'type': 'tip'}
+            else:
+                data = {'type': 'tip_update'}
+
+            data['user'] = user_serialize_user(session, user, user.language)
+            data['tip'] = serialize_rtip(session, rtip, itip, user.language)
+
+            self.process_mail_creation(session, tid, data)
 
         for user in session.query(models.User).filter(and_(models.User.reminder_date < now - timedelta(reminder_time),
                                                            or_(and_(models.User.id == models.ReceiverTip.receiver_id,
@@ -246,9 +142,13 @@ class MailGenerator(object):
                                                                and_(models.User.id == models.ReceiverTip.receiver_id,
                                                                     models.ReceiverTip.last_access < models.InternalTip.update_date,
                                                                     models.ReceiverTip.internaltip_id == models.InternalTip.id,
+
                                                                     models.InternalTip.update_date < now - timedelta(reminder_time)))).distinct():
+            tid = user.tid
             data = {'type': 'unread_tips'}
-            self.process_UnreadTips(session, user, data)
+            data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
+            self.process_mail_creation(session, tid, data)
+
             user.reminder_date = now
 
 

backend/globaleaks/utils/templating.py
@@ -28,10 +28,6 @@ node_keywords = [
     '{LoginUrl}',
 ]
 
-context_keywords = [
-    '{ContextName}'
-]
-
 user_keywords = [
     '{RecipientName}',
     '{Username}'
@@ -220,17 +216,9 @@ class UserNodeKeyword(NodeKeyword, UserKeyword):
     data_keys = NodeKeyword.data_keys + UserKeyword.data_keys
 
 
-class ContextKeyword(Keyword):
-    keyword_list = context_keywords
-    data_keys = ['context']
-
-    def ContextName(self):
-        return self.data['context']['name']
-
-
-class TipKeyword(UserNodeKeyword, ContextKeyword):
-    keyword_list = UserNodeKeyword.keyword_list + ContextKeyword.keyword_list + tip_keywords
-    data_keys = UserNodeKeyword.data_keys + ContextKeyword.data_keys + ['tip']
+class TipKeyword(UserNodeKeyword):
+    keyword_list = UserNodeKeyword.keyword_list + tip_keywords
+    data_keys = UserNodeKeyword.data_keys + ['tip']
 
     def dump_field_entry(self, output, field, entry, indent_n):
         field_type = field['type']

commit cbb21b96c624f89bcdfb1887db2c193bfe27b3b7

Fix regression on unread reports reminder introduced in cf708b294029d827b3c69ff60d0c7a13b9f9ca74

backend/globaleaks/jobs/notification.py
@@ -160,7 +160,7 @@ class MailGenerator(object):
             data = {'type': 'unread_tips'}
 
             try:
-                data['user'] = self.serialize_obj(session, 'user', user, tid, user.language)
+                data['user'] = user_serialize_user(session, user, user.language)
                 self.process_mail_creation(session, tid, data)
             except:
                 pass