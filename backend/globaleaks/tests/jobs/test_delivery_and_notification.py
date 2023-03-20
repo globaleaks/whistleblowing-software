7ac0a60551023e5194d240f0a76d1bbd0f390153
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-03-20 21:50:13 +0100

    Add test for generation of notifications for unread reports

--- backend/globaleaks/tests/jobs/test_delivery_and_notification.py
+++ backend/globaleaks/tests/jobs/test_delivery_and_notification.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 from twisted.internet.defer import inlineCallbacks
 
 from globaleaks import models
 from globaleaks.jobs.delivery import Delivery
 from globaleaks.jobs.notification import Notification
+from globaleaks.orm import transact
 from globaleaks.tests import helpers
+from globaleaks.utils.utility import datetime_now, datetime_null
+
+@transact
+def simulate_unread_tips(session):
+    # Simulate that 8 days has passed recipients have not accessed reports
+    for user in session.query(models.User):
+        user.reminder_date = datetime_null()
+
+    for rtip in session.query(models.ReceiverTip):
+        rtip.last_access = datetime_null()
+
+    for itip in session.query(models.InternalTip):
+        itip.update_date = datetime_now() - timedelta(8)
 
 
 class TestNotification(helpers.TestGLWithPopulatedDB):
@@ -48,6 +63,16 @@ class TestNotification(helpers.TestGLWithPopulatedDB):
         yield notification.generate_emails()
 
         yield self.test_model_count(models.Mail, 4)
+
+        yield notification.spool_emails()
+
+        yield self.test_model_count(models.Mail, 0)
+
+        yield simulate_unread_tips()
+
+        yield notification.generate_emails()
+
+        yield self.test_model_count(models.Mail, 2)
 
         yield notification.spool_emails()
 
