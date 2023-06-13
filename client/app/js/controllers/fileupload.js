1319ce487ad4835682ee8ce93da94a068da41eb7
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-06-13 20:57:41 +0500

    bug fixes and refactoring

--- client/app/js/controllers/fileupload.js
+++ client/app/js/controllers/fileupload.js
@@ -37,7 +37,6 @@ controller("AudioUploadCtrl", ["$scope","flowFactory", function($scope, flowFact
   var mediaStream = null;
   var context = null;
   var mediaRecorder = null;
-
   var leftchannel = [];
   var rightchannel = [];
   var recorder = null;
@@ -48,12 +47,155 @@ controller("AudioUploadCtrl", ["$scope","flowFactory", function($scope, flowFact
   var blob = null;
   var flow = flowFactory.create({ target: $scope.fileupload_url, query: { type: 'audio.webm' } });
   var startTime;
-
-  // Noise reduction parameters
   var noiseGateThreshold = 0.2;
   var noiseReductionAmount = 0.5;
 
   $scope.activeButton = null;
+
+  function applySubtleDistortion(buffer, amount) {
+    var distortedBuffer = new Float32Array(buffer.length);
+    for (var i = 0; i < buffer.length; i++) {
+      var sample = buffer[i];
+      // Apply distortion by adding/subtracting a small random value
+      distortedBuffer[i] = sample + (Math.random() * 2 - 1) * amount;
+    }
+    return distortedBuffer;
+  }
+
+  function applyLowPassFilter(buffer) {
+    var minCutoff = 500; // Minimum cutoff frequency (in Hz)
+    var maxCutoff = 20000; // Maximum cutoff frequency (in Hz)
+
+    // Generate a random cutoff frequency within the range
+    var cutoffFrequency = Math.floor(Math.random() * (maxCutoff - minCutoff + 1)) + minCutoff;
+
+    // Apply the low-pass filter to the buffer
+    for (var i = 1; i < buffer.length; i++) {
+      buffer[i] = (buffer[i - 1] + buffer[i]) / 2;
+    }
+    return buffer;
+  }
+
+  function applyTimeStretching(buffer, amount) {
+    var originalLength = buffer.length;
+    var stretchedLength = Math.floor(originalLength * amount);
+    var stretchedBuffer = new Float32Array(stretchedLength);
+    var index = 0;
+    for (var i = 0; i < stretchedLength; i++) {
+      var position = i / stretchedLength * originalLength;
+      var previousIndex = Math.floor(position);
+      var nextIndex = Math.ceil(position);
+      var weight = position - previousIndex;
+      var previousSample = buffer[previousIndex];
+      var nextSample = buffer[nextIndex];
+      var stretchedSample = previousSample + (nextSample - previousSample) * weight;
+      stretchedBuffer[index++] = stretchedSample;
+    }
+    return stretchedBuffer;
+  }
+  // Function to apply Bitcrusher effect to an audio buffer
+  function applyBitcrusher(buffer) {
+    var minBitDepth = 8; // Minimum bit depth
+    var maxBitDepth = 15; // Maximum bit depth
+    var bitDepth = Math.floor(Math.random() * (maxBitDepth - minBitDepth + 1)) + minBitDepth; // Random bit depth between 8 and 15
+
+    var step = Math.pow(0.5, bitDepth - 1);
+    var crushedBuffer = new Float32Array(buffer.length);
+    for (var i = 0; i < buffer.length; i++) {
+      var sample = buffer[i];
+      var quantized = Math.floor(sample / step + 0.5) * step;
+      crushedBuffer[i] = quantized;
+    }
+    return crushedBuffer;
+  }
+
+
+  function pitchShift(buffer, pitchShiftAmount) {
+    // Apply pitch shifting to the buffer
+    var pitchShiftedBuffer = new Float32Array(buffer.length);
+    var playbackRate = 1 / pitchShiftAmount;
+
+    for (var i = 0; i < buffer.length; i++) {
+      var index = Math.floor(i * playbackRate);
+      pitchShiftedBuffer[i] = buffer[index];
+    }
+
+    return pitchShiftedBuffer;
+  }
+
+  function applyReverb(buffer, reverbTime, decay) {
+    var reverberatedBuffer = new Float32Array(buffer.length);
+    var delayTime = reverbTime * context.sampleRate; // Convert reverb time to samples
+    var delayAmount = decay * 0.7; // Adjust the decay amount as needed
+    var delayBuffer = new Float32Array(Math.ceil(delayTime) + 1);
+    var index = 0;
+
+    for (var i = 0; i < buffer.length; i++) {
+      var sample = buffer[i];
+      var delayedSample = delayBuffer[index];
+      reverberatedBuffer[i] = sample + delayedSample * delayAmount;
+      delayBuffer[index] = sample + delayedSample * decay;
+      index++;
+      if (index >= delayBuffer.length) {
+        index = 0;
+      }
+    }
+
+    return reverberatedBuffer;
+  }
+
+  // Function to apply Reverb effect to an audio buffer
+  function applyReverb(buffer, reverbTime, decay) {
+    var reverberatedBuffer = new Float32Array(buffer.length);
+      sampleRate = Math.floor(Math.random() * (60001 - 38000) + 38000);
+    var delayTime = reverbTime * sampleRate; // Convert reverb time to samples
+    var delayAmount = decay * 0.7; // Adjust the decay amount as needed
+    var delayBuffer = new Float32Array(Math.ceil(delayTime) + 1);
+    var index = 0;
+    for (var i = 0; i < buffer.length; i++) {
+      var sample = buffer[i];
+      var delayedSample = delayBuffer[index];
+      reverberatedBuffer[i] = sample + delayedSample * delayAmount;
+      delayBuffer[index] = sample + delayedSample * decay;
+      index++;
+      if (index >= delayBuffer.length) {
+        index = 0;
+      }
+    }
+    return reverberatedBuffer;
+  }
+
+  function flattenArray(channelBuffer, recordingLength) {
+    var result = new Float32Array(recordingLength);
+    var offset = 0;
+    for (var i = 0; i < channelBuffer.length; i++) {
+      var buffer = channelBuffer[i];
+      result.set(buffer, offset);
+      offset += buffer.length;
+    }
+    return result;
+  }
+
+  function interleave(leftChannel, rightChannel) {
+    var length = leftChannel.length + rightChannel.length;
+    var result = new Float32Array(length);
+
+    var inputIndex = 0;
+
+    for (var index = 0; index < length;) {
+      result[index++] = leftChannel[inputIndex];
+      result[index++] = rightChannel[inputIndex];
+      inputIndex++;
+    }
+    return result;
+  }
+
+  function writeUTFBytes(view, offset, string) {
+    for (var i = 0; i < string.length; i++) {
+      view.setUint8(offset + i, string.charCodeAt(i));
+    }
+  }
+
   $scope.startRecording = function() {
     leftchannel = [];
     rightchannel = [];
@@ -106,119 +248,6 @@ controller("AudioUploadCtrl", ["$scope","flowFactory", function($scope, flowFact
         recorder.connect(context.destination);
       })
   };
-
-function applySubtleDistortion(buffer, amount) {
-  var distortedBuffer = new Float32Array(buffer.length);
-  for (var i = 0; i < buffer.length; i++) {
-    var sample = buffer[i];
-    // Apply distortion by adding/subtracting a small random value
-    distortedBuffer[i] = sample + (Math.random() * 2 - 1) * amount;
-  }
-  return distortedBuffer;
-}
-
-function applyLowPassFilter(buffer) {
-  var minCutoff = 500; // Minimum cutoff frequency (in Hz)
-  var maxCutoff = 20000; // Maximum cutoff frequency (in Hz)
-
-  // Generate a random cutoff frequency within the range
-  var cutoffFrequency = Math.floor(Math.random() * (maxCutoff - minCutoff + 1)) + minCutoff;
-
-  // Apply the low-pass filter to the buffer
-  for (var i = 1; i < buffer.length; i++) {
-    buffer[i] = (buffer[i - 1] + buffer[i]) / 2;
-  }
-  return buffer;
-}
-
-function applyTimeStretching(buffer, amount) {
-  var originalLength = buffer.length;
-  var stretchedLength = Math.floor(originalLength * amount);
-  var stretchedBuffer = new Float32Array(stretchedLength);
-  var index = 0;
-  for (var i = 0; i < stretchedLength; i++) {
-    var position = i / stretchedLength * originalLength;
-    var previousIndex = Math.floor(position);
-    var nextIndex = Math.ceil(position);
-    var weight = position - previousIndex;
-    var previousSample = buffer[previousIndex];
-    var nextSample = buffer[nextIndex];
-    var stretchedSample = previousSample + (nextSample - previousSample) * weight;
-    stretchedBuffer[index++] = stretchedSample;
-  }
-  return stretchedBuffer;
-}
-// Function to apply Bitcrusher effect to an audio buffer
-function applyBitcrusher(buffer) {
-  var minBitDepth = 8; // Minimum bit depth
-  var maxBitDepth = 15; // Maximum bit depth
-  var bitDepth = Math.floor(Math.random() * (maxBitDepth - minBitDepth + 1)) + minBitDepth; // Random bit depth between 8 and 15
-
-  var step = Math.pow(0.5, bitDepth - 1);
-  var crushedBuffer = new Float32Array(buffer.length);
-  for (var i = 0; i < buffer.length; i++) {
-    var sample = buffer[i];
-    var quantized = Math.floor(sample / step + 0.5) * step;
-    crushedBuffer[i] = quantized;
-  }
-  return crushedBuffer;
-}
-
-
-function pitchShift(buffer, pitchShiftAmount) {
-  // Apply pitch shifting to the buffer
-  var pitchShiftedBuffer = new Float32Array(buffer.length);
-  var playbackRate = 1 / pitchShiftAmount;
-
-  for (var i = 0; i < buffer.length; i++) {
-    var index = Math.floor(i * playbackRate);
-    pitchShiftedBuffer[i] = buffer[index];
-  }
-
-  return pitchShiftedBuffer;
-}
-
-function applyReverb(buffer, reverbTime, decay) {
-  var reverberatedBuffer = new Float32Array(buffer.length);
-  var delayTime = reverbTime * context.sampleRate; // Convert reverb time to samples
-  var delayAmount = decay * 0.7; // Adjust the decay amount as needed
-  var delayBuffer = new Float32Array(Math.ceil(delayTime) + 1);
-  var index = 0;
-
-  for (var i = 0; i < buffer.length; i++) {
-    var sample = buffer[i];
-    var delayedSample = delayBuffer[index];
-    reverberatedBuffer[i] = sample + delayedSample * delayAmount;
-    delayBuffer[index] = sample + delayedSample * decay;
-    index++;
-    if (index >= delayBuffer.length) {
-      index = 0;
-    }
-  }
-
-  return reverberatedBuffer;
-}
-
-// Function to apply Reverb effect to an audio buffer
-function applyReverb(buffer, reverbTime, decay) {
-  var reverberatedBuffer = new Float32Array(buffer.length);
-    sampleRate = Math.floor(Math.random() * (60001 - 38000) + 38000);
-  var delayTime = reverbTime * sampleRate; // Convert reverb time to samples
-  var delayAmount = decay * 0.7; // Adjust the decay amount as needed
-  var delayBuffer = new Float32Array(Math.ceil(delayTime) + 1);
-  var index = 0;
-  for (var i = 0; i < buffer.length; i++) {
-    var sample = buffer[i];
-    var delayedSample = delayBuffer[index];
-    reverberatedBuffer[i] = sample + delayedSample * delayAmount;
-    delayBuffer[index] = sample + delayedSample * decay;
-    index++;
-    if (index >= delayBuffer.length) {
-      index = 0;
-    }
-  }
-  return reverberatedBuffer;
-}
 
   $scope.stopRecording = function() {
     isRecording = false;
@@ -301,37 +330,6 @@ function applyReverb(buffer, reverbTime, decay) {
       $scope.uploads[$scope.fileinput] = flow;
     }
   };
-
-  function flattenArray(channelBuffer, recordingLength) {
-    var result = new Float32Array(recordingLength);
-    var offset = 0;
-    for (var i = 0; i < channelBuffer.length; i++) {
-      var buffer = channelBuffer[i];
-      result.set(buffer, offset);
-      offset += buffer.length;
-    }
-    return result;
-  }
-
-  function interleave(leftChannel, rightChannel) {
-    var length = leftChannel.length + rightChannel.length;
-    var result = new Float32Array(length);
-
-    var inputIndex = 0;
-
-    for (var index = 0; index < length;) {
-      result[index++] = leftChannel[inputIndex];
-      result[index++] = rightChannel[inputIndex];
-      inputIndex++;
-    }
-    return result;
-  }
-
-  function writeUTFBytes(view, offset, string) {
-    for (var i = 0; i < string.length; i++) {
-      view.setUint8(offset + i, string.charCodeAt(i));
-    }
-  }
 
 }]).
 controller("ImageUploadCtrl", ["$http", "$scope", "$rootScope", "uploadUtils", "Utils", function($http, $scope, $rootScope, uploadUtils, Utils) {
