fb2dcef1db04708fedbec7ad6d2af487ad3db75c
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-07-02 21:35:01 +0500

    can_privilege renamed

--- backend/globaleaks/handlers/user/__init__.py
+++ backend/globaleaks/handlers/user/__init__.py
@@ -28,8 +28,8 @@ def parse_pgp_options(user, request):
         user.pgp_key_public = pgp_key_public
         user.pgp_key_fingerprint = pgpctx.fingerprint
         user.pgp_key_expiration = pgpctx.expiration
-        user.can_privilege_delete_mask_information = request['can_privilege_delete_mask_information']
-        user.can_privilege_mask_information = request['can_privilege_mask_information']
+        user.can_delete_mask_information = request['can_delete_mask_information']
+        user.can_mask_information = request['can_mask_information']
     else:
         user.pgp_key_public = ''
         user.pgp_key_fingerprint = ''
@@ -83,8 +83,8 @@ def user_serialize_user(session, user, language):
         'can_delete_submission': user.can_delete_submission,
         'can_grant_access_to_reports': user.can_grant_access_to_reports,
         'can_edit_general_settings': user.can_edit_general_settings,
-        'can_privilege_delete_mask_information': user.can_privilege_delete_mask_information,
-        'can_privilege_mask_information': user.can_privilege_mask_information,
+        'can_delete_mask_information': user.can_delete_mask_information,
+        'can_mask_information': user.can_mask_information,
         'clicked_recovery_key': user.clicked_recovery_key,
         'contexts': contexts
     }
