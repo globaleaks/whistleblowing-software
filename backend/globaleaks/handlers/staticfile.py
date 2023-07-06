e8c67a54bb855e1fe6f2b52f08420d9411a1006a
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-07-07 00:37:16 +0200

    Reorganize code inside static file handler

--- backend/globaleaks/handlers/staticfile.py
+++ backend/globaleaks/handlers/staticfile.py
@@ -27,18 +27,19 @@ class StaticFileHandler(BaseHandler):
         abspath = os.path.abspath(os.path.join(self.root, filename))
         directory_traversal_check(self.root, abspath)
 
-        if filename == 'index.html' and not self.state.settings.disable_csp:
-            self.request.setHeader(b'Content-Security-Policy',
-                                   b"base-uri 'none';"
-                                   b"connect-src 'self';"
-                                   b"default-src 'none';"
-                                   b"font-src 'self' data:;"
-                                   b"form-action 'none';"
-                                   b"frame-ancestors 'none';"
-                                   b"frame-src 'self';"
-                                   b"img-src 'self' data:;"
-                                   b"media-src 'self';"
-                                   b"script-src 'self' 'sha256-l4srTx31TC+tE2K4jVVCnC9XfHivkiSs/v+DPWccDDM=';"
-                                   b"style-src 'self' 'sha256-fwyo2zCGlh85NfN4rQUlpLM7MB5cry/1AEDA/G9mQJ8=';")
+        if not self.state.settings.disable_csp:
+            if filename == 'index.html':
+                self.request.setHeader(b'Content-Security-Policy',
+                                       b"base-uri 'none';"
+                                       b"connect-src 'self';"
+                                       b"default-src 'none';"
+                                       b"font-src 'self' data:;"
+                                       b"form-action 'none';"
+                                       b"frame-ancestors 'none';"
+                                       b"frame-src 'self';"
+                                       b"img-src 'self' data:;"
+                                       b"media-src 'self';"
+                                       b"script-src 'self' 'sha256-l4srTx31TC+tE2K4jVVCnC9XfHivkiSs/v+DPWccDDM=';"
+                                       b"style-src 'self' 'sha256-fwyo2zCGlh85NfN4rQUlpLM7MB5cry/1AEDA/G9mQJ8=';")
 
         return self.write_file(filename, abspath)
