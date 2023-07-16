globaleaks/whistleblowing-software

commit 2f9316db757d2118b1aa89eca5fceb6af75d49de

Make it possible to export and import question templates

backend/globaleaks/handlers/admin/field.py
@@ -5,10 +5,11 @@ from globaleaks import models
 from globaleaks.handlers.base import BaseHandler
 from globaleaks.handlers.public import serialize_field, trigger_map
 from globaleaks.models import fill_localized_keys
-from globaleaks.orm import db_add, db_get, db_del, transact
+from globaleaks.orm import db_add, db_get, db_del, transact, tw
 from globaleaks.rest import errors, requests
 from globaleaks.settings import Settings
 from globaleaks.utils.fs import read_json_file
+from globaleaks.utils.utility import datetime_now
 
 
 def fieldtree_ancestors(session, field_id):
@@ -65,20 +66,22 @@ def db_update_fieldoption(session, field_id, fieldoption_id, option_dict, langua
     :param idx: The order index with reference to the other options set
     :return: The serialized descriptor of the option
     """
+    option_dict['fieldoption_id'] = fieldoption_id
     option_dict['field_id'] = field_id
+    option_dict['idx'] = idx
 
     fill_localized_keys(option_dict, models.FieldOption.localized_keys, language)
 
     o = None
     if fieldoption_id is not None:
-        o = session.query(models.FieldOption).filter(models.FieldOption.id == fieldoption_id).one_or_none()
+        o = session.query(models.FieldOption) \
+                   .filter(models.FieldOption.id == fieldoption_id,
+                           models.FieldOption.field_id == field_id).one_or_none()
 
     if o is None:
         o = db_add(session, models.FieldOption, option_dict)
     else:
         o.update(option_dict)
-
-    o.order = idx
 
     return o.id
 
@@ -124,7 +127,10 @@ def db_update_fieldattr(session, field_id, attr_name, attr_dict, language):
     if attr_dict['type'] == 'localized' and language is not None:
         fill_localized_keys(attr_dict, ['value'], language)
 
-    o = session.query(models.FieldAttr).filter(models.FieldAttr.field_id == field_id, models.FieldAttr.name == attr_name).one_or_none()
+    o = session.query(models.FieldAttr) \
+               .filter(models.FieldAttr.field_id == field_id,
+                       models.FieldAttr.name == attr_name).one_or_none()
+
     if o is None:
         attr_dict['id'] = ''
         o = db_add(session, models.FieldAttr, attr_dict)
@@ -187,6 +193,15 @@ def check_field_association(session, tid, request):
             raise errors.InputValidationError("Provided field association would cause recursion loop")
 
 
+def db_get_field(session, tid, field_id, language=None, data=None, serialize_templates=False):
+    field = db_get(session,
+                   models.Field,
+                   (models.Field.tid == tid,
+                    models.Field.id == field_id))
+
+    return serialize_field(session, tid, field, language, data, serialize_templates)
+
+
 def db_create_field(session, tid, request, language):
     """
     Transaction for creating a field
@@ -203,7 +218,17 @@ def db_create_field(session, tid, request, language):
 
     check_field_association(session, tid, request)
 
-    if request.get('template_id', '') != '':
+    if request.get('template_id', '') == '':
+        field = db_add(session, models.Field, request)
+        attrs = request.get('attrs')
+        options = request.get('options')
+
+        db_update_fieldattrs(session, field.id, attrs, language)
+        db_update_fieldoptions(session, field.id, options, language)
+
+        for trigger in request.get('triggered_by_options', []):
+            db_create_option_trigger(session, trigger['option'], 'field', field.id, trigger.get('sufficient', True))
+    else:
         if request['template_id'] == 'whistleblower_identity':
             if request.get('step_id', '') == '':
                 raise errors.InputValidationError("Cannot associate whistleblower identity field to a fieldgroup")
@@ -236,22 +261,10 @@ def db_create_field(session, tid, request, language):
 
         db_update_fieldattrs(session, field.id, attrs, None)
 
-    else:
-        field = db_add(session, models.Field, request)
-        attrs = request.get('attrs')
-        options = request.get('options')
-
-        db_update_fieldattrs(session, field.id, attrs, language)
-        db_update_fieldoptions(session, field.id, options, language)
-
-        for trigger in request.get('triggered_by_options', []):
-            db_create_option_trigger(session, trigger['option'], 'field', field.id, trigger.get('sufficient', True))
-
-    if field.instance != 'reference':
-        for c in request.get('children', []):
-            c['tid'] = field.tid
-            c['fieldgroup_id'] = field.id
-            db_create_field(session, tid, c, language)
+    for c in request.get('children', []):
+        c['tid'] = field.tid
+        c['fieldgroup_id'] = field.id
+        db_create_field(session, tid, c, language)
 
     return field
 
@@ -397,12 +410,22 @@ class FieldTemplatesCollection(BaseHandler):
 
         request = self.validate_message(self.request.content.read(), validator)
 
+        request['instance'] = 'template'
+        for k in ['fieldgroup_id', 'step_id', 'template_id', 'template_override_id', 'triggered_by_options']:
+            del request[k]
+
         return create_field(self.request.tid, request, language)
 
 
 class FieldTemplateInstance(BaseHandler):
     check_roles = 'admin'
     invalidate_cache = True
+
+    def get(self, field_id):
+        """
+        Export field template
+        """
+        return tw(db_get_field, self.request.tid, field_id, None)
 
     def put(self, field_id):
         """

backend/globaleaks/db/appdata.py
@@ -33,6 +33,7 @@ def db_load_default_questionnaires(session):
         questionnaires.append(read_json_file(qfile))
         qids.append(questionnaires[-1]['id'])
 
+    db_del(session, models.Questionnaire, models.Questionnaire.id.in_(qids))
     db_del(session, models.Step, models.Step.questionnaire_id.in_(qids))
 
     for questionnaire in questionnaires:
@@ -53,6 +54,7 @@ def db_load_default_fields(session):
         questions.append(read_json_file(ffile))
         qids.append(questions[-1]['id'])
 
+    db_del(session, models.Field, models.Field.id.in_(qids))
     db_del(session, models.Field, models.Field.fieldgroup_id.in_(qids))
     db_del(session, models.FieldAttr, models.FieldAttr.field_id.in_(qids))
     db_del(session, models.FieldOption, models.FieldOption.field_id.in_(qids))

backend/globaleaks/handlers/admin/questionnaire.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8
-from twisted.internet.defer import inlineCallbacks, returnValue
-
 from globaleaks import models, QUESTIONNAIRE_EXPORT_VERSION
 from globaleaks.handlers.admin.step import db_create_step
 from globaleaks.handlers.base import BaseHandler
@@ -214,6 +212,12 @@ class QuestionnaireInstance(BaseHandler):
     check_roles = 'admin'
     invalidate_cache = True
 
+    def get(self, questionnaire_id):
+        """
+        Export questionnaire JSON
+        """
+        return tw(db_get_questionnaire, self.request.tid, questionnaire_id, None)
+
     def put(self, questionnaire_id):
         """
         Update the specified questionnaire.
@@ -235,16 +239,6 @@ class QuestionnaireInstance(BaseHandler):
                   models.Questionnaire,
                   (models.Questionnaire.tid == self.request.tid,
                    models.Questionnaire.id == questionnaire_id))
-
-    @inlineCallbacks
-    def get(self, questionnaire_id):
-        """
-        Export questionnaire JSON
-        """
-        q = yield tw(db_get_questionnaire, self.request.tid, questionnaire_id, None)
-        q['export_date'] = datetime_now()
-        q['export_version'] = QUESTIONNAIRE_EXPORT_VERSION
-        returnValue(q)
 
 
 class QuestionnareDuplication(BaseHandler):

backend/globaleaks/handlers/public.py
@@ -382,7 +382,7 @@ def serialize_field_attr(attr, language):
     return ret
 
 
-def serialize_field(session, tid, field, language, data=None, serialize_templates=True):
+def serialize_field(session, tid, field, language, data=None, serialize_templates=False):
     """
     Serialize a field
 
@@ -411,7 +411,7 @@ def serialize_field(session, tid, field, language, data=None, serialize_template
         for attr in data['attrs'].get(field.template_id, {}):
             attrs[attr.name] = serialize_field_attr(attr, language)
 
-    children = [serialize_field(session, tid, f, language) for f in data['fields'].get(f_to_serialize.id, [])]
+    children = [serialize_field(session, tid, f, language, data, serialize_templates=serialize_templates) for f in data['fields'].get(f_to_serialize.id, [])]
     children.sort(key=lambda f: (f['y'], f['x']))
 
     ret = {
@@ -439,7 +439,7 @@ def serialize_field(session, tid, field, language, data=None, serialize_template
     return get_localized_values(ret, f_to_serialize, f_to_serialize.localized_keys, language)
 
 
-def serialize_step(session, tid, step, language, serialize_templates=True):
+def serialize_step(session, tid, step, language, serialize_templates=False):
     """
     Serialize a step.
 
@@ -454,7 +454,7 @@ def serialize_step(session, tid, step, language, serialize_templates=True):
 
     data = db_prepare_fields_serialization(session, children)
 
-    children = [serialize_field(session, tid, f, language, data, serialize_templates=serialize_templates) for f in children]
+    children = [serialize_field(session, tid, f, language, data, serialize_templates) for f in children]
     children.sort(key=lambda f: (f['y'], f['x']))
 
     ret = {
@@ -469,7 +469,7 @@ def serialize_step(session, tid, step, language, serialize_templates=True):
     return get_localized_values(ret, step, step.localized_keys, language)
 
 
-def serialize_questionnaire(session, tid, questionnaire, language, serialize_templates=True):
+def serialize_questionnaire(session, tid, questionnaire, language, serialize_templates=False):
     """
     Serialize a questionnaire.
 
@@ -523,7 +523,7 @@ def serialize_receiver(session, user, language, data=None):
     return get_localized_values(ret, user, user.localized_keys, language)
 
 
-def db_get_questionnaires(session, tid, language):
+def db_get_questionnaires(session, tid, language, serialize_templates=False):
     """
     Transaction that serialize the list of public questionnaires
 
@@ -538,7 +538,7 @@ def db_get_questionnaires(session, tid, language):
                                         models.Context.additional_questionnaire_id == models.Questionnaire.id),
                                     models.Context.tid == tid)
 
-    return [serialize_questionnaire(session, tid, questionnaire, language) for questionnaire in questionnaires]
+    return [serialize_questionnaire(session, tid, questionnaire, language, serialize_templates=serialize_templates) for questionnaire in questionnaires]
 
 
 def db_get_contexts(session, tid, language):
@@ -587,7 +587,7 @@ def get_public_resources(session, tid, language):
     """
     ret = {
         'node': db_serialize_node(session, tid, language),
-        'questionnaires': db_get_questionnaires(session, tid, language),
+        'questionnaires': db_get_questionnaires(session, tid, language, True),
         'submission_statuses': db_get_submission_statuses(session, tid, language),
         'receivers': db_get_receivers(session, tid, language),
         'contexts': []

backend/globaleaks/orm.py
@@ -72,11 +72,8 @@ def get_thread_pool():
 
 def db_add(session, model_class, model_fields):
     obj = model_class(model_fields)
-    if not obj.id:
-        session.add(obj)
-        session.flush()
-    else:
-        session.merge(obj)
+    session.add(obj)
+    session.flush()
     return obj
 
 

backend/globaleaks/tests/helpers.py
@@ -197,7 +197,7 @@ def get_dummy_field():
 def get_dummy_fieldoption_list():
     return [
         {
-            'id': 'beefcafe-beef-cafe-beef-cafebeefcafe',
+            'id': uuid4(),
             'label': 'Cafe del mare',
             'order': 0,
             'score_points': 100,
@@ -208,7 +208,7 @@ def get_dummy_fieldoption_list():
             'block_submission': False
         },
         {
-            'id': 'feefbead-feef-bead-feef-feeffeefbead',
+            'id': uuid4(),
             'label': 'skrilx is here',
             'order': 0,
             'score_points': 97,

client/app/js/controllers/admin/fields.js
@@ -1,5 +1,5 @@
-GL.controller("AdminFieldEditorCtrl", ["$scope",
-  function($scope) {
+GL.controller("AdminFieldEditorCtrl", ["$scope", "$http", "FileSaver",
+  function($scope, $http, FileSaver) {
     $scope.admin_receivers_by_id = $scope.Utils.array_to_map($scope.resources.users);
 
     $scope.editing = false;
@@ -197,12 +197,36 @@ GL.controller("AdminFieldEditorCtrl", ["$scope",
     $scope.assignScorePointsDialog = function(option) {
       return $scope.Utils.openConfirmableModalDialog("views/partials/assign_score_points.html", option, $scope);
     };
+
+    $scope.exportQuestion = function(obj) {
+      $http({
+        method: "GET",
+        url: "api/admin/fieldtemplates/" + obj.id,
+        responseType: "blob",
+      }).then(function (response) {
+        FileSaver.saveAs(response.data, obj.label + ".json");
+      });
+    };
   }
 ]).
-controller("AdminFieldTemplatesCtrl", ["$scope", "AdminFieldTemplateResource",
-  function($scope, AdminFieldTemplateResource) {
+controller("AdminFieldTemplatesCtrl", ["$scope", "$http", "AdminFieldTemplateResource",
+  function($scope, $http, AdminFieldTemplateResource) {
     $scope.fieldResource = AdminFieldTemplateResource;
     $scope.parsedFields = $scope.fieldUtilities.parseFields($scope.resources.fieldtemplates, {});
+
+    $scope.importQuestion = function(file) {
+      console.log(file);
+      $scope.Utils.readFileAsText(file).then(function(txt) {
+	console.log(txt);
+        return $http({
+          method: "POST",
+          url: "api/admin/fieldtemplates?multilang=1",
+          data: txt,
+        });
+      }).then(function() {
+         $scope.reload();
+      }, $scope.Utils.displayErrorMsg);
+    };
   }
 ]).
 controller("AdminFieldTemplatesAddCtrl", ["$scope",

client/app/views/admin/questionnaires/fields.html
@@ -58,6 +58,11 @@
                 data-ng-click="toggleEditing(); $event.stopPropagation();">
                 <span data-translate>Cancel</span>
               </button>
+              <button class="downloadButton btn btn-sm btn-outline-secondary"
+                data-ng-click="exportQuestion(field); $event.stopPropagation()">
+                <i class="fas fa-file-export"></i>
+                <span data-translate>Export</span>
+              </button>
               <button class="btn btn-sm btn-danger"
                 data-ng-if="field.editable"
 		data-ng-click="delField(field); $event.stopPropagation();">

commit 496b8f587fefa592f27459bc0f42519a8adbf199

Fix regression on options reordering introduced in 2f9316db

backend/globaleaks/handlers/admin/field.py
@@ -82,6 +82,8 @@ def db_update_fieldoption(session, field_id, fieldoption_id, option_dict, langua
     else:
         o.update(option_dict)
 
+    o.order = idx
+
     return o.id
 
 