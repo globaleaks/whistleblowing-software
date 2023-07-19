365c9384e685daa29f69fac73266ce3199ee149b
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-07-19 07:07:47 +0200

    Revise file handler adding control on tid

--- backend/globaleaks/handlers/file.py
+++ backend/globaleaks/handlers/file.py
@@ -47,7 +47,7 @@ class FileHandler(BaseHandler):
         name = urllib.parse.unquote(name)
 
         id = yield get_file_id_by_name(self.request.tid, name)
-        if not id:
+        if not id and self.request.tid != 1:
             id = yield get_file_id_by_name(1, name)
 
         path = os.path.abspath(os.path.join(self.state.settings.files_path, id))
