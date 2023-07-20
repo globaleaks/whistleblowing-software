25ecc2b89f4672eed99b40a15716f17e6f3cc245
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-07-20 10:47:05 +0200

    Add missing import of rsa library

--- backend/globaleaks/utils/tls.py
+++ backend/globaleaks/utils/tls.py
@@ -7,7 +7,7 @@ import re
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import ec, rsa
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 
