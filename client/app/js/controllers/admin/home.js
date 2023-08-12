a4cbddd267664a50e36fd37172740447eb196dd7
Petru Potrimba <ppotrimba@gmail.com>
2023-08-12 08:47:29 +0200

    Make it possible to configure a Privacy Policy for internal users (#3510)

--- client/app/js/controllers/admin/home.js
+++ client/app/js/controllers/admin/home.js
@@ -1,6 +1,5 @@
 GL.
-  controller("AdminCtrl", ["$scope", "$http", "$uibModal", function ($scope, $http, $uibModal) {
-  $scope.aAgrementModel = false;
+  controller("AdminCtrl", ["$scope", function ($scope) {
   $scope.updateNode = function() {
     $scope.Utils.update($scope.resources.node, function() { $scope.$emit("REFRESH"); });
   };
@@ -19,24 +18,4 @@ GL.
 
     return max + 1;
   };
-
-  $scope.$watch("resources.users", function() {
-    $scope.all_recipients_enabled = false;
-
-    if (!$scope.resources.users) {
-      return;
-    }
-
-    for (var i=0; i<$scope.resources.users.length; i++) {
-      if ($scope.resources.users[i].role === "receiver" && !$scope.resources.users[i].encryption) {
-        return;
-      }
-    }
-
-    $scope.all_recipients_enabled = true;
-  }, true);
-
-  if ($scope.public.node.user_privacy_policy_text && $scope.resources.preferences.accepted_privacy_policy === '1970-01-01T00:00:00Z'){
-    $scope.Utils.acceptPrivacyPolicyDialog();
-  }
 }]);
