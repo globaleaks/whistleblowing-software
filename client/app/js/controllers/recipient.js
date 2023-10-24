3f291bfebc4945fb1f7874685b0223477df1e2b1
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2023-10-24 11:11:30 +0200

    Added possibility to export the report list in form of CSV

--- client/app/js/controllers/recipient.js
+++ client/app/js/controllers/recipient.js
@@ -215,4 +215,25 @@ GL.controller("ReceiverTipsCtrl", ["$scope",  "$filter", "$http", "$location", "
       })(i);
     }
   };
+
+  $scope.getDataCsv = function(){
+    var output = angular.copy($scope.filteredTips);
+    return output.map(function(tip){
+      return {
+        id:tip.id,
+        progressive: tip.progressive,
+        important: tip.important,
+        reportStatus: $scope.markReportStatus(tip.reminder_date),
+        context_name: tip.context_name,
+        label:tip.label,
+        status: tip.submissionStatusStr,
+        creation_date: $filter("date")(tip.creation_date, "dd-MM-yyyy HH:mm"),
+        update_date: $filter("date")(tip.update_date, "dd-MM-yyyy HH:mm"),
+        expiration_date: $filter("date")(tip.expiration_date, "dd-MM-yyyy HH:mm"),
+        last_access: $filter("date")(tip.last_access, "dd-MM-yyyy HH:mm"),
+        comment_count: tip.comment_count,
+        file_count: tip.file_count
+      }
+    })
+  }
 }]);
