96bd54da7a95bda78c4f7d868f080d9f5e323bc9
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-02-20 08:43:15 +0100

    Uniform phone regexp to old angularjs app

--- client/app/src/shared/constants/constants.ts
+++ client/app/src/shared/constants/constants.ts
@@ -4,7 +4,7 @@ export const Constants = {
   emailRegexpOrEmpty: /^([\w+-.]){0,100}[\w]{1,100}@([\w+-.]){0,100}[\w]{1,100}$|^$/,
   countryCodeRegexpOrEmpty: "[A-Za-z]{0,2}",
   numberRegexp: /^\d+$/,
-  phoneNumberRegexp: /^[+]?[ \d]+$/,
+  phoneNumberRegexp: /^[+]?[\d]+$/,
   hostnameRegexp: "^(?:[a-zA-Z0-9\\-]+\\.)*[a-zA-Z0-9\\-]+\\.?$",
   httpsRegexp: /^https:\/\/([a-z0-9-]+)\.(.*)$|^$/,
   secureLocalUrlRegexp: /^https:\/\/([a-z0-9-]+)\.(.*)$|^\/(.*)$|^$/,
