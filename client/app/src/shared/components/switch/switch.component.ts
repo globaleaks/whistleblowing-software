b4be03135909a1fdb4cc3b6453fbaccfd3579499
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-02-22 15:59:54 +0500

    accessibility optimizations

--- client/app/src/shared/components/switch/switch.component.ts
+++ client/app/src/shared/components/switch/switch.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 export class SwitchComponent {
   @Input() label: string = 'Switch';
   @Input() isChecked: boolean = false;
+  @Input() can_upload_files:boolean
   @Output() switchChange = new EventEmitter<boolean>();
 
 }
