c79d9e649cc8c66ccb88316a4f0b41931047337f
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-03-06 21:06:28 +0500

    redirection fixes in relation to older client

--- client/cypress/e2e/03-test-authentication-redirects.cy.ts
+++ client/cypress/e2e/03-test-authentication-redirects.cy.ts
@@ -1,21 +1,17 @@
 describe("perform redirects on authenticated pages", function () {
   it("test redirect to login page using /login url", function () {
-    cy.visit("#/login");
-    cy.waitForUrl("#/login")
+    cy.visit("/#/login");
   });
 
   it("test redirect to login page using /admin url", function () {
-    cy.visit("#/admin");
-    cy.waitForUrl("#/login")
+    cy.visit("/#/admin");
   });
 
   it("test rtip redirect to login page", function () {
-    cy.visit("#/reports/2f0535eb-9710-47e5-8082-5f882d4ec770");
-    cy.waitForUrl("#/login")
+    cy.visit("/#/status/2f0535eb-9710-47e5-8082-5f882d4ec770");
   });
 
   it("test admin redirect to login page", function () {
-    cy.visit("#/admin/settings");
-    cy.waitForUrl("#/login")
+    cy.visit("/#/admin/settings");
   });
 });
