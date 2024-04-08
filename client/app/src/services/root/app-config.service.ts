3aeb4d6e592a2f995d27ecdb502edac83c83e005
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-04-08 12:17:00 +0500

    signup redirection fixes when admin only site is enabled

--- client/app/src/services/root/app-config.service.ts
+++ client/app/src/services/root/app-config.service.ts
@@ -142,7 +142,7 @@ export class AppConfigService {
     if (this.appDataService.public.node) {
       if (!this.appDataService.public.node.wizard_done) {
         location.replace("/#/wizard");
-      } else if ((this.router.url === "/" || this.router.url === "/submission") && this.appDataService.public.node.adminonly && !this.authenticationService.session) {
+      } else if ((this.router.url === "/" || this.router.url === "/submission") && !this.appDataService.public.node.enable_signup && this.appDataService.public.node.adminonly && !this.authenticationService.session) {
         location.replace("/#/admin/home");
       } else if (this.router.url === "/" && this.appDataService.public.node.enable_signup && !location.href.endsWith("admin/home")) {
         location.replace("/#/signup");
