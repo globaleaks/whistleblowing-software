df73575a96372651dacff6409644725f1fc69b80
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-06-04 07:40:25 +0200

    [ci] Revise tests removing extention to screenshots

--- client/cypress/e2e/04-test-user-password-reset.cy.ts
+++ client/cypress/e2e/04-test-user-password-reset.cy.ts
@@ -1,13 +1,13 @@
 describe("user login", function () {
   it("should enable users to request password reset", function () {
     cy.visit("#/login");
-    cy.takeScreenshot("user/login.png");
+    cy.takeScreenshot("user/login");
     cy.get("#passwordreset").click();
     cy.waitForUrl("#/login/passwordreset");
-    cy.takeScreenshot("user/password_reset_1.png");
+    cy.takeScreenshot("user/password_reset_1");
     cy.get('[name="username"]').type("admin");
     cy.get("#submit").click();
     cy.waitForUrl("#/login/passwordreset/requested");
-    cy.takeScreenshot("user/password_reset_2.png");
+    cy.takeScreenshot("user/password_reset_2");
   });
 });
