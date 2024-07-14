ef4ef10127efada1f53adb2932ad7d2aa8c903c9
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-07-14 22:45:34 +0200

    Revise Pie charts UI

--- client/app/src/pages/analyst/statistics/statistics.component.ts
+++ client/app/src/pages/analyst/statistics/statistics.component.ts
@@ -22,46 +22,22 @@ export class StatisticsComponent implements OnInit {
     return ((value / total) * 100).toFixed(1);
   }
 
-  private formatLabels(labels: string[], ...percentages: any): string[] {
-    return labels.map((label, index) => {
-      const percentage = percentages[index];
-      return `${this.translateService.instant(label)} ${percentage}% - (${(this.statisticsResolver.dataModel.reports_count * (percentage / 100)).toFixed()})`;
-    });
-  }
+  private createChart(title: string, labels: string[], values: number[], colors: string[]) {
+    var total: number = 0;
+    var i: any;
 
-  private createChart(title: string, total: number, labels: string[], values: number[], colors: string[]) {
+    for (i in values) {
+      total += values[i];
+    }
+
+    for (i in labels) {
+      labels[i] = this.translateService.instant(labels[i]) + ": " + this.calculatePercentage(values[i], total) + "%";
+    }
+
     return {
       title: this.translateService.instant(title),
-      total: total,
       labels: labels,
-      values: values,
-      colors: colors,
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            data: values,
-            backgroundColor: colors,
-          },
-        ],
-      },
-      options: {
-        plugins: {
-          legend: {
-            display: true,
-            position: 'left',
-            labels: {
-              color: '#333',
-              font: { size: 12 },
-            },
-          },
-          tooltip: {
-            callbacks: {
-              label: (context: any) => context.dataset.label + ': ' + context.parsed,
-            },
-          },
-        },
-      },
+      datasets: [{'labels': labels, 'data': values, 'backgroundColor': colors}],
     };
   }
 
@@ -71,29 +47,20 @@ export class StatisticsComponent implements OnInit {
 
     const a_1: number = dataModel.reports_with_no_access || 0;
     const a_2: number = reports_count - dataModel.reports_with_no_access || 0;
-    const totAcc: number = a_1 + a_2;
 
     const b_1: number = dataModel.reports_anonymous || 0;
     const b_2: number = dataModel.reports_subscribed || 0;
     const b_3: number = dataModel.reports_initially_anonymous || 0;
-    const totId: number = b_1 + b_2 + b_3;
 
     const c_1: number = dataModel.reports_tor || 0;
     const c_2: number = reports_count - dataModel.reports_tor || 0;
-    const totTor: number = c_1 + c_2;
 
     const d_1: number = dataModel.reports_mobile || 0;
     const d_2: number = reports_count - dataModel.reports_mobile || 0;
-    const totMobile: number = d_1 + d_2;
 
-    const torAccLabels = this.formatLabels(["Yes", "No"], this.calculatePercentage(a_1, totAcc), this.calculatePercentage(a_2, totAcc));
-    const anonymityLabels = this.formatLabels(["Anonymous", "Subscribed", "Subscribed later"], this.calculatePercentage(b_1, totId), this.calculatePercentage(b_2, totId), this.calculatePercentage(b_3, totId));
-    const torLabels = this.formatLabels(["Yes", "No"], this.calculatePercentage(c_1, totTor), this.calculatePercentage(c_2, totTor));
-    const mobileLabels = this.formatLabels(["Yes", "No"], this.calculatePercentage(d_1, totMobile), this.calculatePercentage(d_2, totMobile));
-
-    this.charts.push(this.createChart("Returning whistleblowers", totAcc, torAccLabels, [a_1, a_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
-    this.charts.push(this.createChart("Anonymity", totId, anonymityLabels, [b_1, b_2, b_3], ["rgb(96,186,255)", "rgb(0,127,224)", "rgb(0,46,82)"]));
-    this.charts.push(this.createChart("Tor", totTor, torLabels, [c_1, c_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
-    this.charts.push(this.createChart("Mobile", totMobile, mobileLabels, [d_1, d_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
+    this.charts.push(this.createChart("Returning whistleblowers", ["Yes", "No"], [a_1, a_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
+    this.charts.push(this.createChart("Anonymity", ["Anonymous", "Subscribed", "Subscribed later"], [b_1, b_2, b_3], ["rgb(96,186,255)", "rgb(0,127,224)", "rgb(0,46,82)"]));
+    this.charts.push(this.createChart("Tor", ["Yes", "No"], [c_1, c_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
+    this.charts.push(this.createChart("Mobile", ["Yes", "No"], [d_1, d_2], ["rgb(96,186,255)", "rgb(0,127,224)"]));
   }
 }
