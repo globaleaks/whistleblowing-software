542b04dd6d1d779d725e7c9b4eafca75f2b494d2
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-08-26 19:19:36 +0200

    Revise commit 4f3238e83

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -5,20 +5,17 @@
 set -e
 
 if [[ "$1" != "" ]]; then
-
 	echo "Updating version to v$1"
 
 	ROOTDIR=$(pwd)
-	
+
 	sed -i "s/^__version__ =.*/__version__ = '$1'/g" "$ROOTDIR"/backend/globaleaks/__init__.py
-	
+
 	sed -i "s/\"version\":.*/\"version\": \"$1\",/g" "$ROOTDIR"/client/package.json
 
 	awk -v ver="$1" 'BEGIN{cnt=0} /"version":/ && cnt<2 {sub(/"version": "[^"]*"/, "\"version\": \"" ver "\""); cnt++} 1' "$ROOTDIR/client/npm-shrinkwrap.json" > tmp && mv tmp "$ROOTDIR/client/npm-shrinkwrap.json"
 
 	sed -i "s/^releaseDate:.*/releaseDate: '$(date +'%Y-%m-%d')'/g" "$ROOTDIR"/publiccode.yml
-	
-	echo -e "Changes in version $1\n\t\tTODO\n$(cat CHANGELOG)" > "$ROOTDIR"/CHANGELOG
 
 	echo -e "globaleaks ($1) stable; urgency=medium
 
@@ -26,6 +23,9 @@ if [[ "$1" != "" ]]; then
 
  -- GlobaLeaks software signing key <info@globaleaks.org>  $(date --rfc-email)\n\n$(cat debian/changelog)" > "$ROOTDIR"/debian/changelog
 
+        git commit -a -m "Bump to version $1"
+
+        git tag -s v$1 -m 'GlobaLeaks version $1' --force
 else
 	echo -e "Please specify a version"
 	exit 1
