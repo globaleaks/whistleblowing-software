e1cda349833f73bc7b1a9b4fd92a12efb93aa7bd
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-09-23 10:30:40 +0200

    Disable donation badge on mobiles

--- documentation/_static/custom.js
+++ documentation/_static/custom.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 767) {
+        return;
+    }
+
     var banner = document.createElement("div");
     banner.className = 'donation-banner';
     banner.innerHTML = '<img src="https://raw.githubusercontent.com/globaleaks/whistleblowing-software/main/brand/assets/heart.svg" class="donation-icon" alt="Donate" /> Globaleaks is free and open-source whistleblowing software. <a href="https://github.com/sponsors/globaleaks">Donate here to support its development!</a>';
