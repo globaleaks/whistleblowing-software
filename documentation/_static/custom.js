d3e2d4c10c5f5d0a19e0073d24ac0769a9735171
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-10-10 20:52:17 +0200

    [doc] Revise style of donation badge

--- documentation/_static/custom.js
+++ documentation/_static/custom.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth < 767) {
-        return;
+    var mainElement = document.querySelector('[role="main"]');
+
+    if (!mainElement) {
+      return;
     }
 
     var banner = document.createElement("div");
     banner.className = 'donation-banner';
     banner.innerHTML = '<img src="https://raw.githubusercontent.com/globaleaks/whistleblowing-software/main/brand/assets/heart.svg" class="donation-icon" alt="Donate" /> Globaleaks is free and open-source whistleblowing software. <a href="https://github.com/sponsors/globaleaks">Donate here to support its development!</a>';
-    document.body.insertBefore(banner, document.body.firstChild);
+    mainElement.insertBefore(banner, mainElement.firstChild);
 });
