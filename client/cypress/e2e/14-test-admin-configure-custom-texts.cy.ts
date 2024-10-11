b0847f937a69863eaba9f3227d3683ef2325ef92
عبدالمنان <msmannan00@protonmail.com>
2024-10-11 16:41:39 +0500

    Update 14-test-admin-configure-custom-texts.cy.ts

--- client/cypress/e2e/14-test-admin-configure-custom-texts.cy.ts
+++ client/cypress/e2e/14-test-admin-configure-custom-texts.cy.ts
@@ -33,6 +33,7 @@ describe("admin configure custom texts", () => {
     cy.get('[data-cy="text_customization"]').click();
 
     cy.get(".deleteCustomTextButton",{ timeout: 10000 }).click();
+    cy.get(".deleteCustomTextButton",{ timeout: 10000 }).click();
 
     cy.logout();
 
