f4c0a0ecfbdf5dc4dc9e5194f09f322ce5793ccc
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-10-13 12:47:44 +0200

    [ci] Revise tests on internationalization

--- client/cypress/e2e/11-test-admin-configure-languages.cy.ts
+++ client/cypress/e2e/11-test-admin-configure-languages.cy.ts
@@ -29,9 +29,39 @@ describe("admin configure languages", () => {
     cy.get("#save_language").click();
 
     cy.waitForUrl("/#/admin/settings");
-    cy.get('#LanguagePickerBox').should('be.visible', { timeout: 10000 }).find('ng-select').last().click().get('ng-dropdown-panel').contains('Italian').click();
+    cy.get('#LanguagePickerBox').should('be.visible', { timeout: 10000 }).find('ng-select').last().click().get('ng-dropdown-panel').contains('Italiano').click();
     cy.get('[name="node.dataModel.header_title_homepage"]').clear().type("TEXT1_IT");
     cy.get('[name="node.dataModel.presentation"]').clear().type("TEXT2_IT");
+    cy.get('button.btn.btn-primary').eq(0).get("#save_settings").click();
+
+    cy.logout();
+  });
+});
+
+describe("Whistleblower Navigate Home Page", () => {
+  it("should see page properly internationalized", () => {
+
+    cy.visit("/#/?lang=en");
+    cy.reload(true);
+    cy.get('div').should('not.contain', 'TEXT1_IT');
+    cy.get('div').should('not.contain', 'TEXT2_IT');
+
+    cy.visit("/#/?lang=it");
+    cy.reload(true);
+    cy.contains("div", "TEXT1_IT").should("exist");
+    cy.contains("div", "TEXT2_IT").should("exist");
+  });
+});
+
+describe("admin configure languages", () => {
+  it("should reset internationalization texts", () => {
+    cy.login_admin();
+
+    cy.visit("/#/admin/settings");
+    cy.waitForUrl("/#/admin/settings");
+    cy.get('#LanguagePickerBox').should('be.visible', { timeout: 10000 }).find('ng-select').last().click().get('ng-dropdown-panel').contains('Italian').click();
+    cy.get('[name="node.dataModel.header_title_homepage"]').clear();
+    cy.get('[name="node.dataModel.presentation"]').clear();
     cy.get('button.btn.btn-primary').eq(0).get("#save_settings").click();
 
     cy.logout();
