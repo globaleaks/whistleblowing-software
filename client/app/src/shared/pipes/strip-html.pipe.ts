fcb1c6f11ace2e5eabc1afc6ce05b8bf1d596c21
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-10-20 13:44:33 +0200

    Strip HTML from Markdown text using DOMPurify

--- client/app/src/shared/pipes/strip-html.pipe.ts
+++ client/app/src/shared/pipes/strip-html.pipe.ts
@@ -1,4 +1,5 @@
 import {Pipe, PipeTransform} from "@angular/core";
+import * as DOMPurify from 'dompurify';
 
 @Pipe({
   name: "stripHtml"
@@ -6,6 +7,7 @@ import {Pipe, PipeTransform} from "@angular/core";
 export class StripHtmlPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value.replace(/<[^>]*>?/gm, "");
+    // Use DOMPurify to sanitize input
+    return (DOMPurify as any).default.sanitize(value);
   }
 }
